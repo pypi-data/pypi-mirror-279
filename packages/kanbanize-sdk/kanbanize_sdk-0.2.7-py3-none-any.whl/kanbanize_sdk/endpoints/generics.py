55cccee36fd1034c0825d5507bc48a61d314d210
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-14 04:03:19 +0000

    Add package kanbanize-sdk/0.2.7/kanbanize_sdk-0.2.7-py3-none-any.whl

---
+++ packages/kanbanize-sdk/kanbanize_sdk-0.2.7-py3-none-any.whl/kanbanize_sdk/endpoints/generics.py
@@ -0,0 +1,25 @@
+from abc import ABCMeta, abstractmethod
+from kanbanize_sdk.wrapper import KanbanizeSession
+
+
+class GenericRequestMethod(metaclass=ABCMeta):
+
+    endpoint = ''
+
+    def __init__(self, service: KanbanizeSession, *args, **kwargs):
+        self.service = service
+
+    def insert(self, *args, **kwargs):
+        ...
+
+    def update(self, *args, **kwargs):
+        ...
+
+    def get(self, *args, **kwargs):
+        ...
+
+    def list(self, *args, **kwargs):
+        ...
+
+    def delete(self, *args, **kwargs):
+        ...
