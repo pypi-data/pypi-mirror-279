ad60422fa2fd856825aa3b1348a93d5e33f1ed7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-14 04:04:18 +0000

    Add package eth-ape/0.8.4/eth_ape-0.8.4-py3-none-any.whl

---
+++ packages/eth-ape/eth_ape-0.8.4-py3-none-any.whl/ape_networks/__init__.py
@@ -0,0 +1,37 @@
+from typing import Optional
+
+from ape import plugins
+from ape.api import PluginConfig
+
+
+class CustomNetwork(PluginConfig):
+    """
+    A custom network config.
+    """
+
+    name: str
+    """Name of the network e.g. mainnet."""
+
+    chain_id: int
+    """Chain ID (required)."""
+
+    ecosystem: str
+    """The name of the ecosystem."""
+
+    base_ecosystem_plugin: Optional[str] = None
+    """The base ecosystem plugin to use, when applicable. Defaults to the default ecosystem."""
+
+    default_provider: str = "node"
+    """The default provider plugin to use. Default is the default node provider."""
+
+    request_header: dict = {}
+    """The HTTP request header."""
+
+
+class NetworksConfig(PluginConfig):
+    custom: list[CustomNetwork] = []
+
+
+@plugins.register(plugins.Config)
+def config_class():
+    return NetworksConfig
