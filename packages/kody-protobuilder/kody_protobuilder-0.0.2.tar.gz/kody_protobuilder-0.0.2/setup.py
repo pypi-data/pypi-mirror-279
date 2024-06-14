9bcf4eda91070419696bffe47375449c9cb49045
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-14 03:03:12 +0000

    Add package kody-protobuilder/0.0.2/kody_protobuilder-0.0.2.tar.gz

---
+++ packages/kody-protobuilder/kody_protobuilder-0.0.2.tar.gz/kody_protobuilder-0.0.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='kody-protobuilder',
+    version='0.0.2',
+    description='protofile builder',
+    author='kody-protobuilder_author',
+    author_email='kodyprotobuilder@gmail.com',
+    url='https://github.com/byundojin/auto-protobuf',
+    install_requires=['grpcio', 'grpcio-tools',],
+    packages=find_packages(),
+    python_requires='>=3.8'
+)
