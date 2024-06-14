5a9348ee2082421a91c86d8b8f56979fd3adf077
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-14 03:04:12 +0000

    Add package stjames/0.0.30/stjames-0.0.30.tar.gz

---
+++ packages/stjames/stjames-0.0.30.tar.gz/stjames-0.0.30/stjames/base.py
@@ -0,0 +1,40 @@
+from enum import Enum
+from typing import Annotated, Any, Hashable, TypeVar
+
+import numpy as np
+import pydantic
+
+
+class Base(pydantic.BaseModel):
+    @pydantic.field_validator("*", mode="before")
+    @classmethod
+    def coerce_numpy(cls, val: Any):
+        if isinstance(val, np.ndarray):
+            return val.tolist()
+        else:
+            return val
+
+
+class LowercaseStrEnum(str, Enum):
+    """Enum where hyphens and case are ignored."""
+
+    @classmethod
+    def _missing_(cls, value: str):
+        for member in cls:
+            if member.lower().replace("-", "") == value.lower().replace("-", ""):
+                return member
+        return None
+
+
+# cf. https://github.com/pydantic/pydantic-core/pull/820#issuecomment-1670475909
+# there are python details here I don't really grasp
+T = TypeVar("T", bound=Hashable)
+
+
+def _validate_unique_list(v: list[T]) -> list[T]:
+    if len(v) != len(set(v)):
+        raise ValueError("this list must be unique, and isn't!")
+    return v
+
+
+UniqueList = Annotated[list[T], pydantic.AfterValidator(_validate_unique_list)]
