25d1ad16f788a266e2e6eb57b0d05c35ab4c12b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-14 16:03:59 +0000

    Add package kb-st/0.1.0/kb_st-0.1.0.tar.gz

---
+++ packages/kb-st/kb_st-0.1.0.tar.gz/kb_st-0.1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+setup(
+name='kb_st',
+version='0.1.0',
+author='pandyandy',
+author_email='novakovadrea@gmail.com',
+description='A Python library for working with Kbc SAPI Client.',
+packages=find_packages(),
+classifiers=[
+'Programming Language :: Python :: 3',
+'License :: OSI Approved :: MIT License',
+'Operating System :: OS Independent',
+],
+python_requires='>=3.6',
+)
