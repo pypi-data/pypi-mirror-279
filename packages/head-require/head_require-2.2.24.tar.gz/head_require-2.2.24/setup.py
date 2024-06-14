25c30baf3b1fc73ffd75a5f2e1bf2fc345cdef6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-14 03:03:36 +0000

    Add package head-require/2.2.24/head_require-2.2.24.tar.gz

---
+++ packages/head-require/head_require-2.2.24.tar.gz/head_require-2.2.24/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+    "Programming Language :: Python :: 3",
+    "Development Status :: 4 - Beta",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+]
+
+setup(
+    name='head_require',
+    version='2.2.24',
+    description='head-require is a library that aims to simplify the creation of requirements.txt files. head-require generates requirements.txt based on the packages actually used in your project.',
+    long_description_content_type="text/markdown",
+    long_description=open('README.md').read(),
+    author='Thanarak Leenanon',
+    author_email='bzank.lee@hotmail.com',
+    url = 'https://github.com/Thanaraklee/Head-Require',
+    license='MIT',
+    classifiers=classifiers,
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'head-require = head_require.core:main',
+        ],
+    },
+)
