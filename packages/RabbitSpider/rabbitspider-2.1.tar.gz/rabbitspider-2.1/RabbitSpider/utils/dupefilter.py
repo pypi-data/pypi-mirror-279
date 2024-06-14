862aa64c486feead6de2ddd54b959c8509cb9c60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-14 16:03:33 +0000

    Add package RabbitSpider/2.1/rabbitspider-2.1.tar.gz

---
+++ packages/RabbitSpider/rabbitspider-2.1.tar.gz/rabbitspider-2.1/RabbitSpider/utils/dupefilter.py
@@ -0,0 +1,29 @@
+import pickle
+import hashlib
+import redis
+
+
+class RFPDupeFilter(object):
+    def __init__(self, repeat, host,
+                 port, db, password=None):
+        if host:
+            self.repeat = repeat
+            self.redis = redis.StrictRedis(host=host, port=port, db=db, password=password)
+            self.redis.delete(repeat)
+        else:
+            self.redis = None
+            self.repeat = set()
+
+    def request_seen(self, obj):
+        fingerprint = hashlib.sha1(obj).hexdigest()
+        data = pickle.dumps(fingerprint)
+        if self.redis:
+            result = self.redis.sadd(self.repeat, data)
+            return result
+        else:
+            if data in self.repeat:
+                return False
+            else:
+                self.repeat.add(data)
+                return True
+
