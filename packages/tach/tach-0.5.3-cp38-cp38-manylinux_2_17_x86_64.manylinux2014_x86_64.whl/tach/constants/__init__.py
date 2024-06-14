33b3415a43b153c814dc6bc856684777d5c66323
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-14 14:03:27 +0000

    Add package tach/0.5.3/tach-0.5.3-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/tach/tach-0.5.3-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/tach/constants/__init__.py
@@ -0,0 +1,15 @@
+from __future__ import annotations
+
+PACKAGE_NAME = "tach"
+TOOL_NAME = "tach"
+CONFIG_FILE_NAME = TOOL_NAME
+PACKAGE_FILE_NAME = "package"
+ROOT_MODULE_SENTINEL_TAG = "<root>"
+
+__all__ = [
+    "PACKAGE_NAME",
+    "TOOL_NAME",
+    "CONFIG_FILE_NAME",
+    "PACKAGE_FILE_NAME",
+    "ROOT_MODULE_SENTINEL_TAG",
+]
