2261d8b0300b8f7423812fecbb71de27bdb5dbb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-14 14:03:18 +0000

    Add package article-assistant/0.1.2/article_assistant-0.1.2.tar.gz

---
+++ packages/article-assistant/article_assistant-0.1.2.tar.gz/article_assistant-0.1.2/agents/agent_utils.py
@@ -0,0 +1,30 @@
+import os
+import getpass
+from langchain.agents import AgentExecutor, create_openai_tools_agent
+from langchain_core.messages import BaseMessage, HumanMessage
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+
+def _set_if_undefined(var: str):
+    if not os.environ.get(var):
+        os.environ[var] = getpass.getpass(f"Please provide your {var}")
+
+def initialize_environment():
+    _set_if_undefined("OPENAI_API_KEY")
+    _set_if_undefined("TAVILY_API_KEY")
+
+def create_agent(llm: ChatOpenAI, tools: list, system_prompt: str):
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", system_prompt),
+            MessagesPlaceholder(variable_name="messages"),
+            MessagesPlaceholder(variable_name="agent_scratchpad"),
+        ]
+    )
+    agent = create_openai_tools_agent(llm, tools, prompt)
+    executor = AgentExecutor(agent=agent, tools=tools)
+    return executor
+
+def agent_node(state, agent, name):
+    result = agent.invoke(state)
+    return {"messages": [HumanMessage(content=result["output"], name=name)]}
