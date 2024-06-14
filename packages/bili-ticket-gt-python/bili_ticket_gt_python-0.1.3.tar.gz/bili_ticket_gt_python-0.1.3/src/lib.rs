5a24216159ba1882e5d98256305854861a304ebd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-14 04:03:12 +0000

    Add package bili-ticket-gt-python/0.1.3/bili_ticket_gt_python-0.1.3.tar.gz

---
+++ packages/bili-ticket-gt-python/bili_ticket_gt_python-0.1.3.tar.gz/bili_ticket_gt_python-0.1.3/src/lib.rs
@@ -0,0 +1,46 @@
+mod abstraction;
+mod error;
+mod implement;
+mod py;
+
+#[cfg(test)]
+mod tests {
+    use crate::abstraction::Test;
+    use crate::implement::{Click, Slide};
+    use std::panic::AssertUnwindSafe;
+
+    #[test]
+    fn slide_test() {
+        let mut slide = Slide::default();
+        let validate = slide
+            .test("http://127.0.0.1:5000/pc-geetest/register")
+            .unwrap();
+        println!("{}", validate);
+    }
+
+    #[test]
+    fn click_test() {
+        let mut click = Click::default();
+        click
+            .test("https://passport.bilibili.com/x/passport-login/captcha?source=main_web")
+            .unwrap();
+    }
+
+    #[test]
+    fn click_test_batch() {
+        let mut click = Click::default();
+        let mut suc = 0;
+        for _ in 1..=100 {
+            let r = std::panic::catch_unwind(AssertUnwindSafe(|| {
+                click
+                    .test("https://passport.bilibili.com/x/passport-login/captcha?source=main_web")
+                    .unwrap();
+            }));
+
+            if r.is_ok() {
+                suc += 1;
+            }
+        }
+        println!("suc: {}", suc as f32 / 100f32);
+    }
+}
