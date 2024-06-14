5a091b5bf091054738c40971fd717022faca3a5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-14 16:03:48 +0000

    Add package py-seal/0.1.5/py-seal-0.1.5.tar.gz

---
+++ packages/py-seal/py-seal-0.1.5.tar.gz/py-seal-0.1.5/seal/db/mysql/__init__.py
@@ -0,0 +1,5 @@
+from .chained_query import ChainedQuery
+from .chained_query import ChainedQuery as Query
+from .chained_update import ChainedUpdate
+from .chained_update import ChainedUpdate as Update
+from .mysql_connector import MysqlConnector
---
+++ packages/py-seal/py-seal-0.1.5.tar.gz/py-seal-0.1.5/seal/db/sqlite/__init__.py
@@ -0,0 +1,6 @@
+from .chained_query import ChainedQuery
+from .chained_query import ChainedQuery as Query
+from .chained_update import ChainedUpdate
+from .chained_update import ChainedUpdate as Update
+from .sqlite_connector import SqliteConnector
+from .meta import Meta
