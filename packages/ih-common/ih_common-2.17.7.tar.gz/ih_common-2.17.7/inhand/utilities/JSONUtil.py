fd96dd1f2b839eafe9a8ddf48b739d423cff66f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-14 16:03:59 +0000

    Add package ih-common/2.17.7/ih_common-2.17.7.tar.gz

---
+++ packages/ih-common/ih_common-2.17.7.tar.gz/ih_common-2.17.7/inhand/utilities/JSONUtil.py
@@ -0,0 +1,17 @@
+import json
+
+from datetime import datetime
+from bson import ObjectId
+
+def bson_objectId_encoder(obj):
+    if isinstance(obj, ObjectId):
+        return str(obj)
+    return obj
+class JSONEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, datetime):
+            return str(obj)
+        elif isinstance(obj, ObjectId):
+            return str(obj)
+        else:
+            return super(JSONEncoder, self).default(obj)
