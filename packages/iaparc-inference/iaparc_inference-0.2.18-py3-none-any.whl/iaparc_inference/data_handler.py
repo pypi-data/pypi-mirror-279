25cb6fb72fedc54ad1260a8548eed9994782e82e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-14 03:04:32 +0000

    Add package iaparc-inference/0.2.18/iaparc_inference-0.2.18-py3-none-any.whl

---
+++ packages/iaparc-inference/iaparc_inference-0.2.18-py3-none-any.whl/iaparc_inference/data_handler.py
@@ -0,0 +1,136 @@
+"""
+IA Parc Inference data handler
+"""
+import os
+import io
+import logging
+import logging.config
+from tkinter import E
+from PIL.Image import Image
+from io import BytesIO
+import iaparc_inference.decoders as decoders
+
+Error = ValueError | None
+
+LEVEL = os.environ.get('LOG_LEVEL', 'INFO').upper()
+logging.basicConfig(
+    level=LEVEL,
+    force=True,
+    format="%(name)s: %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+LOGGER = logging.getLogger("Inference")
+LOGGER.propagate = True
+
+
+class DataHandler():
+    """
+    Data Handler
+    This a read-only class that handles the data
+    """
+
+    def __init__(self, data: bytes, content_type: str, parameters: dict, conf: dict, uid: str, source: str):
+        """
+        Constructor
+        Arguments:
+        
+        """
+        self._raw = data
+        self._content_type = content_type
+        self._conf = conf
+        self._name = conf["name"]
+        self._parameters = parameters
+        self._uid = uid
+        self._source = source
+        self.items = {}       
+        ## Init to None data kinds
+        self._file: BytesIO | None = None
+        self._text: str | None = None
+        self._image: Image | None = None
+        # self._audio  | None= None
+        # self._video | None= None
+        self._json: dict | None = None
+        #self._table = None
+        self._is_multi = self._conf["type"] == "multimodal"
+        self.error: Error = None
+        self.init_items()
+        
+    def init_items(self):
+        if self._is_multi:
+            if not self.items:
+                raw_items, self.error = decoders.decode_multipart(self._raw, self._conf["items"], self._content_type)
+                for item in self._conf["items"]:
+                    item_data = raw_items.get(item["name"])
+                    if item_data:
+                        dh = DataHandler(item_data, "", {}, item, "", "")
+                        self.items[item["name"]] = dh.get()
+            return self.items
+        else:
+            if not self.items:
+                match self._conf["type"]:
+                    case "file" | "binary" | "audio" | "video":
+                        self.items[self.name] = self.file
+                    case "text" | "string":
+                        self.items[self.name] = self.text
+                    case "image":
+                        self.items[self.name] = self.image
+                    case "json" | "matrix":
+                        self.items[self.name] = self.json
+                    case _:
+                        self.items[self.name] = self._raw
+                self.items[self.name] = self
+            return self.items
+    
+    def get(self, item: str|None = None):
+        if not item:
+            if len(self._conf["items"]) > 1:
+                return self.items
+            else:
+                return self.items[self._conf["items"][0]["name"]]
+        return self.items.get(item, None)
+    
+    @property
+    def name(self) -> str:
+        return self._name
+       
+    @property
+    def raw_data(self) -> bytes:
+        return self._raw
+    @property
+    def parameters(self) -> dict:
+        return self._parameters
+    
+    @property
+    def file(self) -> BytesIO | None:
+        if self._is_multi or self._conf["type"] not in ["file", "image", "binary", "audio", "video"]:
+            return None
+        if not self._file:
+            self._file, self.error = decoders.decode_file(self._raw)
+        return self._file
+        
+    @property
+    def text(self) -> str | None:
+        if self._is_multi or self._conf["type"] not in ["text", "string"]:
+            return None
+        if not self._text:
+            self._text, self.error = decoders.decode_text(self._raw)
+        return self._text
+    
+    @property
+    def image(self) -> Image | None:
+        if self._is_multi or self._conf["type"] != "image":
+            return None
+        if not self._image:
+            self._image, self.error = decoders.decode_image(self._raw)
+        return self._image
+    
+    @property
+    def json(self) -> dict | None:
+        if self._is_multi or self._conf["type"] != "json":
+            return None
+        if not self._json:
+            self._json, self.error = decoders.decode_json(self._raw)
+        return self._json
+
+
+
