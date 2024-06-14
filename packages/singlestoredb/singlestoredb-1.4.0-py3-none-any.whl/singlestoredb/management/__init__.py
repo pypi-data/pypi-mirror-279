5b455f2d87fc7c937a1584ba00976d3fa0f7e0a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-14 05:02:58 +0000

    Add package singlestoredb/1.4.0/singlestoredb-1.4.0-py3-none-any.whl

---
+++ packages/singlestoredb/singlestoredb-1.4.0-py3-none-any.whl/singlestoredb/management/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+from .cluster import manage_cluster
+from .manager import get_organization
+from .manager import get_token
+from .workspace import get_secret
+from .workspace import get_stage
+from .workspace import manage_workspaces
