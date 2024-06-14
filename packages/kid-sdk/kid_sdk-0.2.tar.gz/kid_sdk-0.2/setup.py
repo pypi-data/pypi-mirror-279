a51ef9673cc427060c5a7e534b4f96f636ec6ea5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-14 03:03:35 +0000

    Add package kid-sdk/0.2/kid_sdk-0.2.tar.gz

---
+++ packages/kid-sdk/kid_sdk-0.2.tar.gz/kid_sdk-0.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="kid_sdk",
+    version="0.2",
+    author="Zonesmart",
+    author_email="e.beliakov@dev.kokoc.com",
+    packages=find_packages(),
+    install_requires=[
+        "authlib",
+        "requests",
+    ],
+)
