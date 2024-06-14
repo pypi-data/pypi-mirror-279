ddf2c8bf623a0cb6f3d30818110785461c6b901f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-14 04:04:05 +0000

    Add package statsig/0.33.0/statsig-0.33.0.tar.gz

---
+++ packages/statsig/statsig-0.33.0.tar.gz/statsig-0.33.0/statsig/dynamic_config.py
@@ -0,0 +1,38 @@
+class DynamicConfig:
+    def __init__(self, data, name, rule, group_name=None):
+        if data is None:
+            data = {}
+        self.value = data
+        if name is None:
+            name = ""
+        self.name = name
+        if rule is None:
+            rule = ""
+        self.rule_id = rule
+        self.group_name = group_name
+
+    def get(self, key, default=None):
+        """Returns the value of the config at the given key
+        or the provided default if the key is not found
+        """
+        return self.value.get(key, default)
+
+    def get_typed(self, key, default=None):
+        """Returns the value of the config at the given key
+        iff the type matches the type of the provided default.
+        Otherwise, returns the default value
+        """
+        res = self.value.get(key, default)
+        if default is None:
+            return res
+        if not isinstance(default, type(res)):
+            return default
+        return res
+
+    def get_value(self):
+        """Returns the underlying value of this DynamicConfig"""
+        return self.value
+
+    def get_name(self):
+        """Returns the name of this DynamicConfig"""
+        return self.name
