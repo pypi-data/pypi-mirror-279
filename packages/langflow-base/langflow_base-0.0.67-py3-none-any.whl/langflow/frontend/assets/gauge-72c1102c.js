254d27052a5472b8e84bf0e1f7311168499a9cdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-14 05:03:08 +0000

    Add package langflow-base/0.0.67/langflow_base-0.0.67-py3-none-any.whl

---
+++ packages/langflow-base/langflow_base-0.0.67-py3-none-any.whl/langflow/frontend/assets/gauge-72c1102c.js
@@ -0,0 +1,6 @@
+import{eg as a}from"./index-e2611c7b.js";/**
+ * @license lucide-react v0.394.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const t=a("Gauge",[["path",{d:"m12 14 4-4",key:"9kzdfg"}],["path",{d:"M3.34 19a10 10 0 1 1 17.32 0",key:"19p75a"}]]);export{t as default};
---
+++ packages/langflow-base/langflow_base-0.0.67-py3-none-any.whl/langflow/frontend/assets/signal-low-a835f06e.js
@@ -0,0 +1,6 @@
+import{eg as a}from"./index-e2611c7b.js";/**
+ * @license lucide-react v0.394.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const o=a("SignalLow",[["path",{d:"M2 20h.01",key:"4haj6o"}],["path",{d:"M7 20v-4",key:"j294jx"}]]);export{o as default};
