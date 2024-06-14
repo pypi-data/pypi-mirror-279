42058a5ed9b8e19d7ace41d340510dcddafd5f1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-14 14:03:15 +0000

    Add package salve-ipc/0.4.2/salve_ipc-0.4.2.tar.gz

---
+++ packages/salve-ipc/salve_ipc-0.4.2.tar.gz/salve_ipc-0.4.2/setup.py
@@ -0,0 +1,26 @@
+# pip uninstall salve_ipc -y --break-system-packages; pip install . --break-system-packages --no-build-isolation; python tests/test_ipc.py
+from setuptools import setup
+
+with open("README.md", "r") as file:
+    long_description = file.read()
+
+
+setup(
+    name="salve_ipc",
+    version="0.4.2",
+    description="Salve is an IPC library that can be used by code editors to easily get autocompletions, replacements, editorconfig suggestions, and syntax highlighting.",
+    author="Moosems",
+    author_email="moosems.j@gmail.com",
+    url="https://github.com/Moosems/salve",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=["pygments", "pyeditorconfig"],
+    python_requires=">=3.9",
+    license="MIT license",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Typing :: Typed",
+    ],
+    packages=["salve_ipc"],
+)
