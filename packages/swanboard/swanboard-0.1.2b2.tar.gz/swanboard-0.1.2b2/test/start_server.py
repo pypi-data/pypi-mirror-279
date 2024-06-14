ae6f948850e9ef17a02c5717dd9a3860b8aa959f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-14 16:03:48 +0000

    Add package swanboard/0.1.2b2/swanboard-0.1.2b2.tar.gz

---
+++ packages/swanboard/swanboard-0.1.2b2.tar.gz/swanboard-0.1.2b2/test/start_server.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+r"""
+@DATE: 2023-11-24 21:12:52
+@File: test/start_server.py
+@IDE: vscode
+@Description:
+    开启后端测试服务，访问端口
+    事实上在使用中并不是这样的，而是在命令行执行命令完成的，这里只是为了测试和开发
+    增加了实际开发中不会用到的热启动功能
+"""
+from tutils.config import SWANLAB_LOG_DIR
+from swanboard.db import connect
+import os
+from swanboard import run
+
+os.environ["SWANLAB_LOG_DIR"] = SWANLAB_LOG_DIR
+
+connect()
+
+
+if __name__ == "__main__":
+    run("debug")
