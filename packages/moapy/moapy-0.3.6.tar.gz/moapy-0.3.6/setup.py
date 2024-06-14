115fc2b1cde54dab048b1b4ec858a777d50bab1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-14 15:04:07 +0000

    Add package moapy/0.3.6/moapy-0.3.6.tar.gz

---
+++ packages/moapy/moapy-0.3.6.tar.gz/moapy-0.3.6/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+def readme():
+    with open('README_en.md', encoding='utf-8') as f:
+        return f.read()
+
+
+setup(
+    name='moapy',
+    version='0.3.6',
+    packages=find_packages(),
+    include_package_data=True,
+    description='Midas Open API for Python',
+    long_description=readme(),
+    long_description_content_type='text/markdown',
+    license='MIT',
+    author='bschoi',
+    url='https://github.com/MIDASIT-Co-Ltd/engineers-api-python',
+    install_requires=['mdutils', "numpy", "matplotlib"],
+    extras_require={
+        'full': ["concreteproperties", "sectionproperties", "trimesh", "embreex"],
+    },
+)
