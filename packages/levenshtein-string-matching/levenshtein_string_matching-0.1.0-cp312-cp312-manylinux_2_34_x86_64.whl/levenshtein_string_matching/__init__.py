41d7cad3b328907550b179d4242e690903d7afb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-14 14:03:25 +0000

    Add package levenshtein-string-matching/0.1.0/levenshtein_string_matching-0.1.0-cp312-cp312-manylinux_2_34_x86_64.whl

---
+++ packages/levenshtein-string-matching/levenshtein_string_matching-0.1.0-cp312-cp312-manylinux_2_34_x86_64.whl/levenshtein_string_matching/__init__.py
@@ -0,0 +1,5 @@
+from .levenshtein_string_matching import *
+
+__doc__ = levenshtein_string_matching.__doc__
+if hasattr(levenshtein_string_matching, "__all__"):
+    __all__ = levenshtein_string_matching.__all__
