01eb9a9f5354c026e822cc0d17a24185e9d2a1a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-14 03:04:10 +0000

    Add package vtreat/1.3.1/vtreat-1.3.1-py3-none-any.whl

---
+++ packages/vtreat/vtreat-1.3.1-py3-none-any.whl/vtreat/stats_utils.py
@@ -0,0 +1,247 @@
+"""util for basic statistical steps"""
+
+from typing import Tuple
+
+import numpy
+import pandas
+
+import scipy.stats
+import scipy.optimize
+import sklearn.linear_model
+
+
+# methods to avoid calling statsmodels which seems to be incompatible with many
+# versions of other packages we need:
+#  https://github.com/WinVector/pyvtreat/issues/14
+
+
+def our_corr_score(*, y_true, y_pred) -> Tuple[float, float]:
+    """
+    Compute Pearson correlation. Case-out some corner cases.
+
+    :param y_true: truth values
+    :param y_pred: predictions
+    :return: (pearson r, significance)
+    """
+
+    if not isinstance(y_true, numpy.ndarray):
+        y_true = numpy.asarray(y_true)
+    if not isinstance(y_pred, numpy.ndarray):
+        y_pred = numpy.asarray(y_pred)
+    n = len(y_true)
+    if n < 2:
+        return 1, 1
+    if numpy.min(y_true) >= numpy.max(y_true):
+        return 1, 1
+    if numpy.min(y_pred) >= numpy.max(y_pred):
+        return 0, 1
+    r, sig = scipy.stats.pearsonr(y_true, y_pred)
+    if n < 3:
+        sig = 1
+    return r, sig
+
+
+def est_deviance(*, y, est, epsilon: float = 1.0e-5) -> float:
+    """
+    Estimate the deviance
+
+    :param y: truth values
+    :param est: predictions
+    :param epsilon: how close to get to 0 and 1
+    :return: deviance estimate
+    """
+
+    if not isinstance(y, numpy.ndarray):
+        y = numpy.asarray(y)
+    if not isinstance(est, numpy.ndarray):
+        est = numpy.asarray(est)
+    est = numpy.minimum(est, 1 - epsilon)
+    est = numpy.maximum(est, epsilon)
+    deviance = -2 * numpy.sum(y * numpy.log(est) + (1 - y) * numpy.log(1 - est))
+    return deviance
+
+
+def sklearn_solve_logistic(*, y, x, regularization: float = 1.0e-6):
+    """
+    Single variable logistic regression.
+    Assumes special cases of solve_logistic_regression already eliminated.
+
+    :param y: dependent variable
+    :param x: explanatory variable
+    :param regularization:
+    :return: model predictions
+    """
+
+    if not isinstance(y, numpy.ndarray):
+        y = numpy.asarray(y)
+    if not isinstance(x, numpy.ndarray):
+        x = numpy.asarray(x)
+    fitter = sklearn.linear_model.LogisticRegression(
+        penalty="l2", solver="lbfgs", fit_intercept=True, C=1 / regularization
+    )
+    dependent_vars = x.reshape((len(y), 1))
+    fitter.fit(X=dependent_vars, y=y)
+    preds = fitter.predict_proba(X=dependent_vars)[:, 1]
+    return preds
+
+
+# x, y - numpy numeric vectors, y 0/1.  solve for y- return predictions
+def solve_logistic_regression(*, y, x):
+    """
+    Single variable logistic regression. Returns predictions, corner
+    cases removed.
+
+    :param y: dependent variable
+    :param x: explanatory variable
+    :return: predictions
+    """
+
+    if not isinstance(y, numpy.ndarray):
+        y = numpy.asarray(y)
+    if not isinstance(x, numpy.ndarray):
+        x = numpy.asarray(x)
+    # catch some corner cases
+    n = len(y)
+    if (n < 2) or (numpy.min(y) >= numpy.max(y)):
+        return y.copy()
+    if numpy.min(x) >= numpy.max(x):
+        return numpy.asarray([numpy.mean(y)] * n)
+    # check for fully separable cases
+    big_y_indices = y > 0
+    x_b = x[big_y_indices]
+    x_s = x[numpy.logical_not(big_y_indices)]
+    if (numpy.min(x_b) > numpy.max(x_s)) or (numpy.max(x_b) < numpy.min(x_s)):
+        r = numpy.zeros(n)
+        r[big_y_indices] = 1
+        return r
+    # run a full logistic regression
+    preds = sklearn_solve_logistic(y=y, x=x)
+    return numpy.asarray(preds)
+
+
+# noinspection PyPep8Naming
+def our_pseudo_R2(*, y_true, y_pred) -> Tuple[float, float]:
+    """
+    Return the logistic pseudo-R2
+
+    :param y_true: dependent variable
+    :param y_pred: explanatory variable
+    :return: (pseudo-R2, significance)
+    """
+
+    if not isinstance(y_true, numpy.ndarray):
+        y_true = numpy.asarray(y_true)
+    if not isinstance(y_pred, numpy.ndarray):
+        y_pred = numpy.asarray(y_pred)
+    n = len(y_true)
+    if n < 2:
+        return 1, 1
+    if numpy.min(y_true) >= numpy.max(y_true):
+        return 1, 1
+    if numpy.min(y_pred) >= numpy.max(y_pred):
+        return 0, 1
+    preds = solve_logistic_regression(y=y_true, x=y_pred)
+    deviance = est_deviance(y=y_true, est=preds)
+    null_deviance = est_deviance(y=y_true, est=numpy.zeros(n) + numpy.mean(y_true))
+    r2 = 1 - deviance / null_deviance
+    sig = 1
+    if n >= 3:
+        # https://github.com/WinVector/sigr/blob/master/R/ChiSqTest.R
+        df_null = n - 1
+        df_residual = n - 2
+        delta_deviance = null_deviance - deviance
+        delta_df = df_null - df_residual
+        sig = 1 - scipy.stats.chi2.cdf(x=delta_deviance, df=delta_df)
+    return r2, sig
+
+
+def xicor(xvec, yvec, *, n_reps: int = 5) -> Tuple[float, float]:
+    """
+    xicor calculation built to match from R::caclulateXI() and published article.
+
+    :param xvec: numeric vector with explanatory variable to compute xicor for.
+    :param yvec: numeric dependent variable to relate to.
+    :param n_reps: number of times to repeat calculation.
+    :return: mean and standard error of estimate (under x-tie breaking)
+    """
+    yvec = numpy.asarray(yvec)
+    n = len(yvec)
+    assert n > 1
+    assert isinstance(n_reps, int)
+    assert n_reps > 0
+    xvec = numpy.asarray(xvec)
+    assert n == len(xvec)
+    PI = numpy.zeros(n)
+    fr_orig = scipy.stats.rankdata(yvec, method="max") / n
+    gr = scipy.stats.rankdata(-yvec, method="max") / n
+    CU = numpy.mean(gr * (1 - gr))
+    xi_s = numpy.zeros(n_reps)
+    for rep_i in range(n_reps):
+        perm = numpy.random.permutation(n)  # get random ranking by ranking permuted
+        PI_inv = scipy.stats.rankdata(xvec[perm], method="ordinal")
+        PI[perm] = PI_inv  # invert permutation, self assignment fails
+        ord = numpy.argsort(PI)
+        fr = fr_orig[ord]
+        A1 = numpy.sum(numpy.abs(fr[0 : (n - 1)] - fr[1:n])) / (2 * n)
+        xi = 1 - A1 / CU
+        xi_s[rep_i] = xi
+    return numpy.mean(xi_s), numpy.std(xi_s) / numpy.sqrt(n_reps)
+
+
+def xicor_for_frame(d: pandas.DataFrame, y, *, n_reps=5):
+    """
+    Calculate xicor for all columns of data frame d with respect to dependent column y.
+
+    :param d: data frame of proposed explanatory variables.
+    :param y: vector of the dependent variable values.
+    :param n_reps: number of times to repeat experiment (positive integer)
+    :return: data frame with: variable (name of column), xicor (estimated xicor statistic),
+             xicor_se (standard error of xicor estimate, goes to zero as n_reps grows),
+             xicor_perm_mean (mean value of xicor with y scrambled, goes to zero as n_reps grows),
+             xicor_perm_stddev (sample standard deviation of y scrambled xicor estimates,
+             used to form z or t style estimates).
+    """
+    n = d.shape[0]
+    assert n > 1
+    y = numpy.asarray(y)
+    assert len(y) == n
+    assert isinstance(n_reps, int)
+    assert n_reps > 0
+    res = pandas.DataFrame(
+        {
+            "variable": d.columns,
+            "xicor": 0.0,
+            "xicor_se": 0.0,
+            "xicor_perm_mean": 0.0,
+            "xicor_perm_stddev": 0.0,
+            "xicor_perm_sum": 0.0,
+            "xicor_perm_sum_sq": 0.0,
+        }
+    )
+    # get the xicor estimates
+    for col_i in range(len(d.columns)):
+        xvec = d[d.columns[col_i]]
+        xi_est, xi_est_dev = xicor(xvec, y, n_reps=n_reps)
+        res.loc[col_i, "xicor"] = xi_est
+        res.loc[col_i, "xicor_se"] = xi_est_dev
+    # score all x-columns with the same y-permutation
+    # estimate stddev with expanding squares to cut down storage
+    for rep_j in range(n_reps):
+        y_perm = y[numpy.random.permutation(n)]
+        for col_i in range(len(d.columns)):
+            xvec = d[d.columns[col_i]]
+            xi_perm, _ = xicor(xvec, y_perm, n_reps=1)
+            res.loc[col_i, "xicor_perm_sum"] = (
+                res.loc[col_i, "xicor_perm_sum"] + xi_perm
+            )
+            res.loc[col_i, "xicor_perm_sum_sq"] = (
+                res.loc[col_i, "xicor_perm_sum_sq"] + xi_perm * xi_perm
+            )
+    res["xicor_perm_mean"] = res["xicor_perm_sum"] / n_reps
+    res["xicor_perm_stddev"] = numpy.sqrt(
+        (1 / (n_reps - 1))
+        * (res["xicor_perm_sum_sq"] - (1 / n_reps) * res["xicor_perm_sum"] ** 2)
+    )
+    del res["xicor_perm_sum"]
+    del res["xicor_perm_sum_sq"]
+    return res
---
+++ packages/vtreat/vtreat-1.3.1-py3-none-any.whl/vtreat/util.py
@@ -0,0 +1,316 @@
+"""
+Utility functions for vtreat
+"""
+
+import math
+import re
+import statistics
+from typing import Iterable, List, Optional
+
+import hashlib
+
+import numpy
+import pandas
+
+import vtreat.stats_utils
+from vtreat.partial_pooling_estimator import pooled_effect_estimate
+
+
+def safe_to_numeric_array(x):
+    """
+    Convert array to numeric. Note, will parse strings (due to numpy)!
+
+    :param x: array to process
+    :return: numeric array
+    """
+    # note will parse strings!
+    return numpy.asarray(x, dtype=float)
+
+
+def can_convert_v_to_numeric(x) -> bool:
+    """
+    check if non-empty vector can convert to numeric
+
+    :param x:
+    :return: True if can convert to numeric, false otherwise (no string parsing).
+    """
+    x = numpy.asarray(x)
+    not_bad = numpy.logical_not(pandas.isnull(x))
+    n_not_bad = numpy.sum(not_bad)
+    if n_not_bad < 1:
+        return True  # All nan/None, can convert to numeric
+    try:
+        numpy.asarray(
+            x[not_bad] + 0, dtype=float
+        )  # +0 prevents string parse, leaving strings as strings
+        return True
+    except TypeError:
+        return False
+
+
+def is_bad(x):
+    """
+    For numeric vector x, return logical vector of positions that are null, NaN, infinite.
+
+    :param x:
+    :return:
+    """
+    if can_convert_v_to_numeric(x):
+        x = safe_to_numeric_array(x)
+        return numpy.logical_or(
+            pandas.isnull(x), numpy.logical_or(numpy.isnan(x), numpy.isinf(x))
+        )
+    return pandas.isnull(x)
+
+
+def numeric_has_range(x) -> bool:
+    """
+    Check if a numeric vector has numeric range.
+
+    :param x: vector to check
+    :return: True if max > min values in vector, else False.
+    """
+    x = safe_to_numeric_array(x)
+    not_bad = numpy.logical_not(is_bad(x))
+    n_not_bad = numpy.sum(not_bad)
+    if n_not_bad < 2:
+        return False
+    x = x[not_bad]
+    return numpy.max(x) > numpy.min(x)
+
+
+def summarize_column(x, *, fn=numpy.mean) -> float:
+    """
+    Summarize column to a non-missing scalar.
+
+    :param x: a vector/Series or column of numbers
+    :param fn: summarize function (such as numpy.mean), only passed non-bad positions
+    :return: scalar float summary of the non-None positions of x (otherwise 0)
+    """
+
+    x = safe_to_numeric_array(x)
+    not_bad = numpy.logical_not(is_bad(x))
+    n_not_bad = numpy.sum(not_bad)
+    if n_not_bad < 1:
+        return 0.0
+    x = x[not_bad]
+    v = 0.0 + fn(x)
+    if pandas.isnull(v) or math.isnan(v) or math.isinf(v):
+        return 0.0
+    return v
+
+
+def get_unique_value_count(x):
+    """compute how many unique values in list-x"""
+    if len(x) <= 1:
+        return len(x)
+    p = pandas.DataFrame({"x": x, "o": 1})
+    s = p.groupby("x").sum()  # drops None
+    return numpy.max(pandas.isnull(x)) + s.shape[0]
+
+
+def grouped_by_x_statistics(x, y):
+    """compute some grouped by x vector summaries of numeric y vector (no missing values in y)"""
+    n = len(x)
+    assert n > 0
+    assert n == len(y)
+    y = safe_to_numeric_array(y)
+    eps = 1.0e-3
+    sf = pandas.DataFrame({"x": x, "y": y})
+    sf.reset_index(inplace=True, drop=True)
+    bad_posns = pandas.isnull(sf["x"])
+    sf.loc[bad_posns, "x"] = "_NA_"
+    global_mean = sf["y"].mean()
+    sf["_group_mean"] = sf.groupby("x")["y"].transform("mean")
+    sf["_var"] = (sf["y"] - sf["_group_mean"]) ** 2
+    sf["_ni"] = 1
+    sf = sf.groupby("x").sum()
+    sf.reset_index(inplace=True, drop=False)
+    sf["y"] = sf["y"] / sf["_ni"]
+    sf["_group_mean"] = sf["_group_mean"] / sf["_ni"]
+    sf["_var"] = sf["_var"] / (sf["_ni"] - 1) + eps
+    avg_var = 0
+    bad_vars = is_bad(sf["_var"])
+    if numpy.sum(bad_vars) < len(sf["_var"]):
+        avg_var = numpy.nanmean(sf["_var"])
+    sf.loc[bad_vars, "_var"] = avg_var
+    if sf.shape[0] > 1:
+        sf["_vb"] = statistics.variance(sf["_group_mean"]) + eps
+    else:
+        sf["_vb"] = eps
+    sf["_gm"] = global_mean
+    sf.sort_values(["x"], inplace=True, ignore_index=True)
+    return sf
+
+
+def pooled_impact_estimate(x, y):
+    """
+    compute some pooled grouped by x vector summaries of numeric y vector (no missing values in y)
+    http://www.win-vector.com/blog/2017/09/partial-pooling-for-lower-variance-variable-encoding/
+    https://github.com/WinVector/Examples/blob/main/PartialPooling/PartialPooling.ipynb
+    """
+    n = len(x)
+    assert n > 0
+    assert n == len(y)
+    obs = pandas.DataFrame(
+        {
+            "location_id": x,
+            "observation": safe_to_numeric_array(y),
+        }
+    )
+    res = pooled_effect_estimate(obs)
+    res.rename(columns={"location_id": "x"}, inplace=True)
+    return res
+
+
+def score_variables(
+    cross_frame: pandas.DataFrame,
+    *,
+    variables: Optional[Iterable[str]],
+    outcome,
+    is_classification: bool = False
+) -> Optional[pandas.DataFrame]:
+    """
+    Score the linear relation of variables to outcome.
+
+    :param cross_frame: data frame to score
+    :param variables: names of variables to score
+    :param outcome: numeric outcome
+    :param is_classification: logical, True if a classification problem (0/1)
+    :return: data frame of variable evaluations, None if there are no non-constant variables.
+    """
+
+    assert isinstance(cross_frame, pandas.DataFrame)
+    if variables is not None:
+        assert not isinstance(variables, str)  # prevents str masquerading as iterable
+        variables = list(variables)
+    else:
+        variables = list(cross_frame.columns)
+    if len(variables) <= 0:
+        return None
+    outcome = safe_to_numeric_array(outcome)
+    n = cross_frame.shape[0]
+    assert n == len(outcome)
+    if numpy.max(outcome) <= numpy.min(outcome):
+        return None  # y must have range
+
+    def f(v):
+        """summarizing fn"""
+        col = cross_frame[v]
+        if (
+            (n > 2)
+            and (numpy.max(col) > numpy.min(col))
+            and (numpy.max(outcome) > numpy.min(outcome))
+        ):
+            cor, sig = vtreat.stats_utils.our_corr_score(y_true=outcome, y_pred=col)
+            r2 = cor**2
+            if is_classification:
+                r2, sig = vtreat.stats_utils.our_pseudo_R2(y_true=outcome, y_pred=col)
+            sfi = pandas.DataFrame(
+                {
+                    "variable": [v],
+                    "has_range": [True],
+                    "PearsonR": [cor],
+                    "R2": [r2],
+                    "significance": [sig],
+                }
+            )
+        else:
+            sfi = pandas.DataFrame(
+                {
+                    "variable": [v],
+                    "has_range": [False],
+                    "PearsonR": [numpy.NaN],
+                    "R2": [numpy.NaN],
+                    "significance": [1.0],
+                }
+            )
+        return sfi
+
+    sf_list = [f(v) for v in variables]
+    sf = pandas.concat(sf_list, axis=0, sort=False)
+    sf = sf.reset_index(inplace=False, drop=True)
+    return sf
+
+
+def check_matching_numeric_frames(*, res, expect, tol=1.0e-4):
+    """
+    Check if two numeric pandas.DataFrame s are identical.  assert if not
+    :param res:
+    :param expect:
+    :param tol: numeric tolerance.
+    :return: None
+    """
+    assert isinstance(expect, pandas.DataFrame)
+    assert isinstance(res, pandas.DataFrame)
+    assert res.shape == expect.shape
+    for c in expect.columns:
+        ec = expect[c]
+        rc = res[c]
+        assert numpy.max(numpy.abs(ec - rc)) <= tol
+
+
+def unique_items_in_order(x) -> list:
+    """
+    Return de-duplicated list of items in order they are in supplied array.
+
+    :param x: vector to inspect
+    :return: list
+    """
+    ret = []
+    if x is not None:
+        seen = set()
+        for item in x:
+            if item not in seen:
+                ret.append(item)
+                seen.add(item)
+    return ret
+
+
+def clean_string(s: str) -> str:
+    """
+    Replace common symbols with column-name safe alternatives.
+
+    :param s: incoming string
+    :return: string
+    """
+    mp = {
+        "<": "_lt_",
+        ">": "_gt_",
+        "[": "_osq_",
+        "]": "_csq_",
+        "(": "_op_",
+        ")": "_cp_",
+        ".": "_",
+        " ": "_",
+    }
+    s = s.strip()
+    s = re.sub(r"\s+", " ", s)
+    for k, v in mp.items():
+        s = s.replace(k, v)
+    return s
+
+
+def build_level_codes(incoming_column_name: str, levels: Iterable) -> List[str]:
+    """
+    Pick level names for a set of levels.
+
+    :param incoming_column_name:
+    :param levels:
+    :return:
+    """
+    levels = [str(lev) for lev in levels]
+    levels = [incoming_column_name + "_lev_" + clean_string(lev) for lev in levels]
+    if len(set(levels)) != len(levels):
+        levels = [levels[i] + "_" + str(i) for i in range(len(levels))]
+    return levels
+
+
+def hash_data_frame(d: pandas.DataFrame) -> str:
+    """
+    Get a hash code representing a data frame.
+
+    :param d: data frame
+    :return: hash code as a string
+    """
+    return hashlib.sha256(pandas.util.hash_pandas_object(d).values).hexdigest()
