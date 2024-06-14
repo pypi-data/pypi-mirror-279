e89c651586b174d25ffad650bb8767cad92c6947
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-14 14:03:11 +0000

    Add package OpenLLMTelemetry/0.0.1b9/openllmtelemetry-0.0.1b9-py3-none-any.whl

---
+++ packages/OpenLLMTelemetry/openllmtelemetry-0.0.1b9-py3-none-any.whl/openllmtelemetry/intrument_openai.py
@@ -0,0 +1,34 @@
+import importlib
+import logging
+from typing import Optional
+
+from opentelemetry.trace import Tracer
+
+from openllmtelemetry.guardrails import GuardrailsApi
+
+LOGGER = logging.getLogger(__name__)
+
+
+def init_instrumentors(trace_provider: Tracer, secure_api: Optional[GuardrailsApi]):
+    for instrumentor in [init_openai_instrumentor, init_bedrock_instrumentor]:
+        instrumentor(trace_provider=trace_provider, secure_api=secure_api)
+
+
+def init_openai_instrumentor(trace_provider: Tracer, secure_api: Optional[GuardrailsApi]):
+    if importlib.util.find_spec("openai") is not None:  # type: ignore
+        from openllmtelemetry.instrumentation.openai import OpenAIInstrumentor
+
+        instrumentor = OpenAIInstrumentor(secure_api=secure_api)
+        instrumentor.instrument(trace_provider=trace_provider, guard=secure_api)  # type: ignore
+    else:
+        LOGGER.warning("OpenAPI not found, skipping instrumentation")
+
+
+def init_bedrock_instrumentor(trace_provider: Tracer, secure_api: Optional[GuardrailsApi]):
+    if importlib.util.find_spec("boto3") is not None:  # type: ignore
+        from openllmtelemetry.instrumentation.bedrock import BedrockInstrumentor
+
+        instrumentor = BedrockInstrumentor(secure_api=secure_api)
+        instrumentor.instrument(trace_provider=trace_provider, guard=secure_api)  # type: ignore
+    else:
+        LOGGER.warning("Boto3 not found, skipping instrumentation")
