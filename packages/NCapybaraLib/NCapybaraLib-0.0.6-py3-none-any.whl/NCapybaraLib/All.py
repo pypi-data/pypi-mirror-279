80c6c84d06b7d6408980053733a8f2022a03125a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-14 04:04:18 +0000

    Add package NCapybaraLib/0.0.6/NCapybaraLib-0.0.6-py3-none-any.whl

---
+++ packages/NCapybaraLib/NCapybaraLib-0.0.6-py3-none-any.whl/NCapybaraLib/All.py
@@ -0,0 +1,11 @@
+"""
+NCapybaraLib; By NorbCodes.
+
+A small library with a bunch of functions and thingies I made for fun.
+
+https://pypi.org/project/NCapybaraLib/
+"""
+
+from .String import *
+from .Dictionary import *
+from .Math import *
