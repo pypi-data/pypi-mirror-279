7060ffaee1bf650d4317603f542adc33a7434ca9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-14 15:04:06 +0000

    Add package deepsquads-language-data/2024.5/deepsquads_language_data-2024.5-py3-none-any.whl

---
+++ packages/deepsquads-language-data/deepsquads_language_data-2024.5-py3-none-any.whl/deepsquads_language_data/apps.py
@@ -0,0 +1,11 @@
+# Copyright © Michal Čihař <michal@deepsquads.org>
+#
+# SPDX-License-Identifier: MIT
+
+from django.apps import AppConfig
+
+
+class LanguageDataConfig(AppConfig):
+    name = "deepsquads_language_data"
+    label = "deepsquads_language_data"
+    verbose_name = "Deepsquads Language Data"
---
+++ packages/deepsquads-language-data/deepsquads_language_data-2024.5-py3-none-any.whl/deepsquads_language_data/utils.py
@@ -0,0 +1,7 @@
+# Copyright © Michal Čihař <michal@deepsquads.org>
+#
+# SPDX-License-Identifier: MIT
+
+
+def gettext_noop(message):
+    return message
