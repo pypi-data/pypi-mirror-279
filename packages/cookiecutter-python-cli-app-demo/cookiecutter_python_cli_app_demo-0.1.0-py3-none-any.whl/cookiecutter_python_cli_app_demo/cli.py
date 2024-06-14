98f8002f4a66a2bedffc841cfc52184949016ded
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-14 04:04:06 +0000

    Add package cookiecutter-python-cli-app-demo/0.1.0/cookiecutter_python_cli_app_demo-0.1.0-py3-none-any.whl

---
+++ packages/cookiecutter-python-cli-app-demo/cookiecutter_python_cli_app_demo-0.1.0-py3-none-any.whl/cookiecutter_python_cli_app_demo/cli.py
@@ -0,0 +1,26 @@
+"""Main CLI for cookiecutter-python-cli-app-demo."""
+
+import click
+
+from . import __version__
+
+context_settings = {"help_option_names": ["-h", "--help"]}
+
+
+@click.group(context_settings=context_settings)
+@click.version_option(__version__, "-v", "--version")
+def cli() -> None:
+    """Demo of https://github.com/sgraaf/cookiecutter-python-cli-app."""
+
+
+@cli.command()
+@click.argument("input_", metavar="INPUT")
+@click.option(
+    "-r",
+    "--reverse",
+    is_flag=True,
+    help="Reverse the input.",
+)
+def repeat(input_: str, *, reverse: bool = False) -> None:
+    """Repeat the input."""
+    click.echo(input_ if not reverse else input_[::-1])
