dc062b87a8fd941a36830752a82f944905ef06ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-14 05:03:19 +0000

    Add package typerusx/1.2/typerusx-1.2.tar.gz

---
+++ packages/typerusx/typerusx-1.2.tar.gz/typerusx-1.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+import json
+
+
+setup(
+    name='typerusx',
+    version='1.2',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'curses-menu', 
+    ],
+    package_data={
+        '': ["sentences.json"],
+    },
+    entry_points={
+        'console_scripts': [
+            'typerusx = typing_1:main',  
+        ],
+    },
+)
+
+
+
