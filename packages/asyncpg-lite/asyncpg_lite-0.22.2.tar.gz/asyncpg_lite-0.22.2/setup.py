ebed4a6cbe9741111d99f925030551a1bece17be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-14 16:04:07 +0000

    Add package asyncpg-lite/0.22.2/asyncpg_lite-0.22.2.tar.gz

---
+++ packages/asyncpg-lite/asyncpg_lite-0.22.2.tar.gz/asyncpg_lite-0.22.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='asyncpg_lite',
+    version='0.22.2',
+    packages=find_packages(),
+    install_requires=[
+        'asyncpg>=0.21.0',
+    ],
+    author='Алексей Яковенко',
+    author_email='mr.mnogo@gmail.com',
+    description='Простая асинхронная библиотека, основанная на asyncpg',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/Yakvenalex/asyncpg_lite',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
