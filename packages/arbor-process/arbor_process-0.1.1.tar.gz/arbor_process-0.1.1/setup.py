e7d7539adaae18439f9e3cfea9080d8047a29504
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-14 04:03:11 +0000

    Add package arbor-process/0.1.1/arbor_process-0.1.1.tar.gz

---
+++ packages/arbor-process/arbor_process-0.1.1.tar.gz/arbor_process-0.1.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+# Read the requirements from the requirement.txt file
+with open('requirement.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='arbor_process',
+    version='0.1.1',
+    packages=find_packages(),
+    include_package_data=True,
+    description='A package for preprocessing data in the Arboretum project.',
+    long_description=open('README_for_pypi.md').read(),
+    long_description_content_type='text/markdown',
+    author='Baskar Group',
+    url='https://github.com/baskargroup/Arboretum/tree/main/Arbor-preprocess',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    install_requires=requirements,
+    python_requires='>=3.6',
+)
+
