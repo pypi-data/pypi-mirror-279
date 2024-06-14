b6d336b8a9025576b3feb8206c9f33666ffed2c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-14 14:03:23 +0000

    Add package onnxscript/0.1.0.dev20240614/onnxscript-0.1.0.dev20240614.tar.gz

---
+++ packages/onnxscript/onnxscript-0.1.0.dev20240614.tar.gz/onnxscript-0.1.0.dev20240614/onnxscript/_internal/version_utils.py
@@ -0,0 +1,45 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+"""Version utils for testing."""
+
+import packaging.version
+
+
+def onnx_older_than(version: str) -> bool:
+    """Returns True if the ONNX version is older than the given version."""
+    import onnx  # pylint: disable=import-outside-toplevel
+
+    return (
+        packaging.version.parse(onnx.__version__).release
+        < packaging.version.parse(version).release
+    )
+
+
+def torch_older_than(version: str) -> bool:
+    """Returns True if the torch version is older than the given version."""
+    import torch  # pylint: disable=import-outside-toplevel
+
+    return (
+        packaging.version.parse(torch.__version__).release
+        < packaging.version.parse(version).release
+    )
+
+
+def onnxruntime_older_than(version: str) -> bool:
+    """Returns True if the onnxruntime version is older than the given version."""
+    import onnxruntime  # pylint: disable=import-outside-toplevel
+
+    return (
+        packaging.version.parse(onnxruntime.__version__).release
+        < packaging.version.parse(version).release
+    )
+
+
+def numpy_older_than(version: str) -> bool:
+    """Returns True if the numpy version is older than the given version."""
+    import numpy  # pylint: disable=import-outside-toplevel
+
+    return (
+        packaging.version.parse(numpy.__version__).release
+        < packaging.version.parse(version).release
+    )
