1f3a007921f2b917aa364ec5f81a48d03dd2798d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-14 16:03:46 +0000

    Add package blocksnet/0.0.8/blocksnet-0.0.8-py3-none-any.whl

---
+++ packages/blocksnet/blocksnet-0.0.8-py3-none-any.whl/blocksnet/__init__.py
@@ -0,0 +1,14 @@
+"""
+This package helps to automatically generate master plan requirements for urban areas
+"""
+
+__version__ = "0.0.8"
+__author__ = ""
+__email__ = ""
+__credits__ = []
+__license__ = "BSD-3"
+
+
+from blocksnet.method import *
+from blocksnet.models import *
+from blocksnet.preprocessing import *
