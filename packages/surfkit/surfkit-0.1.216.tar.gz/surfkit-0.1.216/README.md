ca6df2ce56eb778e7090ea1dcc224e2f0131aea3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-14 03:04:55 +0000

    Add package surfkit/0.1.216/surfkit-0.1.216.tar.gz

---
+++ packages/surfkit/surfkit-0.1.216.tar.gz/surfkit-0.1.216/README.md
@@ -0,0 +1,222 @@
+<!-- PROJECT LOGO -->
+<br />
+<p align="center">
+  <!-- <a href="https://github.com/agentsea/skillpacks">
+    <img src="https://project-logo.png" alt="Logo" width="80">
+  </a> -->
+
+  <h1 align="center">Surfkit</h1>
+
+  <p align="center">
+    A toolkit for building AI agents that use devices
+    <br />
+    <a href="https://docs.hub.agentsea.ai/introduction"><strong>Explore the docs »</strong></a>
+    <br />
+    <br />
+    <a href="https://github.com/agentsea/surfkit">View Demo</a>
+    ·
+    <a href="https://github.com/agentsea/surfkit/issues">Report Bug</a>
+    ·
+    <a href="https://github.com/agentsea/surfkit/issues">Request Feature</a>
+  </p>
+  <br>
+</p>
+
+## Installation
+
+```sh
+pip install surfkit
+```
+
+## Usage
+
+### Building Agents
+
+Initialize a new project
+
+```sh
+surfkit new [NAME]
+```
+
+Build a docker container for the agent
+
+```sh
+surfkit build
+```
+
+### Running Agents
+
+Create an agent locally
+
+```sh
+surfkit create agent --name foo
+```
+
+Create an agent on kubernetes
+
+```sh
+surfkit create agent --runtime kube
+```
+
+List running agents
+
+```sh
+surfkit list agents
+```
+
+Get details about a specific agent
+
+```sh
+surfkit get agent --name foo
+```
+
+Fetch logs for a specific agent
+
+```sh
+surfkit logs --name foo
+```
+
+Delete an agent
+
+```sh
+surfkit delete agent --name foo
+```
+
+### Managing Devices
+
+Create a device
+
+```sh
+surfkit create device --type desktop --provicer gce --name bar
+```
+
+List devices
+
+```sh
+surfkit list devices
+```
+
+View device in UI
+
+```sh
+surfkit view --name bar
+```
+
+Delete a device
+
+```sh
+surfkit delete device --name bar
+```
+
+### Tracking Tasks
+
+Create a tracker
+
+```sh
+surfkit create tracker
+```
+
+List trackers
+
+```sh
+surfkit list trackers
+```
+
+Delete a tracker
+
+```sh
+surfkit delete tracker -n foo
+```
+
+### Solving Tasks
+
+Solve a task with an existing setup
+
+```sh
+surfkit solve --description "search for common french ducks" --agent foo --device bar
+```
+
+Solve a task creating the agent ad hoc
+
+```sh
+surfkit solve --description "search for alpaca sweaters" \
+--device bar --agent-file ./agent.yaml
+```
+
+Solve a task and kill the agent post-execution
+
+```sh
+surfkit solve --description "search for the meaning of life" \
+--device bar --agent-file ./agent.yaml --kill
+```
+
+List tasks
+
+```sh
+surfkit list tasks
+```
+
+### Publishing Agents
+
+Login to the hub
+
+```sh
+surfkit login
+```
+
+Publish the agent
+
+```sh
+surfkit publish
+```
+
+List published agent types
+
+```sh
+surfkit list types
+```
+
+Run a published agent
+
+```sh
+surfkit create agent --type SurfPizza --runtime kube
+```
+
+## Integrations
+
+Skillpacks is integrated with:
+
+- [MLLM](https://github.com/agentsea/mllm) A prompt management, routing, and schema validation library for multimodal LLMs
+- [Taskara](https://github.com/agentsea/taskara) A task management library for AI agents
+- [Skillpacks](https://github.com/agentsea/skillpacks) A library to fine tune AI agents on tasks.
+- [Threadmem](https://github.com/agentsea/threadmem) A thread management library for AI agents
+
+## Community
+
+Come join us on [Slack](https://agentsea.slack.com/join/signup)
+
+## Developing
+
+Add the following function to your `~/.zshrc` (or similar)
+
+```sh
+function sk() {
+  local project_dir="/path/to/surfkit/repo"
+  local venv_dir="$project_dir/.venv"
+  local ssh_auth_sock="$SSH_AUTH_SOCK"
+  local ssh_agent_pid="$SSH_AGENT_PID"
+
+  export SSH_AUTH_SOCK="$ssh_auth_sock"
+  export SSH_AGENT_PID="$ssh_agent_pid"
+
+  # Add the Poetry environment's bin directory to the PATH
+  export PATH="$venv_dir/bin:$PATH"
+
+  # Execute the surfkit.cli.main module using python -m
+  surfkit "$@"
+}
+```
+
+Replacing `/path/to/surfkit/repo` with the absolute path to your local repo.
+
+Then calling `sk` will execute the working code in your repo from any location.
---
+++ packages/surfkit/surfkit-0.1.216.tar.gz/surfkit-0.1.216/surfkit/cli/util.py
@@ -0,0 +1,194 @@
+import subprocess
+import sys
+from typing import Optional
+
+from taskara.runtime.base import Tracker, TrackerRuntime
+
+from surfkit.util import find_open_port
+
+
+def get_git_global_user_config():
+    # Command to get the global user name
+    name_command = ["git", "config", "--global", "user.name"]
+    # Command to get the global user email
+    email_command = ["git", "config", "--global", "user.email"]
+
+    try:
+        # Execute the commands
+        name = subprocess.run(
+            name_command,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            text=True,
+            check=True,
+        ).stdout.strip()
+        email = subprocess.run(
+            email_command,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            text=True,
+            check=True,
+        ).stdout.strip()
+
+        return f"{name} <{email}>"
+    except subprocess.CalledProcessError as e:
+        print("Error getting git global user config: ", e)
+        raise
+
+
+def pkg_from_name(name: str) -> str:
+    """Return package name from module name"""
+    name = name.replace("-", "_")
+    return name.lower()
+
+
+def is_poetry_installed():
+    """Check if 'poetry' command is available on the system."""
+    try:
+        subprocess.run(
+            ["poetry", "--version"],
+            check=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
+        return True
+    except subprocess.CalledProcessError:
+        return False
+    except FileNotFoundError:
+        return False
+
+
+def run_poetry_install():
+    """Run 'poetry install' using the subprocess module."""
+    try:
+        print("Running 'poetry install'...")
+        subprocess.run(["poetry", "install"], check=True)
+        print("'poetry install' executed successfully.")
+    except subprocess.CalledProcessError as e:
+        print(f"An error occurred while running 'poetry install': {e}")
+        raise
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+        raise
+
+
+def is_docker_installed():
+    """Check if 'docker' command is available on the system."""
+    try:
+        subprocess.run(
+            ["docker", "version"],
+            check=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
+        return True
+    except subprocess.CalledProcessError:
+        return False
+    except FileNotFoundError:
+        return False
+
+
+def build_docker_image(
+    dockerfile_path: str,
+    tag: str,
+    push: bool = True,
+    builder: str = "surfbuilder",
+    platforms: str = "linux/amd64,linux/arm64",
+):
+    try:
+        # Ensure using the correct Docker context
+        subprocess.run(
+            ["docker", "context", "use", "default"],
+            check=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
+
+        # Create or use an existing buildx builder that supports multi-arch
+        result = subprocess.run(
+            ["docker", "buildx", "create", "--name", builder, "--use", "--append"],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
+        if result.returncode != 0:
+            # If creation failed because it already exists, just use it
+            subprocess.run(
+                ["docker", "buildx", "use", builder],
+                check=True,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+            )
+
+        # Prepare the command for building the image
+        command = ["docker", "buildx", "build"]
+        if push:
+            command.append("--push")
+        command.extend(
+            ["--platform", platforms, "--tag", tag, "--file", dockerfile_path, "."]
+        )
+
+        # Building (and optionally pushing) the Docker image
+        result = subprocess.run(
+            command, check=True, stdout=sys.stdout, stderr=subprocess.STDOUT
+        )
+        print(
+            f"Docker image tagged as {tag} has been successfully built{' and pushed' if push else ''} for platforms {platforms}."
+        )
+
+    except subprocess.CalledProcessError as e:
+        print(
+            f"An error occurred while building {'and pushing ' if push else ''}the Docker image for platforms {platforms}: {e.stderr.decode() if e.stderr else None}"
+        )
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+
+
+def tracker_addr_agent(
+    tracker: Tracker,
+    agent_runtime: str,
+) -> str:
+    if agent_runtime == "process":
+        if tracker.runtime.name() == "process":
+            return tracker.runtime.runtime_local_addr(tracker.name, tracker.owner_id)
+        elif tracker.runtime.name() == "docker":
+            return f"http://localhost:{tracker.port}"
+        elif tracker.runtime.name() == "kube":
+            port = find_open_port(9070, 9090)
+            if not port:
+                raise Exception("No open port found for tracker")
+            tracker.proxy(port)
+            return f"http://localhost:{port}"
+        else:
+            raise ValueError(f"Unknown agent runtime: {agent_runtime}")
+    elif agent_runtime == "docker":
+        if tracker.runtime.name() == "process":
+            raise ValueError("Cannot use Docker agent with a process tracker")
+        elif tracker.runtime.name() == "docker":
+            return tracker.runtime.runtime_local_addr(tracker.name, tracker.owner_id)
+        elif tracker.runtime.name() == "kube":
+            raise ValueError("Cannot use Docker agent with a Kubernetes tracker")
+        else:
+            raise ValueError(f"Unknown agent runtime: {agent_runtime}")
+    elif agent_runtime == "kube":
+        if tracker.runtime.name() == "process":
+            raise ValueError("Cannot use Kubernetes agent with a process tracker")
+        elif tracker.runtime.name() == "docker":
+            raise ValueError("Cannot use Kubernetes agent with a Docker tracker")
+        elif tracker.runtime.name() == "kube":
+            return tracker.runtime.runtime_local_addr(tracker.name, tracker.owner_id)
+        else:
+            raise ValueError(f"Unknown agent runtime: {agent_runtime}")
+    else:
+        raise ValueError(f"Unknown agent runtime: {agent_runtime}")
+
+
+def tracker_addr_local(
+    tracker: Tracker,
+) -> str:
+    local_port = tracker.port
+    if tracker.runtime.requires_proxy():
+        local_port = find_open_port(9070, 10070)
+        if not local_port:
+            raise SystemError("No available ports found")
+        tracker.proxy(local_port=local_port)
+    return f"http://localhost:{local_port}"
