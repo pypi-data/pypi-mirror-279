41ac968bb9cb434a47f2f98e8b852d6f67ef1a8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-14 05:03:10 +0000

    Add package gdmty-django-recaptcha-enterprise/24.5.2/gdmty_django_recaptcha_enterprise-24.5.2-py3-none-any.whl

---
+++ packages/gdmty-django-recaptcha-enterprise/gdmty_django_recaptcha_enterprise-24.5.2-py3-none-any.whl/gdmty_django_recaptcha_enterprise/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class GdmtyDjangoRecaptchaEnterpriseConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "gdmty_django_recaptcha_enterprise"
