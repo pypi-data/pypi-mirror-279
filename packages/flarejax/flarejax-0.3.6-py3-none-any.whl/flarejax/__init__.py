1a8c1cf01edaf3dd472d1469caf8028d05122ce8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-14 03:03:32 +0000

    Add package flarejax/0.3.6/flarejax-0.3.6-py3-none-any.whl

---
+++ packages/flarejax/flarejax-0.3.6-py3-none-any.whl/flarejax/__init__.py
@@ -0,0 +1,22 @@
+from ._frozen import ModuleSequence, Sequential
+from ._module import BoundMethod, BoundMethodWrap, Module
+from ._mtypes import Jit, Partial, VMap
+from ._serial import load, save
+from ._typecheck import typecheck
+
+__version__ = "0.3.6"
+
+__all__ = [
+    "ModuleSequence",
+    "Sequential",
+    "BoundMethod",
+    "BoundMethodWrap",
+    "Module",
+    "Jit",
+    "Partial",
+    "VMap",
+    "load",
+    "save",
+    "typecheck",
+    "__version__",
+]
