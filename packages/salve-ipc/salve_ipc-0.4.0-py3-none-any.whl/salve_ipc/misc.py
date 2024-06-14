297ddabb7e65cc3095ca87ee4d255ce0aaceccbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-14 05:03:16 +0000

    Add package salve-ipc/0.4.0/salve_ipc-0.4.0-py3-none-any.whl

---
+++ packages/salve-ipc/salve_ipc-0.4.0-py3-none-any.whl/salve_ipc/misc.py
@@ -0,0 +1,37 @@
+from typing import NotRequired, TypedDict
+
+COMMANDS: list[str] = ["autocomplete", "replacements", "highlight"]
+
+
+class Message(TypedDict):
+    """Base class for messages in and out of the server"""
+
+    id: int
+    type: str  # Can be "request", "response", "notification"
+
+
+class Request(Message):
+    """Request results/output from the server with command specific input"""
+
+    command: str  # Can only be commands in COMMANDS
+    file: str
+    expected_keywords: NotRequired[list[str]]  # autocomplete, replacements
+    current_word: NotRequired[str]  # autocomplete, replacements
+    language: NotRequired[str]  # highlight
+    text_range: NotRequired[tuple[int, int]]
+
+
+class Notification(Message):
+    """Notifies the server to add/update/remove a file for usage in fulfilling commands"""
+
+    file: str
+    remove: bool
+    contents: NotRequired[str]
+
+
+class Response(Message):
+    """Server responses to requests and notifications"""
+
+    cancelled: bool
+    command: NotRequired[str]
+    result: NotRequired[list[str | tuple[tuple[int, int], int, str]]]
