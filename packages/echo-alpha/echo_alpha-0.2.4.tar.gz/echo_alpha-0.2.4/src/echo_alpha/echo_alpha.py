9efbb85e919993c3ee2bec4fcdff9925e05c88db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-14 03:03:19 +0000

    Add package echo-alpha/0.2.4/echo_alpha-0.2.4.tar.gz

---
+++ packages/echo-alpha/echo_alpha-0.2.4.tar.gz/echo_alpha-0.2.4/src/echo_alpha/echo_alpha.py
@@ -0,0 +1,46 @@
+import os
+from dotenv import load_dotenv
+import requests
+
+# Load environment variables from .env file
+load_dotenv()
+
+SERVICE_URL = os.getenv("SERVICE_URL")
+
+
+class EchoClient:
+  """
+  A client class for interacting with the Echo server.
+  """
+  def __init__(self, server_url = 'https://echov1.onrender.com'):
+
+    self.server_url = server_url
+    print(f'EchoClient initialized')
+
+  def send_and_receive(self, text_prompt, echo_ai_key):
+    data = {"message": text_prompt, 'echo_ai_key':echo_ai_key}
+    response = requests.post(self.server_url, json=data)
+
+    # Check response status code
+    if response.status_code == 200:
+    # Successful response
+        response_data = response.json()
+        echoed_message = response_data.get("message")
+        print(f"ECHO.AI: {echoed_message}")
+    else:
+        # Error response
+        echoed_message = f"Error: {response.status_code} - {response.text}"
+        print(echoed_message)
+        
+
+    return echoed_message
+  
+
+  def close(self):
+    """
+    Closes the socket connection.
+    """
+    self.sock.close()
+
+# Export the EchoClient class
+__all__ = ['EchoClient']
