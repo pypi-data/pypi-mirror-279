deb45feb2bbe7a0c64e4c7b9d49ce18ca2df3f94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-14 16:03:33 +0000

    Add package algebraic-hbm/0.1.3/algebraic_hbm-0.1.3-py3-none-any.whl

---
+++ packages/algebraic-hbm/algebraic_hbm-0.1.3-py3-none-any.whl/algebraic_hbm/__init__.py
@@ -0,0 +1,3 @@
+from algebraic_hbm.algebraic_hbm import Algebraic_HBM
+from algebraic_hbm.ode import ODE_2nd_Order_Poly_Coeffs, softening_Duffing
+from algebraic_hbm.fourier_series import Fourier_Series
