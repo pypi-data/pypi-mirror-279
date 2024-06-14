93f1e51ab4e0d2e84d36c7d804605bd049fbce28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-14 05:03:12 +0000

    Add package gvita/0.4.0/gvita-0.4.0.tar.gz

---
+++ packages/gvita/gvita-0.4.0.tar.gz/gvita-0.4.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='gvita',
+    version='0.4.0',
+    description='Little game based on Game of Life from John Conway',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='me15degrees',
+    author_email='me15degrees@gmail.com',
+    url='https://github.com/me15degrees/game-of-life',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    entry_points={
+        'console_scripts': [
+            'gvita=gvita.main:main',
+        ],
+    },
+)
