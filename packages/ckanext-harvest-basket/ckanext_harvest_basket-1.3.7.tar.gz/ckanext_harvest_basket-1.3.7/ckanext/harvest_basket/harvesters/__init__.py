e15fe26ef9688175adcdadbb1cfb3274b5986ba4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-14 16:03:37 +0000

    Add package ckanext-harvest-basket/1.3.7/ckanext_harvest_basket-1.3.7.tar.gz

---
+++ packages/ckanext-harvest-basket/ckanext_harvest_basket-1.3.7.tar.gz/ckanext_harvest_basket-1.3.7/ckanext/harvest_basket/harvesters/__init__.py
@@ -0,0 +1,16 @@
+from ckanext.harvest_basket.harvesters.dkan_harvester import DKANHarvester
+from ckanext.harvest_basket.harvesters.junar_harvester import JunarHarvester
+from ckanext.harvest_basket.harvesters.socrata_harvester import SocrataHarvester
+from ckanext.harvest_basket.harvesters.arcgis_harvester import ArcGISHarvester
+from ckanext.harvest_basket.harvesters.ckan_harvester import CustomCKANHarvester
+from ckanext.harvest_basket.harvesters.ods_harvester import ODSHarvester
+
+
+__all__ = [
+    "DKANHarvester",
+    "JunarHarvester",
+    "SocrataHarvester",
+    "ArcGISHarvester",
+    "CustomCKANHarvester",
+    "ODSHarvester",
+]
