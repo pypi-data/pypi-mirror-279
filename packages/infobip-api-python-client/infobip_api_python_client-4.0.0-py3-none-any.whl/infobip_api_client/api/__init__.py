b28a337472e8f10e27bae19742b1a7869b45ddc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-14 03:05:25 +0000

    Add package infobip-api-python-client/4.0.0/infobip_api_python_client-4.0.0-py3-none-any.whl

---
+++ packages/infobip-api-python-client/infobip_api_python_client-4.0.0-py3-none-any.whl/infobip_api_client/api/__init__.py
@@ -0,0 +1,8 @@
+# flake8: noqa
+
+# import apis into api package
+from infobip_api_client.api.call_routing_api import CallRoutingApi
+from infobip_api_client.api.calls_api import CallsApi
+from infobip_api_client.api.click_to_call_api import ClickToCallApi
+from infobip_api_client.api.sms_api import SmsApi
+from infobip_api_client.api.tfa_api import TfaApi
