38c35fe451ab6607ff17659e3c0f329cd1e1a434
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-14 16:03:40 +0000

    Add package yplib/5.8.8/yplib-5.8.8.tar.gz

---
+++ packages/yplib/yplib-5.8.8.tar.gz/yplib-5.8.8/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="yplib",
+    version="5.8.8",
+    author="yangpu",
+    author_email="wantwaterfish@gmail.com",
+    description="util",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
