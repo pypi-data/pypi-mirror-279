8e3a59ac98dbc210aa61dab46bb12138e525828c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-14 17:03:24 +0000

    Add package degiro-connector/3.0.21/degiro_connector-3.0.21-py3-none-any.whl

---
+++ packages/degiro-connector/degiro_connector-3.0.21-py3-none-any.whl/degiro_connector/trading/models/favorite.py
@@ -0,0 +1,36 @@
+from pydantic import BaseModel, ConfigDict, Field
+from pydantic.alias_generators import to_camel
+
+
+class FavoriteName(BaseModel):
+    name: str
+
+
+class FavoriteId(BaseModel):
+    data: int
+
+
+class FavoritePosition(BaseModel):
+    model_config = ConfigDict(
+        alias_generator=to_camel,
+        populate_by_name=True,
+    )
+
+    position: int
+    list_id: int
+
+
+class FavoriteItem(BaseModel):
+    model_config = ConfigDict(
+        alias_generator=to_camel,
+        populate_by_name=True,
+    )
+
+    default: int | None = Field(default=None)
+    id: int
+    is_default: bool | None = Field(default=None)
+    name: str | None = Field(default=None)
+
+
+class FavoriteBatch(BaseModel):
+    data: list[FavoriteItem] = Field(default_factory=list)
