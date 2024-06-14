ce9e57bdecea9a76c61beea66d018ac8c6e66319
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-14 03:03:23 +0000

    Add package panoptic/0.3.5rc4/panoptic-0.3.5rc4-py3-none-any.whl

---
+++ packages/panoptic/panoptic-0.3.5rc4-py3-none-any.whl/panoptic/dateformat.py
@@ -0,0 +1,46 @@
+import pendulum
+pendulum.set_locale('fr')
+
+def parse_date(date: str):
+    try:
+        parsed = pendulum.from_format(date, 'DD/MM/YYYY HH:mm')
+        return parsed.strftime('%Y-%m-%dT%H:%M:%SZ')
+    except Exception as e:
+        pass
+    try:
+        parsed = pendulum.from_format(date, 'DD/MM/YYYY HH:mm:ss')
+        return parsed.strftime('%Y-%m-%dT%H:%M:%SZ')
+    except Exception as e:
+        pass
+    try:
+        parsed = pendulum.from_format(date, 'DD/MM/YYYY')
+        return parsed.strftime('%Y-%m-%dT%H:%M:%SZ')
+    except Exception as e:
+        pass
+    try:
+        parsed = pendulum.from_format(date, 'YYYY')
+        return parsed.strftime('%Y-%m-%dT%H:%M:%SZ')
+    except Exception as e:
+        pass
+    try:
+        parsed = pendulum.from_format(date, 'MM/YYYY')
+        return parsed.strftime('%Y-%m-%dT%H:%M:%SZ')
+    except Exception as e:
+        pass
+    # parsed = dateparser.parse(date,
+    #                           date_formats=['%d/%m/%Y', '%d/%m/%Y %H:%M', '%d/%m/%Y %H:%M:%S'],
+    #                           settings={'PREFER_DAY_OF_MONTH': 'first', 'PREFER_MONTH_OF_YEAR': 'first'})
+    parsed = pendulum.parse(date)
+    return parsed.strftime('%Y-%m-%dT%H:%M:%SZ')
+
+
+
+
+# print(parse_date('2022'))
+# print(parse_date('02/2022'))
+# print(parse_date('03/2022'))
+# print(parse_date('04/03/2022'))
+# print(parse_date('04/03/2022 10:30'))
+# print(parse_date('04/03/2022 10:30:55'))
+# print(parse_date("2022-03-04T13:33:00.000Z"))
+# print(parse_date("1995/07/04 10:33:22"))
---
+++ packages/panoptic/panoptic-0.3.5rc4-py3-none-any.whl/panoptic/plugins/PluginBase/plugin_base.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+
+from pydantic import BaseModel
+
+from panoptic.core.plugin.plugin_project_interface import PluginProjectInterface
+from panoptic.core.project.project import Project
+from panoptic.models import ActionContext, Property, PropertyId
+from panoptic.core.plugin.plugin import Plugin
+
+
+class TestParams(BaseModel):
+    base_text: str = None
+    base_number: int = 0
+    base_floating: float = 2.4
+    base_prop: PropertyId = 1
+
+
+class PluginBase(Plugin):
+    def __init__(self, project: PluginProjectInterface, plugin_path: str):
+        super().__init__(name='TestPlugin1', project=project, plugin_path=plugin_path)
+        self.params = TestParams()
+
+        self.add_action_easy(self.test_function, ['execute'])
+
+    async def test_function(self, context: ActionContext, number: int, text: str, floating: float, prop: PropertyId):
+        print(self.params)
+        print(context)
+        print(number, text, floating, prop)
+
