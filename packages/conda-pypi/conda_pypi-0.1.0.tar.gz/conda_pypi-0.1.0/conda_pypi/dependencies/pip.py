3047eb0dd4b9a71e4828b0956f2826e11d87f99d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-14 03:03:11 +0000

    Add package conda-pypi/0.1.0/conda_pypi-0.1.0.tar.gz

---
+++ packages/conda-pypi/conda_pypi-0.1.0.tar.gz/conda_pypi-0.1.0/conda_pypi/dependencies/pip.py
@@ -0,0 +1,28 @@
+from __future__ import annotations
+
+import json
+from logging import getLogger
+from collections import defaultdict
+from ..main import dry_run_pip_json
+
+logger = getLogger(f"conda.{__name__}")
+
+
+def _analyze_with_pip(
+    *packages: str,
+    prefix: str | None = None,
+    force_reinstall: bool = False,
+) -> tuple[dict[str, list[str]], dict[str, list[str]]]:
+    report = dry_run_pip_json(("--prefix", prefix, *packages), force_reinstall)
+    deps_from_pip = defaultdict(list)
+    conda_deps = defaultdict(list)
+    for item in report["install"]:
+        metadata = item["metadata"]
+        logger.debug("Analyzing %s", metadata["name"])
+        logger.debug("  metadata: %s", json.dumps(metadata, indent=2))
+        deps_from_pip[metadata["name"]].append(f"{metadata['name']}=={metadata['version']}")
+        if python_version := metadata.get("requires_python"):
+            conda_deps["python"].append(f"python {python_version}")
+
+    deps_from_pip = {name: list(dict.fromkeys(specs)) for name, specs in deps_from_pip.items()}
+    return conda_deps, deps_from_pip
---
+++ packages/conda-pypi/conda_pypi-0.1.0.tar.gz/conda_pypi-0.1.0/conda_pypi/utils.py
@@ -0,0 +1,82 @@
+from __future__ import annotations
+
+import os
+import sys
+import sysconfig
+from logging import getLogger
+from pathlib import Path
+from subprocess import check_output
+from typing import Iterator
+
+from conda.base.context import context, locate_prefix_by_name
+from conda.models.match_spec import MatchSpec
+
+
+logger = getLogger(f"conda.{__name__}")
+
+
+def get_prefix(prefix: os.PathLike = None, name: str = None) -> Path:
+    if prefix:
+        return Path(prefix)
+    elif name:
+        return Path(locate_prefix_by_name(name))
+    else:
+        return Path(context.target_prefix)
+
+
+def get_env_python(prefix: os.PathLike = None) -> Path:
+    prefix = Path(prefix or sys.prefix)
+    if os.name == "nt":
+        return prefix / "python.exe"
+    return prefix / "bin" / "python"
+
+
+def _get_env_sysconfig_path(key: str, prefix: os.PathLike = None) -> Path:
+    prefix = Path(prefix or sys.prefix)
+    if str(prefix) == sys.prefix:
+        return Path(sysconfig.get_path(key))
+    return Path(
+        check_output(
+            [
+                get_env_python(prefix),
+                "-c",
+                f"import sysconfig; print(sysconfig.get_paths()['{key}'])",
+            ],
+            text=True,
+        ).strip()
+    )
+
+
+def get_env_stdlib(prefix: os.PathLike = None) -> Path:
+    return _get_env_sysconfig_path("stdlib", prefix)
+
+
+def get_env_site_packages(prefix: os.PathLike = None) -> Path:
+    return _get_env_sysconfig_path("purelib", prefix)
+
+
+def get_externally_managed_path(prefix: os.PathLike = None) -> Iterator[Path]:
+    prefix = Path(prefix or sys.prefix)
+    if os.name == "nt":
+        yield Path(prefix, "Lib", "EXTERNALLY-MANAGED")
+    else:
+        found = False
+        for python_dir in sorted(Path(prefix, "lib").glob("python*")):
+            if python_dir.is_dir():
+                found = True
+                yield Path(python_dir, "EXTERNALLY-MANAGED")
+        if not found:
+            raise ValueError("Could not locate EXTERNALLY-MANAGED file")
+
+
+def pypi_spec_variants(spec_str: str) -> Iterator[str]:
+    yield spec_str
+    spec = MatchSpec(spec_str)
+    seen = {spec_str}
+    for name_variant in (
+        spec.name.replace("-", "_"),
+        spec.name.replace("_", "-"),
+    ):
+        if name_variant not in seen:  # only yield if actually different
+            yield str(MatchSpec(spec, name=name_variant))
+            seen.add(name_variant)
