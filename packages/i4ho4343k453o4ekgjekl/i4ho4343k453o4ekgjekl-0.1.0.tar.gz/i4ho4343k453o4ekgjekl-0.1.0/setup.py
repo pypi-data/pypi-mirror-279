85d9be73cfbf684ff8430f7fce4ac3befc19aa8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-14 15:04:09 +0000

    Add package i4ho4343k453o4ekgjekl/0.1.0/i4ho4343k453o4ekgjekl-0.1.0.tar.gz

---
+++ packages/i4ho4343k453o4ekgjekl/i4ho4343k453o4ekgjekl-0.1.0.tar.gz/i4ho4343k453o4ekgjekl-0.1.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+setup(
+    name="i4ho4343k453o4ekgjekl",
+    version="0.1.0",
+    author="niexin",
+    author_email="1208401830@qq.com",
+    description="A short description of the package",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        
+    ],
+)
