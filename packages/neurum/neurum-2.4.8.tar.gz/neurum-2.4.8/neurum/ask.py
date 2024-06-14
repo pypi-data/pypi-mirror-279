e36ae178672b0b51df968e82e7d88faa24cd6fac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-14 16:03:59 +0000

    Add package neurum/2.4.8/neurum-2.4.8.tar.gz

---
+++ packages/neurum/neurum-2.4.8.tar.gz/neurum-2.4.8/neurum/ask.py
@@ -0,0 +1,17 @@
+import requests
+
+class Neurum:
+    def __init__(self, key):
+        self.key = key
+        self.url = "https://api.rnilaweera.lk/api/v1/user/mixtral"
+        self.headers = {"Authorization": f"Bearer rsnai_KCLojKDTOY4b1hNeNFIwFaqN"}
+    
+    def generate(self, prompt):
+        if self.key=="vansh":
+            payload = {"prompt": prompt}
+            response = requests.post(self.url, json=payload, headers=self.headers)
+            jsonans=response.json()
+            answer=jsonans['message']
+            return answer
+        else:
+            print('failedddddd bruhhhh!!!!')
---
+++ packages/neurum/neurum-2.4.8.tar.gz/neurum-2.4.8/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+classifiers = ['Development Status :: 2 - Pre-Alpha', 
+               'Intended Audience :: Developers', 
+               'Operating System :: MacOS :: MacOS X',
+               'Operating System :: Microsoft :: Windows', 
+               'Operating System :: Unix',  
+               'License :: OSI Approved :: MIT License', 
+               'Programming Language :: Python',]
+
+setup(
+    name= 'neurum', 
+    version='2.4.8',
+    description='A powerful multimodal AI sdk for python by Neurum Inc..',
+    url='https://github.com/VanshShah1/notion',
+    author='Vansh Shah', 
+    author_email='vanshshah836@gmail.com',
+    License='MIT', 
+    classifiers=classifiers, 
+    keywords='ai api sdk llm',
+    packages=find_packages(),
+    install_requires=['quickjs'],
+)
