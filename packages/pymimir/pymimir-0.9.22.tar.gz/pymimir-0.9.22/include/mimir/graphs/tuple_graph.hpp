c97d2fa3b92da0f82f37de03d22140faa6149a4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-14 04:03:37 +0000

    Add package pymimir/0.9.22/pymimir-0.9.22.tar.gz

---
+++ packages/pymimir/pymimir-0.9.22.tar.gz/pymimir-0.9.22/include/mimir/graphs/tuple_graph.hpp
@@ -0,0 +1,236 @@
+/*
+ * Copyright (C) 2023 Dominik Drexler and Simon Stahlberg
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef MIMIR_GRAPHS_TUPLE_GRAPH_HPP_
+#define MIMIR_GRAPHS_TUPLE_GRAPH_HPP_
+
+#include "mimir/datasets/state_space.hpp"
+#include "mimir/search/algorithms/iw/dynamic_novelty_table.hpp"
+#include "mimir/search/algorithms/iw/index_mappers.hpp"
+#include "mimir/search/algorithms/iw/tuple_index_generators.hpp"
+#include "mimir/search/state.hpp"
+
+#include <ostream>
+#include <vector>
+
+namespace mimir
+{
+class TupleGraphFactory;
+
+using VertexIndex = int;
+using VertexIndexList = std::vector<VertexIndex>;
+using VertexIndexSet = std::unordered_set<VertexIndex>;
+
+class TupleGraphVertex
+{
+private:
+    VertexIndex m_identifier;
+    TupleIndex m_tuple_index;
+    StateList m_states;
+
+public:
+    TupleGraphVertex(VertexIndex identifier, TupleIndex tuple_index, StateList states);
+
+    /**
+     * Getters.
+     */
+
+    VertexIndex get_identifier() const;
+    TupleIndex get_tuple_index() const;
+    const StateList& get_states() const;
+};
+
+using TupleGraphVertexList = std::vector<TupleGraphVertex>;
+
+class TupleGraph
+{
+private:
+    std::shared_ptr<StateSpaceImpl> m_state_space;
+    std::shared_ptr<FluentAndDerivedMapper> m_atom_index_mapper;
+    std::shared_ptr<TupleIndexMapper> m_tuple_index_mapper;
+    State m_root_state;
+
+    TupleGraphVertexList m_vertices;
+
+    std::vector<VertexIndexList> m_forward_successors;
+    std::vector<VertexIndexList> m_backward_successors;
+
+    std::vector<VertexIndexList> m_vertex_indices_by_distance;
+    std::vector<StateList> m_states_by_distance;
+
+    TupleGraph(std::shared_ptr<StateSpaceImpl> state_space,
+               std::shared_ptr<FluentAndDerivedMapper> atom_index_mapper,
+               std::shared_ptr<TupleIndexMapper> tuple_index_mapper,
+               State root_state,
+               TupleGraphVertexList vertices,
+               std::vector<VertexIndexList> forward_successors,
+               std::vector<VertexIndexList> backward_successors,
+               std::vector<VertexIndexList> vertex_indices_by_distance,
+               std::vector<StateList> states_by_distance);
+    friend class TupleGraphFactory;
+
+public:
+    /// @brief Compute and return an admissible chain for a given tuple of ground atoms.
+    /// Return std::nullopt if no such admissible chain exists.
+    std::optional<VertexIndexList> compute_admissible_chain(const GroundAtomList<Fluent>& fluent_atoms, const GroundAtomList<Derived>& derived_atoms);
+
+    /**
+     * Getters.
+     */
+
+    std::shared_ptr<StateSpaceImpl> get_state_space() const;
+    std::shared_ptr<FluentAndDerivedMapper> get_atom_index_mapper() const;
+    std::shared_ptr<TupleIndexMapper> get_tuple_index_mapper() const;
+    State get_root_state() const;
+    const TupleGraphVertexList& get_vertices() const;
+    const std::vector<VertexIndexList>& get_forward_successors() const;
+    const std::vector<VertexIndexList>& get_backward_successors() const;
+    const std::vector<VertexIndexList>& get_vertex_indices_by_distances() const;
+    const std::vector<StateList>& get_states_by_distance() const;
+};
+
+class TupleGraphFactory
+{
+private:
+    std::shared_ptr<StateSpaceImpl> m_state_space;
+    bool m_prune_dominated_tuples;
+
+    std::shared_ptr<FluentAndDerivedMapper> m_atom_index_mapper;
+    std::shared_ptr<TupleIndexMapper> m_tuple_index_mapper;
+
+    class TupleGraphArityZeroComputation
+    {
+    private:
+        std::shared_ptr<FluentAndDerivedMapper> m_atom_index_mapper;
+        std::shared_ptr<TupleIndexMapper> m_tuple_index_mapper;
+        std::shared_ptr<StateSpaceImpl> m_state_space;
+        State m_root_state;
+        bool m_prune_dominated_tuples;
+        TupleGraphVertexList m_vertices;
+        std::vector<VertexIndexList> m_forward_successors;
+        std::vector<VertexIndexList> m_backward_successors;
+        std::vector<VertexIndexList> m_vertex_indices_by_distances;
+        std::vector<StateList> m_states_by_distance;
+
+    public:
+        TupleGraphArityZeroComputation(std::shared_ptr<FluentAndDerivedMapper> atom_index_mapper,
+                                       std::shared_ptr<TupleIndexMapper> tuple_index_mapper,
+                                       std::shared_ptr<StateSpaceImpl> state_space,
+                                       const State root_state,
+                                       bool prune_dominated_tuples);
+
+        /// @brief Compute the root state layer.
+        void compute_root_state_layer();
+
+        /// @brief Compute the layer at distance 1, assumes that the root state layer exists.
+        void compute_first_layer();
+
+        /// @brief Extract the resulting TupleGraph, leaving the class in an undefined state.
+        TupleGraph extract_tuple_graph();
+    };
+
+    // Bookkeeping for memory reuse when building tuple graph of width greater 0
+    class TupleGraphArityKComputation
+    {
+    private:
+        std::shared_ptr<FluentAndDerivedMapper> atom_index_mapper;
+        std::shared_ptr<TupleIndexMapper> tuple_index_mapper;
+        std::shared_ptr<StateSpaceImpl> state_space;
+        State root_state;
+        bool prune_dominated_tuples;
+        TupleGraphVertexList vertices;
+        std::vector<VertexIndexList> forward_successors;
+        std::vector<VertexIndexList> backward_successors;
+        std::vector<VertexIndexList> vertex_indices_by_distances;
+        std::vector<StateList> states_by_distance;
+
+        /**
+         * Four step procedure to compute the next layer in the graph.
+         */
+
+        StateSet visited_states;
+        StateList cur_states;
+
+        void compute_next_state_layer();
+
+        DynamicNoveltyTable novelty_table;
+        TupleIndexSet novel_tuple_indices_set;
+        TupleIndexList novel_tuple_indices;
+        std::unordered_map<TupleIndex, StateSet> novel_tuple_index_to_states;
+        std::unordered_map<State, TupleIndexList, StateHash> state_to_novel_tuple_indices;
+
+        void compute_next_novel_tuple_indices();
+
+        std::unordered_map<TupleIndex, StateSet> cur_novel_tuple_index_to_extended_state;
+        std::unordered_map<TupleIndex, VertexIndexSet> cur_extended_novel_tuple_index_to_prev_vertices;
+        TupleIndexSet cur_extended_novel_tuple_indices_set;
+        TupleIndexList cur_extended_novel_tuple_indices;
+
+        void extend_optimal_plans_from_prev_layer();
+
+        std::unordered_map<TupleIndex, TupleIndexSet> tuple_index_to_dominating_tuple_indices;
+        VertexIndexList cur_vertices;
+
+        void instantiate_next_layer();
+
+    public:
+        TupleGraphArityKComputation(std::shared_ptr<FluentAndDerivedMapper> atom_index_mapper,
+                                    std::shared_ptr<TupleIndexMapper> tuple_index_mapper,
+                                    std::shared_ptr<StateSpaceImpl> state_space,
+                                    const State root_state,
+                                    bool prune_dominated_tuples);
+
+        /// @brief Compute the root state layer.
+        void compute_root_state_layer();
+
+        /// @brief Compute the next layer, assumes that the root state layer exists
+        /// and return true iff the layer is nonempty.
+        bool compute_next_layer();
+
+        /// @brief Extract the resulting TupleGraph, leaving the class in an undefined state.
+        TupleGraph extract_tuple_graph();
+    };
+
+    /// @brief Create tuple graph for the special case of width 0, i.e.,
+    /// any state with distance at most 1 from the root_state is a subgoal state.
+    TupleGraph create_for_arity_zero(const State root_state) const;
+
+    TupleGraph create_for_arity_k(const State root_state) const;
+
+public:
+    TupleGraphFactory(std::shared_ptr<StateSpaceImpl> state_space, int arity, bool prune_dominated_tuples = false);
+
+    TupleGraph create(const State root_state) const;
+
+    /**
+     * Getters.
+     */
+
+    std::shared_ptr<StateSpaceImpl> get_state_space() const;
+    std::shared_ptr<FluentAndDerivedMapper> get_atom_index_mapper() const;
+    std::shared_ptr<TupleIndexMapper> get_tuple_index_mapper() const;
+};
+
+/**
+ * Pretty printing as dot representation
+ */
+
+extern std::ostream& operator<<(std::ostream& out, std::tuple<const TupleGraph&, const Problem, const PDDLFactories&> data);
+
+}
+
+#endif
---
+++ packages/pymimir/pymimir-0.9.22.tar.gz/pymimir-0.9.22/src/datasets/state_space.cpp
@@ -0,0 +1,303 @@
+/*
+ * Copyright (C) 2023 Simon Stahlberg
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include "mimir/datasets/state_space.hpp"
+
+#include "mimir/common/timers.hpp"
+
+#include <algorithm>
+#include <cstdlib>
+#include <deque>
+
+namespace mimir
+{
+
+static std::vector<int>
+compute_shortest_distances_from_states_impl(const size_t num_total_states, const StateList& states, const std::vector<Transitions>& transitions)
+{
+    auto distances = std::vector<int>(num_total_states, -1);
+    auto fifo_queue = std::deque<int>();
+    for (const auto& state : states)
+    {
+        distances[state.get_id()] = 0;
+        fifo_queue.push_back(state.get_id());
+    }
+
+    while (!fifo_queue.empty())
+    {
+        const auto& state_id = fifo_queue.front();
+        fifo_queue.pop_front();
+        const auto cost = distances[state_id];
+
+        for (const auto& transition : transitions[state_id])
+        {
+            const auto successor_state_id = transition.get_successor_state().get_id();
+
+            if (distances[successor_state_id] != -1)
+            {
+                continue;
+            }
+
+            distances[successor_state_id] = cost + 1;
+
+            fifo_queue.push_back(successor_state_id);
+        }
+    }
+    return distances;
+}
+
+Transition::Transition(State successor_state, GroundAction creating_action) : m_successor_state(successor_state), m_creating_action(creating_action) {}
+
+bool Transition::operator==(const Transition& other) const
+{
+    if (this != &other)
+    {
+        return (m_successor_state.get_id() == other.m_successor_state.get_id()) && (m_creating_action.get_id() == other.m_creating_action.get_id());
+    }
+    return true;
+}
+
+size_t Transition::hash() const { return loki::hash_combine(m_successor_state.get_id(), m_creating_action.get_id()); }
+
+State Transition::get_successor_state() const { return m_successor_state; }
+
+GroundAction Transition::get_creating_action() const { return m_creating_action; }
+
+StateSpaceImpl::StateSpaceImpl(std::unique_ptr<PDDLParser>&& parser,
+                               std::shared_ptr<GroundedAAG> aag,
+                               std::shared_ptr<SuccessorStateGenerator> ssg,
+                               StateList states,
+                               State initial_state,
+                               size_t num_transitions,
+                               std::vector<Transitions> forward_transitions,
+                               std::vector<Transitions> backward_transitions,
+                               std::vector<int> goal_distances,
+                               StateSet goal_states,
+                               StateSet deadend_states) :
+    m_parser(std::move(parser)),
+    m_aag(std::move(aag)),
+    m_ssg(std::move(ssg)),
+    m_states(std::move(states)),
+    m_initial_state(std::move(initial_state)),
+    m_num_transitions(num_transitions),
+    m_forward_transitions(std::move(forward_transitions)),
+    m_backward_transitions(std::move(backward_transitions)),
+    m_goal_distances(std::move(goal_distances)),
+    m_goal_states(std::move(goal_states)),
+    m_deadend_states(std::move(deadend_states)),
+    m_state_indices(),
+    m_states_by_goal_distance()
+{
+    for (size_t index = 0; index < m_states.size(); ++index)
+    {
+        auto state = m_states.at(index);
+        m_state_indices.emplace(state, index);
+
+        auto distance = m_goal_distances.at(index);
+        auto& states_with_distance = m_states_by_goal_distance[distance];
+        states_with_distance.emplace_back(state);
+    }
+}
+
+std::shared_ptr<StateSpaceImpl>
+StateSpaceImpl::create(const fs::path& domain_file_path, const fs::path& problem_file_path, const size_t max_num_states, const size_t timeout_ms)
+{
+    auto stop_watch = StopWatch(timeout_ms);
+
+    auto pddl_parser = std::make_unique<PDDLParser>(domain_file_path, problem_file_path);
+    const auto problem = pddl_parser->get_problem();
+    auto aag = std::make_shared<GroundedAAG>(problem, pddl_parser->get_factories());
+    auto ssg = std::make_shared<SuccessorStateGenerator>(aag);
+    auto initial_state = ssg->get_or_create_initial_state();
+
+    if (!problem->static_goal_holds())
+    {
+        // Unsolvable
+        return nullptr;
+    }
+
+    auto lifo_queue = std::deque<State>();
+    lifo_queue.push_back(initial_state);
+
+    auto states = StateList { initial_state };
+    auto num_transitions = (size_t) 0;
+    auto forward_transitions = std::vector<Transitions>(1);
+    auto backward_transitions = std::vector<Transitions>(1);
+    auto goal_states = StateSet {};
+
+    auto applicable_actions = GroundActionList {};
+    stop_watch.start();
+    while (!lifo_queue.empty() && !stop_watch.has_finished())
+    {
+        const auto state = lifo_queue.back();
+
+        lifo_queue.pop_back();
+
+        if (state.literals_hold(problem->get_fluent_goal_condition()) && state.literals_hold(problem->get_derived_goal_condition()))
+        {
+            goal_states.insert(state);
+        }
+
+        aag->generate_applicable_actions(state, applicable_actions);
+
+        for (const auto& action : applicable_actions)
+        {
+            const auto successor_state = ssg->get_or_create_successor_state(state, action);
+
+            forward_transitions.resize(ssg->get_state_count());
+            backward_transitions.resize(ssg->get_state_count());
+
+            forward_transitions[state.get_id()].emplace_back(successor_state, action);
+            backward_transitions[successor_state.get_id()].emplace_back(state, action);
+            ++num_transitions;
+
+            if (successor_state.get_id() < states.size())
+            {
+                continue;
+            }
+
+            if (states.size() == max_num_states)
+            {
+                // Ran out of state resources
+                return nullptr;
+            }
+
+            states.push_back(successor_state);
+            lifo_queue.push_back(successor_state);
+            // States are stored by index
+            assert(states.back().get_id() == states.size() - 1);
+        }
+    }
+
+    if (stop_watch.has_finished())
+    {
+        // Ran out of time
+        return nullptr;
+    }
+
+    auto goal_distances =
+        compute_shortest_distances_from_states_impl(states.size(), StateList { goal_states.begin(), goal_states.end() }, backward_transitions);
+
+    auto deadend_states = StateSet {};
+    for (const auto& state : states)
+    {
+        if (goal_distances[state.get_id()] == -1)
+        {
+            deadend_states.insert(state);
+        }
+    }
+
+    // Must explicitly call constructor since it is private
+    return std::shared_ptr<StateSpaceImpl>(new StateSpaceImpl(std::move(pddl_parser),
+                                                              std::move(aag),
+                                                              std::move(ssg),
+                                                              std::move(states),
+                                                              initial_state,
+                                                              num_transitions,
+                                                              std::move(forward_transitions),
+                                                              std::move(backward_transitions),
+                                                              std::move(goal_distances),
+                                                              std::move(goal_states),
+                                                              std::move(deadend_states)));
+}
+
+/* Extended functionality */
+std::vector<int> StateSpaceImpl::compute_shortest_distances_from_states(const StateList& states, bool forward) const
+{
+    return compute_shortest_distances_from_states_impl(m_states.size(), states, (forward) ? m_forward_transitions : m_backward_transitions);
+}
+
+std::vector<std::vector<int>> StateSpaceImpl::compute_pairwise_shortest_state_distances(bool forward) const
+{
+    auto distances = std::vector<std::vector<int>> { m_states.size(), std::vector<int>(m_states.size(), -1) };
+    const auto& transitions = (forward) ? m_forward_transitions : m_backward_transitions;
+
+    // Initialize distance adjacency matrix
+    for (size_t state_id = 0; state_id < m_states.size(); ++state_id)
+    {
+        distances[state_id][state_id] = 0;
+        for (const auto& transition : transitions[state_id])
+        {
+            distances[state_id][transition.get_successor_state().get_id()] = 1;
+        }
+    }
+
+    // Compute transitive closure
+    for (size_t state_k = 0; state_k < m_states.size(); ++state_k)
+    {
+        for (size_t state_i = 0; state_i < m_states.size(); ++state_i)
+        {
+            for (size_t state_j = 0; state_j < m_states.size(); ++state_j)
+            {
+                if (distances[state_i][state_j] > distances[state_i][state_k] + distances[state_k][state_j])
+                {
+                    distances[state_i][state_j] = distances[state_i][state_k] + distances[state_k][state_j];
+                }
+            }
+        }
+    }
+
+    return distances;
+}
+
+/* Getters */
+Problem StateSpaceImpl::get_problem() const { return m_aag->get_problem(); }
+
+const StateList& StateSpaceImpl::get_states() const { return m_states; }
+
+State StateSpaceImpl::get_initial_state() const { return m_initial_state; }
+
+const std::vector<Transitions>& StateSpaceImpl::get_forward_transitions() const { return m_forward_transitions; }
+
+const std::vector<Transitions>& StateSpaceImpl::get_backward_transitions() const { return m_backward_transitions; }
+
+const std::vector<int>& StateSpaceImpl::get_goal_distances() const { return m_goal_distances; }
+
+int StateSpaceImpl::get_goal_distance(const State& state) const { return m_goal_distances.at(m_state_indices.at(state)); }
+
+int StateSpaceImpl::get_max_goal_distance() const { return *std::max_element(m_goal_distances.begin(), m_goal_distances.end()); }
+
+const StateSet& StateSpaceImpl::get_goal_states() const { return m_goal_states; }
+
+const StateSet& StateSpaceImpl::get_deadend_states() const { return m_deadend_states; }
+
+size_t StateSpaceImpl::get_num_states() const { return m_states.size(); }
+
+size_t StateSpaceImpl::get_num_transitions() const { return m_num_transitions; }
+
+size_t StateSpaceImpl::get_num_goal_states() const { return m_goal_states.size(); }
+
+size_t StateSpaceImpl::get_num_deadend_states() const { return m_deadend_states.size(); }
+
+bool StateSpaceImpl::is_deadend_state(const State& state) const { return m_goal_distances.at(m_state_indices.at(state)) < 0; }
+
+State StateSpaceImpl::sample_state_with_goal_distance(int goal_distance) const
+{
+    const auto& states = m_states_by_goal_distance.at(goal_distance);
+    const auto index = std::rand() % static_cast<int>(states.size());
+    return states[index];
+}
+
+std::shared_ptr<GroundedAAG> StateSpaceImpl::get_aag() const { return m_aag; }
+
+std::shared_ptr<SuccessorStateGenerator> StateSpaceImpl::get_ssg() const { return m_ssg; }
+
+const PDDLParser& StateSpaceImpl::get_pddl_parser() const { return *m_parser; }
+
+PDDLFactories& StateSpaceImpl::get_factories() { return m_parser->get_factories(); }
+
+}
