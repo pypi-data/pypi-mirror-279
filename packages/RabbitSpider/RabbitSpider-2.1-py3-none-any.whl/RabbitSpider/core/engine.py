5f9d6f98c7bc01b87994288413199179874cc9e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-14 16:03:33 +0000

    Add package RabbitSpider/2.1/RabbitSpider-2.1-py3-none-any.whl

---
+++ packages/RabbitSpider/RabbitSpider-2.1-py3-none-any.whl/RabbitSpider/core/engine.py
@@ -0,0 +1,153 @@
+import asyncio
+import os
+import sys
+import pickle
+from collections.abc import AsyncGenerator, Coroutine
+from traceback import print_exc
+from typing import Optional
+from aio_pika.abc import AbstractIncomingMessage
+from RabbitSpider.http.request import Request
+from RabbitSpider.http.response import Response
+from RabbitSpider.utils.control import TaskManager
+from RabbitSpider.utils.control import SettingManager
+from RabbitSpider.utils.control import PipelineManager
+from RabbitSpider.utils.control import MiddlewareManager
+from RabbitSpider.utils.dupefilter import RFPDupeFilter
+from RabbitSpider.utils.expections import RabbitExpect
+from RabbitSpider.utils.log import Logger
+from RabbitSpider.items import Item
+from RabbitSpider.core.scheduler import Scheduler
+from curl_cffi import CurlHttpVersion
+from aio_pika.exceptions import QueueEmpty
+
+
+class Engine(object):
+    name = os.path.basename(sys.argv[0])
+    http_version = CurlHttpVersion.V1_0
+    impersonate = "chrome120"
+
+    def __init__(self, sync):
+        self.session = None
+        self._future = None
+        self._connection = None
+        self._channel = None
+        self._sync = sync
+        self.settings = SettingManager()
+        self._scheduler = Scheduler(self.settings.get('RABBIT_USERNAME'),
+                                    self.settings.get('RABBIT_PASSWORD'),
+                                    self.settings.get('RABBIT_HOST'),
+                                    self.settings.get('RABBIT_PORT'),
+                                    self.settings.get('RABBIT_VIRTUAL_HOST'))
+        self._filter = RFPDupeFilter(self.name,
+                                     self.settings.get('REDIS_QUEUE_HOST'),
+                                     self.settings.get('REDIS_QUEUE_PORT'),
+                                     self.settings.get('REDIS_QUEUE_DB'))
+        self.logger = Logger(self.settings, self.name).logger
+        self._task_manager = TaskManager(self._sync)
+        self.middlewares = MiddlewareManager.create_instance(self.http_version, self.impersonate, self)
+        self.pipelines = PipelineManager.create_instance(self)
+
+    async def start_requests(self):
+        """初始请求"""
+        pass
+
+    async def parse(self, request: Request, response: Response):
+        """默认回调"""
+        pass
+
+    async def routing(self, result):
+        async def rule(res):
+            if isinstance(res, Request):
+                ret = pickle.dumps(res.model_dump())
+                if res.dupe_filter:
+                    if self._filter.request_seen(ret):
+                        self.logger.info(f'生产数据：{res.model_dump()}')
+                        await self._scheduler.producer(self._channel, queue=self.name, body=res.model_dump())
+                else:
+                    self.logger.info(f'生产数据：{res.model_dump()}')
+                    await self._scheduler.producer(self._channel, queue=self.name, body=res.model_dump())
+            elif isinstance(res, Item):
+                await self.pipelines.process_item(res)
+
+        if isinstance(result, AsyncGenerator):
+            async for r in result:
+                await rule(r)
+        elif isinstance(result, Coroutine):
+            r = await result
+            await rule(r)
+        elif isinstance(result, Request):
+            await rule(result)
+        else:
+            raise RabbitExpect('回调函数返回类型错误！')
+
+    async def produce(self):
+        await self._scheduler.create_queue(self._channel, self.name)
+        await self.routing(self.start_requests())
+
+    async def crawl(self):
+        while True:
+            try:
+                incoming_message: Optional[AbstractIncomingMessage] = await self._scheduler.consumer(self._channel,
+                                                                                                     queue=self.name)
+            except QueueEmpty:
+                if self._task_manager.all_done():
+                    await self._scheduler.delete_queue(self._channel, self.name)
+                    break
+                else:
+                    continue
+            except Exception:
+                break
+            if incoming_message:
+                await self._task_manager.semaphore.acquire()
+                self._task_manager.create_task(self.deal_resp(incoming_message))
+            else:
+                print(incoming_message)
+
+    async def consume(self):
+        await self._scheduler.consumer(self._channel, queue=self.name, callback=self.deal_resp,
+                                       prefetch=self._sync)
+        self._future = asyncio.Future()
+        await self._future
+
+    async def deal_resp(self, incoming_message):
+        ret = pickle.loads(incoming_message.body)
+        self.logger.info(f'消费数据：{ret}')
+        response = await self.middlewares.downloader(ret)
+        if response:
+            try:
+                callback = getattr(self, ret['callback'])
+                result = callback(Request(**ret), response)
+                if result:
+                    await self.routing(result)
+            except Exception as e:
+                self.logger.error(f'解析失败：{e}')
+                print_exc()
+                for task in asyncio.all_tasks():
+                    task.cancel()
+        try:
+            await self._channel.declare_queue(name=self.name, passive=True)
+        except Exception:
+            self._future.set_result('done') if self._future else None
+            self.logger.info(f'队列{self.name}已删除！')
+        else:
+            await incoming_message.ack()
+
+    async def run(self, mode):
+        self.logger.info(f'{self.name}任务开始')
+        self._connection, self._channel = await self._scheduler.connect()
+        self.session = await self.middlewares.download.new_session()
+        await self.pipelines.open_spider()
+        if mode == 'auto':
+            await self.produce()
+            await self.crawl()
+        elif mode == 'm':
+            await self.produce()
+        elif mode == 'w':
+            await self.consume()
+        else:
+            raise RabbitExpect('执行模式错误！')
+        await self.pipelines.close_spider()
+        await self._channel.close()
+        await self._connection.close()
+        await self.middlewares.download.exit(self.session)
+        self.logger.info(f'{self.name}任务完成')
---
+++ packages/RabbitSpider/RabbitSpider-2.1-py3-none-any.whl/RabbitSpider/utils/control.py
@@ -0,0 +1,169 @@
+import os, sys
+import asyncio
+from collections import defaultdict
+from importlib import import_module
+from typing import Final, Dict, List, Callable
+from asyncio import Task, Future, Semaphore, WindowsSelectorEventLoopPolicy
+from RabbitSpider.http.request import Request
+from RabbitSpider.http.response import Response
+from RabbitSpider.core.download import CurlDownload
+
+if sys.platform == 'win32':
+    asyncio.set_event_loop_policy(WindowsSelectorEventLoopPolicy())
+
+sys.path.append(os.path.abspath(os.path.join(os.path.abspath(sys.argv[0]), '../..')))
+
+
+def load_class(_path):
+    if not isinstance(_path, str):
+        if callable(_path):
+            return _path
+        else:
+            raise TypeError(f"args expected string or object, got {type(_path)}")
+    module, name = _path.rsplit('.', 1)
+    mod = import_module(module)
+    try:
+        cls = getattr(mod, name)
+    except AttributeError as exc:
+        raise NameError(f"Module {module!r} doesn't define any project named {name!r}")
+
+    return cls
+
+
+class SettingManager(object):
+    def __init__(self):
+        settings = import_module('settings')
+        self.attribute = {}
+        for key in dir(settings):
+            if key.isupper():
+                self.attribute[key] = getattr(settings, key)
+
+    def __setitem__(self, key, value):
+        self.attribute[key] = value
+
+    def __getitem__(self, key):
+        return self.attribute.get(key)
+
+    def __delitem__(self, key):
+        del self.attribute[key]
+
+    def get(self, key):
+        return self[key] if self[key] else None
+
+    def set(self, key, value):
+        self[key] = value
+
+
+class TaskManager(object):
+    def __init__(self, sync: int):
+        self.current_task: Final[set] = set()
+        self.semaphore = Semaphore(sync)
+
+    def create_task(self, coroutine) -> Task:
+        task = asyncio.create_task(coroutine)
+        self.current_task.add(task)
+
+        def done_callback(_fut: Future):
+            self.current_task.remove(task)
+            self.semaphore.release()
+
+        task.add_done_callback(done_callback)
+        return task
+
+    def all_done(self):
+        return len(self.current_task) == 0
+
+
+class PipelineManager(object):
+    def __init__(self, spider):
+        self.spider = spider
+        self.methods: Dict[str, List[Callable]] = defaultdict(list)
+        pipelines = spider.settings.get('ITEM_PIPELINES')
+        self._add_pipe(pipelines)
+
+    def _add_pipe(self, pipelines):
+        for pipeline in pipelines:
+            pipeline_obj = load_class(pipeline)()
+            if hasattr(pipeline_obj, 'open_spider'):
+                self.methods['open_spider'].append(getattr(pipeline_obj, 'open_spider'))
+            if hasattr(pipeline_obj, 'process_item'):
+                self.methods['process_item'].append(getattr(pipeline_obj, 'process_item'))
+            if hasattr(pipeline_obj, 'close_spider'):
+                self.methods['close_spider'].append(getattr(pipeline_obj, 'close_spider'))
+
+    async def open_spider(self):
+        for open_spider in self.methods['open_spider']:
+            await open_spider(self.spider)
+
+    async def process_item(self, req):
+        for process_item in self.methods['process_item']:
+            await process_item(req, self.spider)
+
+    async def close_spider(self):
+        for close_spider in self.methods['close_spider']:
+            await close_spider(self.spider)
+
+    @classmethod
+    def create_instance(cls, spider):
+        return cls(spider)
+
+
+class MiddlewareManager(object):
+    def __init__(self, spider, http_version, impersonate):
+        self.spider = spider
+        self.download = CurlDownload(http_version, impersonate)
+        self.methods: Dict[str, List[Callable]] = defaultdict(list)
+        middlewares = spider.settings.get('MIDDLEWARES')
+        self._add_middleware(middlewares)
+
+    def _add_middleware(self, middlewares):
+        for middleware in middlewares:
+            middleware_obj = load_class(middleware)()
+            if hasattr(middleware_obj, 'process_request'):
+                self.methods['process_request'].append(getattr(middleware_obj, 'process_request'))
+            if hasattr(middleware_obj, 'process_response'):
+                self.methods['process_response'].append(getattr(middleware_obj, 'process_response'))
+            if hasattr(middleware_obj, 'process_exception'):
+                self.methods['process_exception'].append(getattr(middleware_obj, 'process_exception'))
+
+    async def _process_request(self, request):
+        for process_request in self.methods['process_request']:
+            result = await process_request(Request(**request), self.spider)
+            if isinstance(result, (Request, Response)):
+                return result
+        return await self.download.fetch(self.spider.session, request)
+
+    async def _process_response(self, request, response):
+        for process_response in reversed(self.methods['process_response']):
+            result = await process_response(Request(**request), response, self.spider)
+            if isinstance(result, (Request, Response)):
+                return result
+        return response
+
+    async def _process_exception(self, request, exc):
+        for process_exception in self.methods['process_exception']:
+            result = await process_exception(Request(**request), exc, self.spider)
+            if isinstance(result, (Request, Response)):
+                return result
+        else:
+            raise exc
+
+    async def downloader(self, request):
+        try:
+            resp = await self._process_request(request)
+        except Exception as exc:
+            resp = await self._process_exception(request, exc)
+        if isinstance(resp, Response):
+            resp = await self._process_response(request, resp)
+        if isinstance(resp, Request):
+            if resp.retry < self.spider.settings.get('MAX_RETRY'):
+                resp.retry += 1
+                return await self.spider.routing(resp)
+            else:
+                self.spider.logger.error(f'请求失败：{request}')
+            return None
+        return resp
+
+    @classmethod
+    def create_instance(cls, http_version, impersonate, spider):
+        return cls(spider, http_version, impersonate)
