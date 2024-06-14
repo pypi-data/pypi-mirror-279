a3eb2e0d71e5a6564f9a6956ed755702a7795309
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-14 15:03:26 +0000

    Add package VeriScore/1.0.2/veriscore-1.0.2.tar.gz

---
+++ packages/VeriScore/veriscore-1.0.2.tar.gz/veriscore-1.0.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+   name="VeriScore",
+   version="1.0.2",
+   packages=find_packages(),
+   install_requires=[
+       #'requests', 'numpy'
+      'spacy',
+      'openai',
+      'anthropic',
+      'tiktoken',
+      'tqdm',
+   ],
+   description="Pip package for Verifact",
+   long_description=open('README.md').read(),
+   long_description_content_type="text/markdown",
+   classifiers=[
+       "Programming Language :: Python :: 3",
+       "License :: OSI Approved :: MIT License",
+       "Operating System :: OS Independent",
+   ],
+   python_requires='>=3.9',
+)
