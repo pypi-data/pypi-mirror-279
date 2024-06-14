8712022111eeea1a923387104ce74a8d6b20608b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-14 03:03:36 +0000

    Add package echo-alpha/0.2.4.1/echo_alpha-0.2.4.1.tar.gz

---
+++ packages/echo-alpha/echo_alpha-0.2.4.1.tar.gz/echo_alpha-0.2.4.1/src/echo_alpha/__init__.py
@@ -0,0 +1,8 @@
+from echo_alpha.echo_alpha import EchoClient
+
+def main():
+    """Entry point for the application script"""
+    print("Call your main application code here")
+
+
+_all_ = ["EchoClient"]
