55b510accefe57499620688069983277b076cfc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-14 16:03:44 +0000

    Add package bubble-tester/0.1/bubble_tester-0.1.tar.gz

---
+++ packages/bubble-tester/bubble_tester-0.1.tar.gz/bubble_tester-0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+setup(
+   name="bubble_tester",
+   version="0.1",
+   packages=find_packages(),
+   description="A simple greeting module",
+   author="Hari",
+   author_email="srikarthik@gmail.com",
+   url="https://github.com/SrihariKarthik/my_module",
+   classifiers=[
+       "Programming Language :: Python :: 3",
+       "License :: OSI Approved :: MIT License",
+       "Operating System :: OS Independent",
+   ],
+   python_requires='>=3.6',
+)
