3d3be3da87d32d4edeabca986153ab965a407b04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-14 03:03:32 +0000

    Add package EncryptDecryptPy/2.0.0/encryptdecryptpy-2.0.0.tar.gz

---
+++ packages/EncryptDecryptPy/encryptdecryptpy-2.0.0.tar.gz/encryptdecryptpy-2.0.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='EncryptDecryptPy',
+    version='2.0.0',
+    description='A Python package for encrypting and decrypting strings.',
+    long_description='''This package provides a simple and secure way to encrypt and decrypt strings in Python. It is recommended to use a strong encryption library like cryptography for this purpose. This package requires cryptography to be installed separately.''',
+    long_description_content_type="text/markdown",
+    url='https://github.com/ShanKonduru/EncryptDecryptPy',  # Replace with your project URL
+    author='Shan Konduru',
+    author_email='ShanKonduru@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+        'cryptography'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
