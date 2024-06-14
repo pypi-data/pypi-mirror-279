9941855862596f36d0f1c560549ffd517c0ba473
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-14 05:02:57 +0000

    Add package bda-service-template/0.0.50/bda-service-template-0.0.50.tar.gz

---
+++ packages/bda-service-template/bda-service-template-0.0.50.tar.gz/bda-service-template-0.0.50/bdaservicetemplate/processor_service.py
@@ -0,0 +1,28 @@
+from abc import abstractclassmethod
+from .streaming_service import StreamingService
+from bdaserviceutils import get_kafka_binder_brokers, get_input_channel, get_output_channel
+from kafka import KafkaConsumer, KafkaProducer
+from abc import ABC, abstractclassmethod
+import json
+
+
+class ProcessorService(ABC, StreamingService):
+
+    alida_service_mode = "processor"
+    
+    def __init__(self, parser):
+        super().__init__(parser)
+        self.consumer = self.get_consumer()
+        self.producer = self.get_producer()
+
+    def run(self):
+        for message in self.consumer:
+            json_message = json.loads(message.value.decode('utf-8'))
+            self.on_message(message=json_message)
+
+    @abstractclassmethod
+    def on_message(self, message):
+        pass
+
+    def send_message(self, message):
+        self.producer.send_message(message)
