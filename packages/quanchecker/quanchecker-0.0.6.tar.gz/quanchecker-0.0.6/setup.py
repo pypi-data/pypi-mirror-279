6a2e1c4fa8429824e36f278312e158b3a03947e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-14 05:02:55 +0000

    Add package quanchecker/0.0.6/quanchecker-0.0.6.tar.gz

---
+++ packages/quanchecker/quanchecker-0.0.6.tar.gz/quanchecker-0.0.6/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.6'
+DESCRIPTION = 'Checker Library for QuanC'
+
+# Setting up
+setup(
+    name="quanchecker",
+    version=VERSION,
+    author="QuanC",
+    author_email="<project.quanc@gmail.com>",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['requests', 'docker'],
+    keywords=['python', 'quanc'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
