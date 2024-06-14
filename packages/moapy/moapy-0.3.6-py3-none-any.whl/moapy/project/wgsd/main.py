cfaf7a3982385e5e780d5f54774b823cbdbadf05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-14 15:04:07 +0000

    Add package moapy/0.3.6/moapy-0.3.6-py3-none-any.whl

---
+++ packages/moapy/moapy-0.3.6-py3-none-any.whl/moapy/project/wgsd/main.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI
+from fastapi.openapi.utils import get_openapi
+from wgsd_flow import wgsd_flow_router
+import json
+ 
+app = FastAPI()
+ 
+app.include_router(wgsd_flow_router, prefix="/module1", tags=["module1"])
+ 
+# Example endpoint to check the generated OpenAPI specification
+@app.get("/openapi.json")
+async def get_openapi_spec():
+    return app.openapi()
+ 
+# Function to generate and save OpenAPI spec for each router
+serv={"url": "https://moa.rpm.kr-dv-midasit.com/backend/function-executor/python-execute/"}
+
+def save_openapi_spec(router, filename):
+    temp_app = FastAPI()
+    temp_app.include_router(router)
+    openapi_spec = get_openapi(
+        title=temp_app.title,
+        version=temp_app.version,
+        openapi_version=temp_app.openapi_version,
+        description=temp_app.description,
+        routes=temp_app.routes,
+        servers=[{"url": "https://moa.rpm.kr-dv-midasit.com/backend/function-executor/python-execute/"}],
+    )
+    with open(filename, "w") as f:
+        json.dump(openapi_spec, f, indent=2)
+ 
+if __name__ == "__main__":
+    save_openapi_spec(wgsd_flow_router, "module1_openapi.json")
