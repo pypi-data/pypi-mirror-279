92a8c10fef1935aa743ae5868615103eb82458cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-14 15:03:55 +0000

    Add package mseedlib/0.0.6/mseedlib-0.0.6.tar.gz

---
+++ packages/mseedlib/mseedlib-0.0.6.tar.gz/mseedlib-0.0.6/examples/read_numpy.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+#
+# Read miniSEED file(s) using the mseedlib module and decode
+# data samples directly into NumPy arrays.
+#
+# The data are organized into a list of dictionaries, one for
+# each contiguous trace of data.  The dictionaries contain very
+# basic metadata and a NumPy array of data samples.  This data
+# structure is for illustration only, and would like need
+# adaptation for a real application.
+#
+# This file is part of the Python mseedlib module.
+# Copywrite (c) 2024, EarthScope Data Services
+
+import os
+import sys
+import pprint
+import numpy as np
+from mseedlib import MSTraceList, sourceid2nslc
+
+input_files = []
+
+# Verify that input files are readable
+for arg in sys.argv[1:]:
+    if os.access(arg, os.R_OK):
+        input_files.append(arg)
+    else:
+        sys.exit("Cannot read file: %s" % arg)
+
+if not input_files:
+    sys.exit("No input files specified")
+
+# List of dictionaries for each trace
+traces = []
+
+# Translate libmseed sample type to numpy type
+nptype = {'i': np.int32, 'f': np.float32, 'd': np.float64, 't': np.char}
+
+mstl = MSTraceList()
+
+# Read all input files, creating a record lists and _not_ unpacking data samples
+for file in input_files:
+    print("Reading file: %s" % file)
+    mstl.read_file(file, unpack_data=False, record_list=True)
+
+for traceid in mstl.traceids():
+    for segment in traceid.segments():
+        # Fetch estimated sample size and type
+        (sample_size, sample_type) = segment.sample_size_type
+
+        dtype = nptype[sample_type]
+
+        # Allocate NumPy array for data samples
+        data_samples = np.zeros(segment.samplecnt, dtype=dtype)
+
+        # Unpack data samples into allocated NumPy array
+        segment.unpack_recordlist(buffer_pointer=np.ctypeslib.as_ctypes(data_samples),
+                                  buffer_bytes=data_samples.nbytes)
+
+        # Create a dictionary for the trace with basic metadata
+        trace = {'sourceid': traceid.sourceid,
+                 'NSLC': sourceid2nslc(traceid.sourceid),
+                 'publication_version': traceid.pubversion,
+                 'start_time': segment.starttime_str(),
+                 'end_time': segment.endtime_str(),
+                 'sample_rate': segment.samprate,
+                 'data_samples': data_samples}
+
+        traces.append(trace)
+
+# Pretty print the trace list
+pp = pprint.PrettyPrinter(indent=4, sort_dicts=False)
+pp.pprint(traces)
---
+++ packages/mseedlib/mseedlib-0.0.6.tar.gz/mseedlib-0.0.6/tests/test_msrecord.py
@@ -0,0 +1,79 @@
+import pytest
+import os
+import json
+import math
+import ctypes as ct
+from mseedlib import MS3Record, DataEncoding
+
+test_dir = os.path.abspath(os.path.dirname(__file__))
+test_pack3 = os.path.join(test_dir, 'data', 'packtest_sine500.mseed3')
+test_pack2 = os.path.join(test_dir, 'data', 'packtest_sine500.mseed2')
+
+# A sine wave of 500 samples
+sine_500 = list(map(lambda x: int(math.sin(math.radians(x)) * 500), range(0, 500)))
+
+# A global record buffer
+record_buffer = b''
+
+
+def record_handler(record, handler_data):
+    '''A callback function for MS3Record.set_record_handler()
+    Stores the record in a global buffer for testing
+    '''
+    print("Record handler called, record length: %d" % len(record))
+    global record_buffer
+    record_buffer = bytes(record)
+
+
+def test_msrecord_pack():
+
+    # Test populating an MS3Record object with setters
+    msr = MS3Record()
+    msr.reclen = 512
+    msr.sourceid = "FDSN:XX_TEST__B_S_X"
+    msr.formatversion = 3
+    msr.flags = ct.c_uint8(0x04).value  # Set the 4th bit (clock locked) to 1
+    msr.set_starttime_str("2023-01-02T01:02:03.123456789Z")
+    msr.samprate = 50.0
+    msr.encoding = DataEncoding.STEIM2  # value of 11
+    msr.pubversion = 1
+    msr.extra = json.dumps({"FDSN": {"Time": {"Quality": 80}}})
+
+    assert msr.reclen == 512
+    assert msr.sourceid == "FDSN:XX_TEST__B_S_X"
+    assert msr.formatversion == 3
+    assert msr.flags_dict() == {'clock_locked': True}
+    assert msr.starttime == 1672621323123456789
+    assert msr.starttime_seconds == 1672621323.1234567
+    assert msr.samprate == 50.0
+    assert msr.encoding == DataEncoding.STEIM2
+    assert msr.pubversion == 1
+    assert msr.extra == '{"FDSN":{"Time":{"Quality":80}}}'
+
+    # Test packing of an miniSEED v3 record
+    (packed_samples, packed_records) = msr.pack(record_handler,
+                                                datasamples=sine_500,
+                                                sampletype='i')
+
+    assert packed_samples == 500
+    assert packed_records == 1
+    assert len(record_buffer) == 475
+
+    with open(test_pack3, 'rb') as f:
+        record_v3 = f.read()
+        assert (record_buffer == record_v3)
+
+    # Test packing of an miniSEED v2 record
+    msr.formatversion = 2
+
+    (packed_samples, packed_records) = msr.pack(record_handler,
+                                                datasamples=sine_500,
+                                                sampletype='i')
+
+    assert packed_samples == 500
+    assert packed_records == 1
+    assert len(record_buffer) == 512
+
+    with open(test_pack2, 'rb') as f:
+        record_v2 = f.read()
+        assert (record_buffer == record_v2)
