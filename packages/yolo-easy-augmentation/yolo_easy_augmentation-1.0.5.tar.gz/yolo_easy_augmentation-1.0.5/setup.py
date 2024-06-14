0ddad9fa091c961866cff288ab2d8a5059c5dd7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-14 14:03:47 +0000

    Add package yolo-easy-augmentation/1.0.5/yolo_easy_augmentation-1.0.5.tar.gz

---
+++ packages/yolo-easy-augmentation/yolo_easy_augmentation-1.0.5.tar.gz/yolo_easy_augmentation-1.0.5/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+# README 파일을 UTF-8 인코딩으로 읽어옵니다.
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='yolo_easy_augmentation',
+    version='1.0.5',
+    description='enter dataset path with train, val, test. Then automatically augment every images to ready YOLO object detection train',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Na-I-Eon',
+    author_email='112fkdldjs@naver.com',
+    url='https://github.com/Nyan-SouthKorea/yolo_auto_augmentation',
+    packages=find_packages(),
+    install_requires=[
+        'opencv-python', 'tqdm', 'albumentations', 'natsort'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.8',
+)
