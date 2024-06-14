bb320bf11d88b9e912d6d8ad75e4b5146fa4ce03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-14 16:03:45 +0000

    Add package aiokeitaro/1.0.3/aiokeitaro-1.0.3-py3-none-any.whl

---
+++ packages/aiokeitaro/aiokeitaro-1.0.3-py3-none-any.whl/aiokeitaro/keitaro.py
@@ -0,0 +1,49 @@
+import os
+import aiohttp
+
+from .utils import build_host_url
+from .api import API
+from .resources import Affiliate, Campaign
+
+class Keitaro:
+    api_endpoint = 'admin_api/v1/'
+
+    def __init__(self, api_key: str, host: str, from_env: bool = False) -> None:
+        if from_env:
+            self.api_key = os.environ[api_key]
+            self.host = os.environ[host]
+        else:
+            self.api_key = api_key
+            self.host = host
+
+    @property
+    def api_key(self):
+        return self._api_key
+
+    @property
+    def host(self):
+        return self._host
+
+    @api_key.setter
+    def api_key(self, api_key):
+        self._api_key = api_key
+    
+    @host.setter
+    def host(self, host):
+        self._host = build_host_url(host)
+
+    async def send_request(self, method: str, endpoint: str, *kwargs):
+        """
+        Sends HTTP request to Keitaro Admin API
+        """
+        url = API.build_url(self.host, Keitaro.api_endpoint, endpoint)
+
+        async with aiohttp.ClientSession() as session:
+            async with session.request(
+                method, url, headers={'Api-Key': self.api_key}, *kwargs
+            ) as response:
+                if response.content_type == 'application/json':
+                    data = await response.json()
+                else:
+                    data = await response.text()
+                return data
