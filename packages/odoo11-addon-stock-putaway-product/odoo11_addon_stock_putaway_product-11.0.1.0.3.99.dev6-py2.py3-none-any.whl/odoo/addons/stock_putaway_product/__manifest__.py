1dea4c890a88ac7fbc44e2ce23d85334c8291f7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-14 16:03:47 +0000

    Add package odoo11-addon-stock-putaway-product/11.0.1.0.3.99.dev6/odoo11_addon_stock_putaway_product-11.0.1.0.3.99.dev6-py2.py3-none-any.whl

---
+++ packages/odoo11-addon-stock-putaway-product/odoo11_addon_stock_putaway_product-11.0.1.0.3.99.dev6-py2.py3-none-any.whl/odoo/addons/stock_putaway_product/__manifest__.py
@@ -0,0 +1,24 @@
+# Copyright 2018 Camptocamp SA
+# Copyright 2016 Jos De Graeve - Apertoso N.V. <Jos.DeGraeve@apertoso.be>
+# Copyright 2016 Carlos Dauden - Tecnativa <carlos.dauden@tecnativa.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    'name': 'Putaway strategy per product',
+    'summary': 'Set a product location and put-away strategy per product',
+    'version': '11.0.1.0.3',
+    'category': 'Inventory',
+    'website': 'https://github.com/OCA/stock-logistics-warehouse',
+    'author': 'Apertoso N.V., '
+              'Tecnativa, '
+              'Camptocamp, '
+              'Odoo Community Association (OCA)',
+    'license': 'AGPL-3',
+    'depends': [
+        'stock_putaway_method'
+    ],
+    'data': [
+        'views/product.xml',
+        'views/product_putaway.xml',
+        'security/ir.model.access.csv',
+    ]
+}
