68d47e319715d575625fbb60aadf5adeda67f8a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-15 05:04:28 +0000

    Add package gpt-computer-assistant/0.14.5/gpt_computer_assistant-0.14.5.tar.gz

---
+++ packages/gpt-computer-assistant/gpt_computer_assistant-0.14.5.tar.gz/gpt_computer_assistant-0.14.5/gpt_computer_assistant/audio/tts.py
@@ -0,0 +1,37 @@
+try:
+    from ..llm import *
+    from ..utils.db import artifacts_dir
+except ImportError:
+    from llm import *
+    from utils.db import artifacts_dir
+import os
+
+import hashlib
+
+def text_to_speech(text):
+    """Converts text to speech using an external service.
+
+    Args:
+        text (str): The text to be converted to speech.
+
+    Returns:
+        str: The file path of the generated speech audio file.
+    """
+    # create sha256 hash of text and save it if already exists just return it
+    # if not exists create it and save it
+
+    sha = hashlib.sha256(text.encode()).hexdigest()
+
+    location = os.path.join(artifacts_dir, f"{sha}.mp3")
+
+    if os.path.exists(location):
+        return location
+    else:
+        response = get_client().audio.speech.create(
+            model="tts-1",
+            voice="fable",
+            input=text,
+        )
+
+        response.stream_to_file(location)
+        return location
