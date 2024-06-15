a63ef84f8b7c4ec5396fd4ef18428ed86ea9c5ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-15 15:05:45 +0000

    Add package mlintern010/0.1.2/mlintern010-0.1.2.tar.gz

---
+++ packages/mlintern010/mlintern010-0.1.2.tar.gz/mlintern010-0.1.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mlintern010',
+    version='0.1.2',
+    packages=find_packages(),
+    install_requires =[
+        'numpy',
+        'pandas'
+    ],
+    entry_points={
+        "console_scripts":[
+        "mlintern-010 = mlintern010:intia"
+        ],
+    },
+)
