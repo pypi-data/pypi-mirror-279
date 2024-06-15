459a3044f918a53e271b036952406215da261bf3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-15 05:04:38 +0000

    Add package survesh-distributions/0.1/survesh-distributions-0.1.tar.gz

---
+++ packages/survesh-distributions/survesh-distributions-0.1.tar.gz/survesh-distributions-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='survesh-distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['survesh-distributions'],
+      author='Survesh',
+      zip_safe=False)
