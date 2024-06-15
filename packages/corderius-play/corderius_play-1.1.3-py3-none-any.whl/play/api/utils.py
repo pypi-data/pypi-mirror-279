85ca227935d9bc19b44b437af443fa9a01dd9e5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-15 04:03:53 +0000

    Add package corderius-play/1.1.3/corderius_play-1.1.3-py3-none-any.whl

---
+++ packages/corderius-play/corderius_play-1.1.3-py3-none-any.whl/play/api/utils.py
@@ -0,0 +1,57 @@
+"""Game functions and utilities."""
+
+import asyncio as _asyncio
+import logging as _logging
+
+import pygame  # pylint: disable=import-error
+
+from .events import _when_program_starts_callbacks, _game_loop, _loop
+from ..utils import color_name_to_rgb as _color_name_to_rgb
+from ..globals import backdrop as _backdrop
+
+BACKDROP = _backdrop
+
+
+def start_program():
+    """
+    Calling this function starts your program running.
+
+    play.start_program() should almost certainly go at the very end of your program.
+    """
+    for func in _when_program_starts_callbacks:
+        _loop.create_task(func())
+
+    _loop.call_soon(_game_loop)
+    try:
+        _loop.run_forever()
+    finally:
+        _logging.getLogger("asyncio").setLevel(_logging.CRITICAL)
+        pygame.quit()  # pylint: disable=no-member
+
+
+async def animate():
+    await _asyncio.sleep(0)
+
+
+def set_backdrop(color_or_image_name):
+    """Set the backdrop color or image for the game.
+    :param color_or_image_name: The color or image to set as the backdrop.
+    """
+    global BACKDROP
+    _color_name_to_rgb(color_or_image_name)
+
+    BACKDROP = color_or_image_name
+
+
+async def timer(seconds=1.0):
+    """Wait a number of seconds. Used with the await keyword like this:
+
+    @play.repeat_forever
+    async def do():
+        await play.timer(seconds=2)
+        print('hi')
+    :param seconds: The number of seconds to wait.
+    :return: True after the number of seconds has passed.
+    """
+    await _asyncio.sleep(seconds)
+    return True
