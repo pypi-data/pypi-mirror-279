8ffa74eb1d42c36718ba33f05301177b19d9e82e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-15 04:04:07 +0000

    Add package pyqwe/1.5.2/pyqwe-1.5.2.tar.gz

---
+++ packages/pyqwe/pyqwe-1.5.2.tar.gz/pyqwe-1.5.2/pyqwe/parser.py
@@ -0,0 +1,23 @@
+import argparse
+
+from pyqwe.helpers import Colr
+
+
+class ArgumentParser(argparse.ArgumentParser):
+    def __init__(self, *args, **kwargs):
+        super(ArgumentParser, self).__init__(*args, **kwargs)
+        self.program = {key: kwargs[key] for key in kwargs}
+        self.options = []
+
+    def print_help(self, file=None):
+        print(
+            "\n\r"
+            "Usage: pyqwe <option> "
+            "\n\r\n\r"
+            f" {Colr.OKCYAN}list, ls{Colr.END} => List all commands found in pyproject.toml"
+            "\n\r"
+            f" {Colr.OKCYAN}-h, --help{Colr.END} => Show the help message and exit"
+            "\n\r"
+            f" {Colr.OKCYAN}-v, --version{Colr.END} => Show the version and exit"
+        )
+        print("")
