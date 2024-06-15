df8448baf7424f4beeab7708f510537663807dce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-15 05:04:30 +0000

    Add package pynguin/0.37.0/pynguin-0.37.0.tar.gz

---
+++ packages/pynguin/pynguin-0.37.0.tar.gz/pynguin-0.37.0/tests/testutils.py
@@ -0,0 +1,41 @@
+#  This file is part of Pynguin.
+#
+#  SPDX-FileCopyrightText: 2019–2024 Pynguin Contributors
+#
+#  SPDX-License-Identifier: MIT
+#
+"""Some utilites to make testing easier."""
+import pynguin.utils.generic.genericaccessibleobject as gao
+
+from pynguin.analyses.typesystem import Instance
+from pynguin.analyses.typesystem import ProperType
+from pynguin.analyses.typesystem import TypeSystem
+
+
+def feed_typesystem(system: TypeSystem, generic: gao.GenericAccessibleObject):
+    """Feeds the type system.
+
+    Small helper because TypeInfos need to be aligned, and we don't have one
+    large typesystem during testing but create them in various places.
+
+    Args:
+        system: the type system to feed
+        generic: an accessible to query
+    """
+
+    # TODO(fk) think about making this less hacky.
+    def feed(typ: ProperType):
+        if isinstance(typ, Instance):
+            system.to_type_info(typ.type.raw_type)
+
+    if isinstance(generic, gao.GenericCallableAccessibleObject):
+        feed(generic.inferred_signature.return_type)
+        for para in generic.inferred_signature.original_parameters.values():
+            feed(para)
+
+    if isinstance(generic, gao.GenericConstructor):
+        assert generic.owner
+        system.to_type_info(generic.owner.raw_type)
+
+    if isinstance(generic, gao.GenericField):
+        system.to_type_info(generic.owner.raw_type)
