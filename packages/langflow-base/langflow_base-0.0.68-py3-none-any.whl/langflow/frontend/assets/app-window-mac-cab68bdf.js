7cd2dd2ea80d6cac0e0e37f7f661733a64ea9970
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-15 03:03:47 +0000

    Add package langflow-base/0.0.68/langflow_base-0.0.68-py3-none-any.whl

---
+++ packages/langflow-base/langflow_base-0.0.68-py3-none-any.whl/langflow/frontend/assets/app-window-mac-cab68bdf.js
@@ -0,0 +1,6 @@
+import{eg as e}from"./index-e2611c7b.js";/**
+ * @license lucide-react v0.394.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const p=e("AppWindowMac",[["rect",{width:"20",height:"16",x:"2",y:"4",rx:"2",key:"18n3k1"}],["path",{d:"M6 8h.01",key:"x9i8wu"}],["path",{d:"M10 8h.01",key:"1r9ogq"}],["path",{d:"M14 8h.01",key:"1primd"}]]);export{p as default};
---
+++ packages/langflow-base/langflow_base-0.0.68-py3-none-any.whl/langflow/frontend/assets/fish-symbol-6137c7f3.js
@@ -0,0 +1,6 @@
+import{eg as o}from"./index-e2611c7b.js";/**
+ * @license lucide-react v0.394.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const s=o("FishSymbol",[["path",{d:"M2 16s9-15 20-4C11 23 2 8 2 8",key:"h4oh4o"}]]);export{s as default};
