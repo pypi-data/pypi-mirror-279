282885b53475e5e372bed445b59f5b66431ecbe8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-15 05:04:19 +0000

    Add package acore-conf/0.1.2/acore_conf-0.1.2-py3-none-any.whl

---
+++ packages/acore-conf/acore_conf-0.1.2-py3-none-any.whl/acore_conf/api.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from .impl import read_config_file
+from .impl import read_config_content
+from .impl import update_config_file
+from .impl import update_config_content
+from .impl import write_config_content
+from .impl import write_config_file
+from .impl import apply_changes
