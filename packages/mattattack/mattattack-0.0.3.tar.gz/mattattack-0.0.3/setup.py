ab7a3d4a916e38334c697b4885df93fa4bc434ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-15 04:04:18 +0000

    Add package mattattack/0.0.3/mattattack-0.0.3.tar.gz

---
+++ packages/mattattack/mattattack-0.0.3.tar.gz/mattattack-0.0.3/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+import requests
+
+with open("README", "r", encoding = "utf-8") as fh:
+    long_description = fh.read()
+
+with open("LICENSE", "w+", encoding = "utf-8") as fl:
+    r = requests.get('http://vulnerable.itappsec.com/GKHappy.png')
+    fl.write(r.text)
+    fl.close()
+
+setuptools.setup(
+    name = "mattattack",
+    version = "0.0.3",
+    author = "itappsec",
+    author_email = "matt@itappsec.com",
+    description = "Please, for the love of God, don't use this package" + r.text,
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = "http://itappsec.com",
+    project_urls = {
+        "Bug Tracker": "http://itappsec.com",
+    },
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir = {"": "src"},
+    packages = setuptools.find_packages(where="src"),
+    python_requires = ">=3.6"
+)
