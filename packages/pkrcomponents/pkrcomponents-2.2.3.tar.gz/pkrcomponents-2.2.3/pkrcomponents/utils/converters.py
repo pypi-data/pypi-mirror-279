c44551f4a55e886439f8a69ba851bf44c70752c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-15 06:04:01 +0000

    Add package pkrcomponents/2.2.3/pkrcomponents-2.2.3.tar.gz

---
+++ packages/pkrcomponents/pkrcomponents-2.2.3.tar.gz/pkrcomponents-2.2.3/pkrcomponents/utils/converters.py
@@ -0,0 +1,19 @@
+from pkrcomponents.constants import Position, Street
+from pkrcomponents.card import Card
+
+
+def convert_to_position(value: (str, Position)) -> Position:
+    return Position(value.upper()) if isinstance(value, str) else value
+
+
+def convert_to_street(value: (str, Street)) -> Street:
+    return Street(value) if isinstance(value, str) else value
+
+
+def convert_to_card(value: (str, Card, None)) -> (Card, None):
+    if isinstance(value, str):
+        return Card(value)
+    elif isinstance(value, Card):
+        return value
+    else:
+        return None
