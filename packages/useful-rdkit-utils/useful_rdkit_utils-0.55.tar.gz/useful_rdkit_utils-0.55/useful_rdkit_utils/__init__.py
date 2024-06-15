09c1918e45412e9ffdf9db3d8d6bb09b108fc7d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-15 14:03:52 +0000

    Add package useful-rdkit-utils/0.55/useful_rdkit_utils-0.55.tar.gz

---
+++ packages/useful-rdkit-utils/useful_rdkit_utils-0.55.tar.gz/useful_rdkit_utils-0.55/useful_rdkit_utils/__init__.py
@@ -0,0 +1,17 @@
+"""Some useful RDKit functions"""
+
+# Add imports here
+from .misc_utils import *
+from .reos import REOS
+from .pandas_utils import *
+from .ring_systems import *
+from .seaborn_utils import *
+from .stat_utils import *
+from .units import *
+from .geometry import *
+from .descriptors import *
+from .jupyter_utils import *
+from .reactions import *
+from .split_utils import *
+
+__version__ = "0.55"
