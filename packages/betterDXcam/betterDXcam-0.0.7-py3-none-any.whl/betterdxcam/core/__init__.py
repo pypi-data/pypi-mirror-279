72c1bd88fcf2f59f4ef37ea266c4d00c4147b22c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-15 04:04:08 +0000

    Add package betterDXcam/0.0.7/betterDXcam-0.0.7-py3-none-any.whl

---
+++ packages/betterDXcam/betterDXcam-0.0.7-py3-none-any.whl/betterdxcam/core/__init__.py
@@ -0,0 +1,7 @@
+__all__ = ["Device", "Output", "StageSurface", "Duplicator"]
+
+
+from betterdxcam.core.device import Device
+from betterdxcam.core.output import Output
+from betterdxcam.core.stagesurf import StageSurface
+from betterdxcam.core.duplicator import Duplicator
