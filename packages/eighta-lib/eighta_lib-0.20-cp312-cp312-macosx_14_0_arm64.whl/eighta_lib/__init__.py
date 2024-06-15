185c6f98e17e4766d1090c71ff63a7f3d709d155
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-15 04:03:58 +0000

    Add package eighta-lib/0.20/eighta_lib-0.20-cp312-cp312-macosx_14_0_arm64.whl

---
+++ packages/eighta-lib/eighta_lib-0.20-cp312-cp312-macosx_14_0_arm64.whl/eighta_lib/__init__.py
@@ -0,0 +1,25 @@
+from .slicing import read_slice_h5ad, write_slice_h5ad
+from .file_management import update_h5ad, pop_h5ad, filter_anndata_h5ad
+from .analysis_tools.gene_analysis import gene_variance, top_n_variance, create_mask_top_n_variance
+from .analysis_tools.incremental_pca import incremental_pca_h5ad
+from .data_transformation.log_transform import log_transform
+from .data_transformation.scale import scale
+from .data_transformation.normalize import normalize
+
+__version__ = "0.20"
+
+
+__all__ = [
+    "read_slice_h5ad",
+    "write_slice_h5ad",
+    "update_h5ad",
+    "pop_h5ad",
+    "filter_anndata_h5ad",
+    "gene_variance",
+    "top_n_variance",
+    "create_mask_top_n_variance",
+    "incremental_pca_h5ad",
+    "log_transform",
+    "scale",
+    "normalize"
+]
