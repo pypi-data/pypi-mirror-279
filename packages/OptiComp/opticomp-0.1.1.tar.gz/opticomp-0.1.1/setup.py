6f23da70b0abc9bd8f34f3d51d5717e6e13bdc0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-15 04:03:49 +0000

    Add package OptiComp/0.1.1/opticomp-0.1.1.tar.gz

---
+++ packages/OptiComp/opticomp-0.1.1.tar.gz/opticomp-0.1.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as f:
+    description = f.read()
+
+setup(
+    name="OptiComp",
+    version="0.1.1",
+    description="Library to easily compare optimizers on a custom or common objective.",
+    long_description=description,
+    long_description_content_type="text/markdown",
+    author="SJWRD",
+    url="https://github.com/OptiComp/OptiComp",
+    packages=find_packages(include=['opticomp', 'opticomp.*']),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "numpy",
+        "tqdm",
+    ]
+)
