e8c839261d86c158a60874d62edaa23ff6c4f3aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-15 14:03:41 +0000

    Add package paquete-hackademy/1.0.0/paquete_hackademy-1.0.0-py3-none-any.whl

---
+++ packages/paquete-hackademy/paquete_hackademy-1.0.0-py3-none-any.whl/calculadora/basica.py
@@ -0,0 +1,30 @@
+def sumar(a, b):
+    """
+    Retorna la suma dos números
+    """
+    return a + b
+
+
+def restar(a, b):
+    """
+    Retorna la resta dos números
+    """
+    return a - b
+
+
+def multip(a, b):
+    """
+    Retorna la multiplicación dos números
+    """
+    print("Esto es el módulo de calculadora básica")
+    return a * b
+
+
+def div(a, b):
+    """
+    Retorna la división dos números
+    """
+
+    if b == 0:
+        print("No se puede dividir por ceros.")
+    return a / b
