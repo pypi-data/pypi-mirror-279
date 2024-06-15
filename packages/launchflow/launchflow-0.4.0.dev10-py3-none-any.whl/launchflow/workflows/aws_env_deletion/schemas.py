52cbc0f879349e120dcf3e8b1a9cd016741719c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-15 06:03:59 +0000

    Add package launchflow/0.4.0.dev10/launchflow-0.4.0.dev10-py3-none-any.whl

---
+++ packages/launchflow/launchflow-0.4.0.dev10-py3-none-any.whl/launchflow/workflows/aws_env_deletion/schemas.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel
+
+from launchflow.models.launchflow_uri import LaunchFlowURI
+
+
+class AWSEnvironmentDeletionInputs(BaseModel):
+    launchflow_uri: LaunchFlowURI
+    aws_region: str
+    artifact_bucket: str
+    lock_id: str
+
+
+class AWSEnvironmentDeletionOutputs(BaseModel):
+    success: bool
