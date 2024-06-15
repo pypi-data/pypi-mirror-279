c3cd26c0a809ad3b37a43eda544c5a7c1879e789
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-15 03:04:18 +0000

    Add package zer/0.0.5/zer-0.0.5.tar.gz

---
+++ packages/zer/zer-0.0.5.tar.gz/zer-0.0.5/README.md
@@ -0,0 +1,4 @@
+Zer is a Python module for code reduction and automation.
+
+Module version: 0.0.5 alpha.
+Publisher: ZerProg studio.
---
+++ packages/zer/zer-0.0.5.tar.gz/zer-0.0.5/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='zer',
+    version='0.0.5',
+    author='ZerProg studio',
+    install_requires=['tqdm'],
+    description='Zer is a Python module for code reduction and automation.',
+    packages=find_packages()
+)
