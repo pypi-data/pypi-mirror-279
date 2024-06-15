f074d820530d402bb3f40ae374fce8559320ff47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-15 06:04:12 +0000

    Add package LearningNashQLearning/0.11/LearningNashQLearning-0.11.tar.gz

---
+++ packages/LearningNashQLearning/LearningNashQLearning-0.11.tar.gz/LearningNashQLearning-0.11/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='LearningNashQLearning',
+    version='0.11',
+    packages=find_packages(),
+    description='A library for learning NashQ-learning',
+    install_requires=[
+        'numpy',
+        'matplotlib',
+        'seaborn',
+        'networkx',
+        'pygambit',
+        'netgraph',
+        'ipywidgets',
+        'notebook',
+        'ipympl',
+    ]
+)
