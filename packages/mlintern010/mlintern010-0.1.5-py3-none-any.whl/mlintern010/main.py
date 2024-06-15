4792682ddc8071df5cd94d16804737097b345eb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-15 15:05:45 +0000

    Add package mlintern010/0.1.5/mlintern010-0.1.5-py3-none-any.whl

---
+++ packages/mlintern010/mlintern010-0.1.5-py3-none-any.whl/mlintern010/main.py
@@ -0,0 +1,5 @@
+from termcolor import colored
+ 
+def inita():
+    text = colored("This package made by:\nR KIRAN KUMAR REDDY\n22CSEAIML010\nGIETU (GUNPUR)", 'red', attrs=['reverse', 'blink'])
+    print(text)
