9d05a344b6bbfd5a61bc3b795f81b2ed299ebd94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-15 05:04:39 +0000

    Add package meerschaum/2.2.2rc1/meerschaum-2.2.2rc1-py3-none-any.whl

---
+++ packages/meerschaum/meerschaum-2.2.2rc1-py3-none-any.whl/meerschaum/api/dash/callbacks/__init__.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+
+"""
+Define callbacks for pages.
+"""
+
+import meerschaum.api.dash.callbacks.dashboard
+import meerschaum.api.dash.callbacks.login
+import meerschaum.api.dash.callbacks.plugins
+import meerschaum.api.dash.callbacks.jobs
+import meerschaum.api.dash.callbacks.register
