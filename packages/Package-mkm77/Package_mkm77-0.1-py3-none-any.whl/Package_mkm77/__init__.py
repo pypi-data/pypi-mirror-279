8c52adf8717840a305ae94ebaf7497096862d3e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-15 06:03:39 +0000

    Add package Package-mkm77/0.1/Package_mkm77-0.1-py3-none-any.whl

---
+++ packages/Package-mkm77/Package_mkm77-0.1-py3-none-any.whl/Package_mkm77/__init__.py
@@ -0,0 +1,38 @@
+
+### `__init__.py`
+
+# Package_mkm77/__init__.py
+
+def add(a, b):
+    """Function to sum two numbers."""
+    print("Adding two numbers.")
+    return a + b
+
+def subtract(a, b):
+    """Function to subtract two numbers."""
+    print("Subtracting two numbers.")
+    return a - b
+
+def multiply(a, b):
+    """Function to multiply two numbers."""
+    print("Multiplying two numbers.")
+    return a * b
+
+def divide(a, b):
+    """Function to divide two numbers."""
+    print("Dividing two numbers.")
+    return a / b
+
+def remainder(a, b):
+    """Function to get the remainder."""
+    print("Calculating remainder of two numbers.")
+    return a % b  
+
+def exponent(a, b):
+    """Function to calculate a to the power of b."""
+    print("Calculating exponentiation of two numbers.")
+    return a ** b
+
+# Metadata
+__version__ = '0.1'
+__author__ = 'Mukesh'
