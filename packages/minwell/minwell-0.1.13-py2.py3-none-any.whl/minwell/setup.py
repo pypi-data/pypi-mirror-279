ad9cb498607c039741e8a0e389f4fc5937018b47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-15 15:05:25 +0000

    Add package minwell/0.1.13/minwell-0.1.13-py2.py3-none-any.whl

---
+++ packages/minwell/minwell-0.1.13-py2.py3-none-any.whl/minwell/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+  name='minwell_boltzmann_distribution',
+  version='1.0.0',
+  description='This is my first module',
+  python_requires='>=3.7'
+)
