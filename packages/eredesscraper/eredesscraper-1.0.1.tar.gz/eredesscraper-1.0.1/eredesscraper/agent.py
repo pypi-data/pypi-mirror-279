0215f8ffcf777da8174f4d01d0bdbd777f41b822
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-15 03:03:42 +0000

    Add package eredesscraper/1.0.1/eredesscraper-1.0.1.tar.gz

---
+++ packages/eredesscraper/eredesscraper-1.0.1.tar.gz/eredesscraper-1.0.1/eredesscraper/agent.py
@@ -0,0 +1,235 @@
+import datetime
+import re
+from pathlib import Path
+from random import randint
+from uuid import uuid4
+
+import typer
+from playwright.sync_api import sync_playwright
+from playwright_stealth import stealth_sync
+from rich.progress import Progress, SpinnerColumn, TextColumn
+
+from eredesscraper.meta import user_agent_list
+from eredesscraper.utils import get_screen_resolution, map_month_matrix_names, pw_nav_year_back
+
+ENTRYPOINT = "https://balcaodigital.e-redes.pt/consumptions/history"
+
+
+class ScraperFlowError(Exception):
+    pass
+
+
+class EredesScraper:
+    def __init__(self, nif, password, cpe_code, quiet: bool, headless: bool = True, uuid: uuid4 = uuid4()):
+        self.dwnl_file = None
+        self.session_id = uuid
+        self.browser = None
+        self.context = None
+        self.page = None
+        self.headless = headless
+        self.__nif = nif
+        self.__password = password
+        self.__cpe_code = cpe_code
+        self.__implicit_wait = 90
+        self.__quiet = quiet
+
+        # Create a staging area for the Playwright
+        try:
+            Path.mkdir(Path.cwd() / 'ers_tmp', exist_ok=True)
+        except PermissionError:
+            print("Permission denied to create a staging area for the Scraper")
+
+        self.tmp = Path(Path.cwd() / 'ers_tmp').__str__()
+
+    @property
+    def implicit_wait(self):
+        return self.__implicit_wait
+
+    @implicit_wait.setter
+    def implicit_wait(self, value):
+        if isinstance(value, int):
+            self.__implicit_wait = value
+        else:
+            raise TypeError("Expected an integer value")
+
+    @property
+    def nif(self):
+        return self.__nif
+
+    @nif.setter
+    def nif(self, value):
+        if isinstance(value, str):
+            self.__nif = value
+        else:
+            raise TypeError("Expected a string value")
+
+    @property
+    def password(self):
+        return self.__password
+
+    @password.setter
+    def password(self, value):
+        if isinstance(value, str):
+            self.__password = value
+        else:
+            raise TypeError("Expected a string value")
+
+    @property
+    def cpe_code(self):
+        return self.__cpe_code
+
+    @cpe_code.setter
+    def cpe_code(self, value):
+        if isinstance(value, str):
+            self.__cpe_code = value
+        else:
+            raise TypeError("Expected a string value")
+
+    def teardown(self):
+        self.context.close()
+        self.browser.close()
+
+    def readings(self, month: int, year: int):
+        with Progress(
+                SpinnerColumn(),
+                TextColumn("[progress.description]{task.description}"),
+                transient=True,
+                disable=self.__quiet
+        ) as progress:
+
+            date = datetime.datetime(year, month, 1)
+
+            current_date = datetime.datetime.now()
+
+            assert date <= datetime.datetime.now(), "Selected date is in the future"
+
+            t1 = progress.add_task(description=" 🔐 Logging in...", total=None)
+
+            self.page.goto(ENTRYPOINT)
+
+            self.page.get_by_text("Particular").click()
+
+            self.page.get_by_label("NIF").fill(f"{self.__nif}")
+
+            self.page.get_by_label("Password").fill(f"{self.__password}")
+
+            self.page.get_by_role("button", name="Entrar").click()
+            # self.page.get_by_label("Password").press("Enter")
+            # self.page.get_by_text("Entrar").click()
+
+            # try:
+            #     self.page.get_by_text("Leituras, contadores,").click()
+            #     # self.page.get_by_role("heading", name="Os meus locais").click()                
+            # except ScraperFlowError:
+            #     self.page.screenshot(path=f"{Path.cwd()}/ers_login_error.png")
+            #     with open(f"{Path.cwd()}/ers_login_error.html", "w") as f:
+            #         f.write(self.page.content())
+            #     if self.page.locator(has_text="Dados inválidos").is_visible():
+            #         raise ScraperFlowError("🔐 Invalid Credentials!\nCheck your NIF and "
+            #                                "Password with `ers config show`.\nA screenshot was saved in the current "
+            #                                "directory for debugging purposes.")
+
+            try:
+                captcha = self.page.locator("h1").filter(has_text="Validação de Segurança")
+
+                if captcha.is_visible():
+                    print("🔐 Captcha detected. Try again later")
+                    raise ScraperFlowError(
+                        "🔐 Captcha detected. A screenshot was saved in the current directory for debugging purposes"
+                        "\nPlease try again later.")
+            except ScraperFlowError:
+                self.page.screenshot(path=f"{Path.cwd()}/ers_captcha_error.png")
+                raise ScraperFlowError(
+                    "🔐 Captcha detected. A screenshot was saved in the current directory for debugging purposes"
+                    "\nPlease try again later.")
+
+            progress.remove_task(t1)
+            t2 = progress.add_task(description=" 💡 Finding your CPE...", total=None)
+
+            # self.page.get_by_text("Produção, consumos e potências").click()
+
+            # self.page.get_by_text("Consultar histórico").click()
+
+            # self.page.locator("p").filter(has_text=re.compile(self.__cpe_code)).click()
+
+            try:
+                self.page.locator("p").filter(has_text=re.compile(r"^" + self.__cpe_code + "$")).click(timeout=120000)
+            except ScraperFlowError:
+                self.page.screenshot(path=f"{Path.cwd()}/ers_cpe_error.png")
+                raise ScraperFlowError("💥 Failed to find the CPE code. A screenshot was "
+                                       "saved in the current directory for debugging purposes")
+
+            progress.remove_task(t2)
+            t3 = progress.add_task(description=" 📊 Downloading your data...", total=None)
+
+            if date.strftime("%Y-%m") != current_date.strftime("%Y-%m"):
+
+                month_str = map_month_matrix_names(date)
+
+                self.page.get_by_role("textbox", name="Select month").click()
+
+                if date.year != current_date.now().year:
+
+                    self.page.get_by_role("button", name=f"{current_date.year}").click()
+
+                    target_year = self.page.get_by_text(f"{date.year}", exact=True)
+
+                    if not target_year.is_visible():
+                        self.page = pw_nav_year_back(date=date, pw_page=self.page)
+
+                    target_year = self.page.get_by_text(f"{date.year}", exact=True)
+
+                    is_disabled = bool(target_year.get_attribute("aria-disabled"))
+
+                    if is_disabled:
+                        raise ScraperFlowError("There is no available data for the selected year")
+                    else:
+                        target_year.click()
+
+                if self.page.get_by_role("gridcell", name=f"{month_str}").is_disabled():
+                    self.page.screenshot(path=f"{Path.cwd()}/ers_month_error.png")
+                    raise ScraperFlowError("Selected month is not available. A screenshot was saved in the current "
+                                           "directory for debugging purposes")
+
+                self.page.get_by_role("gridcell", name=f"{month_str}").click()
+
+            try:
+                with self.page.expect_event("download") as download_info:
+
+                    # self.page.get_by_text("Exportar excel").click()
+                    self.page.locator("a").filter(has_text="Exportar excel").click()
+
+                    download = download_info.value
+
+                download.save_as(f"{self.tmp}/{year}_{month}_{self.session_id.__str__().split('-')[0]}_readings.xlsx")
+
+            except ScraperFlowError:
+                self.page.screenshot(path=f"{Path.cwd()}/ers_download_error.png")
+                raise ScraperFlowError("Failed to find the 'Exportar excel' element")
+
+            self.dwnl_file = Path(self.tmp) / f"{year}_{month}_{self.session_id.__str__().split('-')[0]}_readings.xlsx"
+
+            assert self.dwnl_file.exists(), "Failed to download the file"
+
+        progress.remove_task(t3)
+        if not self.__quiet:
+            typer.echo(f"📁\tDownloaded file: {self.dwnl_file}")
+
+        return self.dwnl_file
+
+    def run(self, month, year):
+        ua = user_agent_list[randint(0, len(user_agent_list) - 1)]
+        # get system screen resolution
+
+        with sync_playwright() as p:
+            self.browser = p.webkit.launch(headless=self.headless, downloads_path=self.tmp)
+            self.context = self.browser.new_context(
+                # user_agent=ua,
+                viewport={"width": get_screen_resolution()[0], "height": get_screen_resolution()[1]},
+                screen={"width": get_screen_resolution()[0], "height": get_screen_resolution()[1]}
+            )
+            self.context.set_default_timeout(self.__implicit_wait * 1000)
+            self.page = self.context.new_page()
+            stealth_sync(self.page)
+            self.readings(month, year)
+            self.teardown()
---
+++ packages/eredesscraper/eredesscraper-1.0.1.tar.gz/eredesscraper-1.0.1/eredesscraper/workflows.py
@@ -0,0 +1,167 @@
+# package imports
+import os
+from datetime import datetime
+from pathlib import Path
+from uuid import uuid4
+
+import typer
+
+from eredesscraper.agent import EredesScraper
+from eredesscraper.db_clients import InfluxDB
+from eredesscraper.utils import parse_config
+from eredesscraper.models import ERSSession
+
+user_config_path = Path().home() / ".ers"
+Path.mkdir(user_config_path, exist_ok=True)
+
+date = datetime.now()
+
+def switchboard(config_path: Path, name: str, db: None | list = None, month: int = date.month, year: int = date.year,
+                delta: bool = False, keep: bool = False, quiet: bool = False, output: Path = Path.home() / ".ers",
+                uuid: uuid4 = uuid4(), headless: bool = True) -> ERSSession:
+    """
+    The run function is the entry point.
+
+    :param name: str: Specify which workflow to run.
+    :type name: str
+    :param db: list: Specify the list of database connections to use.`
+    :type db: list
+    :param config_path: Path: Specify the path to the config file
+    :type config_path: pathlib.Path
+    :param month: int: Specify the month to load (1-12).
+    :type month: int
+    :param year: int: Specify the year to load (YYYY).
+    :type year: int
+    :param delta: bool: Specify if the data should be loaded as a delta. [Optional]
+    :type delta: bool
+    :param keep: bool: Specify if the source data file should be kept after loading. [Optional]
+    :type keep: bool
+    :param quiet: bool: Specify if the function should run in quiet mode. [Optional]
+    :type quiet: bool
+    :param output: Path: Specify the path to write the source data file. [Optional]
+    :type output: pathlib.Path
+    :param uuid: uuid4: Specify the UUID for the session. [Optional]
+    :type uuid: uuid4
+    :return: ERSSession: The result object of the workflow run.
+    :doc-author: Ricardo Filipe dos Santos
+    """
+
+    date = datetime.now()
+
+    output = Path(output) if output else Path.home() / ".ers"
+
+    if name not in ['current', 'previous', 'select']:
+        if not quiet:
+            typer.echo(f"??\tWorkflow {typer.style(name, fg=typer.colors.GREEN)} not supported")
+        raise typer.Exit(code=1)
+
+    if name == 'select' and (month is None or year is None):
+        if not quiet:
+            typer.echo(f"??\tSpecify both month and year for the {typer.style(name, fg=typer.colors.GREEN)} workflow")
+        raise typer.Exit(code=1)
+
+    config = parse_config(config_path=config_path)
+    if not quiet:
+        typer.echo(f"🚀\tRunning {typer.style(name, fg=typer.colors.GREEN)} workflow")
+
+        typer.echo(f"📇\tE-REDES client info: "
+                   f"NIF: {typer.style(config['eredes']['nif'], fg=typer.colors.GREEN, bold=True)}, "
+                   f"CPE: {typer.style(config['eredes']['cpe'], fg=typer.colors.GREEN, bold=True)}")
+
+    bot = EredesScraper(
+        nif=config['eredes']['nif'],
+        password=config['eredes']['pwd'],
+        cpe_code=config['eredes']['cpe'],
+        quiet=quiet,
+        headless=headless,
+        uuid=uuid
+    )
+
+    match name:
+        case 'current':
+            bot.run(month=date.month, year=date.year)
+
+        case 'previous':
+            bot.run(month=date.month - 1, year=date.year)
+
+        case 'select':
+            bot.run(month=month, year=year)
+
+        case _:
+            if not quiet:
+                typer.echo(f"??\tWorkflow {typer.style(name, fg=typer.colors.GREEN)} not supported")
+            raise typer.Exit(code=1)
+
+    for conn in db:
+        match conn:
+            case 'influxdb':
+                client = InfluxDB(
+                    token=config['influxdb']['token'],
+                    org=config['influxdb']['org'],
+                    host=config['influxdb']['host'],
+                    port=config['influxdb']['port'],
+                    bucket=config['influxdb']['bucket'],
+                    quiet=quiet)
+                client.connect()
+                client.load(source_data=bot.dwnl_file, cpe_code=config['eredes']['cpe'], delta=delta)
+                if not quiet:
+                    typer.echo(f"📈\tLoaded data from {bot.dwnl_file} into the InfluxDB database")
+            case None:
+                pass
+
+            case '':
+                pass
+
+            case _:
+                if not quiet:
+                    typer.echo(f"??\tDatabase {typer.style(db, fg=typer.colors.GREEN)} not supported")
+
+    if not keep:
+        try:
+            bot.dwnl_file.unlink()
+            Path.rmdir(bot.tmp)
+            if not quiet:
+                typer.echo(f"💀\tRemoved the source data file and the staging area: {bot.dwnl_file}")
+
+            result = ERSSession(
+                session_id=bot.session_id,
+                workflow=name,
+                databases=db,
+                source_data=None,
+                status="completed",
+                timestamp=datetime.now()
+            )
+
+            return result
+        except PermissionError:
+            if not quiet:
+                typer.echo("💥\tPermission denied to remove the source data and/or staging area for the ERS")
+
+            result = ERSSession(
+                session_id=bot.session_id,
+                workflow=name,
+                databases=db,
+                source_data=bot.dwnl_file,
+                status="completed",
+                timestamp=datetime.now()
+            )
+
+            return result
+    else:
+        out = Path(output / bot.session_id.__str__())
+        Path.mkdir(out, exist_ok=True, parents=True)
+        os.rename(bot.dwnl_file, out / bot.dwnl_file.name)
+        bot.dwnl_file = Path(out / bot.dwnl_file.name)
+        if not quiet:
+            typer.echo(f"📂\tSource data file written to: {bot.dwnl_file}")
+
+        result = ERSSession(
+            session_id=bot.session_id,
+            workflow=name,
+            databases=db,
+            source_data=bot.dwnl_file,
+            status="completed",
+            timestamp=datetime.now()
+        )
+
+        return result
