4ce30aa2d5cd6706b5cd22b868bf0c9fa18e4b67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-15 03:03:33 +0000

    Add package zerru/0.0.5/zerru-0.0.5.tar.gz

---
+++ packages/zerru/zerru-0.0.5.tar.gz/zerru-0.0.5/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='zerru',
+    version='0.0.5',
+    author='ZerProg studio',
+    install_requires=['tqdm'],
+    description='Zerru — это модуль zer на русском! Для сокращения и автоматизации кода.',
+    packages=find_packages()
+)
---
+++ packages/zerru/zerru-0.0.5.tar.gz/zerru-0.0.5/zerru/__init__.py
@@ -0,0 +1,14 @@
+"""
+Zerru — это модуль zer на русском! Для сокращения и автоматизации кода.
+
+Версия модуля: 0.0.5 alpha.
+Издатель: ZerProg studio.
+"""
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import os as oss
+import datetime
+import sys
+from tqdm import tqdm
+from .zerru import *
