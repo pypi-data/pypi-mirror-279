1521b2983f8199ddc5bbfeabf6b87da6e8d6751b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-15 04:03:58 +0000

    Add package modulos-client/0.6.3/modulos_client-0.6.3.tar.gz

---
+++ packages/modulos-client/modulos_client-0.6.3.tar.gz/modulos_client-0.6.3/modulos_client/testing/__init__.py
@@ -0,0 +1,14 @@
+from modulos_client.testing.testing_log import (
+    create_metric,
+    get_metric,
+    get_metrics,
+    log_metric,
+)
+
+
+__all__ = [
+    "create_metric",
+    "get_metric",
+    "get_metrics",
+    "log_metric",
+]
