b95d9abee409c27b131520259b06d4c0c848e73d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-15 06:03:38 +0000

    Add package NCapybaraLib/1.0.0/NCapybaraLib-1.0.0-py3-none-any.whl

---
+++ packages/NCapybaraLib/NCapybaraLib-1.0.0-py3-none-any.whl/NCapybaraLib/_internal/init.py
@@ -0,0 +1,42 @@
+"""
+NCapybaraLib; By NorbCodes.
+
+A small library with a bunch of functions and thingies I made for fun.
+
+https://pypi.org/project/NCapybaraLib/
+"""
+
+import os
+import sys
+from typing import Any, Callable
+
+def print_hello_world() -> None:
+    """
+    Hello world program straight from NCapybaraLib itself.
+    """
+    print("Hello world!")
+
+def inject_function(obj: Any, func_name: str, func: Callable[[Any], Any]) -> Any:
+    """
+    Does exactly what it sounds like. Injects *func* as a method for *obj* under the name *func_name*.
+
+    :param obj: The object instance name referring to the object to inject into.
+    :param func_name: The method name.
+    :param func: The function to inject.
+    :return: The *obj* that was passed in, now with the function injected in it.
+    """
+    scope = {"target": obj, "injection": func}
+    try:
+        exec(f"target.{func_name}=classmethod(injection).__get__(None, target)", scope)
+    except AttributeError as A:
+        raise AttributeError(f"Cannot inject function {func.__name__} as {func_name} into object of type {type(obj)}.") from A
+    return scope["target"]
+
+def clear_console() -> None:
+    """
+    Simple shrimple function to clear the console.
+    """
+    if sys.platform.casefold() == "win32":
+        os.system("cls")
+    else:
+        os.system("clear")
