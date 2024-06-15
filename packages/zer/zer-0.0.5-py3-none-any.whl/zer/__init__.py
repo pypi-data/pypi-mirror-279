02a806dc29384364d7403a913ed179855b6fdd8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-15 03:04:18 +0000

    Add package zer/0.0.5/zer-0.0.5-py3-none-any.whl

---
+++ packages/zer/zer-0.0.5-py3-none-any.whl/zer/__init__.py
@@ -0,0 +1,13 @@
+"""
+Zer is a Python module for code reduction and automation.
+
+Module version: 0.0.5 alpha.
+Publisher: ZerProg studio.
+"""
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import os as oss
+import datetime
+import sys
+from .zer import *
