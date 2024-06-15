b4cb7c1dc7ebe95b34fdcbb3676f08c60d1e5d6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-15 03:04:07 +0000

    Add package mltrainer/0.1.128/mltrainer-0.1.128.tar.gz

---
+++ packages/mltrainer/mltrainer-0.1.128.tar.gz/mltrainer-0.1.128/mltrainer/__init__.py
@@ -0,0 +1,6 @@
+from mltrainer.settings import TrainerSettings, ReportTypes
+from mltrainer.trainer import Trainer
+
+__all__ = ["Trainer", "TrainerSettings", "ReportTypes"]
+
+__version__ = "0.1.128"
