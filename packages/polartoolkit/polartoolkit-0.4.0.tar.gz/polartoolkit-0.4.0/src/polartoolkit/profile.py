f4c625f90c40474f44d6acf73d78cdb6d21b9361
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-15 03:03:39 +0000

    Add package polartoolkit/0.4.0/polartoolkit-0.4.0.tar.gz

---
+++ packages/polartoolkit/polartoolkit-0.4.0.tar.gz/polartoolkit-0.4.0/src/polartoolkit/profile.py
@@ -0,0 +1,7 @@
+import logging
+
+logging.critical(
+    "The PolarToolkit module `profile` has been renamed to `profiles` to match the "
+    "plural nature of the `maps` and `regions` modules. \n"
+    "Please change any imports with the new name."
+)
