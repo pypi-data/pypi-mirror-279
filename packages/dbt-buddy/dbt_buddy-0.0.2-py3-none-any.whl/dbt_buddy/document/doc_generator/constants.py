d6462a7f1f7cb9925a93acbfb9c63a2d18aed9b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-15 03:03:42 +0000

    Add package dbt-buddy/0.0.2/dbt_buddy-0.0.2-py3-none-any.whl

---
+++ packages/dbt-buddy/dbt_buddy-0.0.2-py3-none-any.whl/dbt_buddy/document/doc_generator/constants.py
@@ -0,0 +1,19 @@
+DOTENV_API_KEY_NAME: str = "API_KEY"
+DOTENV_CATALOG_ID_NAME: str = "CATALOG_ID"
+
+DBT_CODEGEN_MACRO: str = """
+{{{{ codegen.generate_model_yaml(model_names=['{model}'], include_data_types=False) }}}}
+"""
+DBT_CLI_ARGS: list = [
+    "--quiet",
+    "--no-print",
+    "compile",
+]
+
+GPT_BASE_URL: str = "https://llm.api.cloud.yandex.net/foundationModels/v1/completion"
+GPT_MODEL_URI: str = "gpt://{catalog_id}/yandexgpt"
+GPT_TEMPERATURE: float = 0.3
+GPT_MAX_TOKENS: str = "8000"
+GPT_ANSWER_TEMPLATE: str = """
+    [{"column_name": <название колонки>,"description": <описание колонки>,"possible_values": "<возможные значения>"},..]
+"""
