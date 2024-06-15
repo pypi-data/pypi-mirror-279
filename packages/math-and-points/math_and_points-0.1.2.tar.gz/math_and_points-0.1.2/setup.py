611992f7d4b2756b3622f799d079e5eaf135ee5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-15 05:04:39 +0000

    Add package math-and-points/0.1.2/math_and_points-0.1.2.tar.gz

---
+++ packages/math-and-points/math_and_points-0.1.2.tar.gz/math_and_points-0.1.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages, setup
+
+with open("README.md","r",encoding="utf-8") as fh:
+	long_description=fh.read()
+
+setup(
+name="math_and_points",
+version="0.1.2",
+packages=find_packages(),
+install_requires=[],
+author="Lucas Gongora",
+description="Operaciones basicas con dos o mas numeros, y por otra partes operaciones con puntos en dos dimensiones",
+long_description=long_description,
+long_description_content_type="text/markdown",
+url="",
+)
