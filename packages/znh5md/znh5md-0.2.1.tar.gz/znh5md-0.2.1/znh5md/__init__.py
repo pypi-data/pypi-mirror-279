133b8d6af6df4302371043db04ccf1813a443b26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-15 03:04:07 +0000

    Add package znh5md/0.2.1/znh5md-0.2.1.tar.gz

---
+++ packages/znh5md/znh5md-0.2.1.tar.gz/znh5md-0.2.1/znh5md/__init__.py
@@ -0,0 +1,18 @@
+"""ZnH5MD: High Performance Interface for H5MD Trajectories."""
+
+import contextlib
+import importlib.metadata
+
+from znh5md import io, utils
+from znh5md.format import FormatHandler
+from znh5md.znh5md import ASEH5MD
+
+__all__ = ["DaskH5MD", "ASEH5MD", "io", "FormatHandler", "utils"]
+
+with contextlib.suppress(ImportError):
+    from znh5md.znh5md.h5dask import DaskH5MD
+
+    __all__.append("DaskH5MD")
+
+
+__version__ = importlib.metadata.version("znh5md")
