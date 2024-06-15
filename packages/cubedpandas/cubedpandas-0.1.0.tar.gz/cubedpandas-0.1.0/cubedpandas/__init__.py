586b2d250c8d70c34eca3ba3c3e764bb38c711f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-15 05:04:54 +0000

    Add package cubedpandas/0.1.0/cubedpandas-0.1.0.tar.gz

---
+++ packages/cubedpandas/cubedpandas-0.1.0.tar.gz/cubedpandas-0.1.0/cubedpandas/__init__.py
@@ -0,0 +1,28 @@
+from cubedpandas.schema import Schema
+from cubedpandas.dimension import Dimension
+from cubedpandas.measure import Measure
+from cubedpandas.dimension_collection import DimensionCollection
+from cubedpandas.measure_collection import MeasureCollection
+from cubedpandas.caching_strategy import CachingStrategy
+from cubedpandas.cube import Cube
+from cubedpandas.cube_aggregation import CubeAggregationFunctionType, CubeAggregationFunction
+from cubedpandas.pandas_extension import cubed, CubedPandasAccessor, EAGER_CACHING_THRESHOLD
+
+VERSION = "0.1.0"
+
+__all__ = [
+    "Schema",
+    "Dimension",
+    "Measure",
+    "DimensionCollection",
+    "MeasureCollection",
+    "CachingStrategy",
+    "EAGER_CACHING_THRESHOLD",
+    "Cube",
+    "CubeAggregationFunctionType",
+    "CubeAggregationFunction",
+    "cubed",
+    "CubedPandasAccessor",
+    "VERSION"
+]
+
