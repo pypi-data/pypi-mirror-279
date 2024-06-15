fb1e2297cadac2996ae47dcbc0b4f14ee97f767e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-15 06:03:59 +0000

    Add package launchflow/0.4.0.dev10/launchflow-0.4.0.dev10.tar.gz

---
+++ packages/launchflow/launchflow-0.4.0.dev10.tar.gz/launchflow-0.4.0.dev10/launchflow/workflows/import_tofu_resource/import_tofu_resource.py
@@ -0,0 +1,58 @@
+import tempfile
+
+from launchflow.workflows.commands.tf_commands import TFApplyCommand
+from launchflow.workflows.import_tofu_resource.schemas import (
+    ImportResourceTofuInputs,
+    ImportResourceTofuOutputs,
+)
+
+
+async def import_tofu_resource(inputs: ImportResourceTofuInputs):
+    state_prefix = inputs.launchflow_uri.tf_state_prefix(
+        inputs.lock_id, module=inputs.resource.product.value
+    )
+
+    tf_vars = {}
+    if inputs.gcp_env_config:
+        tf_vars.update(
+            {
+                "gcp_project_id": inputs.gcp_env_config.project_id,
+                "gcp_region": inputs.gcp_env_config.default_region,
+                "resource_name": inputs.launchflow_uri.resource_name,
+                "artifact_bucket": inputs.gcp_env_config.artifact_bucket,
+                "environment_service_account_email": inputs.gcp_env_config.service_account_email,
+                **inputs.resource.inputs,
+            }
+        )
+    else:
+        tf_vars.update(
+            {
+                "aws_region": inputs.aws_env_config.region,
+                "resource_name": inputs.launchflow_uri.resource_name,
+                "artifact_bucket": inputs.aws_env_config.artifact_bucket,
+                "env_role_name": inputs.aws_env_config.iam_role_arn.split("/")[-1],
+                "vpc_id": inputs.aws_env_config.vpc_id,
+                "launchflow_environment": inputs.launchflow_uri.environment_name,
+                "launchflow_project": inputs.launchflow_uri.project_name,
+                **inputs.resource.inputs,
+            }
+        )
+
+    tf_apply_command = TFApplyCommand(
+        tf_module_dir=f"workflows/tf/resources/{inputs.resource.product.value}",
+        backend=inputs.backend,
+        tf_state_prefix=state_prefix,
+        tf_vars=tf_vars,
+        logging_dir=inputs.logs_dir,
+    )
+
+    with tempfile.TemporaryDirectory() as tempdir:
+        # First import everything that is needed for the resource
+        for key, value in inputs.imports.items():
+            await tf_apply_command.import_resource(tempdir, key, value)
+        # Next run the apply command to create any artifacts we need
+        output = await tf_apply_command.run(tempdir)
+
+    return ImportResourceTofuOutputs(
+        gcp_id=output.get("gcp_id"), aws_arn=output.get("aws_arn")
+    )
