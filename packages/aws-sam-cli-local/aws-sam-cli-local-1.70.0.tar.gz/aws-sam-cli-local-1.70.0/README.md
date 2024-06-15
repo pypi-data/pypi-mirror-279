54ff915728aae188523ac5dc64d041bb4acb270f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-15 05:04:20 +0000

    Add package aws-sam-cli-local/1.70.0/aws-sam-cli-local-1.70.0.tar.gz

---
+++ packages/aws-sam-cli-local/aws-sam-cli-local-1.70.0.tar.gz/aws-sam-cli-local-1.70.0/README.md
@@ -0,0 +1,40 @@
+# AWS SAM CLI for LocalStack
+
+This project provides the `samlocal` command line, a simple wrapper around the [AWS SAM CLI](https://github.com/aws/aws-sam-cli) for use with [LocalStack](https://github.com/localstack/localstack).
+
+## Installation
+
+Simply use `pip` to install `samlocal` as a Python library on your machine:
+
+```
+pip install aws-sam-cli-local
+```
+
+## Usage
+
+The `samlocal` command has the exact same usage as the underlying `sam` command. The main difference is that for commands like `samlocal deploy` the operations will be executed against the LocalStack endpoints (`http://localhost:4566` by default) instead of real AWS endpoints.
+
+```
+samlocal --help
+```
+
+## Configuration
+
+* `AWS_ENDPOINT_URL`: URL at which the `boto3` client can reach LocalStack, e.g. `http://localhost.localstack.cloud:4566` (default: `http://localhost:4566`)
+* `EDGE_PORT`: **Deprecated** Port number under which the LocalStack edge service is available (default: `4566`)
+* `LOCALSTACK_HOSTNAME`: **Deprecated** Host under which the LocalStack edge service is available (default: `localhost`)
+
+## Change Log
+
+* v1.70.0: Fix regex pattern to detect ECR URLs with dashes
+* v1.69.0: Fix repo related cli options and add support to Lambdas with `Image` type
+* v1.68.0: Support `AWS_ENDPOINT_URL` for configuring the `boto3.client` endpoint
+* v1.67.0: Patch underlying `boto3.Session.client` instead of `boto3.client`
+* v1.55.0: Fix s3 URl detection when using a nested template
+* v1.53.1: Fix logic around local URLs for ECR image repositories
+* v1.1.0.1: Minor fix, release initial version of `samlocal` command
+* v1.1.0: Initial version
+
+## License
+
+This software library is released under the Apache License, Version 2.0.
---
+++ packages/aws-sam-cli-local/aws-sam-cli-local-1.70.0.tar.gz/aws-sam-cli-local-1.70.0/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+import re
+from setuptools import find_packages, setup
+
+VERSION = '1.70.0'
+
+# parameter variables
+install_requires = []
+extras_requires = {
+    "dev": ["flake8"]
+}
+package_data = {}
+
+# determine requirements
+with open('requirements.txt') as f:
+    requirements = f.read()
+for line in re.split('\n', requirements):
+    if line and line[0] != '#':
+        lib_stripped = line.split(' #')[0].strip()
+        install_requires.append(lib_stripped)
+
+# get the long description from the README.md
+with open("README.md") as f:
+    long_description = f.read()
+
+
+if __name__ == '__main__':
+
+    setup(
+        name='aws-sam-cli-local',
+        version=VERSION,
+        description='Simple wrapper around AWS SAM CLI for use with LocalStack',
+        long_description=long_description,
+        author='LocalStack Team',
+        author_email='info@localstack.cloud',
+        url='https://github.com/localstack/aws-sam-cli-local',
+        scripts=['bin/samlocal', 'bin/samlocal.bat'],
+        packages=find_packages(exclude=('tests', 'tests.*')),
+        package_data=package_data,
+        install_requires=install_requires,
+        extras_require=extras_requires,
+        license='Apache License 2.0',
+        zip_safe=False,
+        classifiers=[
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.6',
+            'Programming Language :: Python :: 3.7',
+            'Programming Language :: Python :: 3.8',
+            'Programming Language :: Python :: 3.9',
+            'Programming Language :: Python :: 3.10',
+            'License :: OSI Approved :: Apache Software License',
+            'Topic :: Software Development :: Testing',
+        ]
+    )
