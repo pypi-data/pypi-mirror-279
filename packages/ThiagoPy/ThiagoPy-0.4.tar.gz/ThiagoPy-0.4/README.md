d2fab0adb0111039cadc4c072f97f4e962743d01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-15 05:04:19 +0000

    Add package ThiagoPy/0.4/ThiagoPy-0.4.tar.gz

---
+++ packages/ThiagoPy/ThiagoPy-0.4.tar.gz/ThiagoPy-0.4/README.md
@@ -0,0 +1,12 @@
+# MyPackage
+
+ThiagoPy é uma biblioteca Python que utiliza funções escritas em Julia para proporcionar desempenho superior.
+
+O intuito da biblioteca é usar funções para o cálculo de probabilidades e entropias associadas aos microestados de recorrência
+
+## Instalação
+
+Você pode instalar a biblioteca via `pip`:
+
+```sh
+pip install ThiagoPy
---
+++ packages/ThiagoPy/ThiagoPy-0.4.tar.gz/ThiagoPy-0.4/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ThiagoPy',
+    version='0.4',
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        'ThiagoPy': ['__init__.py','microstates.py', 'microstates.jl'],
+    },
+    install_requires=[
+        'julia',
+    ],
+    author='Jorge Vinicius Malosti da Silveira',
+    author_email='jorge.malosti@outlook.com',
+    description='Uma biblioteca Python que usa funções Julia',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    #url='https://github.com/seuusuario/mypackage',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
