d3a9000bfe7f8fefae252bdf6482d379f4fefa39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-15 05:04:33 +0000

    Add package py2ls/0.1.4.1/py2ls-0.1.4.1.tar.gz

---
+++ packages/py2ls/py2ls-0.1.4.1.tar.gz/py2ls-0.1.4.1/py2ls/__init__.py
@@ -0,0 +1,17 @@
+"""
+    __init__ of the pyos module
+"""
+
+from . import (
+    brain_atlas, 
+    correlators, 
+    dbhandler, 
+    freqanalysis, 
+    internet_finder, 
+    ips, 
+    netfinder, 
+    sleep_events_detectors, 
+    translator, 
+    version, 
+    wb_detector, 
+    )
