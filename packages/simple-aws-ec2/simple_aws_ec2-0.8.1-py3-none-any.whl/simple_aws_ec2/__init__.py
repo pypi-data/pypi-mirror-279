a6fbae0e27e2a4fa83b39acf9f3336876de27c3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-15 15:05:42 +0000

    Add package simple-aws-ec2/0.8.1/simple_aws_ec2-0.8.1-py3-none-any.whl

---
+++ packages/simple-aws-ec2/simple_aws_ec2-0.8.1-py3-none-any.whl/simple_aws_ec2/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from ._version import __version__
+
+__short_description__ = "Pythonic AWS EC2 boto3 API, for human."
+__license__ = "MIT"
+__author__ = "Sanhe Hu"
+__author_email__ = "husanhe@gmail.com"
+__github_username__ = "MacHu-GWU"
