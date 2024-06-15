183982647742bf873f4461147d80cdc1aae8bf0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-15 05:04:56 +0000

    Add package ThiagoPy/0.2/ThiagoPy-0.2.tar.gz

---
+++ packages/ThiagoPy/ThiagoPy-0.2.tar.gz/ThiagoPy-0.2/ThiagoPy/__init__.py
@@ -0,0 +1,11 @@
+from julia import Main
+
+# Inicializa PyJulia
+from julia import Julia
+Julia(compiled_modules=False)
+
+# Carrega o módulo Julia
+Main.include("microstates.jl")
+Microstates = Main.Microstates
+
+from .microstates import MicrostateS, MS2
