8e2166ad0b5cc30acfc2609119840883cca4337a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-15 05:04:38 +0000

    Add package wcosmo/0.2.1/wcosmo-0.2.1.tar.gz

---
+++ packages/wcosmo/wcosmo-0.2.1.tar.gz/wcosmo-0.2.1/doc/source/conf.py
@@ -0,0 +1,40 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+# import importlib.metadata
+
+project = "wcosmo"
+copyright = "2024, Colm Talbot, Amanda Farah"
+author = "Colm Talbot, Amanda Farah"
+# release = importlib.metadata.version("wcosmo").split("+")[0]
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.githubpages",
+    "sphinx.ext.autosummary",
+    "numpydoc",
+    "nbsphinx",
+    "sphinx.ext.viewcode",
+]
+
+templates_path = ["_templates"]
+exclude_patterns = []
+
+root_doc = "index"
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "pydata_sphinx_theme"
+
+autosummary_generate = True
+numpydoc_show_class_members = False
