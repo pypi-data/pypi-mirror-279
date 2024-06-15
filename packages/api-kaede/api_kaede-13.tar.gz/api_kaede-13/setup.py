2c46a6e7dddb441618f4e61a10e6f61be99ed28b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-15 03:04:02 +0000

    Add package api-kaede/13/api_kaede-13.tar.gz

---
+++ packages/api-kaede/api_kaede-13.tar.gz/api_kaede-13/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="api-kaede",
+    version="V13",
+    author="Kento Hinode",
+    author_email="cleaverdeath@gmail.com",
+    description="A Python package to interact with Kaede API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=[
+        "patch-ng",  # Install patch-ng first
+        "requests",
+        "pillow",
+        "paddlepaddle",
+        "paddleocr",
+        "yt-dlp", 
+        "lmdb"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
