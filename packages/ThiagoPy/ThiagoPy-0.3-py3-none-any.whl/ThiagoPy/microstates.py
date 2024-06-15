e9e2b71496978296bccc36c82b1ced62260ed1e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-15 05:04:18 +0000

    Add package ThiagoPy/0.3/ThiagoPy-0.3-py3-none-any.whl

---
+++ packages/ThiagoPy/ThiagoPy-0.3-py3-none-any.whl/ThiagoPy/microstates.py
@@ -0,0 +1,37 @@
+from julia import Main
+import julia
+
+julia.install()
+
+# Carrega o módulo Julia
+Main.include("microstates.jl")
+Microstates = Main.Microstates
+
+def MicrostateS(Serie, Threshold, Window_Size=None, StatsBlock=2, RP_percent=0.05):
+    '''
+    Function to calculate the entropy and the microstates probability
+    ------------------------------------------------------------------
+    Serie       : Time Series to be evaluated (can be multi-dimensional)
+    Threshold   : Recurrence threshold (epsilon)
+    Window_Size : Number of points in data to be evaluated (if not declared it will be initialized as len(Serie)
+    StatsBlock  : Size of the MicroState, matrix with size StatsBlock x StatsBlock
+    RP_percent  : Percent of samples in RP to be randomly selected
+    '''
+     
+    if Window_Size is None:
+        Window_Size = len(Serie)
+    return Microstates.MicrostateS(Serie, Threshold, Window_Size, StatsBlock, RP_percent)
+
+def MS2(Serie, Threshold, Window_Size=None, StatsBlock=2, RP_percent=0.05):
+    '''
+    Function to calculate the entropy and the microstates probability
+    ------------------------------------------------------------------
+    Serie       : Time Series to be evaluated (can be multi-dimensional)
+    Threshold   : Recurrence threshold (epsilon)
+    Window_Size : Number of points in data to be evaluated (if not declared it will be initialized as len(Serie)
+    StatsBlock  : Size of the MicroState, matrix with size StatsBlock x StatsBlock
+    RP_percent  : Percent of samples in RP to be randomly selected
+    '''
+    if Window_Size is None:
+        Window_Size = len(Serie)
+    return Microstates.MS2(Serie, Threshold, Window_Size, StatsBlock, RP_percent)
