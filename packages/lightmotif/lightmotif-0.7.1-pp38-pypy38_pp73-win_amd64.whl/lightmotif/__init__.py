2965c88cca5ec7550234df8c9716e82791faf8ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-15 04:04:31 +0000

    Add package lightmotif/0.7.1/lightmotif-0.7.1-pp38-pypy38_pp73-win_amd64.whl

---
+++ packages/lightmotif/lightmotif-0.7.1-pp38-pypy38_pp73-win_amd64.whl/lightmotif/__init__.py
@@ -0,0 +1,18 @@
+__version__ = "0.7.1"
+
+from . import lib
+from .lib import (
+    EncodedSequence,
+    StripedSequence,
+    CountMatrix,
+    WeightMatrix,
+    ScoringMatrix,
+    Motif,
+    StripedScores,
+    create,
+    stripe,
+)
+
+__author__ = lib.__author__
+__license__ = "MIT"
+__doc__ = lib.__doc__
