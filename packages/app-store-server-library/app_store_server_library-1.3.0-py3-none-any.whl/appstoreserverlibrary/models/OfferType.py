280157d266257981f825559c079fd4fab35a8221
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-15 04:03:44 +0000

    Add package app-store-server-library/1.3.0/app_store_server_library-1.3.0-py3-none-any.whl

---
+++ packages/app-store-server-library/app_store_server_library-1.3.0-py3-none-any.whl/appstoreserverlibrary/models/OfferType.py
@@ -0,0 +1,15 @@
+# Copyright (c) 2023 Apple Inc. Licensed under MIT License.
+
+from enum import IntEnum
+
+from .LibraryUtility import AppStoreServerLibraryEnumMeta
+
+class OfferType(IntEnum, metaclass=AppStoreServerLibraryEnumMeta):
+    """
+    The type of subscription offer.
+    
+    https://developer.apple.com/documentation/appstoreserverapi/offertype
+    """
+    INTRODUCTORY_OFFER = 1
+    PROMOTIONAL_OFFER = 2
+    SUBSCRIPTION_OFFER_CODE = 3
