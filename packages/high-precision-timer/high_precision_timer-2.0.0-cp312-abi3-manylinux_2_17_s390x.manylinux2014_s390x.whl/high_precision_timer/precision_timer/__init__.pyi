d4ebbeb5fb2912202ce9db2ad789d705747320dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-15 03:04:10 +0000

    Add package high-precision-timer/2.0.0/high_precision_timer-2.0.0-cp312-abi3-manylinux_2_17_s390x.manylinux2014_s390x.whl

---
+++ packages/high-precision-timer/high_precision_timer-2.0.0-cp312-abi3-manylinux_2_17_s390x.manylinux2014_s390x.whl/high_precision_timer/precision_timer/__init__.pyi
@@ -0,0 +1,4 @@
+from .timer_benchmark import benchmark as benchmark
+from .timer_class import PrecisionTimer as PrecisionTimer
+from .utilities import convert_time as convert_time
+from .utilities import get_timestamp as get_timestamp
