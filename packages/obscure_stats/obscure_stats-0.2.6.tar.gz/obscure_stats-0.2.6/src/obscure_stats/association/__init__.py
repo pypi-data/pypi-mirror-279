3c4d23b9e8130bc6fec229d2bb3a49c1566a8bd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-15 15:04:33 +0000

    Add package obscure_stats/0.2.6/obscure_stats-0.2.6.tar.gz

---
+++ packages/obscure_stats/obscure_stats-0.2.6.tar.gz/obscure_stats-0.2.6/src/obscure_stats/association/__init__.py
@@ -0,0 +1,31 @@
+"""Association module."""
+
+from .association import (
+    blomqvistbeta,
+    chatterjeexi,
+    concordance_corrcoef,
+    concordance_rate,
+    gaussain_rank_correlation,
+    quantile_correlation,
+    rank_minrelation_coefficient,
+    symmetric_chatterjeexi,
+    tanimoto_similarity,
+    tukey_correlation,
+    winsorized_correlation,
+    zhangi,
+)
+
+__all__ = [
+    "blomqvistbeta",
+    "chatterjeexi",
+    "concordance_corrcoef",
+    "concordance_rate",
+    "gaussain_rank_correlation",
+    "quantile_correlation",
+    "rank_minrelation_coefficient",
+    "symmetric_chatterjeexi",
+    "tanimoto_similarity",
+    "tukey_correlation",
+    "winsorized_correlation",
+    "zhangi",
+]
---
+++ packages/obscure_stats/obscure_stats-0.2.6.tar.gz/obscure_stats-0.2.6/src/obscure_stats/dispersion/__init__.py
@@ -0,0 +1,33 @@
+"""Dispersion module."""
+
+from .dispersion import (
+    coefficient_of_lvariation,
+    coefficient_of_range,
+    coefficient_of_variation,
+    cole_index_of_dispersion,
+    dispersion_ratio,
+    fisher_index_of_dispersion,
+    gini_mean_difference,
+    morisita_index_of_dispersion,
+    quartile_coefficient_of_dispersion,
+    robust_coefficient_of_variation,
+    shamos_estimator,
+    standard_quantile_absolute_deviation,
+    studentized_range,
+)
+
+__all__ = [
+    "coefficient_of_lvariation",
+    "coefficient_of_range",
+    "coefficient_of_variation",
+    "cole_index_of_dispersion",
+    "dispersion_ratio",
+    "fisher_index_of_dispersion",
+    "gini_mean_difference",
+    "morisita_index_of_dispersion",
+    "quartile_coefficient_of_dispersion",
+    "robust_coefficient_of_variation",
+    "shamos_estimator",
+    "standard_quantile_absolute_deviation",
+    "studentized_range",
+]
