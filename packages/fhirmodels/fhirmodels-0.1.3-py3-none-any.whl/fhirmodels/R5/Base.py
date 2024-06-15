21bf4e1e339e9095c10e0f05d8db8555bae333d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-15 05:04:48 +0000

    Add package fhirmodels/0.1.3/fhirmodels-0.1.3-py3-none-any.whl

---
+++ packages/fhirmodels/fhirmodels-0.1.3-py3-none-any.whl/fhirmodels/R5/Base.py
@@ -0,0 +1,29 @@
+"""
+Generated class for Base. 
+Time: 2024-06-14 18:56:04
+"""
+from fhirmodels.fhir_base_model import FhirBaseModel
+
+
+class Base(FhirBaseModel):
+    """ Base Type: Base definition for all types defined in FHIR type system.
+    """
+    # needed for complex properties where the element name is different from the class name
+    property_class_info = {
+        }
+    def __init__(self, ):
+        
+        self.resourceType = "Base"
+        
+        
+
+    @classmethod
+    def from_dict(cls, data: dict) -> "Base":
+        return super().from_dict(data)
+    
+    @classmethod
+    def from_obj(self, obj: object) -> "Base":
+        return super().from_obj(obj)
+
+    def as_dict(self) -> dict:
+        return super().as_dict()
