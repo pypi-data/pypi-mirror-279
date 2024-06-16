f92b0f5853e0ab57dc406443fdfaaf3d415623f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-16 04:03:48 +0000

    Add package pyspark-graph/0.0.5/pyspark_graph-0.0.5.tar.gz

---
+++ packages/pyspark-graph/pyspark_graph-0.0.5.tar.gz/pyspark_graph-0.0.5/src/pyspark_graph/algorithms/triangle_count.py
@@ -0,0 +1,9 @@
+from src.pyspark_graph.algorithms import Algorithm
+from src.pyspark_graph.graph import Graph
+from src.pyspark_graph.util import match_structure, order_edges
+
+
+class TriangleCount(Algorithm):
+    def run(self, g: Graph) -> int:
+        return match_structure(order_edges(g.edges),
+                               [("a", "b"), ("b", "c"), ("a", "c")]).count()
