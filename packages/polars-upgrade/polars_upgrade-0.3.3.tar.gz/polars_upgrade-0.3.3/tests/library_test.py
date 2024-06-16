aa93429b0b60b9cca1319c937bb8bc784d0117bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-16 14:04:00 +0000

    Add package polars-upgrade/0.3.3/polars_upgrade-0.3.3.tar.gz

---
+++ packages/polars-upgrade/polars_upgrade-0.3.3.tar.gz/polars_upgrade-0.3.3/tests/library_test.py
@@ -0,0 +1,61 @@
+from __future__ import annotations
+
+from polars_upgrade import Settings
+from polars_upgrade import rewrite
+
+
+def test_library() -> None:
+    src = """\
+import polars as pl
+df.select(pl.count())
+"""
+    settings = Settings(target_version=(0, 20, 4))
+    result = rewrite(src, settings=settings)
+    expected = """\
+import polars as pl
+df.select(pl.len())
+"""
+    assert result == expected
+
+
+def test_library_aliases() -> None:
+    src = """\
+df.select(pl.count())
+"""
+    settings = Settings(target_version=(0, 20, 4))
+    result = rewrite(src, settings=settings, aliases={"pl"})
+    expected = """\
+df.select(pl.len())
+"""
+    assert result == expected
+
+
+def test_library_aliases_polars() -> None:
+    src = """\
+df.select(polars.count())
+"""
+    settings = Settings(target_version=(0, 20, 4))
+    result = rewrite(src, settings=settings, aliases={"polars"})
+    expected = """\
+df.select(polars.len())
+"""
+    assert result == expected
+
+
+def test_library_no_aliases() -> None:
+    src = """\
+df.select(polars.count())
+"""
+    settings = Settings(target_version=(0, 20, 4))
+    result = rewrite(src, settings=settings)
+    assert result == result
+
+
+def test_library_pandas() -> None:
+    src = """\
+df = (pd.concat(my_list).groupby('ITEM_ID').apply(lambda x: np.any(x['VALUE'].notnull())).\
+reset_index().rename(columns={0: 'HAS_DATA'}))
+"""
+    settings = Settings(target_version=(0, 20, 10))
+    result = rewrite(src, settings=settings)
+    assert result == src
---
+++ packages/polars-upgrade/polars_upgrade-0.3.3.tar.gz/polars_upgrade-0.3.3/tests/string_functions_test.py
@@ -0,0 +1,34 @@
+from __future__ import annotations
+
+import pytest
+
+from polars_upgrade._data import Settings
+from polars_upgrade._main import fix_plugins
+
+
+@pytest.mark.parametrize(
+    ("s", "version"),
+    (
+        pytest.param(
+            "import polars as pl\n" 'pl.col("a").str.json_extract()\n',
+            (0, 17, 0),
+            id="too old",
+        ),
+    ),
+)
+def test_fix_capture_output_noop(s, version):
+    assert fix_plugins(s, settings=Settings(target_version=version)) == s
+
+
+@pytest.mark.parametrize(
+    ("s", "expected"),
+    (
+        pytest.param(
+            "import polars as pl\n" 'pl.col("a").str.json_extract()\n',
+            "import polars as pl\n" 'pl.col("a").str.json_decode()\n',
+        ),
+    ),
+)
+def test_fix_capture_output(s, expected):
+    ret = fix_plugins(s, settings=Settings(target_version=(0, 19, 18)))
+    assert ret == expected
