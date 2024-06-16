8d29ab1ff81e0f1aa5d9a927cd99df034c949047
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-16 14:04:08 +0000

    Add package kamangir/3.87.1/kamangir-3.87.1.tar.gz

---
+++ packages/kamangir/kamangir-3.87.1.tar.gz/kamangir-3.87.1/kamangir/content.py
@@ -0,0 +1,79 @@
+import abcli
+import articraft
+import blue_geo
+import blue_stability
+import hubblescope
+import ferfereh
+import gizai
+import notebooks_and_scripts
+import openai_commands
+import roofAI
+import vancouver_watching
+
+content = {
+    "cols": 4,
+    "items": {
+        "blue-geo": {
+            "module": blue_geo,
+        },
+        "notebooks-and-scripts": {
+            "module": notebooks_and_scripts,
+        },
+        "vancouver-watching": {
+            "module": vancouver_watching,
+        },
+        "giza": {
+            "module": gizai,
+        },
+        "roofAI": {
+            "module": roofAI,
+        },
+        "openai-commands": {
+            "module": openai_commands,
+        },
+        "hubble": {
+            "module": hubblescope,
+        },
+        "blue-stability": {
+            "module": blue_stability,
+        },
+        "aiart": {
+            "module": articraft,
+        },
+        "awesome-bash-cli": {
+            "module": abcli,
+        },
+        "ferfereh": {
+            "module": ferfereh,
+        },
+        "Kanata": {
+            "image": "https://kamangir-public.s3.ca-central-1.amazonaws.com/Canadians_v11.gif",
+            "legacy": True,
+            "description": "a multi-screen video feed that is comprised of a matrix of animated faces that slide to the right.",
+        },
+        "dec82": {
+            "image": "https://github.com/kamangir/blue-bracket/raw/main/images/dec82-6.jpg",
+            "legacy": True,
+            "description": "A wearable Raspberry Pi + Grove / Qwiic + Camera.",
+        },
+        "blue-rvr": {
+            "image": "https://github.com/kamangir/blue-rvr/raw/master/abcli/assets/marquee.jpeg",
+            "legacy": True,
+            "description": "a bash cli for Sphero RVR SDK - runs deep learning vision models on a Raspberry Pi using Python and TensorFlow.",
+        },
+        "blue-bracket": {
+            "image": "https://github.com/kamangir/blue-bracket/raw/main/images/marquee.jpg",
+            "legacy": True,
+            "description": "a parametric 3d-printed bracket to build hardware for machine vision & ai on raspberry pi and jetson nano on the edge.",
+        },
+        "blue-sbc": {
+            "image": "https://github.com/kamangir/blue-bracket/raw/main/images/blue3-1.jpg",
+            "legacy": True,
+            "description": "python + bash bootstrap for edge computing on single board computers.",
+        },
+        "template": {
+            "image": "TBA",
+            "module": abcli,
+        },
+    },
+}
