cef40f21e95d305f6234c8193fc0b29649804715
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-16 04:03:52 +0000

    Add package ezrules/0.5.0/ezrules-0.5.0.tar.gz

---
+++ packages/ezrules/ezrules-0.5.0.tar.gz/ezrules-0.5.0/ezrules/backend/ezrulevalapp.py
@@ -0,0 +1,21 @@
+from flask import Flask, request
+from ezrules.backend.rule_executors.executors import LocalRuleExecutorSQL
+from ezrules.models.database import db_session
+import os
+
+app = Flask(__name__)
+# TODO calling this needs to be parametrised, e.g. for a remote service
+o_id = int(os.getenv("O_ID", "1"))
+lre = LocalRuleExecutorSQL(db=db_session, o_id=o_id)
+
+
+@app.route("/evaluate", methods=["POST"])
+def evaluate():
+    request_data = request.get_json()
+    response = lre.evaluate_rules(request_data)
+    return response
+
+
+@app.route("/ping", methods=["GET"])
+def ping():
+    return f"OK"
