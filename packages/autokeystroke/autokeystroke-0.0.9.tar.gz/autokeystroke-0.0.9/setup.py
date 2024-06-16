a52274b2e160803e109ff02bc488e8e9476d1195
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-16 14:03:46 +0000

    Add package autokeystroke/0.0.9/autokeystroke-0.0.9.tar.gz

---
+++ packages/autokeystroke/autokeystroke-0.0.9.tar.gz/autokeystroke-0.0.9/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='autokeystroke',
+    version='0.0.9',
+    packages=find_packages(),
+    install_requires=[
+        # Add dependencies here
+    ],
+)
