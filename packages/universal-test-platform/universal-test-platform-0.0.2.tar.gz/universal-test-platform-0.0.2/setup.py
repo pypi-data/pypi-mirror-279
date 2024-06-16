28dcf88b55132c7b74c981b653c68f9c9654026e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-16 03:04:24 +0000

    Add package universal-test-platform/0.0.2/universal-test-platform-0.0.2.tar.gz

---
+++ packages/universal-test-platform/universal-test-platform-0.0.2.tar.gz/universal-test-platform-0.0.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='universal-test-platform',
+    version='0.0.2',
+    py_modules=['utp'],
+    install_requires=['Click', 'pyyaml' ],
+    entry_points={
+        'console_scripts': [
+            'utp = utp:cli'
+        ]
+    }
+)
