82baa9ee058710e02154037797b831007e5b6dd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-16 04:03:33 +0000

    Add package drepr-v2/1.3.13/drepr_v2-1.3.13-py3-none-any.whl

---
+++ packages/drepr-v2/drepr_v2-1.3.13-py3-none-any.whl/drepr/models/prelude.py
@@ -0,0 +1,12 @@
+from .align import *
+from .attr import *
+from .drepr import *
+from .drepr_builder import *
+from .format import OutputFormat
+from .parse_v1.resource_parser import ResourceParser
+from .path import *
+from .preprocessing import *
+from .resource import *
+from .sm import *
+
+DEFAULT_RESOURCE_ID = ResourceParser.DEFAULT_RESOURCE_ID
