87eb22cef33ac596c3e829dcac1ba2f721c4b4d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-16 04:04:00 +0000

    Add package mtko/0.1/mtko-0.1.tar.gz

---
+++ packages/mtko/mtko-0.1.tar.gz/mtko-0.1/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+from setuptools.command.install import install
+import subprocess
+
+class PostInstallCommand(install):
+    """Post-installation for installation mode."""
+    def run(self):
+        install.run(self)
+        # Run the post install script
+        subprocess.call(['python', 'post_install.py'])
+
+setup(
+    name='mtko',
+    version='0.1',
+    description='A CLI tool for sending commands, error messages, tracebacks, and file contents directly to OpenAI GPT models without leaving your terminal.',
+    author='Van',
+    packages=find_packages(),
+    install_requires=[
+        'openai',
+        'python-dotenv'
+    ],
+    cmdclass={
+        'install': PostInstallCommand,
+    },
+    entry_points={
+        'console_scripts': [
+            'mtko=mtko.main:main',
+        ],
+    },
+)
