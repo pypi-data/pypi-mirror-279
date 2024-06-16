79781f7807afd5d9c009c8ff2a78288e83e289ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-16 03:04:10 +0000

    Add package audio2vec/0.5.9/audio2vec-0.5.9.tar.gz

---
+++ packages/audio2vec/audio2vec-0.5.9.tar.gz/audio2vec-0.5.9/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+import tetx
+
+
+setup(
+    name='audio2vec',
+    version='0.5.9',
+    packages=find_packages(),
+    url='https://github.com/dallo7/audio2Vec.git',
+    license='License: GPL version 3, excluding DRM provisions',
+    author='Dalmas Chituyi',
+    author_email='cwakhusama@gmail.com',
+    description='An Audio2Vec annotation library',
+    long_description=tetx.longtext,
+    long_description_content_type='text/markdown',
+    install_requires=["setuptools==68.2.2", "pandas==2.1.1", "scikit-learn==1.5.0"],
+)
+
