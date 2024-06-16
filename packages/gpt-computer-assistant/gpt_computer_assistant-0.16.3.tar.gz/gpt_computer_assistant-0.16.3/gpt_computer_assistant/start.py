0e97ad3cfca574ebab473e36a7e655ed998f198e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-16 04:03:34 +0000

    Add package gpt-computer-assistant/0.16.3/gpt_computer_assistant-0.16.3.tar.gz

---
+++ packages/gpt-computer-assistant/gpt_computer_assistant-0.16.3.tar.gz/gpt_computer_assistant-0.16.3/gpt_computer_assistant/start.py
@@ -0,0 +1,46 @@
+import os
+
+def start():
+    """
+    Starts the computer assistant application.
+
+    This function starts the computer assistant application, which includes parsing command-line arguments
+    to set the profile, initializing the graphical user interface, and starting the application event loop.
+
+    Command-line Arguments:
+    --profile (str): The profile to use for the application.
+
+    Raises:
+    ImportError: If the required modules or packages are not found.
+
+    Returns:
+    None
+    """
+
+    try:
+        import crewai
+    except:
+        pass
+
+    # get --profile argument with library
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--profile", help="profile to use")
+    args = parser.parse_args()
+    profile = args.profile
+    print("Profile:", profile)
+
+    if profile is not None:
+        from .utils.db import set_profile
+        set_profile(profile)
+
+    try:
+        from .gpt_computer_assistant import QApplication, MainWindow, sys
+    except ImportError:
+        from gpt_computer_assistant import QApplication, MainWindow, sys
+    os.environ["QT_AUTO_SCREEN_SCALE_FACTOR"] = "1"
+
+    app = QApplication(sys.argv)
+    ex = MainWindow()
+    sys.exit(app.exec_())
