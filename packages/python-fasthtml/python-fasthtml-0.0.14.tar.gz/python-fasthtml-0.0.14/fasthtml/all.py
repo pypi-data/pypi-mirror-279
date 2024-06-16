565eb10d317e0cd933d55e42a5f870eb87ed0ea7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-16 04:03:47 +0000

    Add package python-fasthtml/0.0.14/python-fasthtml-0.0.14.tar.gz

---
+++ packages/python-fasthtml/python-fasthtml-0.0.14.tar.gz/python-fasthtml-0.0.14/fasthtml/all.py
@@ -0,0 +1,10 @@
+import uvicorn
+
+from .starlette import *
+from fastcore.utils import *
+from fastcore.xml import *
+from sqlite_minutils import Database
+from fastlite import *
+from . import *
+from .js import *
+
