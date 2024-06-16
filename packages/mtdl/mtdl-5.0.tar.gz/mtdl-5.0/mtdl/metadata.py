cdd77f4d325043ad52d746cbc050628325d1ecdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-16 03:04:04 +0000

    Add package mtdl/5.0/mtdl-5.0.tar.gz

---
+++ packages/mtdl/mtdl-5.0.tar.gz/mtdl-5.0/mtdl/metadata.py
@@ -0,0 +1,14 @@
+import sys
+import platform
+
+__version__ = "5.0"
+__author__ = "Bill Loic"
+__author_email__ = "billloic6@gmail.com"
+__release_time__ = "2024/06/09T08:12:00"
+__operating_system__ = platform.platform()
+python_ver = sys.version
+
+def format_metadata():
+    return "\n".join([f"Multi-Thread downloader version {__version__} by {__author__}, release {__release_time__}", 
+    f"Python Ver. {python_ver}", 
+    f"OS Ver. {__operating_system__}"])
