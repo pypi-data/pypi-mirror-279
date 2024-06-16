815cf23e7723d9b09d9b068a9b0e9230b50ff287
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-16 14:03:33 +0000

    Add package notebooks-and-scripts/4.551.1/notebooks_and_scripts-4.551.1.tar.gz

---
+++ packages/notebooks-and-scripts/notebooks_and_scripts-4.551.1.tar.gz/notebooks_and_scripts-4.551.1/notebooks_and_scripts/.abcli/aka.sh
@@ -0,0 +1,17 @@
+#! /usr/bin/env bash
+
+function nbs() {
+    notebooks_and_scripts "$@"
+}
+
+function notebooks() {
+    abcli_notebooks "$@"
+}
+
+function jupyter-notebook() {
+    abcli_notebooks "$@"
+}
+
+function ukraine() {
+    ukraine_timemap "$@"
+}
