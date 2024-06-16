d7f158a601ff5fee64bd4a525e89722d24f4247b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-16 04:04:27 +0000

    Add package universal-test-platform/0.0.8/universal_test_platform-0.0.8.tar.gz

---
+++ packages/universal-test-platform/universal_test_platform-0.0.8.tar.gz/universal_test_platform-0.0.8/readme.md
@@ -0,0 +1,20 @@
+## run
+
+- `$ conda activate utp `
+- `$ conda run -n utp python utp.py`
+- `$ pip install pyyaml click`
+
+## install locally
+
+- `$ pip install --editable .`
+
+## upload
+
+requirements
+
+- `$ pip install twine`
+- `$ chmod + ./upload.sh`
+
+upload
+
+- `$ ./upload.sh`
---
+++ packages/universal-test-platform/universal_test_platform-0.0.8.tar.gz/universal_test_platform-0.0.8/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='universal-test-platform',
+    version='0.0.8',
+
+    package_dir  = {'': 'src'},
+    package_data = {'utp': ['*']},
+    packages     = find_packages('src'),
+    install_requires=['Click', 'pyyaml' ],
+    entry_points={
+        'console_scripts': [
+            'utp = utp:cli'
+        ]
+    }
+)
