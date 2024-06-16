c4926bb6d547477d6b38dced6ce13682cea1e916
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-16 03:05:29 +0000

    Add package plotvizard/0.1.1/plotvizard-0.1.1.tar.gz

---
+++ packages/plotvizard/plotvizard-0.1.1.tar.gz/plotvizard-0.1.1/README.md
@@ -0,0 +1,16 @@
+# PlotVizard
+
+PlotVizard is a powerful Python library for data visualization. It simplifies the process of creating interactive plots and visualizations, making it easy for data scientists and analysts to explore and present their data.
+
+## Features
+
+- Easy-to-use data processing functions
+- Interactive scatter plots and other visualizations
+- Integration with Dash for web-based interactivity
+
+## Installation
+
+You can install PlotVizard from PyPI using pip:
+
+```bash
+pip install plotvizard==0.1.0
---
+++ packages/plotvizard/plotvizard-0.1.1.tar.gz/plotvizard-0.1.1/setup.py
@@ -0,0 +1,31 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name='plotvizard',
+    version='0.1.1',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'pandas',
+        'matplotlib',
+        'dash',
+        'plotly',
+    ],
+    entry_points='''
+        [console_scripts]
+        plotvizard=plotvizard:main
+    ''',
+    author='Chanaka Prasanna',
+    author_email='chanakapinfo@gmail.com',
+    description='A data visualization library with interractive functionalities',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/Chanaka-Prasanna/plotvizard',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
