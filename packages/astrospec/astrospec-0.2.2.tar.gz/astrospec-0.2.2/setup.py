747cdb56b7d9838b0e8d9505d1c0d1147c0c2298
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-16 15:04:14 +0000

    Add package astrospec/0.2.2/astrospec-0.2.2.tar.gz

---
+++ packages/astrospec/astrospec-0.2.2.tar.gz/astrospec-0.2.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="astrospec",
+    version="0.2.2",
+    author="liangchen",
+    author_email="lcpcsky@gmail.com",
+    description="astronomy spectroheliograph data processing tools",
+    url="https://github.com/liangchen-harold/astrospec",
+    packages=find_packages(),
+    install_requires=[
+        "einops~=0.8.0",
+        "lsq_ellipse~=2.2.1",
+        "numpy~=1.23.4",
+        "opencv_python~=4.6.0.66",
+        "tqdm~=4.63.0"
+    ],
+    entry_points = {
+        'console_scripts': [
+            'ascli = astrospec.cli:main',                  
+        ],              
+    },
+)
+
