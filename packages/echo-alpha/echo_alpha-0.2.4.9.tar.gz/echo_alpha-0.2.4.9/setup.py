6514018b13804b500a9f189be21742794ea3b70d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-16 03:04:05 +0000

    Add package echo-alpha/0.2.4.9/echo_alpha-0.2.4.9.tar.gz

---
+++ packages/echo-alpha/echo_alpha-0.2.4.9.tar.gz/echo_alpha-0.2.4.9/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+# Replace with your library name and version
+LIBRARY_NAME = 'echo_alpha'
+VERSION = '0.2.4.9'
+
+setup(
+  name=LIBRARY_NAME,
+  version=VERSION,
+  package_dir = {"": "src"},
+  packages=find_packages(where='src',
+                         exclude=['tests*']),  # Exclude test directories
+  author='Niro',
+  author_email='niro@echo.com',
+  description='ECHO AI client distributed as a python package',
+  install_requires=['requests','python-dotenv'],
+)
---
+++ packages/echo-alpha/echo_alpha-0.2.4.9.tar.gz/echo_alpha-0.2.4.9/src/echo_alpha/echo_alpha.py
@@ -0,0 +1,60 @@
+import os
+from dotenv import load_dotenv
+import requests
+
+# Load environment variables from .env file
+load_dotenv()
+
+SERVICE_URL = os.getenv("SERVICE_URL")
+
+
+class EchoClient:
+  """
+  A client class for interacting with the Echo server.
+  """
+  def __init__(self, 
+               echo_ai_key, 
+               org_id, 
+               server_url = "https://echov1.onrender.com",
+               canary_prob = 0.1):
+    self.server_url = server_url
+    self.echo_ai_key = echo_ai_key
+    self.org_id = org_id
+    self.canary_prob = canary_prob
+
+  def chat(self, text_prompt, **kwargs):
+    data = {
+       "message": text_prompt, 
+       'echo_ai_key':self.echo_ai_key,
+       'org_id':self.org_id,
+       'canary_prob':self.canary_prob,
+       }
+    
+    for k in kwargs:
+       data[k] = k
+    
+    response = requests.post(self.server_url, json=data)
+
+    # Check response status code
+    if response.status_code == 200:
+    # Successful response
+        response_data = response.json()
+        echoed_message = response_data.get("message")
+        # print(f"ECHO.AI: {echoed_message}")
+    else:
+        # Error response
+        echoed_message = f"Error: {response.status_code} - {response.text}"
+        # print(echoed_message)
+        
+
+    return echoed_message
+  
+
+  def close(self):
+    """
+    Closes the socket connection.
+    """
+    self.sock.close()
+
+# Export the EchoClient class
+__all__ = ['EchoClient']
