13e74f1810bb89a81a1ad1c5a7286ce92fbdde0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-16 03:04:14 +0000

    Add package aa-buybackprogram/2.0.1/aa_buybackprogram-2.0.1-py3-none-any.whl

---
+++ packages/aa-buybackprogram/aa_buybackprogram-2.0.1-py3-none-any.whl/buybackprogram/constants.py
@@ -0,0 +1,52 @@
+# Includes all asteroids, moon ore and ice. All items that can be compressed or refined for minerals*
+ORE_EVE_GROUPS = [
+    450,
+    451,
+    452,
+    453,
+    454,
+    455,
+    456,
+    457,
+    458,
+    459,
+    460,
+    461,
+    462,
+    465,
+    467,
+    468,
+    469,
+    1884,
+    1911,
+    1920,
+    1921,
+    1922,
+    1923,
+    2024,
+    2029,
+    4029,
+    4030,
+    4031,
+    4094,
+    4513,
+    4514,
+    4515,
+    4516,
+]
+
+OPE_EVE_GROUPS = [493]
+
+BONDS_EVE_GROUPS = [1248]
+
+BLUE_LOOT_TYPE_IDS = [
+    30744,
+    30745,
+    30746,
+    30747,
+]
+
+RED_LOOT_TYPE_IDS = [
+    48121,
+    60459,
+]
