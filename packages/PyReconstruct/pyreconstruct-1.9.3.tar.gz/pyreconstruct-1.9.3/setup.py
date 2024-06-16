8c7f61fa32abb37752c5fa21161abb8bfa9f4197
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-16 15:04:19 +0000

    Add package PyReconstruct/1.9.3/pyreconstruct-1.9.3.tar.gz

---
+++ packages/PyReconstruct/pyreconstruct-1.9.3.tar.gz/pyreconstruct-1.9.3/setup.py
@@ -0,0 +1,52 @@
+import pathlib
+from setuptools import setup, find_packages
+
+ROOT = pathlib.Path(__file__).parent
+
+VERSION = '1.9.3'
+PACKAGE_NAME = 'PyReconstruct'
+AUTHOR = 'Julian Falco & Michael Chirillo'
+AUTHOR_EMAIL = 'julian.falco@utexas.edu'
+URL = 'https://github.com/SynapseWeb/PyReconstruct'
+LICENSE = 'GNU GLPv3'
+DESCRIPTION = 'RECONSTRUCT in Python'
+
+LONG_DESCRIPTION = (ROOT / "readme.md").read_text()
+LONG_DESC_TYPE = "text/markdown"
+
+INSTALL_REQUIRES = [
+      'PySide6==6.5.2',
+      'opencv-python==4.7.0.68',
+      'numpy',
+      'scikit-image',
+      'trimesh==3.18.1',
+      'vedo==2023.4.7',
+      'zarr',
+      'lxml',
+      'gitpython',
+      'qdarkstyle'
+]
+
+
+SHELL_SCRIPTS = []  # any shell scripts to install
+
+setup(
+    name=PACKAGE_NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type=LONG_DESC_TYPE,
+    author=AUTHOR,
+    license=LICENSE,
+    author_email=AUTHOR_EMAIL,
+    url=URL,
+    install_requires=INSTALL_REQUIRES,
+    packages=find_packages(),
+    package_data={'PyReconstruct': ['assets/**/*', 'assets/welcome_series/.welcome/*']},
+    scripts=SHELL_SCRIPTS,
+    entry_points={
+        'console_scripts': [
+            'PyReconstruct=PyReconstruct.cli:main'
+        ]
+    }
+)
