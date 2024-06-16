0336a626ab0a90ab53bbbb097dfd66a408fd150f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-16 14:03:40 +0000

    Add package openai-commands/3.24.1/openai_commands-3.24.1-py3-none-any.whl

---
+++ packages/openai-commands/openai_commands-3.24.1-py3-none-any.whl/openai_commands/__init__.py
@@ -0,0 +1,9 @@
+NAME = "openai_commands"
+
+ICON = "🛠️"
+
+DESCRIPTION = f"{ICON} a command interface to the OpenAI API."
+
+VERSION = "3.24.1"
+
+REPO_NAME = "openai-commands"
---
+++ packages/openai-commands/openai_commands-3.24.1-py3-none-any.whl/openai_commands/__main__.py
@@ -0,0 +1,60 @@
+import argparse
+from openai_commands import NAME, VERSION, DESCRIPTION
+from openai_commands.logger import logger
+from blueness.argparse.generic import sys_exit
+
+parser = argparse.ArgumentParser(NAME, description=f"{NAME}-{VERSION}")
+parser.add_argument(
+    "task",
+    type=str,
+    help="complete|version",
+)
+parser.add_argument(
+    "--prompt",
+    type=str,
+)
+parser.add_argument(
+    "--max_token",
+    type=int,
+    default=2000,
+)
+parser.add_argument(
+    "--show_description",
+    type=bool,
+    default=0,
+    help="0|1",
+)
+parser.add_argument(
+    "--verbose",
+    type=int,
+    help="0|1",
+    default=0,
+)
+args = parser.parse_args()
+
+success = False
+if args.task == "complete":
+    from openai_commands.completion.api import complete_prompt
+
+    success, text, _ = complete_prompt(
+        args.prompt,
+        args.max_token,
+        args.verbose == 1,
+    )
+    print(text)
+elif args.task == "version":
+    import openai
+
+    print(
+        "{}-{}-{}{}".format(
+            NAME,
+            VERSION,
+            openai.version.VERSION,
+            "\\n{}".format(DESCRIPTION) if args.show_description else "",
+        )
+    )
+    success = True
+else:
+    success = None
+
+sys_exit(logger, NAME, args.task, success)
