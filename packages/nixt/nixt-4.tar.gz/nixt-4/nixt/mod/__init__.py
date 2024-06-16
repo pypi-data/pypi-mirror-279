0ecefb98b3ae91663009478949bcf19dea17a6ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-16 14:03:53 +0000

    Add package nixt/4/nixt-4.tar.gz

---
+++ packages/nixt/nixt-4.tar.gz/nixt-4/nixt/mod/__init__.py
@@ -0,0 +1,21 @@
+# This file is placed in the Public Domain.
+
+
+"modules"
+
+
+from . import cmd, err, fnd, log, ini, mod, tdo, thr, ver
+
+
+def __dir__():
+    return (
+        'cmd',
+        'err',
+        'fnd',
+        'log',
+        'ini',
+        'mod',
+        'tdo',
+        'thr',
+        'ver'
+    )
