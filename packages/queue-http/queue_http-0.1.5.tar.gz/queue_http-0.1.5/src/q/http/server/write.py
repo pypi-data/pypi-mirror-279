4c5de52e0cdac54f0bde8839f42e3ce8f40ac531
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-16 15:04:17 +0000

    Add package queue-http/0.1.5/queue_http-0.1.5.tar.gz

---
+++ packages/queue-http/queue_http-0.1.5.tar.gz/queue_http-0.1.5/src/q/http/server/write.py
@@ -0,0 +1,49 @@
+from typing import TypeVar, Callable, overload
+from q.api import WriteQueue, QueueError
+from fastapi import FastAPI, Request
+from haskellian import either as E, Either, Right, Left
+
+T = TypeVar('T')
+
+@overload
+def write_api(
+  queue: WriteQueue[T], *,
+  parse: Callable[[bytes], Either[QueueError, T]] = Right
+) -> FastAPI:
+  ...
+@overload
+def write_api(
+  queue: WriteQueue[T], *, Type: type[T]
+) -> FastAPI:
+  ...
+def write_api(queue, *, parse = None, Type = None): # type: ignore
+  if Type is not None:
+    from pydantic import TypeAdapter
+    Adapter = TypeAdapter(Type)
+    def parse(x):
+      try:
+        return Right(Adapter.validate_json(x))
+      except Exception as e:
+        return Left(QueueError(str(e)))
+  elif parse is None:
+    raise ValueError('Either `parse` or `Type` must be provided')
+  
+  return _write_api(queue, parse=parse)
+  
+def _write_api(
+  queue: WriteQueue[T], *,
+  parse: Callable[[bytes], Either[QueueError, T]] = Right
+) -> FastAPI:
+
+  from .util import with_status
+  app = FastAPI(generate_unique_id_function=lambda route: route.name)
+
+  @app.post('/push')
+  @with_status
+  @E.do[QueueError]()
+  async def push(key: str, req: Request):
+    body = await req.body()
+    val = parse(body).unsafe()
+    return (await queue.push(key, val)).unsafe()
+
+  return app
