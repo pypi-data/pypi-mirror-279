831534a5f695451c7fc94276e09ad71f63414108
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-16 04:03:38 +0000

    Add package mlintern010/0.5/mlintern010-0.5.tar.gz

---
+++ packages/mlintern010/mlintern010-0.5.tar.gz/mlintern010-0.5/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mlintern010',
+    version='0.5',
+    packages=find_packages(),
+    author='R Kiran Kumar Reddy',
+    author_email='rkirankumarreddy599@gmail.com',
+    description='This is a package for data preprocessing',
+    install_requires =[
+        'numpy',
+        'pandas',
+        'termcolor',
+        'matplotlib',
+    ],
+    entry_points={
+        'console_scripts': ['mlintern-010=mlintern010:About.intern'],
+    },
+)
