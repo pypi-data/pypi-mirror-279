60f25bac677d87dd1a4fffd95fe18ce099297dc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-16 04:03:34 +0000

    Add package fractlang/0.1.25/fractlang-0.1.25.tar.gz

---
+++ packages/fractlang/fractlang-0.1.25.tar.gz/fractlang-0.1.25/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='fractlang',
+    version='0.1.25',
+    packages=find_packages(),
+    install_requires=[
+        'watchdog',
+        'colorama',
+    ],
+    entry_points={
+        'console_scripts': [
+            'frx = fractlang.fractlang.frxparser:main',
+        ],
+    },
+   author='ryznxx',
+    author_email='7ryznxx@gmail.com',
+    description='A custom programming syntax for Fractlang in python',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/yourusername/fractlang',
+    license='MIT',
+)
