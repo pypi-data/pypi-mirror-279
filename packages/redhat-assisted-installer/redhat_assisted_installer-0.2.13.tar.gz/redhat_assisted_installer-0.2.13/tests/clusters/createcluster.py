4f11d469b5484ae7f2f9e18b948f9011c65a411b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-16 14:04:12 +0000

    Add package redhat-assisted-installer/0.2.13/redhat_assisted_installer-0.2.13.tar.gz

---
+++ packages/redhat-assisted-installer/redhat_assisted_installer-0.2.13.tar.gz/redhat_assisted_installer-0.2.13/tests/clusters/createcluster.py
@@ -0,0 +1,23 @@
+import sys, os
+## Code to disable creating pycache dir after running
+sys.dont_write_bytecode = True
+###################################################
+
+sys.path.append(os.path.abspath(f"{os.getcwd()}/src/"))
+
+from redhat_assisted_installer import assisted_installer
+from requests.exceptions import HTTPError
+
+installer = assisted_installer.assisted_installer()
+
+try:
+    cluster = installer.post_cluster("ocp-testing", "4.15")
+
+except HTTPError as e:
+    print("bad response code")
+    print(e)
+
+except Exception as e:
+    print(e)
+
+    
