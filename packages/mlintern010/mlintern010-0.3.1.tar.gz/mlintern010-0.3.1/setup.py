2c013d38bc417f7b45ae4dae56df0c93f1d3a563
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-16 03:05:08 +0000

    Add package mlintern010/0.3.1/mlintern010-0.3.1.tar.gz

---
+++ packages/mlintern010/mlintern010-0.3.1.tar.gz/mlintern010-0.3.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mlintern010',
+    version='0.3.1',
+    packages=find_packages(),
+    author='R Kiran Kumar Reddy',
+    author_email='rkirankumarreddy599@gmail.com',
+    description='This is a package for data preprocessing',
+    install_requires =[
+        'numpy',
+        'pandas',
+        'termcolor',
+    ],
+    entry_points={
+        'console_scripts': ['mlintern-010=mlintern010:about.inita'],
+    },
+)
