8bb3e2a3644abd58bb3f12bf6a726f175719622e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-16 04:03:59 +0000

    Add package ezrules/0.5.0/ezrules-0.5.0-py3-none-any.whl

---
+++ packages/ezrules/ezrules-0.5.0-py3-none-any.whl/ezrules/models/database.py
@@ -0,0 +1,17 @@
+import os
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+from ezrules.models.history_meta import versioned_session
+
+Base = declarative_base()
+
+if "DB_ENDPOINT" in os.environ:
+    engine = create_engine(os.environ["DB_ENDPOINT"])
+    db_session = scoped_session(
+        sessionmaker(autocommit=False, autoflush=False, bind=engine)
+    )
+    versioned_session(db_session)
+    Base.query = db_session.query_property()
