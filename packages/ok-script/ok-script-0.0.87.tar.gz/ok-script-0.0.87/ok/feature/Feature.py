f0960d84b9e67bb30f0b14afafb5493fc601852d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-16 03:04:10 +0000

    Add package ok-script/0.0.87/ok-script-0.0.87.tar.gz

---
+++ packages/ok-script/ok-script-0.0.87.tar.gz/ok-script-0.0.87/ok/feature/Feature.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+
+class Feature:
+    def __init__(self, mat: np.ndarray, x: int, y: int, width: int, height: int) -> None:
+        """
+        Initialize a Feature with an image (Mat) and its bounding box coordinates.
+
+        Args:
+            mat (np.ndarray): The OpenCV Mat object representing the image.
+            x (int): The x-coordinate of the top-left corner of the bounding box.
+            y (int): The y-coordinate of the top-left corner of the bounding box.
+            width (int): The width of the bounding box.
+            height (int): The height of the bounding box.
+        """
+        self.mat = mat
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+
+    def __str__(self) -> str:
+        return f'self.x: {self.x}, self.y: {self.y}, width: {self.width}, height: {self.height}'
