a315bcbe37e0daa39eedbfe7cd72f82e9b337641
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-16 14:03:53 +0000

    Add package qwertyuioptesttest/0.0.1/qwertyuioptesttest-0.0.1.tar.gz

---
+++ packages/qwertyuioptesttest/qwertyuioptesttest-0.0.1.tar.gz/qwertyuioptesttest-0.0.1/README.md
@@ -0,0 +1,3 @@
+# qwertyuioptesttest
+
+test
---
+++ packages/qwertyuioptesttest/qwertyuioptesttest-0.0.1.tar.gz/qwertyuioptesttest-0.0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='qwertyuioptesttest',
+    version='0.0.1',
+    packages=find_packages(),
+    install_requires=['python-math'],
+    description='test',
+    author='TYA',
+    author_email='tya@gmail.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
+    
