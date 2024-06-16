be519285e7184c6cecfdcd4e59ca0949a6c92b74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-16 14:04:21 +0000

    Add package dsnd-distributions-logan-lev/1.0/dsnd_distributions_logan_lev-1.0.tar.gz

---
+++ packages/dsnd-distributions-logan-lev/dsnd_distributions_logan_lev-1.0.tar.gz/dsnd_distributions_logan_lev-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dsnd_distributions_logan_lev',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['dsnd_distributions_logan_lev'],
+      author = 'Logan Levine',
+      authoremail = 'loganlevine29@gmail.com',
+      zip_safe=False)
