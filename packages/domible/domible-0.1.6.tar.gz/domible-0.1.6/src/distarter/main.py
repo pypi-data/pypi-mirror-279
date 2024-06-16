c9ddda732d78bdbe9ae619567a7b351225ede34b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-16 14:04:07 +0000

    Add package domible/0.1.6/domible-0.1.6.tar.gz

---
+++ packages/domible/domible-0.1.6.tar.gz/domible-0.1.6/src/distarter/main.py
@@ -0,0 +1,49 @@
+""" domible/src/distarter/main.py 
+
+this file serves as Python code to get started with domible
+The datetime and webbrowser imports and openPage() funtion 
+are not needed to use domible.
+They're here only to be able to open the starter page in your default browser.
+"""
+
+from datetime import datetime as dt
+import webbrowser as wb
+
+from domible.elements import Html, Body, Title
+from domible.elements import Heading, Anchor, Paragraph
+from domible.starterDocuments import basic_head_empty_body
+
+
+def openPage(htmlDoc: Html) -> None:
+    """create temp html file to use webbrowser to open passed in Html doc"""
+    thf = f"tmp_html_{dt.timestamp(dt.now())}.html"
+    with open(thf, "w") as f:
+        f.write(f"{htmlDoc}")
+    wb.open(thf)
+
+
+#######
+def run() -> None:
+    """
+    generate, and show in browser,
+    a very simple HTML document
+    """
+    title = "BareBones with a Minimal Body"
+    bga = Anchor(
+        href="https://blindgumption.com", contents="the Blind Gumption Website"
+    )
+    htmlDoc = basic_head_empty_body(title)
+    body = htmlDoc.get_body_element()
+    body.add_content(
+        [
+            Heading(1, title),
+            Paragraph(f"You might find more interesting content at {bga}"),
+        ]
+    )
+    openPage(htmlDoc)
+
+
+if __name__ == "__main__":
+    run()
+
+## end of file
