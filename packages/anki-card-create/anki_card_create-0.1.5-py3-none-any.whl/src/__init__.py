c407b09a4da598d70872949655b8ac9db62120a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-16 15:04:23 +0000

    Add package anki-card-create/0.1.5/anki_card_create-0.1.5-py3-none-any.whl

---
+++ packages/anki-card-create/anki_card_create-0.1.5-py3-none-any.whl/src/__init__.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+
+DIR_PATH = Path(__file__).resolve().parent.parent
+API_URL = "http://127.0.0.1:8765"
+DECK_NAME = "korean"
+MODEL_NAME = "Basic (裏表反転カード付き)+sentense"
+MP3_PATH = DIR_PATH / "data"
