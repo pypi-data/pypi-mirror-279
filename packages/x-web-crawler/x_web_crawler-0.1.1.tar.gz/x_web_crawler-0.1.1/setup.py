29c1f2d4dddad9cff40bcedfc8a43e2cf9653a0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-16 04:03:44 +0000

    Add package x-web-crawler/0.1.1/x_web_crawler-0.1.1.tar.gz

---
+++ packages/x-web-crawler/x_web_crawler-0.1.1.tar.gz/x_web_crawler-0.1.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='x-web-crawler',
+    version='0.1.1',
+    packages=find_packages(),
+    install_requires=[
+        'selenium',
+    ],
+    entry_points={
+        'console_scripts': [
+            'x_automation=autofollow.x:main',
+            'github_automation=autofollow.github:main',
+        ],
+    },
+    author='Jacob Somer',
+    author_email='somerjacob@gmail.com',
+    description='A web crawler for x.com',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/jacobsomer/autofollow',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
