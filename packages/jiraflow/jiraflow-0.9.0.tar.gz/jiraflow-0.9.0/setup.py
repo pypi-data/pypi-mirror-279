a0508ed432f01042a38392c0b4eaab861d664677
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-16 03:04:04 +0000

    Add package jiraflow/0.9.0/jiraflow-0.9.0.tar.gz

---
+++ packages/jiraflow/jiraflow-0.9.0.tar.gz/jiraflow-0.9.0/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='jiraflow',
+    version='0.9.0',
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        'jiraflow': ["ExampleConfig.json"],
+    },
+    install_requires=[
+        "argparse", "flowmetricscsv", "montecarlocsv", "requests"
+    ],
+    entry_points={
+        'console_scripts': [
+            'jiraflow=jiraflow.main:main',
+        ],
+    },
+    author='Benjamin Huser-Berta',
+    author_email='benj.huser@gmail.com',
+    description='A package to generate flow metrics charts and run Monte Carlo Simulation based Forecasts based on JQL queries against Jira.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://letpeople.work',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
