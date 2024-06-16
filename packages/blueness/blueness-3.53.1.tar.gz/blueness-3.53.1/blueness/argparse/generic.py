5967a8fab9a7d04c54295aef3b4804af06de6441
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-16 14:03:36 +0000

    Add package blueness/3.53.1/blueness-3.53.1.tar.gz

---
+++ packages/blueness/blueness-3.53.1.tar.gz/blueness-3.53.1/blueness/argparse/generic.py
@@ -0,0 +1,67 @@
+import argparse
+import sys
+from typing import Tuple, Union
+from logging import Logger
+
+list_of_tasks = ["locate", "version"]
+
+
+def ending(
+    logger: Logger,
+    NAME: str,
+    task: str,
+    success: Union[bool, None],
+    log: bool = True,
+):
+    if log:
+        if success is None:
+            logger.error(f"-{NAME}: {task}: command not found.")
+        elif not success:
+            logger.error(f"-{NAME}: {task}: failed.")
+
+    sys.exit(1 - int(success))
+
+
+def main(
+    main_filename: str,
+    NAME: str,
+    VERSION: str,
+    DESCRIPTION: str,
+    ICON: str,
+) -> Tuple[bool, str]:
+    parser = argparse.ArgumentParser(NAME, description=f"{NAME}-{VERSION}")
+    parser.add_argument(
+        "task",
+        type=str,
+        help="|".join(list_of_tasks),
+    )
+    parser.add_argument(
+        "--show_description",
+        type=int,
+        default=0,
+        help="0|1",
+    )
+    parser.add_argument(
+        "--show_icon",
+        type=int,
+        default=0,
+        help="0|1",
+    )
+    args = parser.parse_args()
+
+    success = args.task in list_of_tasks
+    if args.task == "locate":
+        print(main_filename)
+    elif args.task == "version":
+        print(
+            "{}{}-{}{}".format(
+                f"{ICON} " if args.show_icon else "",
+                NAME,
+                VERSION,
+                "\\n{}".format(DESCRIPTION) if args.show_description else "",
+            )
+        )
+    else:
+        return False, f"-{NAME}: {args.task}: command not found."
+
+    return success, ("" if success else f"-{NAME}: {args.task}: failed.")
