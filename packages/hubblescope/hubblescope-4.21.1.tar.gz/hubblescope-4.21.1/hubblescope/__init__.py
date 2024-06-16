77d4e7f960711ebaa23c16b4d6403bea0e7095b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-16 14:03:38 +0000

    Add package hubblescope/4.21.1/hubblescope-4.21.1.tar.gz

---
+++ packages/hubblescope/hubblescope-4.21.1.tar.gz/hubblescope-4.21.1/hubblescope/__init__.py
@@ -0,0 +1,7 @@
+NAME = "hubblescope"
+
+ICON = "🔭"
+
+DESCRIPTION = f"{ICON} tools to access and process Hubble Space Telescope imagery and other datasets on AWS Open Data Registry."
+
+VERSION = "4.21.1"
---
+++ packages/hubblescope/hubblescope-4.21.1.tar.gz/hubblescope-4.21.1/hubblescope/__main__.py
@@ -0,0 +1,70 @@
+import argparse
+from hubblescope.datasets import get
+from hubblescope.ingest import ingest
+from hubblescope import NAME, VERSION, DESCRIPTION
+from hubblescope.logger import logger
+from blueness.argparse.generic import ending
+
+parser = argparse.ArgumentParser(NAME, description=f"{NAME}-{VERSION}")
+parser.add_argument(
+    "task",
+    type=str,
+    help="get|ingest|version",
+)
+parser.add_argument(
+    "--dataset_name",
+    type=str,
+    default="",
+)
+parser.add_argument(
+    "--show_description",
+    type=bool,
+    default=0,
+    help="0|1",
+)
+parser.add_argument(
+    "--object_name",
+    type=str,
+    default="",
+)
+parser.add_argument(
+    "--hubble_object_name",
+    type=str,
+    default="",
+)
+parser.add_argument(
+    "--what",
+    type=str,
+    default="",
+)
+args = parser.parse_args()
+
+success = False
+if args.task == "get":
+    print(
+        get(
+            args.dataset_name,
+            args.what,
+            args.object_name,
+        )
+    )
+    success = True
+elif args.task == "ingest":
+    success = ingest(
+        args.object_name,
+        args.dataset_name,
+        args.hubble_object_name,
+    )
+elif args.task == "version":
+    print(
+        "{}-{}{}".format(
+            NAME,
+            VERSION,
+            "\\n{}".format(DESCRIPTION) if args.show_description else "",
+        )
+    )
+    success = True
+else:
+    success = None
+
+ending(logger, NAME, args.task, success)
