06cabd12e95d0454d6529123128279e230a39267
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-16 04:04:09 +0000

    Add package libchangelog/0.0.1/libchangelog-0.0.1.tar.gz

---
+++ packages/libchangelog/libchangelog-0.0.1.tar.gz/libchangelog-0.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+# def install_requires():
+#     with open('requirements.txt', 'r') as f:
+#         return [line.strip() for line in f.readlines()]
+
+setup(
+    name='libchangelog',
+    version='0.0.1',
+    packages=['libchangelog'],
+    # install_requires=install_requires(),
+    install_requires=[],
+    classifiers=[],
+)
