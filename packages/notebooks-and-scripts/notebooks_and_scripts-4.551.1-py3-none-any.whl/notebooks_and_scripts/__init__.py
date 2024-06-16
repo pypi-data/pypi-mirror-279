123ef249e2bca3059bc58c8207dc305bc09e5286
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-16 14:03:52 +0000

    Add package notebooks-and-scripts/4.551.1/notebooks_and_scripts-4.551.1-py3-none-any.whl

---
+++ packages/notebooks-and-scripts/notebooks_and_scripts-4.551.1-py3-none-any.whl/notebooks_and_scripts/__init__.py
@@ -0,0 +1,9 @@
+NAME = "notebooks_and_scripts"
+
+ICON = "📜"
+
+DESCRIPTION = f"{ICON} notebooks and scripts for ai experiments and aws batch jobs."
+
+VERSION = "4.551.1"
+
+REPO_NAME = "notebooks-and-scripts"
