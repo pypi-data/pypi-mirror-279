9aeb1a503af06ffbad001e7ef723f4fbf4e2593c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-16 04:04:06 +0000

    Add package scalellm/0.1.4/scalellm-0.1.4-cp310-cp310-manylinux1_x86_64.whl

---
+++ packages/scalellm/scalellm-0.1.4-cp310-cp310-manylinux1_x86_64.whl/scalellm/__init__.py
@@ -0,0 +1,33 @@
+__version__ = "0.1.4"
+try:
+    # torch needs to be imported first, otherwise it will segfault upon import.
+    import torch  # noqa: F401
+except ImportError:
+    pass
+
+from scalellm._C import (LLMHandler, LogProb, LogProbData, Message, Priority,
+                         RequestOutput, SamplingParams, SequenceOutput, Status,
+                         StatusCode, Usage, get_metrics)
+from scalellm.errors import ValidationError
+from scalellm.llm import LLM
+from scalellm.llm_engine import AsyncLLMEngine, OutputAsyncStream, OutputStream
+
+__all__ = [
+    "Message",
+    "LLM",
+    "LogProb",
+    "LogProbData",
+    "AsyncLLMEngine",
+    "OutputAsyncStream",
+    "OutputStream",
+    "ValidationError",
+    "Priority",
+    "RequestOutput",
+    "SamplingParams",
+    "SequenceOutput",
+    "Status",
+    "StatusCode",
+    "Usage",
+    "LLMHandler",
+    "get_metrics",
+]
