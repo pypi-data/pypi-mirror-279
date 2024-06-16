3729b1b90a6a75dec5bf12403ac4ba561b1a5910
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-16 03:04:06 +0000

    Add package series-intro-recognizer/0.5.0/series_intro_recognizer-0.5.0-py3-none-any.whl

---
+++ packages/series-intro-recognizer/series_intro_recognizer-0.5.0-py3-none-any.whl/series_intro_recognizer/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+from series_intro_recognizer.tp.interval import Interval
+from series_intro_recognizer.processors.audio_samples import recognise_from_audio_samples
+from series_intro_recognizer.processors.audio_files import recognise_from_audio_files
