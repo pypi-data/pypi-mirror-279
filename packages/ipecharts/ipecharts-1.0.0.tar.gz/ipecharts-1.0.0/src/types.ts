8f4ad1c1698e659d13324648f4587abf0acf3f73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-16 14:03:37 +0000

    Add package ipecharts/1.0.0/ipecharts-1.0.0.tar.gz

---
+++ packages/ipecharts/ipecharts-1.0.0.tar.gz/ipecharts-1.0.0/src/types.ts
@@ -0,0 +1,16 @@
+// Copyright (c) Trung Le
+// Distributed under the terms of the Modified BSD License.
+
+import { DOMWidgetModel } from '@jupyter-widgets/base';
+import { Token } from '@lumino/coreutils';
+
+export interface IUpdateManager {
+  findRoot(modeId: DOMWidgetModel): DOMWidgetModel[];
+  sendUpdateSignal(modeId: DOMWidgetModel): void;
+  registerModel(model: DOMWidgetModel): void;
+  unregisterModel(model: DOMWidgetModel): void;
+}
+
+export const IUpdateManagerToken = new Token<IUpdateManager>(
+  'ipechartUpdateManager'
+);
