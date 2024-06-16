ba2d5d469a1f1e976f55fe08ff524d9efea2065e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-16 14:03:38 +0000

    Add package pywebwizard/1.2.48/pywebwizard-1.2.48.tar.gz

---
+++ packages/pywebwizard/pywebwizard-1.2.48.tar.gz/pywebwizard-1.2.48/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt', 'r') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="pywebwizard",
+    version="1.2.48",
+    description="Magic Browser library",
+    long_description=open('README.md', 'r').read(),
+    long_description_content_type="text/markdown",
+    author="Parendum OÜ",
+    author_email="info@parendum.com",
+    url="https://github.com/tuusuario/webwizard",
+    packages=find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+)
