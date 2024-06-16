f3e1f8c98c25ae5d5283bfccd9afcf22efc69373
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-16 14:03:49 +0000

    Add package python-ember-mug/1.0.1/python_ember_mug-1.0.1.tar.gz

---
+++ packages/python-ember-mug/python_ember_mug-1.0.1.tar.gz/python_ember_mug-1.0.1/ember_mug/scanner.py
@@ -0,0 +1,70 @@
+"""Scanning tools for finding mugs."""
+from __future__ import annotations
+
+import asyncio
+import contextlib
+import logging
+import sys
+from typing import TYPE_CHECKING, Any
+
+from bleak import BleakScanner
+
+from .consts import DEVICE_SERVICE_UUIDS, IS_LINUX
+
+if sys.version_info < (3, 11):
+    # library required before Python 3.11
+    import async_timeout
+else:
+    async_timeout = asyncio
+
+if TYPE_CHECKING:
+    from bleak.backends.device import BLEDevice
+    from bleak.backends.scanner import AdvertisementData
+
+
+DEFAULT_TIMEOUT = 30
+
+logger = logging.getLogger(__name__)
+
+
+def build_scanner_kwargs(adapter: str | None = None) -> dict[str, Any]:
+    """Add Adapter to kwargs for scanner if specified and using BlueZ."""
+    if adapter and IS_LINUX is not True:
+        msg = "The adapter option is only valid for the Linux BlueZ Backend."
+        raise ValueError(msg)
+    kwargs = {"service_uuids": DEVICE_SERVICE_UUIDS}
+    return kwargs | {"adapter": adapter} if adapter else kwargs
+
+
+async def discover_mugs(
+    mac: str | None = None,
+    adapter: str | None = None,
+    wait: int = 5,
+) -> list[tuple[BLEDevice, AdvertisementData]]:
+    """Discover new mugs in pairing mode."""
+    async with BleakScanner(**build_scanner_kwargs(adapter)) as scanner:
+        await asyncio.sleep(wait)
+        return [
+            (d, a)
+            for (d, a) in scanner.discovered_devices_and_advertisement_data.values()
+            if mac is None or d.address.lower() == mac.lower()
+        ]
+
+
+async def find_mug(
+    mac: str | None = None,
+    adapter: str | None = None,
+    timeout: int = DEFAULT_TIMEOUT,
+) -> tuple[BLEDevice, AdvertisementData] | tuple[None, None]:
+    """Find a mug."""
+    if mac is not None:
+        mac = mac.lower()
+    async with BleakScanner(**build_scanner_kwargs(adapter)) as scanner:
+        with contextlib.suppress(asyncio.TimeoutError):
+            async with async_timeout.timeout(timeout):
+                async for device, advertisement in scanner.advertisement_data():
+                    if (not mac and device.name and device.name.startswith("Ember")) or (
+                        mac and device.address.lower() == mac
+                    ):
+                        return device, advertisement
+    return None, None
---
+++ packages/python-ember-mug/python_ember_mug-1.0.1.tar.gz/python_ember_mug-1.0.1/tests/test_scanner.py
@@ -0,0 +1,62 @@
+"""Tests for scanner."""
+from unittest.mock import AsyncMock, MagicMock, patch
+
+import pytest
+from bleak.backends.device import BLEDevice
+
+from ember_mug.consts import DEVICE_SERVICE_UUIDS
+from ember_mug.scanner import build_scanner_kwargs, discover_mugs, find_mug
+
+from .conftest import TEST_MUG_ADVERTISEMENT
+
+MUG_1 = BLEDevice(address="32:36:a5:be:88:cb", name="Ember Ceramic Mug", details={}, rssi=1)
+MUG_2 = BLEDevice(address="9c:da:8c:19:27:da", name="Ember Ceramic Mug", details={}, rssi=1)
+EXAMPLE_MUGS = [MUG_1, MUG_2]
+
+
+@patch("ember_mug.scanner.IS_LINUX", True)
+def test_build_scanner_kwargs_linux() -> None:
+    assert build_scanner_kwargs() == {"service_uuids": DEVICE_SERVICE_UUIDS}
+    assert build_scanner_kwargs(adapter="hci0") == {"adapter": "hci0", "service_uuids": DEVICE_SERVICE_UUIDS}
+
+
+@patch("ember_mug.scanner.IS_LINUX", False)
+def test_build_scanner_kwargs_other() -> None:
+    error = "The adapter option is only valid for the Linux BlueZ Backend."
+    with pytest.raises(ValueError, match=error):
+        assert build_scanner_kwargs(adapter="hci0")
+
+
+@patch("asyncio.sleep")
+@patch("ember_mug.scanner.BleakScanner")
+async def test_discover_mugs(mock_scanner: AsyncMock, mock_sleep: AsyncMock) -> None:
+    mock_scanner.return_value.__aenter__.return_value.discovered_devices_and_advertisement_data = {
+        m.address: (m, TEST_MUG_ADVERTISEMENT) for m in EXAMPLE_MUGS
+    }
+    mugs = await discover_mugs()
+    assert len(mugs) == 2
+    mugs = await discover_mugs(mac="32:36:a5:be:88:cb")
+    assert len(mugs) == 1
+    device_1, advertisement_1 = mugs[0]
+    assert device_1.address == "32:36:a5:be:88:cb"
+    mock_sleep.assert_called_with(5)
+
+
+@patch("asyncio.sleep")
+@patch("ember_mug.scanner.BleakScanner")
+async def test_find_mug(mock_scanner: AsyncMock, mock_sleep: AsyncMock) -> None:
+    mock_data_iterator = MagicMock()
+    mock_data_iterator().__aiter__.return_value = [(m, TEST_MUG_ADVERTISEMENT) for m in EXAMPLE_MUGS]
+    mock_scanner.return_value.__aenter__.return_value.advertisement_data = mock_data_iterator
+
+    # Without filter
+    mug, advertisement = await find_mug()
+    assert mug is not None
+    assert mug.name == "Ember Ceramic Mug"
+    assert mug.address == MUG_1.address
+
+    # With Filter
+    mug, advertisement = await find_mug(mac=MUG_2.address)
+    assert mug is not None
+    assert mug.name == "Ember Ceramic Mug"
+    assert mug.address == MUG_2.address
