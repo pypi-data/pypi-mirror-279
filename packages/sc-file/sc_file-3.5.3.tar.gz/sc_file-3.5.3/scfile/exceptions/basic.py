fcdd317e76ce447980d2129a44f61add8e074338
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-16 04:03:34 +0000

    Add package sc-file/3.5.3/sc_file-3.5.3.tar.gz

---
+++ packages/sc-file/sc_file-3.5.3.tar.gz/sc_file-3.5.3/scfile/exceptions/basic.py
@@ -0,0 +1,49 @@
+from pathlib import Path
+
+from scfile.utils.types import PathLike
+
+from .base import ScFileException
+
+
+class FileBasicError(ScFileException):
+    """Basic files exception. Occurring when file processing."""
+
+    def __init__(self, path: PathLike):
+        self.path = Path(path)
+
+    @property
+    def posix_path(self) -> str:
+        return self.path.as_posix()
+
+    def __str__(self):
+        return f"File '{self.posix_path}'"
+
+
+class FileNotFound(FileBasicError):
+    """Exception occurring when file not found or does not exists."""
+
+    def __str__(self):
+        return f"{super().__str__()} not found (not exists)."
+
+
+class FileSuffixUnsupported(FileBasicError):
+    """Exception occurring when file cannot be decoded or encoded."""
+
+    def __str__(self):
+        return f"{super().__str__()} with suffix '{self.path.suffix}' is unsupported."
+
+
+class FileSignatureInvalid(FileBasicError):
+    """Exception occurring when file signature does not match file type."""
+
+    def __init__(self, path: PathLike, readed: int, signature: int):
+        self.path = Path(path)
+        self.readed = readed
+        self.signature = signature
+
+    def __str__(self):
+        return (
+            f"{super().__str__()} has invalid signature - "
+            f"{hex(self.readed)} != {hex(self.signature)}. "
+            "(File suffix does not match file type)."
+        )
