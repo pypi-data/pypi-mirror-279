17e27846a3bba5a7b7c5e0be4bbf23bc180d457a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-16 14:03:39 +0000

    Add package hubblescope/4.22.1/hubblescope-4.22.1-py3-none-any.whl

---
+++ packages/hubblescope/hubblescope-4.22.1-py3-none-any.whl/hubblescope/.abcli/hubble.sh
@@ -0,0 +1,16 @@
+#! /usr/bin/env bash
+
+function hubble() {
+    local task=$(abcli_unpack_keyword $1 help)
+
+    if [ $task == "help" ]; then
+        hubble_download "$@"
+        hubble_list "$@"
+        hubble_select "$@"
+        return
+    fi
+
+    abcli_generic_task \
+        plugin=hubble,task=$task \
+        "${@:2}"
+}
