e3dd98d7753cbad8897d0ba40cbb5f204d2b9fce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-17 04:04:35 +0000

    Add package JsonAsDatabase/0.1.0/jsonasdatabase-0.1.0.tar.gz

---
+++ packages/JsonAsDatabase/jsonasdatabase-0.1.0.tar.gz/jsonasdatabase-0.1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+setup(
+name='JsonAsDatabase',
+version='0.1.0',
+author='Baltej Singh',
+author_email='baltej963@gmail.com',
+description='Treat json files as databases, when want to store unencrypted data',
+packages=find_packages(),
+classifiers=[
+'Programming Language :: Python :: 3',
+'License :: OSI Approved :: MIT License',
+'Operating System :: OS Independent',
+],
+python_requires='>=3.6',
+)
+
