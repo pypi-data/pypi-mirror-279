3842fc40c13daa2abd4a72cb263b216a128d1956
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-17 03:04:26 +0000

    Add package satellitepy/0.1.0/satellitepy-0.1.0.tar.gz

---
+++ packages/satellitepy/satellitepy-0.1.0.tar.gz/satellitepy-0.1.0/src/satellitepy/__init__.py
@@ -0,0 +1,13 @@
+from fastapi import Request
+from starlette.middleware.base import BaseHTTPMiddleware
+
+class RealWildCardCors(BaseHTTPMiddleware):
+  def __init__(self, app):
+    super().__init__(app)
+
+  async def dispatch(self, request: Request, call_next):
+    response = await call_next(request)
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers["Access-Control-Allow-Methods"] = "*"
+    response.headers["Access-Control-Allow-Headers"] = "*"
+    return response
