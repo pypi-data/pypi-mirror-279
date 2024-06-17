d09b384cc90c6ac971701bf386199f7ec36a5252
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-17 03:05:30 +0000

    Add package pyHoneygain/0.3.6/pyHoneygain-0.3.6.tar.gz

---
+++ packages/pyHoneygain/pyHoneygain-0.3.6.tar.gz/pyHoneygain-0.3.6/pyHoneygain/exceptions.py
@@ -0,0 +1,19 @@
+class HoneygainAPIError(Exception):
+	"""Base Honeygain API Exception"""
+	def __init__(self, *args: object) -> None:
+		super().__init__(*args)
+
+class NotLoggedInError(HoneygainAPIError):
+	"""Raised when you're not logged in and try to access protected endpoints. """
+	def __init__(self, *args: object) -> None:
+		super().__init__(*args)
+
+class UserConfirmationRequiredError(HoneygainAPIError):
+	"""Raised when user confirmation is required. """
+	def __init__(self, *args: object) -> None:
+		super().__init__(*args)
+
+class DeprecatedEndpointError(HoneygainAPIError):
+	"""Raised when the endpoint is deprecated. """
+	def __init__(self, *args: object) -> None:
+		super().__init__(*args)
