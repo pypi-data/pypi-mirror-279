ccc53f12dae500e5294eafd27af07fb41d5d8988
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-17 15:04:41 +0000

    Add package slurpit_nautobot/0.8.85/slurpit_nautobot-0.8.85.tar.gz

---
+++ packages/slurpit_nautobot/slurpit_nautobot-0.8.85.tar.gz/slurpit_nautobot-0.8.85/src/slurpit_nautobot/filtersets.py
@@ -0,0 +1,78 @@
+import django_filters
+from django.db.models import Q
+from nautobot.apps.filters import BaseFilterSet
+from .models import SlurpitLog, SlurpitPlanning, SlurpitSnapshot, SlurpitImportedDevice
+from django.utils.translation import gettext as _
+
+
+class LoggingFilterSet(BaseFilterSet):
+    q = django_filters.CharFilter(
+        method='search',
+        label=_('Search'),
+    )
+
+    class Meta:
+        model = SlurpitLog
+        fields = [
+            'log_time', 'level', 'category', 'message'
+        ]
+
+    def search(self, queryset, name, value):
+        if not value.strip():
+            return queryset
+        return queryset.filter(
+            Q(level__icontains=value) | 
+            Q(category__icontains=value) |
+            Q(message__icontains=value)
+        )
+
+class SlurpitPlanningFilterSet(BaseFilterSet):
+    q = django_filters.CharFilter(
+        method='search',
+        label=_('Search'),
+    )
+
+    class Meta:
+        model = SlurpitPlanning
+        fields = ["id", "name", "planning_id"]
+
+    def search(self, queryset, name, value):
+        if not value.strip():
+            return queryset
+        return queryset.filter(
+            Q(name__icontains=value)
+        )
+    
+class SlurpitSnapshotFilterSet(BaseFilterSet):
+    q = django_filters.CharFilter(
+        method='search',
+        label=_('Search'),
+    )
+
+    class Meta:
+        model = SlurpitSnapshot
+        fields = ["id", "hostname", "planning_id"]
+
+    def search(self, queryset, name, value):
+        if not value.strip():
+            return queryset
+        return queryset.filter(
+            Q(name__icontains=value)
+        )
+
+class SlurpitImportedDeviceFilterSet(BaseFilterSet):
+    q = django_filters.CharFilter(
+        method='search',
+        label=_('Search'),
+    )
+
+    class Meta:
+        model = SlurpitImportedDevice
+        fields = ["id", "hostname"]
+
+    def search(self, queryset, name, value):
+        if not value.strip():
+            return queryset
+        return queryset.filter(
+            Q(name__icontains=value)
+        )
