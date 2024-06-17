bbfdf5c49633e021e19775d3a73d4aa67a91d670
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-17 04:04:20 +0000

    Add package hidiffusion/0.1.10/hidiffusion-0.1.10.tar.gz

---
+++ packages/hidiffusion/hidiffusion-0.1.10.tar.gz/hidiffusion-0.1.10/setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="hidiffusion",
+    version="0.1.10",
+    author="Shen Zhang, Zhaowei Chen, Zhenyu Zhao, Yuhao Chen, Yao Tang, Jiajun Liang",
+    url="",
+    description="HiDiffusion: A training-free method to increase the resolution and speed of diffusion models.",
+    packages=find_packages(),
+    include_package_data=True,
+    long_description=open("README.md", "r", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+)
