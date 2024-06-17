0c93ef3a65febbce4d4a307d553e9bc9905d74d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-17 14:05:03 +0000

    Add package langchain-teddynote/0.0.3/langchain_teddynote-0.0.3-py3-none-any.whl

---
+++ packages/langchain-teddynote/langchain_teddynote-0.0.3-py3-none-any.whl/langchain_teddynote/messages.py
@@ -0,0 +1,14 @@
+from langchain_core.messages import AIMessageChunk
+
+
+def stream_response(response):
+    answer = ""
+    for token in response:
+        if isinstance(token, AIMessageChunk):
+            answer += token.content
+            print(token.content, end="", flush=True)
+        elif isinstance(token, str):
+            answer += token
+            print(token, end="", flush=True)
+
+    return answer
