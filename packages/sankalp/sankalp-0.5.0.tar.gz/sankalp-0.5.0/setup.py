a65e225b744f4a8cff3a04bae9ef5efc2515a6f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-17 16:04:27 +0000

    Add package sankalp/0.5.0/sankalp-0.5.0.tar.gz

---
+++ packages/sankalp/sankalp-0.5.0.tar.gz/sankalp-0.5.0/setup.py
@@ -0,0 +1,29 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='sankalp',
+    version='0.5.0',
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        "sankalp": [],
+    },
+    install_requires=[
+        "curses",
+        "webbrowser",
+    ],
+    entry_points={
+        'console_scripts': [
+            'sankalp = sankalp.cli:run_cli',
+        ],
+    },
+    python_requires='>=3.6',
+    author='Sankalp Shrivastava',
+    author_email='s@sankalp.sh',
+    description='A simple CLI package for Sankalp Shrivastava',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/1sankalp',
+)
