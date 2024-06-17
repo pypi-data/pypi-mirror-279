a13a168cba923681ba3cb7a330de2c24ae62bb3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-17 03:05:06 +0000

    Add package ekn/0.0.2/ekn-0.0.2.tar.gz

---
+++ packages/ekn/ekn-0.0.2.tar.gz/ekn-0.0.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+
+def readme():
+  with open('README.md', 'r') as f:
+    return f.read()
+
+
+setup(
+  name='ekn',
+  version='0.0.2',
+  author='damirem',
+  author_email='bd-spam@mail.ru',
+  description='ekn',
+  long_description=readme(),
+  long_description_content_type='text/markdown',
+  packages=find_packages(),
+  install_requires=['requests>=2.25.1'],
+  classifiers=[
+    'Programming Language :: Python :: 3.11',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent'
+  ],
+  keywords='ekn',
+  python_requires='>=3.6'
+)
