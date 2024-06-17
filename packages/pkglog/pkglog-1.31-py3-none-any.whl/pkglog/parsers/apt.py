3b5297b1e6c73dbee35ae6d84c8c7ba385d2eeff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-17 14:04:22 +0000

    Add package pkglog/1.31/pkglog-1.31-py3-none-any.whl

---
+++ packages/pkglog/pkglog-1.31-py3-none-any.whl/pkglog/parsers/apt.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+'Parse Debian/Ubuntu APT log messages'
+import re
+from datetime import datetime
+
+logfile = '/var/log/apt/history.log'
+
+ACTIONS = {
+    'Upgrade': 'upgraded',
+    'Downgrade': 'downgraded',
+    'Install': 'installed',
+    'Remove': 'removed',
+    'Reinstall': 'reinstalled',
+}
+
+class g:
+    action = None
+    line = None
+
+def get_time(line):
+    vals = line.split(':', maxsplit=1)
+    if len(vals) != 2:
+        return None
+
+    func, rest = vals
+
+    if func == 'Start-Date':
+        g.action = None
+        return None
+
+    if func == 'End-Date':
+        try:
+            return datetime.fromisoformat(rest[1:].strip().replace('  ', ' '))
+        except Exception:
+            return None
+
+    action = ACTIONS.get(func)
+    if action:
+        g.action = action
+        g.line = rest[1:]
+    return None
+
+def get_packages():
+    if g.action:
+        for pline in g.line.strip().split('),'):
+            pkg, vers = pline.split(maxsplit=1)
+            pkg = re.sub(':.*', '', pkg)
+            vers = vers.strip().strip('()')
+            vers = vers.replace(', automatic', '')
+            vers = vers.replace(', ', ' -> ')
+            yield g.action, pkg, vers
