6d0dab15f755f0f71554c4fb8adf74cc01953802
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-17 15:05:23 +0000

    Add package thekey/1.0.4/thekey-1.0.4.tar.gz

---
+++ packages/thekey/thekey-1.0.4.tar.gz/thekey-1.0.4/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='thekey',
+    version='1.0.4',
+    description='Help You Find Any Text Queqly!|帮你快速查询文本！',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Python学霸',
+    author_email='python@xueba.com',
+    py_modules=['thekey'],
+    install_requires=[],
+    entry_points={
+        'console_scripts': [
+            'findit=thekey:findit']
+    }
+)
