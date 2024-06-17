dd613b39d33d18d3ca04edbd0d6c4e3286516b43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-17 03:04:25 +0000

    Add package ekn/0.1.0/ekn-0.1.0.tar.gz

---
+++ packages/ekn/ekn-0.1.0.tar.gz/ekn-0.1.0/ekn/negr.py
@@ -0,0 +1,15 @@
+def helpp():
+    # Открываем файл с указанием кодировки и обработкой ошибок
+    f = open("data/1.txt", "r", encoding="utf-8", errors='ignore')
+    for s in f:
+        print(s, end="")
+
+
+def teor(num):
+    from PIL import Image
+
+    # Открытие изображения
+    img = Image.open(f'data/teor/Screenshot_{num}.png')
+
+    # Вывод изображения
+    img.show()
