b7005db50cde451f0766a648b305f28753a9d8b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-17 03:04:10 +0000

    Add package robusta-api/0.13.2a0/robusta_api-0.13.2a0-py3-none-any.whl

---
+++ packages/robusta-api/robusta_api-0.13.2a0-py3-none-any.whl/robusta/utils/parsing.py
@@ -0,0 +1,23 @@
+import datetime
+import json
+import logging
+from typing import Any, Union
+
+
+def load_json(s: Union[str, bytes]) -> Any:
+    """
+    Like json.loads() but prints the input if parsing fails
+    """
+    try:
+        return json.loads(s)
+    except json.decoder.JSONDecodeError:
+        sep = "----------------"
+        logging.error(f"could not parse json:\n{sep}\n{s}\n{sep}\n")
+        raise
+
+
+def datetime_to_db_str(datetime_obj: datetime.datetime) -> str:
+    """
+    Return datetime string (DB parsable)
+    """
+    return datetime_obj.strftime("%Y-%m-%dT%H:%M:%S.%f%z")
