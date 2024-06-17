7e522d654c77a8d64b2c60feb53edcc7fb8475ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-17 04:04:44 +0000

    Add package ReverseBox/0.11.8/ReverseBox-0.11.8.tar.gz

---
+++ packages/ReverseBox/ReverseBox-0.11.8.tar.gz/ReverseBox-0.11.8/reversebox/common/common.py
@@ -0,0 +1,29 @@
+"""
+Copyright © 2022-2024  Bartłomiej Duda
+License: GPL-3.0 License
+"""
+
+
+def convert_int_to_hex_string(input_value: int) -> str:
+    return "0x%02X" % int(input_value)
+
+
+def convert_hex_string_to_int(input_string: str) -> int:
+    return int(input_string, 16)
+
+
+def calculate_padding_length(input_length: int, div: int) -> int:
+    return (div - (input_length % div)) % div
+
+
+def get_length_with_padding(input_length: int, div: int):
+    padding_length: int = calculate_padding_length(input_length, div)
+    return input_length + padding_length
+
+
+def convert_bits_str_to_int(input_bits_str: str) -> int:
+    return int(input_bits_str, 2)
+
+
+def convert_int_to_bool(input_number: int) -> bool:
+    return bool(input_number)
