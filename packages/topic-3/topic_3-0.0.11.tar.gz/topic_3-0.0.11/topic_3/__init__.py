e40ee3743f27e360ab446c0cc81ed7251c45f7b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-17 15:05:18 +0000

    Add package topic-3/0.0.11/topic_3-0.0.11.tar.gz

---
+++ packages/topic-3/topic_3-0.0.11.tar.gz/topic_3-0.0.11/topic_3/__init__.py
@@ -0,0 +1,18 @@
+# Designer:Yudong Pan
+# Coder:God's hand
+# Time:2024/6/9 18:06
+
+import topic_3.util
+import topic_3.models
+import topic_3.scripts
+import topic_3.show_img
+import topic_3.evaluate
+import topic_3.main
+import topic_3.test
+import topic_3.train
+
+__all__ = ['util', 'models', 'scripts', 'show_img', 'evaluate',
+           'main', 'test', 'train']
+
+print("hello world!")
+
