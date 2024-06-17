d0de8bb9954798632c6b5725c93d71b1b9bbc077
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-17 14:05:36 +0000

    Add package utils-base-nuuuwan/1.0.11/utils_base_nuuuwan-1.0.11.tar.gz

---
+++ packages/utils-base-nuuuwan/utils_base_nuuuwan-1.0.11.tar.gz/utils_base_nuuuwan-1.0.11/src/utils_base/console/constants/LEVEL_TO_STYLE.py
@@ -0,0 +1,34 @@
+import logging
+
+from utils_base.console.constants.COLOR_BACKGROUND import COLOR_BACKGROUND
+from utils_base.console.constants.COLOR_FOREGROUND import COLOR_FOREGROUND
+from utils_base.console.constants.COLOR_FORMAT import COLOR_FORMAT
+
+LEVEL_TO_STYLE = {
+    logging.CRITICAL: dict(
+        foreground=COLOR_FOREGROUND.WHITE,
+        background=COLOR_BACKGROUND.RED,
+        format=COLOR_FORMAT.BOLD,
+    ),
+    logging.ERROR: dict(
+        foreground=COLOR_FOREGROUND.RED,
+        background=COLOR_BACKGROUND.BLACK,
+    ),
+    logging.WARNING: dict(
+        foreground=COLOR_FOREGROUND.YELLOW,
+        background=COLOR_BACKGROUND.BLACK,
+    ),
+    logging.INFO: dict(
+        foreground=COLOR_FOREGROUND.GREEN,
+        background=COLOR_BACKGROUND.BLACK,
+    ),
+    logging.DEBUG: dict(
+        foreground=COLOR_FOREGROUND.WHITE,
+        background=COLOR_BACKGROUND.BLACK,
+        format=COLOR_FORMAT.FAINT,
+    ),
+    logging.NOTSET: dict(
+        foreground=COLOR_FOREGROUND.WHITE,
+        background=COLOR_BACKGROUND.BLACK,
+    ),
+}
