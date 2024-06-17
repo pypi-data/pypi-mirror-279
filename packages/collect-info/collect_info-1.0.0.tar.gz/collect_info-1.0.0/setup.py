c14b63caf051a900ed4449181e05f09639b5f956
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-17 03:04:05 +0000

    Add package collect-info/1.0.0/collect_info-1.0.0.tar.gz

---
+++ packages/collect-info/collect_info-1.0.0.tar.gz/collect_info-1.0.0/setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(name='collect_info',
+      version='1.0.0',
+      description='A package for practicum use',
+      long_description=long_description,
+      author='Jayden',
+      install_requires=[],
+      license='MIT',
+      packages=find_packages(),
+      platforms=["all"],
+      classifiers=[
+          'Intended Audience :: Developers',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.9',
+          'Programming Language :: Python :: 3.10',
+          'Programming Language :: Python :: 3.11',
+          'Programming Language :: Python :: 3.12',
+          'Topic :: Software Development :: Libraries'
+      ],
+      )
