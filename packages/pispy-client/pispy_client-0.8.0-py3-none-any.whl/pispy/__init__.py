e1b27634ade4110aaed98d0ab03067da644610e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-17 03:04:16 +0000

    Add package pispy-client/0.8.0/pispy_client-0.8.0-py3-none-any.whl

---
+++ packages/pispy-client/pispy_client-0.8.0-py3-none-any.whl/pispy/__init__.py
@@ -0,0 +1,13 @@
+"""PISpy - A Terminal-Based Python Package Index Inspector"""
+
+######################################################################
+# Main app information.
+__author__ = "Dave Pearson"
+__copyright__ = "Copyright 2022-2024, Dave Pearson"
+__credits__ = ["Dave Pearson"]
+__maintainer__ = "Dave Pearson"
+__email__ = "davep@davep.org"
+__version__ = "0.8.0"
+__licence__ = "GPLv3+"
+
+### __init__.py ends here
---
+++ packages/pispy-client/pispy_client-0.8.0-py3-none-any.whl/pispy/screens/__init__.py
@@ -0,0 +1,12 @@
+"""The screens that make up the application."""
+
+##############################################################################
+# Local imports.
+from .stats import Stats
+from .lookup import Lookup
+
+##############################################################################
+# Export the screens.
+__all__ = ["Stats", "Lookup"]
+
+### __init__.py ends here
