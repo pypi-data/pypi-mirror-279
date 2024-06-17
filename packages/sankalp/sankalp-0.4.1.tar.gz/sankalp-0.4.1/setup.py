1c98edfd1fec8cc004c8c1bf884f8cb897bf1abc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-17 15:05:29 +0000

    Add package sankalp/0.4.1/sankalp-0.4.1.tar.gz

---
+++ packages/sankalp/sankalp-0.4.1.tar.gz/sankalp-0.4.1/setup.py
@@ -0,0 +1,25 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='sankalp',
+    version='0.4.1',
+    packages=find_packages(),
+    install_requires=[
+        'rich',
+        'PyInquirer'
+    ],
+    entry_points={
+        'console_scripts': [
+            'sankalp = sankalp.cli:run_cli',
+        ],
+    },
+    python_requires='>=3.6',
+    author='Sankalp Shrivastava',
+    author_email='s@sankalp.sh',
+    description='A simple CLI package for Sankalp Shrivastava',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/yourusername/sankalp',
+)
