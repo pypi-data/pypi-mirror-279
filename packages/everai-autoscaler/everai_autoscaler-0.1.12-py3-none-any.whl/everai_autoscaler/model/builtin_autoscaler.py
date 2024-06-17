ad28d4a8ef2799fc6e73b9781bf8ef414fe288a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-17 15:06:09 +0000

    Add package everai-autoscaler/0.1.12/everai_autoscaler-0.1.12-py3-none-any.whl

---
+++ packages/everai-autoscaler/everai_autoscaler-0.1.12-py3-none-any.whl/everai_autoscaler/model/builtin_autoscaler.py
@@ -0,0 +1,26 @@
+from __future__ import annotations
+import typing
+from abc import ABC, abstractmethod
+from .autoscaler import AutoScaler
+
+
+T = typing.TypeVar('T', int, float, str)
+
+
+ArgumentType: typing.TypeAlias = typing.Union[T, typing.Callable[[], T]]
+
+
+class BuiltinAutoScaler(AutoScaler):
+    ArgumentType = typing.Union[T, typing.Callable[[], T]]
+
+    @classmethod
+    @abstractmethod
+    def autoscaler_name(cls) -> str: ...
+
+    @abstractmethod
+    def autoscaler_arguments(self) -> typing.Dict[str, ArgumentType]: ...
+
+    @classmethod
+    @abstractmethod
+    def from_arguments(cls, arguments: typing.Dict[str, str]) -> BuiltinAutoScaler: ...
+
