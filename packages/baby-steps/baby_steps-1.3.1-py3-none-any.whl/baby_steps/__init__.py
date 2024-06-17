1e48387a1c202e5e6e616041ffcbbda9ee3372fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-17 03:04:11 +0000

    Add package baby-steps/1.3.1/baby_steps-1.3.1-py3-none-any.whl

---
+++ packages/baby-steps/baby_steps-1.3.1-py3-none-any.whl/baby_steps/__init__.py
@@ -0,0 +1,21 @@
+from ._step import Step
+
+
+class Given(Step):
+    pass
+
+
+class When(Step):
+    pass
+
+
+class Then(Step):
+    pass
+
+
+given = Given()
+when = When()
+then = Then()
+
+__all__ = ("given", "when", "then", "Step",)
+__version__ = "1.3.1"
---
+++ packages/baby-steps/baby_steps-1.3.1-py3-none-any.whl/baby_steps/_step.py
@@ -0,0 +1,57 @@
+from types import GeneratorType, TracebackType
+from typing import Any, List, Optional, Type, Union
+
+from .hooks import _hooks
+
+__all__ = ("Step",)
+
+
+class Step:
+    def __init__(self) -> None:
+        self._name: Union[str, None] = None
+        self._stack: List[GeneratorType[Any, None, None]] = []
+
+    def __enter__(self) -> None:
+        for hook in _hooks:
+            maybe_gen = hook(self.__class__, self._name)
+            if isinstance(maybe_gen, GeneratorType):
+                next(maybe_gen)
+                self._stack.append(maybe_gen)
+
+    async def __aenter__(self) -> None:
+        return self.__enter__()
+
+    def __exit__(self,
+                 exc_type: Optional[Type[BaseException]],
+                 exc_val: Optional[BaseException],
+                 exc_tb: Optional[TracebackType]) -> bool:
+        while len(self._stack) > 0:
+            gen = self._stack.pop()
+            if exc_type is None:
+                try:
+                    next(gen)
+                except StopIteration:
+                    pass
+            else:
+                try:
+                    gen.throw(exc_type, exc_val, exc_tb)
+                except StopIteration:
+                    pass
+                except BaseException as e:
+                    if e is not exc_val:
+                        raise
+
+        self._name = None
+        self._stack = []
+
+        return exc_type is None
+
+    async def __aexit__(self,
+                        exc_type: Optional[Type[BaseException]],
+                        exc_val: Optional[BaseException],
+                        exc_tb: Optional[TracebackType]) -> bool:
+        return self.__exit__(exc_type, exc_val, exc_tb)
+
+    def __call__(self, name: str) -> "Step":
+        self._name = name
+        return self
