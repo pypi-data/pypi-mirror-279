ea62074a906fd688503e00a502bad4ac98372847
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-17 14:04:56 +0000

    Add package libkers/0.0.2.2/libkers-0.0.2.2.tar.gz

---
+++ packages/libkers/libkers-0.0.2.2.tar.gz/libkers-0.0.2.2/libkers/__init__.py
@@ -0,0 +1,1 @@
+from libkers.slagroom import Slagroom
---
+++ packages/libkers/libkers-0.0.2.2.tar.gz/libkers-0.0.2.2/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, 'README.md'), encoding='utf-8') as fh:
+    long_description = '\n' + fh.read()
+
+VERSION = '0.0.2.2'
+DESCRIPTION = 'Enumerating networks and finding optimal pathways'
+
+# Setting up
+setup(
+    name='libkers',
+    version=VERSION,
+    author='a-ware (Project A-ware)',
+    author_email='<all@stickybits.red>',
+    description=DESCRIPTION,
+    long_description_content_type='text/markdown',
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=['requests', 'python3-nmap', 'paramiko', 'bs4', 'scp', 'tqdm'],
+    keywords=['python', 'enumeration', 'reconnaissance', 'c2', 'breach-simulation', 'network-analysis'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+        'Operating System :: Unix',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+    ]
+)
