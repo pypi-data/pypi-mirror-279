51b9cfb0cccec66c8f297458e1bddaff6705ad04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-17 05:04:08 +0000

    Add package spermix95/3.0/spermix95-3.0.tar.gz

---
+++ packages/spermix95/spermix95-3.0.tar.gz/spermix95-3.0/setup.py
@@ -0,0 +1,28 @@
+
+
+from setuptools import setup, find_namespace_packages
+import os
+
+# Ensure that every directory has an __init__.py file
+for root, dirs, files in os.walk('src/smper/files'):
+    for dir in dirs:
+        init_file = os.path.join(root, dir, '__init__.py')
+        if not os.path.exists(init_file):
+            with open(init_file, 'w') as f:
+                pass
+
+setup(
+    name='spermix95',
+    version='3.0',
+    url='https://github.com/parlorsky/sempaiper',
+    license='MIT',
+    author='Levap Vobayr',
+    author_email='tffriend015@gmail.com',
+    description='',
+    packages=find_namespace_packages(where="src"),
+    package_dir={"": "src"},
+    package_data={
+        "smper.files": ["**/*.jpg"],
+    },
+    zip_safe=False
+)
