54e094e16a3af087fb7b99290abbfeafe1b92eb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-17 15:05:46 +0000

    Add package authivate/0.2.0/authivate-0.2.0.tar.gz

---
+++ packages/authivate/authivate-0.2.0.tar.gz/authivate-0.2.0/README.md
@@ -0,0 +1,54 @@
+## Authivate (Python SDK)
+
+This is the official python package for the [Authivate](https://authivate.com) service.
+
+Authivate is an easy-to-use User Authentication and Management service.
+
+### Install
+To Install, using pip
+```sh
+pip install authivate
+```
+
+Using poetry
+```shell
+poetry add authivate
+```
+
+### Example Usage
+
+```python
+from authivate import  Authivate, AuthivateConfig
+from pprint import pprint
+
+def add_user_to_waitlist(authivate: Authivate):
+    # Adds a user to the waitlist
+    response = authivate.add_user_to_waitlist(
+        email_address="user@example.com"
+    )
+    if response.was_successful:
+        print("User added to waitlist")
+        pprint(response.json_data)
+    else:
+        print(f"Error: {response.status_code} - {response.json_data}")
+
+
+# Initialize AuthivateConfig
+authivate_config = AuthivateConfig(api_key="your-api-key", project_id="project-id")
+
+
+# Create an instance of Authivate
+authivate_instance = Authivate(config=authivate_config)
+
+# Adds a user to the waitlist
+'''Response
+{'message': 'Yah!, you are now on the waitlist for {Project name}. Please confirm your email to seal your spot'}
+ '''
+add_user_to_waitlist(authivate_instance)
+
+### PS
+If you have used authivate before now June 17, 2024, what you think of authivate is definitely different from what it is now.
+it was completely rewritten to support only waitlist collection.
+
+Thanks,
+Peter.
---
+++ packages/authivate/authivate-0.2.0.tar.gz/authivate-0.2.0/authivate/src/authivate.py
@@ -0,0 +1,59 @@
+from pprint import pprint
+
+import requests
+
+from utils.authivate_config import AuthivateConfig
+from utils.authivate_response import AuthivateResponse
+
+
+
+
+class Authivate:
+    def __init__(self, config: AuthivateConfig):
+        self.config = config
+        self._headers = {"Authorization": f"Bearer {config.api_key}"}
+        self.client = requests.Session()
+
+    def post_request(self, uri, body):
+        try:
+            response = self.client.post(uri, headers=self._headers, json=body)
+            response.raise_for_status()
+            return AuthivateResponse(response.status_code, response.json())
+        except requests.exceptions.HTTPError as err:
+            status_code = err.response.status_code
+            json_data = (
+                err.response.json()
+                if "application/json" in err.response.headers.get("content-type", "")
+                else None
+            )
+            return AuthivateResponse(status_code, json_data)
+        except Exception as e:
+            return AuthivateResponse(500, {"error": str(e)})
+
+    def add_user_to_waitlist(self, **kwargs):
+        url = f"api/v1/p/project/{self.config.project_id}/user_records/"
+        body = {
+            **kwargs,
+        }
+        uri = f"https://{self.config.host}/{url}"
+        response = self.post_request(uri, body)
+        return response
+
+
+# Example Usage
+if __name__ == "__main__":
+
+    # Initialize AuthivateConfig
+    authivate_config = AuthivateConfig(api_key="your-api-key", project_id="project-id")
+
+    # Create an instance of Authivate
+    authivate_instance = Authivate(config=authivate_config)
+
+    # Add user to waitlist
+    '''Response
+    {'message': 'Yah!, you are now on the waitlist for {project name}. Please confirm your email to seal your spot'}
+    '''
+
+    authivate_instance.add_user_to_waitlist(authivate_instance)
+
+
