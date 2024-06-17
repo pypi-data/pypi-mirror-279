3ae4c24217474ea291cf0e61b724bc624a708370
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-17 16:04:35 +0000

    Add package u-copsapi/0.2.1/u_copsapi-0.2.1-py3-none-any.whl

---
+++ packages/u-copsapi/u_copsapi-0.2.1-py3-none-any.whl/copspy/api_urls.py
@@ -0,0 +1,17 @@
+"""
+Basic API url endpoints.
+
+"""
+
+import requests
+
+
+
+
+base_api_uri = f"https://api-cops.criticalforce.fi/api/public"
+
+username_api_uri = f"https://api-cops.criticalforce.fi/api/public/profile?usernames="
+
+id_api_url  = f"https://api-cops.criticalforce.fi/api/public/profile?ids="
+
+servers_api_url = f"https://api-cops.criticalforce.fi/api/public/status/servers"
