a18b89d49fa503518a443037db084cc6a8a85e36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-17 03:05:29 +0000

    Add package scratchcommunication/2.10.6/scratchcommunication-2.10.6.tar.gz

---
+++ packages/scratchcommunication/scratchcommunication-2.10.6.tar.gz/scratchcommunication-2.10.6/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+
+with open("README.md", encoding="utf-8") as f:
+    long_description = f.read()
+    
+VERSION = '2.10.6'
+
+setup(
+    name='scratchcommunication',
+    version=VERSION,
+    author='Simon Gilde',
+    author_email='simon.c.gilde@gmail.com',
+    description='A python module for communicating with scratch projects',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/thecommcraft/scratchcommunication',
+    packages=find_packages(exclude=[]),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    keywords=['scratch', 'api'],
+    install_requires=[
+        'requests',
+        'websocket-client',
+        'func-timeout',
+        'pycryptodome',
+        'attrs',
+        'browsercookie',
+        'cryptography'
+    ],
+    python_requires='>=3.11',
+)
