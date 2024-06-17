f35c1242e626d07d344d1a5847fde8f1db55219f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-17 16:04:27 +0000

    Add package petprep-extract-tacs/0.0.2/petprep_extract_tacs-0.0.2.tar.gz

---
+++ packages/petprep-extract-tacs/petprep_extract_tacs-0.0.2.tar.gz/petprep_extract_tacs-0.0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='petprep_extract_tacs',
+    version='0.0.2',
+    packages=find_packages(),
+    url='https://github.com/mnoergaard/petprep_extract_tacs',
+    author='Martin Norgaard',
+    author_email='martin.noergaard@di.ku.dk',
+    description='PETPrep Extraction of Time Activity Curves'
+)
