a7f583d111815dcf41c5c58643647d0b307a36fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-17 14:05:15 +0000

    Add package firstmodulejeremylaitter/0.0.2/firstmodulejeremylaitter-0.0.2.tar.gz

---
+++ packages/firstmodulejeremylaitter/firstmodulejeremylaitter-0.0.2.tar.gz/firstmodulejeremylaitter-0.0.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+VERSION= '0.0.2'
+DESCRIPTION = 'my first python package'
+LONG_DESCRIPTION = 'my first python package - long description'
+
+setup(
+        name="firstmodulejeremylaitter",
+        version=VERSION,
+        author="jeremy laitter",
+        author_email="jeremy.laitter@proton.me",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=[],
+
+        keywords=['python', 'mytag'],
+        classifiers= [
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 3",
+        ]
+)
+
