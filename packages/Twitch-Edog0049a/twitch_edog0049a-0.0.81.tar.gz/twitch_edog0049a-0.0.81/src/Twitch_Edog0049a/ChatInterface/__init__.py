308d881f01c888f59e5b152df45082ebf556e3b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-17 04:04:23 +0000

    Add package Twitch-Edog0049a/0.0.81/twitch_edog0049a-0.0.81.tar.gz

---
+++ packages/Twitch-Edog0049a/twitch_edog0049a-0.0.81.tar.gz/twitch_edog0049a-0.0.81/src/Twitch_Edog0049a/ChatInterface/__init__.py
@@ -0,0 +1,4 @@
+from src.EventHandler import EventHandler
+from Twitch_Edog0049a.ChatInterface.MessageHandler import Message as MessageType
+from Twitch_Edog0049a.ChatInterface.TwitchChatInterface import TCISettings ,TCI as Chat
+from Twitch_Edog0049a.ChatInterface import Exceptions
