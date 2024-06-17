2e22b0f7ade8d314a9719bcddbe65271403d9ace
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-17 15:04:59 +0000

    Add package hoymiles-wifi/0.2.2/hoymiles_wifi-0.2.2.tar.gz

---
+++ packages/hoymiles-wifi/hoymiles_wifi-0.2.2.tar.gz/hoymiles_wifi-0.2.2/setup.py
@@ -0,0 +1,18 @@
+"""Setup for the hoymiles-wifi package."""
+
+from setuptools import setup
+
+setup(
+    name="hoymiles-wifi",
+    packages=["hoymiles_wifi", "hoymiles_wifi.protobuf"],
+    install_requires=["protobuf", "crcmod"],
+    version="0.2.2",
+    description="A python library for interfacing with the Hoymiles DTUs and the HMS-XXXXW-2T series of micro-inverters using protobuf messages.",
+    author="suaveolent",
+    include_package_data=True,
+    entry_points={
+        "console_scripts": [
+            "hoymiles-wifi = hoymiles_wifi.__main__:run_main",
+        ],
+    },
+)
