f16c8127d69d91ee8ac51826fa90e3fbebd51e3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-17 04:04:34 +0000

    Add package bezzanlabs.treemachine/1.1.3/bezzanlabs_treemachine-1.1.3.tar.gz

---
+++ packages/bezzanlabs.treemachine/bezzanlabs_treemachine-1.1.3.tar.gz/bezzanlabs_treemachine-1.1.3/src/bezzanlabs/treemachine/__init__.py
@@ -0,0 +1,15 @@
+# flake8: noqa
+"""
+API entrypoint for the package.
+"""
+
+__package_name__ = "bezzanlabs.treemachine"
+__version__ = "1.1.3"
+
+from .auto_trees.base import BaseAuto
+from .auto_trees.classifier import Classifier
+from .auto_trees.regressor import Regressor
+from .deep_trees.base import BaseDeep
+from .deep_trees.classifier import DeepTreeClassifier
+from .deep_trees.regressor import DeepTreeRegressor
+from .splitter_proto import SplitterLike
