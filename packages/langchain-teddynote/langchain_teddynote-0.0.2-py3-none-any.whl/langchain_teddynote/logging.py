bcca0dc039bb76db8b5d328270ab993949669e48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-17 14:05:38 +0000

    Add package langchain-teddynote/0.0.2/langchain_teddynote-0.0.2-py3-none-any.whl

---
+++ packages/langchain-teddynote/langchain_teddynote-0.0.2-py3-none-any.whl/langchain_teddynote/logging.py
@@ -0,0 +1,13 @@
+import os
+
+
+def langsmith(project_name=None, set_enable=True):
+    if set_enable:
+        os.environ["LANGCHAIN_TRACING_V2"] = "true"  # true: 활성화
+        os.environ["LANGCHAIN_PROJECT"] = project_name  # 프로젝트명
+    else:
+        os.environ["LANGCHAIN_TRACING_V2"] = "false"  # false: 비활성화
+
+
+def env_variable(key, value):
+    os.environ[key] = value
