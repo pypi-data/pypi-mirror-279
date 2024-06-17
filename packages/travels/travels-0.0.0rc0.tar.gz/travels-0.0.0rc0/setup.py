07a8610ba9d317ae8bb0ded4a05fdac09b3f15a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-17 03:05:26 +0000

    Add package travels/0.0.0rc0/travels-0.0.0rc0.tar.gz

---
+++ packages/travels/travels-0.0.0rc0.tar.gz/travels-0.0.0rc0/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name="travels",
+    author="Nobody in particular",
+    author_email="jeff@bitprophet.org",
+    url="https://pypi.org/project/travels",
+    version="0.0.0-pre",
+    description="with Hynek?",
+    long_description="maybe?",
+    long_description_content_type="text/x-rst",
+    packages=[],
+    classifiers=[],
+)
