84c79a6c56dd52ca31589d6796c30a0fdd8d6723
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-17 15:04:46 +0000

    Add package fireducks/0.12.1/fireducks-0.12.1-cp38-cp38-manylinux_2_28_x86_64.whl

---
+++ packages/fireducks/fireducks-0.12.1-cp38-cp38-manylinux_2_28_x86_64.whl/fireducks/imhook.py
@@ -0,0 +1,29 @@
+# Copyright (c) 2023 NEC Corporation. All Rights Reserved.
+
+import os.path
+import runpy
+import sys
+
+from . import importhook
+from . import pandas as apandas
+
+# ignore this script itself
+importhook.ImportHook.ignore_names.append(os.path.abspath(__file__))
+
+
+def load_ipython_extension(ipython):
+    # cf. https://ipython.readthedocs.io/en/stable/config/extensions/
+    hook = importhook._get_current_hook()
+    if hook is not None:
+        raise RuntimeError(f"another import-hook is already active: {hook!r}")
+    importhook.activate_hook(apandas.__name__)
+
+
+def unload_ipython_extension(ipython):
+    # cf. https://ipython.readthedocs.io/en/stable/config/extensions/
+    importhook.deactivate_hook()
+
+
+if __name__ == "__main__":
+    sys.argv.insert(1, apandas.__name__)
+    runpy.run_module(importhook.__name__, run_name=__name__, alter_sys=True)
