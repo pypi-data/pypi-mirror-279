f8bfef9e8bf94800131f51df231567d8f52a1fa1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-17 05:04:02 +0000

    Add package seismic-rna/0.17.6/seismic_rna-0.17.6.tar.gz

---
+++ packages/seismic-rna/seismic_rna-0.17.6.tar.gz/seismic_rna-0.17.6/src/seismicrna/align/__init__.py
@@ -0,0 +1,49 @@
+"""
+
+Alignment Initialization Module
+
+========================================================================
+
+Expose cli, params, and run at the level of the `align` sub-package, so
+that they can be imported in any of the following manners:
+
+>>> import seismicrna
+>>> seismicrna.align.run
+
+or
+
+>>> from seismicrna import align
+>>> align.run
+
+or
+
+>>> from seismicrna.align import run
+>>> run
+
+------------------------------------------------------------------------
+
+"""
+
+from . import main, write, fqops
+from .main import cli, params, run
+
+########################################################################
+#                                                                      #
+# © Copyright 2024, the Rouskin Lab.                                   #
+#                                                                      #
+# This file is part of SEISMIC-RNA.                                    #
+#                                                                      #
+# SEISMIC-RNA is free software; you can redistribute it and/or modify  #
+# it under the terms of the GNU General Public License as published by #
+# the Free Software Foundation; either version 3 of the License, or    #
+# (at your option) any later version.                                  #
+#                                                                      #
+# SEISMIC-RNA is distributed in the hope that it will be useful, but   #
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANT- #
+# ABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General     #
+# Public License for more details.                                     #
+#                                                                      #
+# You should have received a copy of the GNU General Public License    #
+# along with SEISMIC-RNA; if not, see <https://www.gnu.org/licenses>.  #
+#                                                                      #
+########################################################################
---
+++ packages/seismic-rna/seismic_rna-0.17.6.tar.gz/seismic_rna-0.17.6/src/seismicrna/core/extern/depend.py
@@ -0,0 +1,39 @@
+from shutil import which
+
+
+def dependency_exists(dependency: str) -> bool:
+    """ Check whether a dependency exists. """
+    return which(dependency) is not None
+
+
+def require_dependency(dependency: str, module: str = ""):
+    """ If a dependency does not exist, return an error message. """
+    if dependency_exists(dependency):
+        # The dependency exists: no error message.
+        return ""
+    # The dependency is missing: error message.
+    by = f"by '{module}' " if module else ""
+    return (f"{repr(dependency)} is required {by}but was not found. "
+            f"Please install it (if not yet) and place the executable for "
+            f"{repr(dependency)} in your PATH.")
+
+########################################################################
+#                                                                      #
+# © Copyright 2024, the Rouskin Lab.                                   #
+#                                                                      #
+# This file is part of SEISMIC-RNA.                                    #
+#                                                                      #
+# SEISMIC-RNA is free software; you can redistribute it and/or modify  #
+# it under the terms of the GNU General Public License as published by #
+# the Free Software Foundation; either version 3 of the License, or    #
+# (at your option) any later version.                                  #
+#                                                                      #
+# SEISMIC-RNA is distributed in the hope that it will be useful, but   #
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANT- #
+# ABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General     #
+# Public License for more details.                                     #
+#                                                                      #
+# You should have received a copy of the GNU General Public License    #
+# along with SEISMIC-RNA; if not, see <https://www.gnu.org/licenses>.  #
+#                                                                      #
+########################################################################
