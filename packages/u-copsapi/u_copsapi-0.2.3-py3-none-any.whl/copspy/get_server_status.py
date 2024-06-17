0670d07c3b67bc59deb6ca87067999fc368cf376
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-17 16:04:34 +0000

    Add package u-copsapi/0.2.3/u_copsapi-0.2.3-py3-none-any.whl

---
+++ packages/u-copsapi/u_copsapi-0.2.3-py3-none-any.whl/copspy/get_server_status.py
@@ -0,0 +1,18 @@
+import requests
+from copspy import api_urls
+from requests.exceptions import ConnectionError as cnerr
+from copspy.apierror import NoNetwork, ResponseNotOK
+headers = {
+        "content-type": "application/json",
+    }
+
+def get_all():
+    """Get a list of all servers. Returns in json format."""
+    try:
+        json_servers = requests.get(api_urls.servers_api_url, headers=headers)
+        if json_servers.ok:
+            return json_servers.json()
+        else:
+            raise ResponseNotOK()
+    except cnerr:
+        raise NoNetwork()
