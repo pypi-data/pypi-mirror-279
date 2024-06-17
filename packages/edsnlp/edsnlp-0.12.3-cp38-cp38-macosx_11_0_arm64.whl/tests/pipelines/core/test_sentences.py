e3ef0e18854f156254b746ba6988d991045dae17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-17 16:04:32 +0000

    Add package edsnlp/0.12.3/edsnlp-0.12.3-cp38-cp38-macosx_11_0_arm64.whl

---
+++ packages/edsnlp/edsnlp-0.12.3-cp38-cp38-macosx_11_0_arm64.whl/tests/pipelines/core/test_sentences.py
@@ -0,0 +1,49 @@
+import spacy
+from pytest import mark
+from spacy.pipeline.sentencizer import Sentencizer
+
+from edsnlp.pipelines.core.sentences import SentenceSegmenter, terms
+
+text = (
+    "Le patient est admis pour des douleurs dans le bras droit. "
+    "mais n'a pas de problème de locomotion. \n"
+    "Historique d'AVC dans la famille\n"
+    "Mais ne semble pas en être un\n"
+    "Pourrait être un cas de rhume.\n"
+    "Motif :\n"
+    "Douleurs dans le bras droit."
+)
+
+
+@mark.parametrize("endlines", [True, False])
+def test_sentences(nlp, endlines):
+    nlp_blank = spacy.blank("fr")
+
+    sentencizer = Sentencizer()
+    segmenter = SentenceSegmenter(nlp, punct_chars=terms.punctuation, use_endlines=True)
+
+    doc = nlp(text)
+    doc_blank = nlp_blank(text)
+
+    if endlines:
+        doc_blank[28].tag_ = "EXCLUDED"
+
+    doc_blank = segmenter(doc_blank)
+
+    if endlines:
+        assert len(list(doc_blank.sents)) == 6
+    else:
+        assert len(list(doc_blank.sents)) == 7
+    assert len(list(sentencizer(doc).sents)) == 7
+
+    segmenter(nlp_blank(""))
+
+
+def test_false_positives(blank_nlp):
+    false_positives = [
+        "02.04.2018",
+    ]
+
+    for fp in false_positives:
+        doc = blank_nlp(fp)
+        assert len(list(doc.sents)) == 1
