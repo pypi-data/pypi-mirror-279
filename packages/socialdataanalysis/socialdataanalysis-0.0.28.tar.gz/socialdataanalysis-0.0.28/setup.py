9eff74018a3217cc3b49945abfe4dde4ffb0b8ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-17 14:05:17 +0000

    Add package socialdataanalysis/0.0.28/socialdataanalysis-0.0.28.tar.gz

---
+++ packages/socialdataanalysis/socialdataanalysis-0.0.28.tar.gz/socialdataanalysis-0.0.28/setup.py
@@ -0,0 +1,46 @@
+from setuptools import setup, find_packages
+
+#import glob
+
+# Listando todos os arquivos .ipynb na pasta notebooks
+#notebooks_files = glob.glob('notebooks/*.ipynb')
+
+setup(
+    name='socialdataanalysis',
+    version='0.0.28',
+    packages=find_packages(),
+    #include_package_data=True,
+    #package_data={
+    #    'socialdataanalysis': ['notebooks/*.ipynb'],
+    #},
+    #data_files=[
+    #    ('share/socialdataanalysis', notebooks_files),
+    #],
+    install_requires=[
+        'pandas',
+        'numpy',
+        'statsmodels',
+        'scipy',
+        'matplotlib',
+        'sympy',
+        'prince',
+        'altair',
+        'requests',
+        'tempfile',
+        'os',
+        'pyreadstat',
+        'pingouin',
+    ],
+    author='Ricardo Mergulhao',
+    author_email='ricardomergulhao@gmail.com',
+    description='Funções personalizadas para análise de dados nas ciências sociais, complementando o uso do SPSS.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/rcmergulhao/socialdataanalysis',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
