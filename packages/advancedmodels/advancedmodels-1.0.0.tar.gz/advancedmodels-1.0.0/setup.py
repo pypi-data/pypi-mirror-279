5a43b7f54b3b2939122e37a7c36b7e7448bb456c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-17 04:04:55 +0000

    Add package advancedmodels/1.0.0/advancedmodels-1.0.0.tar.gz

---
+++ packages/advancedmodels/advancedmodels-1.0.0.tar.gz/advancedmodels-1.0.0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+def readme():
+  with open('README.md', 'r') as f:
+    return f.read()
+
+setup(
+  name='advancedmodels',
+  version='1.0.0',
+  author='pom',
+  author_email='',
+  description='This is my first module',
+  long_description=readme(),
+  long_description_content_type='text/markdown',
+  url='https://github.com/powerpom/advancedmodels/archive/master.zip',
+  download_url='https://github.com/powerpom/advancedmodels/archive/master.zip',
+  packages=find_packages(),
+  install_requires=['requests>=2.25.1'],
+  classifiers=[
+    'Programming Language :: Python :: 3.11',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent'
+  ],
+  keywords='',
+  project_urls={
+    'Documentation': 'https://github.com/powerpom/advancedmodels'
+  },
+  python_requires='>=3.7'
+)
