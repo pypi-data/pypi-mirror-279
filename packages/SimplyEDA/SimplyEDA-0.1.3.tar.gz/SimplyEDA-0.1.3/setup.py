3ba5ce4bba5e1b44cea801801554f9217fa2e3d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-17 03:04:09 +0000

    Add package SimplyEDA/0.1.3/SimplyEDA-0.1.3.tar.gz

---
+++ packages/SimplyEDA/SimplyEDA-0.1.3.tar.gz/SimplyEDA-0.1.3/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+import pathlib
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+setup(
+    name='SimplyEDA',  # Ensure this name is unique on PyPI
+    version='0.1.3',
+    description='A simple library for exploratory data analysis',
+    long_description=README,
+    long_description_content_type='text/markdown',
+    url='https://github.com/kitranet/SimpleEDA',  # This URL remains the same
+    author='M.R.Vijay Krishnan',
+    author_email='vijaykrishnanmr@gmail.com',
+    py_modules=['SimplyEDA'],  # Specify the module here
+    install_requires=[
+        'seaborn',
+        'matplotlib',
+        'numpy',
+        'pandas',
+        'statsmodels'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    keywords='EDA data-analysis',
+    python_requires='>=3.6',
+)
