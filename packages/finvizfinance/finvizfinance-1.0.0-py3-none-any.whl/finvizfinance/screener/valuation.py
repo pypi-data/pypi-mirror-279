ed92b09ad60bb3e04d826689cb24739417c06420
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-17 15:05:15 +0000

    Add package finvizfinance/1.0.0/finvizfinance-1.0.0-py3-none-any.whl

---
+++ packages/finvizfinance/finvizfinance-1.0.0-py3-none-any.whl/finvizfinance/screener/valuation.py
@@ -0,0 +1,16 @@
+"""
+.. module:: screen.valuation
+   :synopsis: screen valuation table.
+
+.. moduleauthor:: Tianning Li <ltianningli@gmail.com>
+"""
+
+from finvizfinance.screener.base import Base
+
+
+class Valuation(Base):
+    """Valuation
+    Getting information from the finviz screener valuation page.
+    """
+
+    v_page = 121
