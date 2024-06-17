b4456b067b5c7defe6d313f19ac2cc824ecd2b60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-17 03:04:51 +0000

    Add package win32material/1.0.4/win32material-1.0.4.tar.gz

---
+++ packages/win32material/win32material-1.0.4.tar.gz/win32material-1.0.4/setup.py
@@ -0,0 +1,24 @@
+"""Setup for win32style"""
+from distutils.core import setup
+
+with open("README.md", "r") as file:
+    long_description = file.read()
+
+setup(
+    name="win32material",
+    version="1.0.4",
+    description="Apply some window effects to the Win32 Applications",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="littlewhitecloud",
+    exclude_package_data={
+        "": ["win32style.def"],
+        "": ["win32style.h"],
+        "": ["win32style.cpp"],
+    },
+    package_data={
+        "": ["*.dll"],
+    },
+    url="https://github.com/littlewhitecloud/win32material",
+    packages=["win32style"],
+)
