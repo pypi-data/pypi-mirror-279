c18b340d39fd5d423baacd3fce617d5bbdf07957
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-17 14:05:12 +0000

    Add package unismod/1.1/unismod-1.1.tar.gz

---
+++ packages/unismod/unismod-1.1.tar.gz/unismod-1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+ 
+setup(
+   name='unismod',
+   version='1.1',
+   author='Uni',
+   packages=['unismod'],
+   install_requires=['numpy', 'pandas', 'scipy', 'statsmodels']
+)
