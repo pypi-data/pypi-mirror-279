facc1a013fa62397755051f81918fa65a87c2bb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-17 04:04:14 +0000

    Add package nonebot-plugin-mcsm/0.1.5/nonebot_plugin_mcsm-0.1.5-py3-none-any.whl

---
+++ packages/nonebot-plugin-mcsm/nonebot_plugin_mcsm-0.1.5-py3-none-any.whl/nonebot_plugin_mcsm/__init__.py
@@ -0,0 +1,13 @@
+from nonebot.plugin import PluginMetadata
+from . import __main__ as __main__
+from .config import Config
+
+__plugin_meta__ = PluginMetadata(
+    name="MCSM小助手",
+    description="对接MCSM，可查看面板、节点、实例信息以及管理实例",
+    usage="私聊或群里发送消息",
+    type="application",
+    homepage="https://github.com/LiLuo-B/nonebot-plugin-mcsm",
+    config=Config,
+    supported_adapters={"~onebot.v11"},
+)
