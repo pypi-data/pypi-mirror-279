95a3c5142a262a09a884c42d5bccabaf770e101b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-17 04:04:20 +0000

    Add package tuparser/0.1.5.1/tuparser-0.1.5.1-py3-none-any.whl

---
+++ packages/tuparser/tuparser-0.1.5.1-py3-none-any.whl/tuparser/constants.py
@@ -0,0 +1,8 @@
+from .utils import get_time_now
+
+__all__ = ["LAUNCH_TIME", "TELEGRAPH_URL", "TELEGRAPH_URL_MIRROR"]
+
+LAUNCH_TIME = get_time_now()
+
+TELEGRAPH_URL = "https://telegra.ph"
+TELEGRAPH_URL_MIRROR = "https://graph.org"
