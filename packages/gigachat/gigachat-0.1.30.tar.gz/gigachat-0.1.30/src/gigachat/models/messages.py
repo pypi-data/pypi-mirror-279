df5b90dc0a17584e8d8340dedd0257c7d2b5743a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-17 16:04:26 +0000

    Add package gigachat/0.1.30/gigachat-0.1.30.tar.gz

---
+++ packages/gigachat/gigachat-0.1.30.tar.gz/gigachat-0.1.30/src/gigachat/models/messages.py
@@ -0,0 +1,26 @@
+from typing import Any, List, Optional
+
+from gigachat.models.function_call import FunctionCall
+from gigachat.models.messages_role import MessagesRole
+from gigachat.pydantic_v1 import BaseModel, Field
+
+
+class Messages(BaseModel):
+    """Сообщение"""
+
+    role: MessagesRole
+    """Роль автора сообщения"""
+    content: str = ""
+    """Текст сообщения"""
+    function_call: Optional[FunctionCall] = None
+    """Вызов функции"""
+    name: Optional[str] = None
+    """Наименование функции. Заполняется, если role = "function" """
+    attachments: Optional[List[str]] = None
+    """Идентификаторы предзагруженных ранее файлов """
+    data_for_context: Optional[List["Messages"]] = None
+    """Данные для контекста"""
+    id_: Optional[Any] = Field(alias="id", default=None)
+
+    class Config:
+        use_enum_values = True
