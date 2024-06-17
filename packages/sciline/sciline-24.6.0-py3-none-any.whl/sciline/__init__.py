3c7d52a140b010ead5d44e2dcf765b83f053e4d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-17 14:04:24 +0000

    Add package sciline/24.6.0/sciline-24.6.0-py3-none-any.whl

---
+++ packages/sciline/sciline-24.6.0-py3-none-any.whl/sciline/__init__.py
@@ -0,0 +1,35 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright (c) 2024 Scipp contributors (https://github.com/scipp)
+# ruff: noqa: E402, F401
+
+import importlib.metadata
+
+try:
+    __version__ = importlib.metadata.version(__package__ or __name__)
+except importlib.metadata.PackageNotFoundError:
+    __version__ = "0.0.0"
+
+from . import scheduler
+from ._provider import UnboundTypeVar
+from .domain import Scope, ScopeTwoParams
+from .handler import (
+    HandleAsBuildTimeException,
+    HandleAsComputeTimeException,
+    UnsatisfiedRequirement,
+)
+from .pipeline import Pipeline
+from .task_graph import TaskGraph
+
+__all__ = [
+    "Pipeline",
+    "scheduler",
+    "Scope",
+    "ScopeTwoParams",
+    'TaskGraph',
+    "UnboundTypeVar",
+    "UnsatisfiedRequirement",
+    "HandleAsBuildTimeException",
+    "HandleAsComputeTimeException",
+]
+
+del importlib
