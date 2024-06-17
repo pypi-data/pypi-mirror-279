70f8b5bebe503d1afcb5102d952e6582782b3ef0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-17 03:05:22 +0000

    Add package sharepay/0.2.1/sharepay-0.2.1-py3-none-any.whl

---
+++ packages/sharepay/sharepay-0.2.1-py3-none-any.whl/sharepay/rate.py
@@ -0,0 +1,50 @@
+from __future__ import annotations
+
+from datetime import datetime
+from functools import cache
+
+import requests
+from pydantic import BaseModel
+from pydantic import field_validator
+from requests.utils import default_headers
+
+
+class Rate(BaseModel):
+    source: str
+    target: str
+    value: float
+    time: datetime
+
+    @field_validator("time", mode="before")
+    @classmethod
+    def convert_int(cls, v: datetime | int) -> datetime:
+        if isinstance(v, int):
+            return datetime.fromtimestamp(v // 1000)
+
+        return v
+
+
+class RateRequest(BaseModel):
+    source: str
+    target: str
+
+    def do(self) -> Rate:
+        resp = requests.get(
+            "https://wise.com/rates/live",
+            params=self.model_dump(),
+            headers=default_headers(),
+            timeout=10,
+        )
+        return Rate(**resp.json())
+
+
+@cache
+def query_rate(source: str, target: str) -> float:
+    source = source.upper()
+    target = target.upper()
+
+    if source == target:
+        return 1.0
+
+    rate = RateRequest(source=source, target=target).do()
+    return rate.value
