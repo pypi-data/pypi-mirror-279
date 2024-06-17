6ca860d26feb5a7ec1bbbbe54b1d8716c146bc83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-17 03:04:09 +0000

    Add package ognibuild/0.0.23/ognibuild-0.0.23-cp312-cp312-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/ognibuild/ognibuild-0.0.23-cp312-cp312-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/ognibuild/_ognibuild_rs.pyi
@@ -0,0 +1,24 @@
+from typing import Callable, TypeVar
+
+from breezy.tree import Tree
+
+from .fix_build import BuildFixer
+
+DEFAULT_LIMIT = 200
+
+def sanitize_session_name(name: str) -> str: ...
+def generate_session_id(name: str) -> str: ...
+def export_vcs_tree(
+    tree: Tree, path: str, subpath: str | None = None
+) -> None: ...
+def resolve_error(error, phase, fixers) -> bool: ...
+
+T = TypeVar("T")
+
+def iterate_with_build_fixers(
+    fixers: list[BuildFixer],
+    phase: list[str],
+    cb: Callable[[], T], limit=DEFAULT_LIMIT
+) -> T: ...
+
+def dupe_vcs_tree(tree: Tree, directory: str): ...
