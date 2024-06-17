f9acec7882deb5e40f0bb9fa76ea5f7d5762c3be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-17 03:04:25 +0000

    Add package rs-ncmpy/0.1.0/rs_ncmpy-0.1.0-cp38-abi3-win_amd64.whl

---
+++ packages/rs-ncmpy/rs_ncmpy-0.1.0-cp38-abi3-win_amd64.whl/rs_ncmpy/__init__.py
@@ -0,0 +1,5 @@
+from .rs_ncmpy import *
+
+__doc__ = rs_ncmpy.__doc__
+if hasattr(rs_ncmpy, "__all__"):
+    __all__ = rs_ncmpy.__all__
---
+++ packages/rs-ncmpy/rs_ncmpy-0.1.0-cp38-abi3-win_amd64.whl/rs_ncmpy/rs_ncmpy.pyi
@@ -0,0 +1,10 @@
+def sum_as_string(a: int, b: int) -> str:
+    """Sum two integers and return by str.
+
+    :param a:
+    :param b:
+    :return:
+    """
+    ...
+
+def guess_the_number() -> None: ...
