554280e81a5b8690afe9461dae0d121271534b0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-17 14:05:42 +0000

    Add package getallcolumnname/0.1/getallcolumnname-0.1.tar.gz

---
+++ packages/getallcolumnname/getallcolumnname-0.1.tar.gz/getallcolumnname-0.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.1'
+
+# Read the contents of your README file
+with open("README.txt", "r") as fh:
+    long_description = fh.read()
+
+# Setting up
+setup(
+    name='getallcolumnname',
+    version=VERSION,
+    description='getallcolumns',
+    long_description=long_description,
+    long_description_content_type='text/plain',  # Specify the format of the long description
+    author='srini',
+    packages=find_packages(),  # Automatically find packages
+    install_requires=[],
+    keywords=['python', 'pyspark',"catalog","schema"],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows"
+    ]
+)
