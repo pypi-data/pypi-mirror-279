27a90ca233e55e4d75d717650c1438fe6fe60455
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-18 04:04:16 +0000

    Add package pytximport/0.4.0/pytximport-0.4.0.tar.gz

---
+++ packages/pytximport/pytximport-0.4.0.tar.gz/pytximport-0.4.0/docs/source/index.rst
@@ -0,0 +1,37 @@
+Welcome to the `pytximport` documentation!
+==========================================
+
+.. meta::
+   :google-site-verification: WSst_i-1TKo-4rHXYKM6UNvsiQYj_rTLFDoI1iVE4yY
+
+.. toctree::
+   :caption: Start
+   :maxdepth: 4
+   :glob:
+
+   start
+   installation
+   example
+   releases
+
+.. toctree::
+   :caption: API Documentation
+   :maxdepth: 4
+   :glob:
+
+   autoapi/pytximport/index
+
+.. toctree::
+   :caption: Additional Information
+   :maxdepth: 2
+
+   legal
+   privacy
+   license
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
---
+++ packages/pytximport/pytximport-0.4.0.tar.gz/pytximport-0.4.0/test/test_correctness.py
@@ -0,0 +1,119 @@
+"""Test importing salmon quantification files."""
+
+from pathlib import Path
+from typing import List
+
+import pandas as pd
+import xarray as xr
+
+from pytximport import tximport
+
+
+def test_correctness(
+    fabry_disease_files: List[Path],
+) -> None:
+    """Test importing salmon quantification files.
+
+    Args:
+        salmon_multiple_files (Path): [description]
+    """
+    fabry_directory = fabry_disease_files[0].parent
+
+    for counts_from_abundance in [None, "scaled_tpm", "length_scaled_tpm"]:
+        result = tximport(
+            fabry_disease_files,
+            "salmon",
+            fabry_directory / "transcript_gene_mapping_human.csv",
+            ignore_transcript_version=True,
+            ignore_after_bar=True,
+            output_type="xarray",
+            counts_from_abundance=counts_from_abundance,  # type: ignore
+        )
+
+        assert isinstance(result, xr.Dataset), "The result is not an xarray Dataset."
+
+        df_result = pd.DataFrame(
+            result["counts"].data,
+            index=result.coords["gene_id"],
+            columns=result.coords["file_path"],
+        ).sort_index()
+
+        # load in the comparison data generated with the R package tximport
+        if counts_from_abundance is None:
+            df_result_tximport = pd.read_csv(
+                fabry_directory / "counts_tximport_no.csv",
+                index_col=0,
+                header=0,
+            )
+        elif counts_from_abundance == "scaled_tpm":
+            df_result_tximport = pd.read_csv(
+                fabry_directory / "counts_tximport_scaledTPM.csv",
+                index_col=0,
+                header=0,
+            )
+        elif counts_from_abundance == "length_scaled_tpm":
+            df_result_tximport = pd.read_csv(
+                fabry_directory / "counts_tximport_lengthScaledTPM.csv",
+                index_col=0,
+                header=0,
+            )
+
+        # since tximport does not use the file path as column, use the columns from the comparison data
+        df_result.columns = df_result_tximport.columns
+
+        # check that the data is the same
+        pd.testing.assert_frame_equal(df_result, df_result_tximport)
+
+
+def test_correctness_transcript_level(
+    salmon_file: Path,
+) -> None:
+    """Test that the transcript level data is correct.
+
+    Args:
+        salmon_file (Path): Path to the salmon quantification file.
+    """
+    data_directory = salmon_file.parent.parent
+
+    for counts_from_abundance in ["scaled_tpm", "dtu_scaled_tpm"]:
+        result = tximport(
+            [salmon_file],
+            "salmon",
+            data_directory / "fabry_disease" / "transcript_gene_mapping_human.csv",
+            return_transcript_data=True,
+            ignore_transcript_version=True,
+            ignore_after_bar=True,
+            output_type="xarray",
+            counts_from_abundance=counts_from_abundance,  # type: ignore
+        )
+
+        assert isinstance(result, xr.Dataset), "The result is not an xarray Dataset."
+
+        df_result = pd.DataFrame(
+            result["counts"].data,
+            index=result.coords["transcript_id"],
+            columns=result.coords["file_path"],
+        ).sort_index()
+
+        # load in the comparison data generated with the R package tximport
+        if counts_from_abundance == "dtu_scaled_tpm":
+            df_result_tximport = pd.read_csv(
+                data_directory / "salmon" / "counts_tximport_dtuScaledTPM.csv",
+                index_col=0,
+                header=0,
+            ).sort_index()
+        elif counts_from_abundance == "scaled_tpm":
+            df_result_tximport = pd.read_csv(
+                data_directory / "salmon" / "counts_tximport_scaledTPM.csv",
+                index_col=0,
+                header=0,
+            ).sort_index()
+
+        # remove the transcript version from the index since tximport does not remove it
+        df_result_tximport.index = df_result_tximport.index.str.split(".").str[0]
+
+        # since tximport does not use the file path as column, use the columns from the comparison data
+        df_result.columns = df_result_tximport.columns
+
+        # check that the data is the same
+        pd.testing.assert_frame_equal(df_result, df_result_tximport)
