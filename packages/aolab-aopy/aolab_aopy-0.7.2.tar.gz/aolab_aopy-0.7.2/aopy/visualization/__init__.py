ed8a4648a71a9a2e240fb61a53adbe4734109660
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-18 14:05:15 +0000

    Add package aolab-aopy/0.7.2/aolab_aopy-0.7.2.tar.gz

---
+++ packages/aolab-aopy/aolab_aopy-0.7.2.tar.gz/aolab_aopy-0.7.2/aopy/visualization/__init__.py
@@ -0,0 +1,7 @@
+# For legacy code, include these modules in the top-level analysis module
+# since they existed in base before becoming sub-modules
+from .base import *
+from .animation import *
+
+# For new submodules, import just the namespace
+from . import eye
