6e8a70539eb237e8fcbeaf77e9d927bd556624f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-18 03:04:44 +0000

    Add package operacionescursopythonjjfr/0.1/operacionescursopythonjjfr-0.1.tar.gz

---
+++ packages/operacionescursopythonjjfr/operacionescursopythonjjfr-0.1.tar.gz/operacionescursopythonjjfr-0.1/setup.py
@@ -0,0 +1,29 @@
+from distutils.core import setup
+
+setup(
+    name="operacionescursopythonjjfr",
+    packages=["ejercicio13"],
+    version="0.1",
+    license="MIT",
+    description="Libreria de mates",
+    author="JJFR",
+    author_email="josejuanfloresraposo@gmail.com",
+    keywords=[
+        "operaciones",
+        "suma",
+        "resta",
+        "multiplicaciones",
+        "divisiones",
+        "ejemplo",
+    ],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+    ],
+)
