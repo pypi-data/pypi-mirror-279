0cc593c91c38c4d77e7a65eb41a732c05c8f3562
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-18 05:05:28 +0000

    Add package gbizinfo-lod/0.1.1/gbizinfo_lod-0.1.1-py3-none-any.whl

---
+++ packages/gbizinfo-lod/gbizinfo_lod-0.1.1-py3-none-any.whl/gbizinfo_lod/mappers/todokede.py
@@ -0,0 +1,24 @@
+from rdflib import Literal, URIRef
+
+from ..namespace import *
+from . import _TripleMapType, bpo
+from ._katsudo import GbizInfoKatsudoMapper
+
+
+class GbizInfoTodokedeMapper(GbizInfoKatsudoMapper):
+    """届出認定情報"""
+
+    @staticmethod
+    def map_to_triples(row: dict[str, str]) -> list[_TripleMapType]:
+        ss = HJ_EXT[f"{row['ID-識別値']}_{row['キー情報']}"]
+
+        triples = GbizInfoKatsudoMapper.map_to_triples(row)
+        triples.extend(
+            [
+                (ss, RDF.type, HJ.届出認定型),
+            ]
+        )
+        return triples
+
+
+__all__ = ["GbizInfoTodokedeMapper"]
