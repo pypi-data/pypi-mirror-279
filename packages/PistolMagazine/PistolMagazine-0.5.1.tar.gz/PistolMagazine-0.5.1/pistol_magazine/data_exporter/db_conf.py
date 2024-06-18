72a2fcdbe897c4138a3bea4569fae4ea923e0b2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-18 16:05:07 +0000

    Add package PistolMagazine/0.5.1/PistolMagazine-0.5.1.tar.gz

---
+++ packages/PistolMagazine/PistolMagazine-0.5.1.tar.gz/PistolMagazine-0.5.1/pistol_magazine/data_exporter/db_conf.py
@@ -0,0 +1,9 @@
+def get_db_config():
+    # Update with your actual database connection details
+    return {
+        'user': 'USER',
+        'password': 'PWD',
+        'host': 'HOST',
+        'port': 0000,
+        'database': 'DB'
+    }
