656d6ba783c1157de8248de59f838948e6a16a8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-18 16:05:14 +0000

    Add package samoosa/6.0/samoosa-6.0.tar.gz

---
+++ packages/samoosa/samoosa-6.0.tar.gz/samoosa-6.0/samoosa/summary_ai.py
@@ -0,0 +1,13 @@
+import google.generativeai as genai
+
+
+def get_api():
+    with open("google-api.txt", r) as file:
+        return file.read()
+
+def summarise(data):
+    key = get_api()
+    model = genai.GenerativeModel('gemini-pro')
+    genai.configure(api_key=key)
+    response = model.generate_content("Summarise the content: "+ data)
+    return response.text
