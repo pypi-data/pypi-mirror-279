76c50465c3517808dd47d11194bd080756d2af54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-18 16:05:03 +0000

    Add package st-span-annotation/0.0.8/st_span_annotation-0.0.8-py3-none-any.whl

---
+++ packages/st-span-annotation/st_span_annotation-0.0.8-py3-none-any.whl/st_span_annotation/__init__.py
@@ -0,0 +1,41 @@
+import os
+from typing import TypedDict
+import streamlit.components.v1 as components
+
+_RELEASE = True
+
+if not _RELEASE:
+    _component_func = components.declare_component(
+        "st_span_annotation",
+        url="http://localhost:3001",
+    )
+else:
+    parent_dir = os.path.dirname(os.path.abspath(__file__))
+    build_dir = os.path.join(parent_dir, "frontend/build")
+    _component_func = components.declare_component("st_span_annotation", path=build_dir)
+
+
+class Span(TypedDict):
+    start: int
+    end: int
+    label: str
+    text: str
+
+
+def st_span_annotation(
+    text: str,
+    labels: list[str],
+    spans: list[Span] = [],
+    color_palette: dict[str, str] = {},
+    is_editable: bool = True,
+    key: str | None = None,
+) -> Span:
+    component_value = _component_func(
+        text=text,
+        labels=labels,
+        spans=spans,
+        color_palette=color_palette,
+        is_editable=is_editable,
+        key=key,
+    )
+    return component_value
