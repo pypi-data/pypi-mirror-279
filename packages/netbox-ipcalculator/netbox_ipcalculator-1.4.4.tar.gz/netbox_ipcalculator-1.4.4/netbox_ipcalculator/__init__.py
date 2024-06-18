757ff680b22e1413e43473e572001544c45af384
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-18 04:03:55 +0000

    Add package netbox-ipcalculator/1.4.4/netbox_ipcalculator-1.4.4.tar.gz

---
+++ packages/netbox-ipcalculator/netbox_ipcalculator-1.4.4.tar.gz/netbox_ipcalculator-1.4.4/netbox_ipcalculator/__init__.py
@@ -0,0 +1,24 @@
+from netbox.settings import VERSION # type: ignore
+if VERSION.startswith("3."):
+    from extras.plugins import PluginConfig # type: ignore
+else:
+    from netbox.plugins import PluginConfig # type: ignore
+
+class IPCalcultorConfig(PluginConfig):
+    name = 'netbox_ipcalculator'
+    verbose_name = 'IP Calculator'
+    description = 'Netbox IP Calculator'
+    version = '1.4.4'
+    author = 'Pieter Lambrecht'
+    author_email = 'pieter.lambrecht@gmail.com'
+    base_url = 'netbox_ipcalculator'
+    min_version = '3.0'
+    required_settings = []
+    default_settings = {
+        'loud': False
+    }
+    caching_config = {
+        '*': None
+    }
+
+config = IPCalcultorConfig
---
+++ packages/netbox-ipcalculator/netbox_ipcalculator-1.4.4.tar.gz/netbox_ipcalculator-1.4.4/setup.py
@@ -0,0 +1,21 @@
+from setuptools import find_packages, setup
+
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name='netbox_ipcalculator',
+    version='1.4.4',
+    description='Netbox IP Calculator',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/PieterL75/netbox_ipcalculator',
+    author='Pieter Lambrecht',
+    license='Apache 2.0',
+    install_requires=[],
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+)
