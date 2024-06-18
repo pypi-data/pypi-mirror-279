2031c6ee7d9e0042c045ad7ce0688a7934a59c73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-18 14:04:44 +0000

    Add package starlyng-server-metrics/0.9.8/starlyng_server_metrics-0.9.8-py3-none-any.whl

---
+++ packages/starlyng-server-metrics/starlyng_server_metrics-0.9.8-py3-none-any.whl/server_metrics/utils/prom/machine.py
@@ -0,0 +1,25 @@
+"""
+machine.py
+"""
+
+from typing import Dict
+from server_metrics.utils.filter import filter_dict_by_keys
+
+def parse_machine_for_prom(hostname: str, machine: Dict[str, str]) -> str:
+    """
+    Args:
+        hostname (str):
+        machine (Dict[str, str]):
+
+    Returns:
+        str:
+    """
+
+    expected_keys = [
+        'aer_error_count',
+        'pending_updates_count',
+    ]
+
+    filtered_machine = filter_dict_by_keys(machine, expected_keys)
+
+    return '\n'.join(f'node_exporter_machine_{key}{{hostname="{hostname}"}} {value}' for key, value in filtered_machine.items())
