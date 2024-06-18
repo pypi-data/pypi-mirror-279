c669b1229675b1ad53e15dbe06d1e0c1ca3660db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-18 04:04:39 +0000

    Add package prefect/3.0.0rc3/prefect-3.0.0rc3.tar.gz

---
+++ packages/prefect/prefect-3.0.0rc3.tar.gz/prefect-3.0.0rc3/src/prefect/server/ui/assets/WorkPoolQueueCreate-24LLP6OF.js
@@ -0,0 +1,2 @@
+import{d as r,W as n,c as s,g as a,h as u,o as p,j as t,k as o,dg as m,dh as c}from"./index-Cjd-zuQH.js";import{u as _}from"./usePageTitle-BFRz6fNu.js";const P=r({__name:"WorkPoolQueueCreate",setup(k){const e=n("workPoolName");return _("Create Work Pool Queue"),(d,f)=>{const l=u("p-layout-default");return p(),s(l,null,{header:a(()=>[t(o(m),{"work-pool-name":o(e)},null,8,["work-pool-name"])]),default:a(()=>[t(o(c),{"work-pool-name":o(e)},null,8,["work-pool-name"])]),_:1})}}});export{P as default};
+//# sourceMappingURL=WorkPoolQueueCreate-24LLP6OF.js.map
---
+++ packages/prefect/prefect-3.0.0rc3.tar.gz/prefect-3.0.0rc3/src/prefect/server/ui/assets/usePrefectApi-eMMnrjGh.js
@@ -0,0 +1,2 @@
+import{cO as e,cP as r}from"./index-Cjd-zuQH.js";function c(){return e(r)}export{c as u};
+//# sourceMappingURL=usePrefectApi-eMMnrjGh.js.map
