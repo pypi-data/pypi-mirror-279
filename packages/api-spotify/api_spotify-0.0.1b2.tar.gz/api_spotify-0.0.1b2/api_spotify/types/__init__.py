3bb8ed4d8bd804285ed97179cdbbfa045504a9b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-18 04:04:27 +0000

    Add package api-spotify/0.0.1b2/api_spotify-0.0.1b2.tar.gz

---
+++ packages/api-spotify/api_spotify-0.0.1b2.tar.gz/api_spotify-0.0.1b2/api_spotify/types/__init__.py
@@ -0,0 +1,18 @@
+from .token import Token
+from .track import Track
+from .playlist import Playlist
+from .new_releases import New_Releases
+
+from .album import (
+	Album, Album_Tracks
+)
+
+
+__all__ = (
+	'Token',
+	'Track',
+	'Album',
+	'Playlist',
+	'Album_Tracks',
+	'New_Releases'
+)
