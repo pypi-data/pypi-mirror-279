ca6d588b4853f48866764b4aecbbf1135fe69db1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-18 05:05:06 +0000

    Add package modern-treasury/1.35.1/modern_treasury-1.35.1-py3-none-any.whl

---
+++ packages/modern-treasury/modern_treasury-1.35.1-py3-none-any.whl/modern_treasury/types/account_detail_create_params.py
@@ -0,0 +1,20 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from typing_extensions import Literal, Required, TypedDict
+
+__all__ = ["AccountDetailCreateParams"]
+
+
+class AccountDetailCreateParams(TypedDict, total=False):
+    accounts_type: Required[Literal["external_accounts"]]
+
+    account_number: Required[str]
+    """The account number for the bank account."""
+
+    account_number_type: Literal["clabe", "hk_number", "iban", "nz_number", "other", "pan", "wallet_address"]
+    """One of `iban`, `clabe`, `wallet_address`, or `other`.
+
+    Use `other` if the bank account number is in a generic format.
+    """
