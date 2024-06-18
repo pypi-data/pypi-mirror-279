81a0e4cf2654c39b3800f1dfdf578a88af3b067c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-18 06:05:44 +0000

    Add package time-split/0.4.0/time_split-0.4.0.tar.gz

---
+++ packages/time-split/time_split-0.4.0.tar.gz/time_split-0.4.0/src/time_split/_frontend/_split.py
@@ -0,0 +1,45 @@
+from .._backend import DatetimeIndexSplitter
+from .._docstrings import docs
+from ..types import DatetimeIterable, DatetimeSplits, ExpandLimits, Schedule, Span
+
+
+@docs
+def split(
+    schedule: Schedule,
+    *,
+    before: Span = "7d",
+    after: Span = 1,
+    step: int = 1,
+    n_splits: int = 0,
+    available: DatetimeIterable | None = None,
+    expand_limits: ExpandLimits = "auto",
+) -> DatetimeSplits:
+    """Create time-based cross-validation splits.
+
+    To visualize the folds, pass the same arguments to the :func:`.plot`-function.
+
+    Args:
+        schedule: {schedule}
+        before: {before}
+        after: {after}
+        step: {step}
+        n_splits: {n_splits}
+        available: {available} Passing a tuple ``(min, max)`` is enough.
+        expand_limits: {expand_limits}
+
+    {USER_GUIDE}
+
+    Returns:
+        A list of tuples ``[(start, mid, end), ...]``.
+
+    Examples:
+        .. minigallery:: time_split.split
+    """
+    return DatetimeIndexSplitter(
+        schedule,
+        before=before,
+        after=after,
+        step=step,
+        n_splits=n_splits,
+        expand_limits=expand_limits,
+    ).get_splits(available)
---
+++ packages/time-split/time_split-0.4.0.tar.gz/time_split-0.4.0/src/time_split/support/__init__.py
@@ -0,0 +1,24 @@
+"""Supporting functions.
+
+These functions are used internally, but are exposed here as well so that user may create their own logic using the
+internal logic, or just to test things out.
+
+.. warning::
+
+   Not part of the stable API.
+
+This module may change without notice. Stick to the top-level :mod:`time_split`-module, or lock down your
+dependencies if you need to use the ``support`` module.
+"""
+
+from .._backend import DatetimeIndexSplitter, expand_limits, process_available
+from .._frontend import default_metrics_formatter, fold_weight, to_string
+
+__all__ = [
+    "DatetimeIndexSplitter",
+    "expand_limits",
+    "process_available",
+    "default_metrics_formatter",
+    "fold_weight",
+    "to_string",
+]
