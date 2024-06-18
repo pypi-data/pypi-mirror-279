81c1dfc39e89958b56f2fc8cdef7d54413cef4ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-18 05:04:45 +0000

    Add package beholdr-io-sdk/0.1.3/beholdr_io_sdk-0.1.3.tar.gz

---
+++ packages/beholdr-io-sdk/beholdr_io_sdk-0.1.3.tar.gz/beholdr_io_sdk-0.1.3/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='beholdr_io_sdk',
+    version='0.1.3',
+    description='A Python SDK for interacting with Beholdr.io\'s Metrics API.',
+    author='Beholdr.io',
+    author_email='devin@beholdr.io',
+    packages=find_packages(),
+    install_requires=[
+        'requests'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.12',
+)
