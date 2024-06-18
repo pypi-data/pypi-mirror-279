4d156ec6dbb0e0c10396516d7d8e2cb82d56d1df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-18 07:05:24 +0000

    Add package pystab/3/pystab-3.tar.gz

---
+++ packages/pystab/pystab-3.tar.gz/pystab-3/setup.py
@@ -0,0 +1,60 @@
+from setuptools import setup, Extension
+from sysconfig import get_config_var
+from subprocess import check_output
+import sys
+
+extra_compile_args = get_config_var('CFLAGS').split()
+
+extra_link_args = []
+if sys.platform == "darwin":
+    extra_link_args = ["-L /usr/local/lib"]
+
+extensions = [Extension("libstab",
+                        ["stab/stab.c", "stab/mcculloch.c"],
+                        include_dirs=['stab', "/usr/local/include"],
+                        library_dirs=["/usr/local/lib"],
+                        extra_compile_args=extra_compile_args,
+                        extra_link_args=extra_link_args,
+                        depends=["stab/stab.h"])]
+
+# from https://stackoverflow.com/a/38525461
+from distutils.command.install_lib import install_lib as _install_lib
+import os
+import re
+def batch_rename(src, dst, src_dir_fd=None, dst_dir_fd=None):
+    '''Same as os.rename, but returns the renaming result.'''
+    os.rename(src, dst,
+              src_dir_fd=src_dir_fd,
+              dst_dir_fd=dst_dir_fd)
+    return dst
+
+class _CommandInstall(_install_lib):
+    def __init__(self, *args, **kwargs):
+        _install_lib.__init__(self, *args, **kwargs)
+
+    def install(self):
+        # let the distutils' install_lib do the hard work
+        outfiles = _install_lib.install(self)
+        # batch rename the outfiles:
+        # for each file, match string between
+        # second last and last dot and trim it
+        matcher = re.compile('\.([^.]+)\.so$')
+        return [batch_rename(file, re.sub(matcher, '.so', file))
+                for file in outfiles]
+
+setup(name="pystab",
+      version='3',
+      author="Enric Meinhardt-Llopis",
+      author_email="enric.meinhardt@fastmail.com",
+      description="Python wrapper of C functions for stable distributions",
+      url='https://git.sr.ht/~coco/pystab',
+      classifiers=[
+          "Operating System :: OS Independent",
+      ],
+      py_modules=['stab'],
+      ext_modules=extensions,
+      cmdclass={
+          'install_lib': _CommandInstall,
+      },
+)
+
---
+++ packages/pystab/pystab-3.tar.gz/pystab-3/stab.py
@@ -0,0 +1,136 @@
+
+
+# globally accessible C functions
+__stab_uniform = 0
+__stab_normal  = 0
+__stab_stable  = 0
+__stab_uniform_fill = 0
+__stab_normal_fill  = 0
+__stab_stable_fill  = 0
+__stab_mcculloch_fit  = 0
+#__stab_qfit  = 0
+
+
+# internal function to initialize the C interface
+def __setup_functions():
+	global __stab_uniform
+	global __stab_normal
+	global __stab_stable
+	global __stab_uniform_fill
+	global __stab_normal_fill
+	global __stab_stable_fill
+	global __stab_mcculloch_fit
+	if __stab_uniform != 0: return
+
+	from os.path import abspath, dirname
+	from ctypes import CDLL, POINTER, c_double, c_int
+
+	libstab = CDLL(f"{abspath(dirname(__file__))}/libstab.so")
+
+	__stab_uniform = libstab.random_uniform
+	__stab_uniform.argtypes = None
+	__stab_uniform.restype = c_double
+
+	__stab_normal = libstab.random_normal
+	__stab_normal.argtypes = None
+	__stab_normal.restype = c_double
+
+	__stab_stable = libstab.random_stable
+	__stab_stable.argtypes = [c_double, c_double]
+	__stab_stable.restype = c_double
+
+	__stab_uniform_fill = libstab.random_uniform_fill
+	__stab_uniform_fill.argtypes = [POINTER(c_double), c_int]
+	__stab_uniform_fill.restype = None
+
+	__stab_normal_fill = libstab.random_normal_fill
+	__stab_normal_fill.argtypes = [POINTER(c_double), c_int]
+	__stab_normal_fill.restype = None
+
+	__stab_stable_fill = libstab.random_stable_fill
+	__stab_stable_fill.argtypes = [POINTER(c_double), c_int, c_double, c_double]
+	__stab_stable_fill.restype = None
+
+	__stab_mcculloch_fit = libstab.mcculloch_fit
+	__stab_mcculloch_fit.argtypes = [POINTER(c_double), c_int,
+			POINTER(c_double), POINTER(c_double),
+			POINTER(c_double), POINTER(c_double) ]
+	__stab_mcculloch_fit.restype = None
+
+
+
+# API
+def uniform():
+	__setup_functions()
+	x = __stab_uniform()
+	return x
+
+# API
+def normal():
+	__setup_functions()
+	x = __stab_normal()
+	return x
+
+# API
+def stable(α, β):
+	__setup_functions()
+	x = __stab_stable(α, β)
+	return x
+
+# API
+def uniforms(n):
+	import numpy
+	from ctypes import POINTER, c_double
+	__setup_functions()
+	x = numpy.zeros(n)
+	p = x.ctypes.data_as(POINTER(c_double))
+	__stab_uniform_fill(p, n)
+	return x
+
+# API
+def normals(n):
+	import numpy
+	from ctypes import POINTER, c_double
+	__setup_functions()
+	x = numpy.zeros(n)
+	p = x.ctypes.data_as(POINTER(c_double))
+	__stab_normal_fill(p, n)
+	return x
+
+# API
+def stables(α, β, n):
+	import numpy
+	from ctypes import POINTER, c_double
+	__setup_functions()
+	x = numpy.zeros(n)
+	p = x.ctypes.data_as(POINTER(c_double))
+	__stab_stable_fill(p, n, α, β)
+	return x
+
+# API
+def fit_quantiles(x):
+	from ctypes import POINTER, c_double
+	from numpy import ascontiguousarray
+	__setup_functions()
+	pp = ascontiguousarray(x, dtype='float64')
+	p = pp.ctypes.data_as(POINTER(c_double))
+	n = x.size
+	α = c_double()
+	β = c_double()
+	c = c_double()
+	z = c_double()
+	__stab_mcculloch_fit(p, n, α, β, c, z)
+	return α.value, β.value, c.value, z.value
+
+
+
+
+
+# API
+version = 3
+
+__all__ = [
+		"uniform", "normal", "stable",
+		"uniforms", "normals", "stables",
+		"fit_quantiles",
+	"version" ]
