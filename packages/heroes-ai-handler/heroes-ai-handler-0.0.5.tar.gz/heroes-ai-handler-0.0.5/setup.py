c7d3a92915505ba6e5c3c0d1064afd3dd417a913
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-18 04:04:09 +0000

    Add package heroes-ai-handler/0.0.5/heroes-ai-handler-0.0.5.tar.gz

---
+++ packages/heroes-ai-handler/heroes-ai-handler-0.0.5.tar.gz/heroes-ai-handler-0.0.5/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='heroes-ai-handler',
+    version = "0.0.5",
+    packages = find_packages(),
+    install_requires = [],
+    include_package_data = True,
+    description = "A package for handling GPT, embeddings, and data for an AI Assistant.",
+    long_description = open("README.md").read(),
+    long_description_content_type = "text/markdown",
+    url = "https://pypi.org/project/heroes-ai-handler/",
+    author = "Stefan Jansen",
+    author_email = 'stefan.jansen@heroes.nl',
+    license = "MIT",
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires = '>=3.10',
+)
