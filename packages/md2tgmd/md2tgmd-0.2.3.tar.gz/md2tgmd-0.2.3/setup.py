4240a6f227059587c08800e841ca5640df4b2a3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-18 06:06:24 +0000

    Add package md2tgmd/0.2.3/md2tgmd-0.2.3.tar.gz

---
+++ packages/md2tgmd/md2tgmd-0.2.3.tar.gz/md2tgmd-0.2.3/setup.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+
+from setuptools import setup, find_packages
+
+setup(
+    name="md2tgmd",
+    version="0.2.3",
+    description="md2tgmd is a Markdown to Telegram-specific-markdown converter.",
+    long_description=Path.open(Path("README.md"), encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    py_modules=["md2tgmd"]
+)
