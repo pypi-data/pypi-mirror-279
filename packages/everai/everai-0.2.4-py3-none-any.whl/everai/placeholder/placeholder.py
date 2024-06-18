98f3bf94a3edddebc78dc9cb05a82bc755288b10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-18 15:05:48 +0000

    Add package everai/0.2.4/everai-0.2.4-py3-none-any.whl

---
+++ packages/everai/everai-0.2.4-py3-none-any.whl/everai/placeholder/placeholder.py
@@ -0,0 +1,59 @@
+import typing
+
+from everai.app.context import context
+from generated.apps import Commonv1Value, V1ValueFrom, V1KeyReference
+
+
+class Placeholder:
+    def __init__(self, name: str, key: str,
+                 kind: typing.Literal['ConfigMap', "Secret"] = 'Secret'):
+        self.name = name
+        self.key = key
+        self.kind = kind
+
+    def __call__(self) -> str:
+
+        match self.kind:
+            case 'ConfigMap':
+                holder = context.get_configmap(self.name)
+            case 'Secret':
+                holder = context.get_secret(self.name)
+            case _:
+                raise ValueError(f'Unsupported placeholder kind {self.kind}')
+
+        assert holder is not None
+
+        value = holder.get(self.key)
+        assert value is not None
+
+        return value
+
+
+class PlaceholderValue:
+    value: typing.Union[str, Placeholder]
+
+    def __init__(
+            self,
+            value: typing.Optional[str] = None,
+            placeholder: typing.Optional[Placeholder] = None,
+    ):
+        assert value is not None or placeholder is not None
+        self.value = value if value is not None else placeholder
+
+    def to_proto(self) -> Commonv1Value:
+        if isinstance(self.value, str):
+            return Commonv1Value(value=self.value)
+        else:
+            if isinstance(self.value, Placeholder):
+                if self.value.kind == "Secret":
+                    return Commonv1Value(value_from=V1ValueFrom(
+                        secret_key_ref=V1KeyReference(name=self.value.name, key=self.value.key)
+                    ))
+                elif self.value.kind == "ConfigMap":
+                    return Commonv1Value(value_from=V1ValueFrom(
+                        config_map_key_ref=V1KeyReference(name=self.value.name, key=self.value.key)
+                    ))
+                else:
+                    raise ValueError(f'unsupported placeholder kind {self.value.kind}')
+            else:
+                raise ValueError(f'unsupported username value {type(self)}')
