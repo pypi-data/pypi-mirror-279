6cdd1373e179983dcc41b010af3ee1455c1a64e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-18 16:05:27 +0000

    Add package PistolMagazine/0.5.2/PistolMagazine-0.5.2.tar.gz

---
+++ packages/PistolMagazine/PistolMagazine-0.5.2.tar.gz/PistolMagazine-0.5.2/pistol_magazine/data_exporter/db_conf.py
@@ -0,0 +1,9 @@
+def get_db_config():
+    # Update with your actual database connection details
+    return {
+        'user': 'dbhub',
+        'password': 'dbhub',
+        'host': '10.20.42.67',
+        'port': 33061,
+        'database': 'dbriska'
+    }
