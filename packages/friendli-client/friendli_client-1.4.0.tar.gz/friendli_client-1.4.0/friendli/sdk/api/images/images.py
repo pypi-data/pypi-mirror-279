0a3be1fb0421a7e15e6d560267532a337d8a5b59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-18 16:04:59 +0000

    Add package friendli-client/1.4.0/friendli_client-1.4.0.tar.gz

---
+++ packages/friendli-client/friendli_client-1.4.0.tar.gz/friendli_client-1.4.0/friendli/sdk/api/images/images.py
@@ -0,0 +1,45 @@
+# Copyright (c) 2024-present, FriendliAI Inc. All rights reserved.
+
+"""Friendli Images API."""
+
+from __future__ import annotations
+
+from typing import Optional
+
+import httpx
+
+from friendli.sdk.api.images.text_to_image import AsyncTextToImage, TextToImage
+
+
+class Images:
+    """Images API."""
+
+    text_to_image: TextToImage
+
+    def __init__(
+        self,
+        base_url: Optional[str] = None,
+        endpoint_id: Optional[str] = None,
+        client: Optional[httpx.Client] = None,
+    ) -> None:
+        """Initialize Images."""
+        self.text_to_image = TextToImage(
+            base_url=base_url, endpoint_id=endpoint_id, client=client
+        )
+
+
+class AsyncImages:
+    """Asynchronous images API."""
+
+    text_to_image: AsyncTextToImage
+
+    def __init__(
+        self,
+        base_url: Optional[str] = None,
+        endpoint_id: Optional[str] = None,
+        client: Optional[httpx.AsyncClient] = None,
+    ) -> None:
+        """Initialize Images."""
+        self.text_to_image = AsyncTextToImage(
+            base_url=base_url, endpoint_id=endpoint_id, client=client
+        )
