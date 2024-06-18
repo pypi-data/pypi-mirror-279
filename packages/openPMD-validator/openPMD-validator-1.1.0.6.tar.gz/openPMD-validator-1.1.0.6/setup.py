b0cc78d0d5eb9331f25ca46e8dad74a3ff507691
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-18 16:05:31 +0000

    Add package openPMD-validator/1.1.0.6/openPMD-validator-1.1.0.6.tar.gz

---
+++ packages/openPMD-validator/openPMD-validator-1.1.0.6.tar.gz/openPMD-validator-1.1.0.6/setup.py
@@ -0,0 +1,44 @@
+from setuptools import setup
+
+
+def read_requirements():
+    with open('requirements.txt') as f:
+        return [line.strip() for line in f.readlines()]
+
+
+def read_readme():
+    with open('./README.md', encoding='utf-8') as f:
+        return f.read()
+
+
+setup(
+    name='openPMD-validator',
+    version='1.1.0.6',
+    url='https://github.com/openPMD/openPMD-validator',
+    # author=...,  # TODO
+    # author_email=...,  # TODO
+    # maintainer=...,  # TODO
+    # maintainer_email=...,  # TODO
+    license='ISC',
+    install_requires=read_requirements(),
+    description='Validator and examples for openPMD format',
+    long_description=read_readme(),
+    long_description_content_type='text/markdown',
+    classifiers=[
+        # 'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: ISC License (ISCL)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Topic :: Scientific/Engineering :: Physics',
+    ],
+    packages=['openpmd_validator'],
+    entry_points={
+        'console_scripts': [
+        'openPMD_check_h5 = openpmd_validator.check_h5:main',
+        'openPMD_createExamples_h5 = openpmd_validator.createExamples_h5:main',
+        ]
+    },
+)
