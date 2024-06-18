9118d2aafa1ac65d88f081364fdc3ed8b66879e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-18 03:04:13 +0000

    Add package changedetection.io/0.45.24/changedetection.io-0.45.24-py3-none-any.whl

---
+++ packages/changedetection.io/changedetection.io-0.45.24-py3-none-any.whl/changedetectionio/static/js/notifications.js
@@ -0,0 +1,55 @@
+$(document).ready(function() {
+
+  $('#add-email-helper').click(function (e) {
+    e.preventDefault();
+    email = prompt("Destination email");
+    if(email) {
+      var n = $(".notification-urls");
+      var p=email_notification_prefix;
+      $(n).val( $.trim( $(n).val() )+"\n"+email_notification_prefix+email );
+    }
+  });
+
+  $('#send-test-notification').click(function (e) {
+    e.preventDefault();
+
+    // this can be global
+    var csrftoken = $('input[name=csrf_token]').val();
+    $.ajaxSetup({
+        beforeSend: function(xhr, settings) {
+            if (!/^(GET|HEAD|OPTIONS|TRACE)$/i.test(settings.type) && !this.crossDomain) {
+                xhr.setRequestHeader("X-CSRFToken", csrftoken)
+            }
+        }
+    })
+
+    data = {
+      notification_body: $('#notification_body').val(),
+      notification_format: $('#notification_format').val(),
+      notification_title: $('#notification_title').val(),
+      notification_urls: $('.notification-urls').val(),
+      tags: $('#tags').val(),
+      window_url: window.location.href,
+    }
+
+
+    $.ajax({
+      type: "POST",
+      url: notification_base_url,
+      data : data,
+        statusCode: {
+        400: function() {
+            // More than likely the CSRF token was lost when the server restarted
+          alert("There was a problem processing the request, please reload the page.");
+        }
+      }
+    }).done(function(data){
+      console.log(data);
+      alert(data);
+    }).fail(function(data){
+      console.log(data);
+      alert('There was an error communicating with the server.');
+    })
+  });
+});
+
