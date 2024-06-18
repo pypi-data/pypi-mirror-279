eabe305b5219600b7d9107cb52152fda5eb8cbc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-18 17:07:24 +0000

    Add package mobileiron-api/0.1.9/mobileiron_api-0.1.9-py3-none-any.whl

---
+++ packages/mobileiron-api/mobileiron_api-0.1.9-py3-none-any.whl/mobileiron_api/client.py
@@ -0,0 +1,18 @@
+from mobileiron_api.api.device_management_api import DeviceManagementAPI
+from mobileiron_api.api.user_management_api import UserManagementAPI
+from typing import Optional, Tuple
+
+
+class MobileIronCloudAPI(object):
+    device_management_api: DeviceManagementAPI
+    user_management_api: UserManagementAPI
+
+    def __init__(self, username: str, password: str, fqdn: str, endpoint: str, dmPartitionId: int, timeout: Tuple[int, int] = (10, 60)):
+        self._username = username
+        self._password = password
+        self._fqdn = fqdn
+        self._endpoint = endpoint
+        self.device_management_api = DeviceManagementAPI(username=username, password=password, fqdn=fqdn, endpoint=endpoint, dmPartitionId=dmPartitionId,
+                                                         timeout=timeout)
+        self.user_management_api = UserManagementAPI(username=username, password=password, fqdn=fqdn, endpoint=endpoint, dmPartitionId=dmPartitionId,
+                                                     timeout=timeout)
