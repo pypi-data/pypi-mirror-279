68f53012d0e2f0cacfcf2f01e6311a61cbce1afe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-18 06:06:29 +0000

    Add package Doggel-Inc/1.5.2/doggel_inc-1.5.2.tar.gz

---
+++ packages/Doggel-Inc/doggel_inc-1.5.2.tar.gz/Doggel_Inc/DICord/user.py
@@ -0,0 +1,41 @@
+class User:
+  def __init__(self, user_data, client):
+    self.id = user_data["id"]
+    self.username = user_data['username']
+    self.display = user_data["global_name"]
+    self.bot = user_data.get('bot', False)
+    self.client = client
+    self.dm = None
+    self.session = client.session
+
+  def set_dm(self, id: int):
+    self.dm = id
+  
+  async def create_dm(self):
+    url = f'https://discord.com/api/v9/users/@me/channels'
+    headers = {
+      'Authorization': f'Bot {self.client.token}',
+      'Content-Type': 'application/json',
+    }
+    payload = {'recipient_id': self.id}
+    async with self.session.post(url, headers=headers, json=payload) as response:
+      if response.status_code == 200:
+        idd = await response.json().get('id')
+        self.set_dm(idd)
+      else:
+        raise Exception(f"Failed to create DM channel: {dm_response.json()}")
+  
+  async def send(self, content, reference=None):
+    if self.dm == None:
+      raise Exception("You haven't created a DM before trying to send something to user")
+    url = f'https://discord.com/api/v6/channels/{self.dm}/messages'
+    headers = {
+      'Authorization': f'Bot {self.client.token}',
+      'Content-Type': 'application/json',
+    }
+    payload = {
+        'content': str(content),
+        'message_reference': {'message_id': reference} if reference else None
+    }
+    async with self.session.post(url, headers=headers, json=payload) as response:
+      return await response.json()
---
+++ packages/Doggel-Inc/doggel_inc-1.5.2.tar.gz/doggel_inc-1.5.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="Doggel-Inc",
+    version="1.5.2",
+    author="Lordpomind",
+    author_email="lordpomind@gmail.com",
+    description="A package with all Doggeł Inc services",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(where='src'),
+    install_requires=[
+        "websockets",
+        "aiohttp",
+    ],
+    python_requires='>=3.8',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
