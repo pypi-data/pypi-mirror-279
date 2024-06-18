c5896ba7cc7b5052ef9d4b9f3d75c7d5115f09b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-18 05:04:58 +0000

    Add package gbizinfo-lod/0.1.0/gbizinfo_lod-0.1.0-py3-none-any.whl

---
+++ packages/gbizinfo-lod/gbizinfo_lod-0.1.0-py3-none-any.whl/gbizinfo_lod/mappers/hojyokin.py
@@ -0,0 +1,54 @@
+from rdflib import Literal, URIRef
+
+from ..namespace import *
+from . import _TripleMapType, bpo
+from ._katsudo import GbizInfoKatsudoMapper
+
+
+class GbizInfoHojyokinMapper(GbizInfoKatsudoMapper):
+    """補助金情報"""
+
+    @staticmethod
+    def map_to_triples(row: dict[str, str]) -> list[_TripleMapType]:
+        ss = HJ_EXT[f"{row['ID-識別値']}_{row['キー情報']}"]
+
+        triples = GbizInfoKatsudoMapper.map_to_triples(row)
+        triples.extend(
+            [
+                (ss, RDF.type, HJ.補助金型),
+                # 49
+                (
+                    ss,
+                    HJ.採択日,
+                    bpo(
+                        [
+                            (RDF.type, IC.日付型),
+                            (
+                                IC.標準型日付,
+                                Literal(row["補助金-採択日"], datatype=XSD.date),
+                            ),
+                        ]
+                    ),
+                ),
+                # 50
+                (
+                    ss,
+                    HJ.交付決定日,
+                    bpo(
+                        [
+                            (RDF.type, IC.日付型),
+                            (
+                                IC.標準型日付,
+                                Literal(row["補助金-交付決定日"], datatype=XSD.date),
+                            ),
+                        ]
+                    ),
+                ),
+                # 51
+                (ss, HJ.補助金財源, row["補助金-財源"]),
+            ]
+        )
+        return triples
+
+
+__all__ = ["GbizInfoHojyokinMapper"]
