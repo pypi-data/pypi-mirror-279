9d458185f1658266ce519d498507db812b757b25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-18 16:05:23 +0000

    Add package autopipeline/0.1.376/autopipeline-0.1.376-py3-none-any.whl

---
+++ packages/autopipeline/autopipeline-0.1.376-py3-none-any.whl/autopipeline/PipelineExec.py
@@ -0,0 +1,14 @@
+import sqlite3
+import pandas as pd
+
+def query_exec(db, code, status, verbose):
+    print(code)
+    conn = sqlite3.connect(db)
+    df = pd.read_sql_query(code, conn)
+    conn.close()
+    return df, status, False, ""
+
+def display(result, status, verbose):
+    print(result)
+    status.append('displayed')
+    return status
