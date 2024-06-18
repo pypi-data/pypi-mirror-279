3c2ce9c935d59a49bef3801871b4c4ec62b430e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-18 04:04:18 +0000

    Add package kognic-io/2.2.0/kognic_io-2.2.0-py3-none-any.whl

---
+++ packages/kognic-io/kognic_io-2.2.0-py3-none-any.whl/kognic/io/model/hosts.py
@@ -0,0 +1,3 @@
+DEFAULT_INPUT_API_HOST = "https://input.app.kognic.com"
+DEFAULT_ORDER_EXECUTION_API_HOST = "https://order-execution.app.kognic.com"
+DEFAULT_SHAPE_INTEGRATION_API_HOST = "https://shape-integration.app.kognic.com"
