15102068be34169a69b6e70822be2defbb233b5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-18 05:05:10 +0000

    Add package datasette-faiss/0.2.1/datasette_faiss-0.2.1.tar.gz

---
+++ packages/datasette-faiss/datasette_faiss-0.2.1.tar.gz/datasette_faiss-0.2.1/tests/test_faiss.py
@@ -0,0 +1,103 @@
+from datasette.app import Datasette
+from datasette_faiss import encode
+import pytest
+import pytest_asyncio
+import sqlite3
+import urllib.parse
+
+
+@pytest.fixture(scope="session")
+def path(tmp_path_factory):
+    db_directory = tmp_path_factory.mktemp("dbs")
+    db_path = str(db_directory / "demo.db")
+    conn = sqlite3.connect(db_path)
+    with conn:
+        conn.executescript(
+            """
+        create table documents (id integer primary key, title text);
+        create table embeddings (id integer primary key, embedding blob);
+        insert into documents (id, title) values (1, 'First document');
+        insert into documents (id, title) values (2, 'Second document');
+        """
+        )
+        # Now the embeddings
+        conn.execute(
+            "insert into embeddings (id, embedding) values (?, ?)",
+            (1, encode([1, 2.5, 3])),
+        )
+        conn.execute(
+            "insert into embeddings (id, embedding) values (?, ?)",
+            (2, encode([4, 5.5, 6])),
+        )
+    conn.close()
+    return db_path
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "sql,expected",
+    (
+        (
+            "select faiss_search('demo', 'embeddings', faiss_encode('[1, 2, 3]'), 2) as results",
+            "[1, 2]",
+        ),
+        (
+            "select faiss_search_with_scores('demo', 'embeddings', faiss_encode('[1, 2, 3]'), 2) as results",
+            "[[1, 0.25], [2, 30.25]]",
+        ),
+        (
+            "select faiss_encode('[1, 2, 3]') as results",
+            {"$base64": True, "encoded": "AACAPwAAAEAAAEBA"},
+        ),
+        (
+            "select faiss_decode(faiss_encode('[1, 2, 3]')) as results",
+            "[1.0, 2.0, 3.0]",
+        ),
+    ),
+)
+async def test_faiss(path, sql, expected):
+    ds = Datasette(
+        [path],
+        metadata={"plugins": {"datasette-faiss": {"tables": [["demo", "embeddings"]]}}},
+    )
+    await ds.invoke_startup()
+    response = await ds.client.get(
+        "/demo.json?"
+        + urllib.parse.urlencode(
+            {
+                "_shape": "array",
+                "sql": sql,
+            }
+        )
+    )
+    assert response.json()[0]["results"] == expected
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "fn,input,expected",
+    (
+        ("faiss_agg", "[1, 2, 3]", "[1, 2]"),
+        ("faiss_agg", "[4, 5, 6]", "[2, 1]"),
+        ("faiss_agg_with_scores", "[1, 2, 3]", "[[1, 0.25], [2, 30.25]]"),
+        ("faiss_agg_with_scores", "[4, 5, 6]", "[[2, 0.25], [1, 24.25]]"),
+    ),
+)
+async def test_faiss_agg(path, fn, input, expected):
+    ds = Datasette(
+        [path],
+        metadata={"plugins": {"datasette-faiss": {"tables": [["demo", "embeddings"]]}}},
+    )
+    await ds.invoke_startup()
+    sql = f"select {fn}(id, embedding, faiss_encode(:embedding), 2) as results from embeddings"
+    response = await ds.client.get(
+        "/demo.json?"
+        + urllib.parse.urlencode(
+            {
+                "_shape": "array",
+                "sql": sql,
+                "embedding": input,
+            }
+        )
+    )
+    assert response.json()[0]["results"] == expected
