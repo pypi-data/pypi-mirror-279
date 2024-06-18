3d2432cb0c2ce8d5d8327d2f80c14ba8595b48b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-18 03:04:22 +0000

    Add package universal123/2/universal123-2.tar.gz

---
+++ packages/universal123/universal123-2.tar.gz/universal123-2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='universal123',
+    version='2',
+    description='this sdk is a working protype',
+    author='Rishabh Sharma',
+    author_email='rishabhsharmabwr@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+        # Add any other dependencies here
+    ],
+)
