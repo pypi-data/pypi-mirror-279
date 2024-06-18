132f037e220a32f733214c43430c4224473d595d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-18 04:04:05 +0000

    Add package rs-docs-loader/0.0.8/rs_docs_loader-0.0.8-py3-none-any.whl

---
+++ packages/rs-docs-loader/rs_docs_loader-0.0.8-py3-none-any.whl/loaders/google_drive_loader/common/base_loader.py
@@ -0,0 +1,20 @@
+from abc import abstractmethod
+from collections.abc import Iterator
+
+from loaders.common.base_loader import Document, RsBaseLoader
+from loaders.google_drive_loader.common.config import CREDENTIAL_PATH, FOLDER_ID, TOKEN_PATH
+
+
+class BaseLoader(RsBaseLoader):
+    def __init__(self, recursive):
+        self.credentials_path = CREDENTIAL_PATH
+        self.token_path = TOKEN_PATH
+        self.folder_id = FOLDER_ID
+        self.recursive = recursive
+
+    @abstractmethod
+    def lazy_load(self) -> Iterator[Document]:
+        """
+        Used to load documents one by one lazily. Use for production code.
+        Implement this method using generators to avoid loading all Documents into memory at once.
+        """
