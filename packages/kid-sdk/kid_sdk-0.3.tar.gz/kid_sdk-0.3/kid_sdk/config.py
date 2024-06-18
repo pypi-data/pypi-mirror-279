63f3a81f902321d27788649f2437dbdec503855e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-18 17:08:03 +0000

    Add package kid-sdk/0.3/kid_sdk-0.3.tar.gz

---
+++ packages/kid-sdk/kid_sdk-0.3.tar.gz/kid_sdk-0.3/kid_sdk/config.py
@@ -0,0 +1,6 @@
+TEST_DOMAIN = "https://dev-smartid.zonesmart.com"
+DOMAIN = "https://smartid.zonesmart.com"
+
+AUTHORIZATION_URL = "{domain}"
+TOKEN_ISSUE_URL = "{domain}/api/oauth2/issue/"
+GET_USER_DATA_URL = "{domain}/api/user/auth_data/"
