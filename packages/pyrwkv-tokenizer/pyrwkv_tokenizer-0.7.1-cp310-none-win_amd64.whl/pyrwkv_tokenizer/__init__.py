552438bb4b18f554f827c019d1ddd8a1fe83d3da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-18 05:05:28 +0000

    Add package pyrwkv-tokenizer/0.7.1/pyrwkv_tokenizer-0.7.1-cp310-none-win_amd64.whl

---
+++ packages/pyrwkv-tokenizer/pyrwkv_tokenizer-0.7.1-cp310-none-win_amd64.whl/pyrwkv_tokenizer/__init__.py
@@ -0,0 +1,24 @@
+# import the contents of the Rust library into the Python extension
+from .pyrwkv_tokenizer import *
+from .pyrwkv_tokenizer import __all__
+
+# optional: include the documentation from the Rust module
+from .pyrwkv_tokenizer import __doc__  # noqa: F401
+
+__all__ = __all__ + ["RWKVTokenizer"]
+__version__ = "0.7.0"
+
+
+class RWKVTokenizer:
+    def __init__(self, name="WorldTokenizer") -> None:
+        if name != "WorldTokenizer":
+            raise Exception(f"The {name} is not supported.")
+        self.tokenizer = WorldTokenizer()
+
+    def encode(self, text: str):
+        tokens_ids = self.tokenizer.encode(text)
+        return tokens_ids
+
+    def decode(self, tokens_ids):
+        text = self.tokenizer.decode(tokens_ids)
+        return text
