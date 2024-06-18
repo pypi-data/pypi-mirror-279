e6d8d3a3be08dd67c31ad78a05b8ec72b499ee74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-18 16:05:26 +0000

    Add package grid3d-maps/1.5.9/grid3d_maps-1.5.9.tar.gz

---
+++ packages/grid3d-maps/grid3d_maps-1.5.9.tar.gz/grid3d_maps-1.5.9/tests/test_scripts/test_grid3d_contacts1.py
@@ -0,0 +1,16 @@
+"""Testing contacts, not finished!"""
+
+import grid3d_maps.contact.grid3d_contact_map as grid3d_contacts
+import pytest
+
+# =============================================================================
+# Do tests
+# =============================================================================
+
+
+@pytest.mark.skip("Take this later")
+def test_contact1a(datatree):
+    """Test HC contacts with YAML config example 1a"""
+    result = datatree / "contacts1a_folder"
+    result.mkdir(parents=True)
+    grid3d_contacts.main(["--config", "tests/yaml/contact1a.yml"])
