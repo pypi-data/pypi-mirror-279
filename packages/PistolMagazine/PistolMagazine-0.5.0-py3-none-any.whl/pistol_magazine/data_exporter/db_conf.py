06187d8fb894ca9f03cdd40cb2d4350b89140922
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-18 16:05:17 +0000

    Add package PistolMagazine/0.5.0/PistolMagazine-0.5.0-py3-none-any.whl

---
+++ packages/PistolMagazine/PistolMagazine-0.5.0-py3-none-any.whl/pistol_magazine/data_exporter/db_conf.py
@@ -0,0 +1,8 @@
+def get_db_config():
+    # Update with your actual database connection details
+    return {
+        'user': 'your_username',
+        'password': 'your_password',
+        'host': '127.0.0.1',
+        'database': 'your_database',
+    }
