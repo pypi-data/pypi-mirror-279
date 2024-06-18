e90ae727e6adb7ab2d2bfc36b380a742f3651c87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-18 05:04:24 +0000

    Add package pyopenweathermap/0.0.10/pyopenweathermap-0.0.10-py3-none-any.whl

---
+++ packages/pyopenweathermap/pyopenweathermap-0.0.10-py3-none-any.whl/pyopenweathermap/__init__.py
@@ -0,0 +1,9 @@
+from .exception import (
+    RequestError,
+    UnauthorizedError,
+    TooManyRequestsError
+)
+from .owm_client import OWMClient
+from .weather import (
+    CurrentWeather, HourlyWeatherForecast, DailyWeatherForecast, WeatherReport, DailyTemperature, WeatherCondition
+)
