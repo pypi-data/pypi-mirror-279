46f707f656851b17c04d5629b2fc9f59c4f21b5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-18 04:04:09 +0000

    Add package rpis-lib/0.0.3/rpis_lib-0.0.3-py3-none-any.whl

---
+++ packages/rpis-lib/rpis_lib-0.0.3-py3-none-any.whl/rpis_lib/classes/enums.py
@@ -0,0 +1,7 @@
+from enum import Enum
+
+
+class HypothesisType(Enum):
+    LEFT = "L"
+    RIGHT = "R"
+    BOTH = "O"
---
+++ packages/rpis-lib/rpis_lib-0.0.3-py3-none-any.whl/rpis_lib/consts.py
@@ -0,0 +1,5 @@
+inf = float('inf')
+ECLIPSE = 3
+
+MAX_ALFA = 0.20
+DEFAULT_ALFA = 0.05
