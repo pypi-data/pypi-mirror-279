208c75c22b2574e3128905171928ab23be9b5237
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-18 15:05:46 +0000

    Add package question-answer-db/0.5/question_answer_db-0.5.tar.gz

---
+++ packages/question-answer-db/question_answer_db-0.5.tar.gz/question_answer_db-0.5/app1/models.py
@@ -0,0 +1,54 @@
+# from django.db import models
+
+# class User(models.Model):
+#     username = models.CharField(max_length=100, unique=True)
+
+# from django.db import models
+
+# class ChatSession(models.Model):
+#     user_name = models.CharField(max_length=255)
+#     session_id = models.CharField(max_length=255)
+#     question = models.TextField()
+#     answer = models.TextField()
+
+
+# class SimilarQuestion(models.Model):
+#     user = models.ForeignKey(User, on_delete=models.CASCADE)
+#     data = models.JSONField()  # Updated to use the new JSONField
+
+
+
+
+
+
+from django.db import models
+
+class User(models.Model):
+    username = models.CharField(max_length=100, unique=True)
+
+from django.db import models
+
+class ChatSession(models.Model):
+    user_name = models.CharField(max_length=255)
+    session_id = models.CharField(max_length=255)
+    question = models.TextField()
+    answer = models.TextField()
+
+
+class SimilarQuestion(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    session_id = models.CharField(max_length=255)
+    question = models.TextField()
+    answer = models.TextField()
+
+
+
+
+
+
+
+
+
+
+
