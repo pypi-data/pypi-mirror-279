db5e29a6fba5e0fdad3c7b78d62748ade28d22c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-18 16:05:18 +0000

    Add package nacos-python-lib/0.0.3/nacos-python-lib-0.0.3.tar.gz

---
+++ packages/nacos-python-lib/nacos-python-lib-0.0.3.tar.gz/nacos-python-lib-0.0.3/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name='nacos-python-lib',
+    version='0.0.3',
+    description='nacos python zy30651自用',
+    author='zy30651',
+    author_email='zy30651@qq.com',
+    packages=find_packages(),
+    url='https://github.com/zy30651/python-nacos',
+    license="apache 3.0",
+    install_requires=[],
+)
