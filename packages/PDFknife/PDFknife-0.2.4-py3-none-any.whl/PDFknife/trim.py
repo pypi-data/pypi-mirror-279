fb49a87142e30e47af18b6e07ffa7e989286dae7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-18 17:07:18 +0000

    Add package PDFknife/0.2.4/PDFknife-0.2.4-py3-none-any.whl

---
+++ packages/PDFknife/PDFknife-0.2.4-py3-none-any.whl/PDFknife/trim.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import subprocess
+import logging
+
+from ._utils import default_output
+
+logger = logging.getLogger()
+
+
+def trim(filename, top=1, bottom=1, right=1, left=1, output=None):
+    """
+    Trim a PDF
+
+    :param filename: PDF filepath
+    :param output: PDF output
+    """
+    if output is None:
+        output = default_output('TRIMMED', filename)
+
+    command = ['pdfjam',
+               filename,
+               '--trim',
+               f'\'{left}mm {bottom}mm {right}mm {top}mm\'',
+               '--clip',
+               'true',
+               '--outfile',
+               output,
+               ]
+    logger.debug(command)
+    logger.debug(f'Executed command: {command}')
+    process = subprocess.Popen(command, stdout=subprocess.PIPE)
+    stdout, stderr = process.communicate()
