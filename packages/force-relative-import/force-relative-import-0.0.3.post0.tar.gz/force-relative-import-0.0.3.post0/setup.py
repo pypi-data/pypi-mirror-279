307cb1257901101d67b3f8f65391ef33507ccd9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-18 06:06:39 +0000

    Add package force-relative-import/0.0.3.post0/force-relative-import-0.0.3.post0.tar.gz

---
+++ packages/force-relative-import/force-relative-import-0.0.3.post0.tar.gz/force-relative-import-0.0.3.post0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+ver_str = '0.0.3.post0'
+
+long_description = open('README.md', 'r', encoding='utf8').read()
+
+setup(
+    name='force-relative-import',
+    version=ver_str,
+    description='Allow users to bypass Python relative import restrictions.',
+    author='onesixth',
+    author_email='noexist@noexist.noexist',
+    url='https://github.com/One-sixth/force-relative-import',
+    install_requires=[],
+    entry_points={'console_scripts': []},
+    packages=['force_relative_import'],
+    package_data={},
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
