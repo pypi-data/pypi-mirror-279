92026bbd2f72803538fcf667660d59801d9d8c66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-18 04:03:56 +0000

    Add package foxplot/0.6.0/foxplot-0.6.0.tar.gz

---
+++ packages/foxplot/foxplot-0.6.0.tar.gz/foxplot-0.6.0/foxplot/functions.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# SPDX-License-Identifier: Apache-2.0
+# Copyright 2024 Inria
+
+"""Functions that can be applied to frozen series."""
+
+import numpy as np
+
+from .frozen_series import FrozenSeries
+
+
+def std(series: FrozenSeries, window_size: int) -> FrozenSeries:
+    """Return the rolling standard deviation of the series.
+
+    Args:
+        series: Series to compute standard deviations from.
+        window_size: Size of the rolling window in which to compute
+            standard deviations.
+
+    Returns:
+        Array of windowed standard deviations along the series.
+    """
+    label = f"std({series._label}, {window_size})"
+    values = np.std(
+        np.lib.stride_tricks.sliding_window_view(series._values, window_size),
+        axis=-1,
+    )
+    return FrozenSeries(label, values)
