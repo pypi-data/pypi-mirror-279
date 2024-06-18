b0cfa766a0b80c452ca29418304572207f18a00e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-18 15:05:43 +0000

    Add package autofunction/0.1/autofunction-0.1.tar.gz

---
+++ packages/autofunction/autofunction-0.1.tar.gz/autofunction-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup 
+
+setup(name='autofunction',
+      version='0.1',
+      description='Some description',
+      url='',
+      author='Daniel Reuter',
+      author_email='dreuter14@gmail.com',
+      license='MIT',
+      packages=['test'],
+      zip_safe=False)
