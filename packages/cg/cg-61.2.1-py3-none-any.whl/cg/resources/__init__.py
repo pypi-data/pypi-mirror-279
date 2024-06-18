86dc442f5d90582aafd49bdeab1d26e85d00c658
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-18 16:05:23 +0000

    Add package cg/61.2.1/cg-61.2.1-py3-none-any.whl

---
+++ packages/cg/cg-61.2.1-py3-none-any.whl/cg/resources/__init__.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+
+import pkg_resources
+
+from cg.constants import FileExtensions
+
+RNAFUSION_BUNDLE_FILENAMES: str = (
+    Path("resources", "rnafusion_bundle_filenames").with_suffix(FileExtensions.YAML).as_posix()
+)
+
+TAXPROFILER_BUNDLE_FILENAMES: str = (
+    Path("resources", "taxprofiler_bundle_filenames").with_suffix(FileExtensions.YAML).as_posix()
+)
+
+TOMTE_BUNDLE_FILENAMES: str = (
+    Path("resources", "tomte_bundle_filenames").with_suffix(FileExtensions.YAML).as_posix()
+)
+
+RNAFUSION_BUNDLE_FILENAMES_PATH: Path = Path(
+    pkg_resources.resource_filename("cg", RNAFUSION_BUNDLE_FILENAMES)
+)
+
+TAXPROFILER_BUNDLE_FILENAMES_PATH: Path = Path(
+    pkg_resources.resource_filename("cg", TAXPROFILER_BUNDLE_FILENAMES)
+)
+
+TOMTE_BUNDLE_FILENAMES_PATH: Path = Path(
+    pkg_resources.resource_filename("cg", TOMTE_BUNDLE_FILENAMES)
+)
