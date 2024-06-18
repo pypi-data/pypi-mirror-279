011b48a52b076909255e25552024b4674ae9075a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-18 16:04:56 +0000

    Add package tripawnOpen/0.1.2/tripawnopen-0.1.2.tar.gz

---
+++ packages/tripawnOpen/tripawnopen-0.1.2.tar.gz/tripawnopen-0.1.2/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+setuptools.setup(
+    name = "tripawnOpen",
+    version='0.1.2',
+    description="Under Tacker package creation written by MitrePat",
+    author="anomaly",
+    author_email="anomaly@test.com",
+    url="https://eu4ng.tistory.com",
+    install_require=["requests"],
+    packages=setuptools.find_packages(exclude=[]),
+    python_requires='>=3.6',
+    package_data={},
+    zip_sage=False,
+    classifiers=[
+         'Programming Language :: Python :: 3.6',
+         'Programming Language :: Python :: 3.7',
+         'Programming Language :: Python :: 3.8',
+    ],
+)
