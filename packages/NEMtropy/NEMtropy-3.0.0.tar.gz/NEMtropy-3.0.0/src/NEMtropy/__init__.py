39b3193811b36947e752807224a5f1ca8e663e13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-18 03:04:13 +0000

    Add package NEMtropy/3.0.0/NEMtropy-3.0.0.tar.gz

---
+++ packages/NEMtropy/NEMtropy-3.0.0.tar.gz/NEMtropy-3.0.0/src/NEMtropy/__init__.py
@@ -0,0 +1,14 @@
+import bicm
+from .graph_classes import *
+from .matrix_generator import *
+from .ensemble_generator import *
+from .models_functions import *
+from .network_functions import *
+from bicm.graph_classes import *
+from bicm.models_functions import *
+from bicm.network_functions import *
+
+__version__ = "3.0.0"
+__author__ = """Nicolo' Vallarano (nico.vallarano@gmail.com)
+Emiliano Marchese (emilianomarcheserc@gmail.com)
+Matteo Bruno (matteobruno180@gmail.com)"""
