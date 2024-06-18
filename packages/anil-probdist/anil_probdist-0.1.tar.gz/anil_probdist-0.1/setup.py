a8ee9e3222620b404326aa08547514c17c007662
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-18 03:04:40 +0000

    Add package anil-probdist/0.1/anil_probdist-0.1.tar.gz

---
+++ packages/anil-probdist/anil_probdist-0.1.tar.gz/anil_probdist-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='anil_probdist',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['anil_probdist'],
+      zip_safe=False)
