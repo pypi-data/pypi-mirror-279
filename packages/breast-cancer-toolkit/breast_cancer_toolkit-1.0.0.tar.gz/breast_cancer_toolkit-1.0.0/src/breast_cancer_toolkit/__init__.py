d1fd65cb97bfea609b8417bbb97d23c0351de38d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-18 05:04:45 +0000

    Add package breast-cancer-toolkit/1.0.0/breast_cancer_toolkit-1.0.0.tar.gz

---
+++ packages/breast-cancer-toolkit/breast_cancer_toolkit-1.0.0.tar.gz/breast_cancer_toolkit-1.0.0/src/breast_cancer_toolkit/__init__.py
@@ -0,0 +1,43 @@
+import logging
+import sys
+
+import dotenv
+from pillow_heif import register_heif_opener
+
+from breast_cancer_toolkit.io.assets import register_pandas_plugins
+from breast_cancer_toolkit.io.DicomImagePlugin import register_dicom_plugin
+
+register_pandas_plugins()
+register_heif_opener()
+register_dicom_plugin()
+
+dotenv.load_dotenv()
+
+if sys.version_info[:2] >= (3, 8):
+    # TODO: Import directly (no need for conditional) when `python_requires = >= 3.8`
+    from importlib.metadata import PackageNotFoundError, version  # pragma: no cover
+else:
+    from importlib_metadata import PackageNotFoundError, version  # pragma: no cover
+
+try:
+    # Change here if project is renamed and does not equal the package name
+    dist_name = __name__
+    __version__ = version(dist_name)
+except PackageNotFoundError:  # pragma: no cover
+    __version__ = "unknown"
+finally:
+    del version, PackageNotFoundError
+
+_logger = logging.getLogger(__name__)
+
+
+def setup_logging(loglevel):
+    """Setup basic logging
+
+    Args:
+      loglevel (int): minimum loglevel for emitting messages
+    """
+    logformat = "[%(asctime)s] %(levelname)s:%(name)s:%(message)s"
+    logging.basicConfig(
+        level=loglevel, stream=sys.stdout, format=logformat, datefmt="%Y-%m-%d %H:%M:%S"
+    )
