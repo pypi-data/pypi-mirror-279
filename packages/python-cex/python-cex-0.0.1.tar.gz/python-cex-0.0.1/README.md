2adce79469342865db495f45050cb302412fb4ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-18 15:05:47 +0000

    Add package python-cex/0.0.1/python-cex-0.0.1.tar.gz

---
+++ packages/python-cex/python-cex-0.0.1.tar.gz/python-cex-0.0.1/README.md
@@ -0,0 +1,34 @@
+# CEX
+
+<img src="https://drive.google.com/uc?export=view&id=1XwHm3fgt0oQU1vcx-dlEOgTZA2q1HMli" alt="Thumbnail">
+
+The ultimate kit for Data Scientists to do data Cleaning and EXploration with 40+ built-in functions.
+
+## Features
+<ul>
+  <li>40+ built-in functions for data cleaning and exploration</li>
+  <li>Compatible with popular data science libraries such as pandas and numpy</li>
+  <li>Comprehensive error handling and validation</li>
+</ul>
+
+## Installation
+```python
+pip install cex
+```
+
+## Usage
+Quick example:
+```python
+import cex
+import pandas as pd
+
+df = pd.read_csv("data.csv")
+cex.null_check_graph(df)
+```
+
+Result:
+
+<img src="https://drive.google.com/uc?export=view&id=1zLtv2IgZelCBqPFcsA4HFwhmFjpxo6kW" alt="Sample">
+
+## Contributing
+Contributions are welcomed!, feel free to fork the repository and don't hesitate to contact if there is error or bugs.
---
+++ packages/python-cex/python-cex-0.0.1.tar.gz/python-cex-0.0.1/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.1'
+DESCRIPTION = 'The Cleaning and EXploration kit for Data Scientist.'
+LONG_DESCRIPTION = 'A package that allows data scientist to do data exploration and cleaning in more convenient way.'
+
+# Setting up
+setup(
+    name="python-cex",
+    version=VERSION,
+    author="John Willliam (Yande)",
+    author_email="<satwikayasayande@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=['pandas', 'numpy', 'matplotlib', 'scikit-learn', 'seaborn', 'scipy'],
+    keywords=['python', 'data science', 'cleaning', 'exploring', 'clean', 'explore'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
