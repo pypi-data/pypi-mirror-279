fbaf39d9369e04b4ebb5e78468bf2c34e8f0eae2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-18 16:05:35 +0000

    Add package scriptpilot/1.2/scriptpilot-1.2.tar.gz

---
+++ packages/scriptpilot/scriptpilot-1.2.tar.gz/scriptpilot-1.2/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="scriptpilot",
+    version="1.2",
+    description="A tool to manage the execution of multiple Python scripts.",
+    long_description="""
+        ScriptPilot is designed to manage the execution of multiple Python scripts based on their status 
+        defined in a text file named 'sp_on_board.txt'. ScriptPilot provides functionalities to start, stop, 
+        and monitor these scripts. It ensures that scripts are not duplicated and manages their lifecycle 
+        efficiently.
+    """,
+    long_description_content_type="text/markdown",
+    author="Javer Valino",
+    url="https://github.com/yourusername/scriptpilot",
+    packages=find_packages(),
+    install_requires=[
+        "fastapi",
+        "uvicorn",
+        "pydantic",
+        "psutil"
+    ],
+    entry_points={
+        'console_scripts': [
+            'scriptpilot=scriptpilot.main:run',  # Adjust as needed to your main script
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
