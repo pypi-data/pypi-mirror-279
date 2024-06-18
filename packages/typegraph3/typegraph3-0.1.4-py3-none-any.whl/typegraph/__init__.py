4c8c06db340613b1002df0c3035ba36d7f4aafbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-18 16:05:34 +0000

    Add package typegraph3/0.1.4/typegraph3-0.1.4-py3-none-any.whl

---
+++ packages/typegraph3/typegraph3-0.1.4-py3-none-any.whl/typegraph/__init__.py
@@ -0,0 +1,9 @@
+from .converter import (
+    TypeConverter,
+    PdtConverter,
+    TypeVarModel,
+    infer_generic_type,
+    iter_deep_type,
+    gen_typevar_model,
+)
+from .type_utils import deep_type
