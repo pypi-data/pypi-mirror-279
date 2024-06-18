057fe5188306399a996c0a509f454eb6cd6cb330
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-18 07:05:33 +0000

    Add package mfpymake/1.2.10/mfpymake-1.2.10.tar.gz

---
+++ packages/mfpymake/mfpymake-1.2.10.tar.gz/mfpymake-1.2.10/pymake/config.py
@@ -0,0 +1,12 @@
+__author__ = "Joseph D. Hughes"
+__date__ = "June 17, 2024"
+__version__ = "1.2.10"
+__maintainer__ = "Joseph D. Hughes"
+__email__ = "jdhughes@usgs.gov"
+__status__ = "Production"
+__description__ = (
+    "This is the pymake program for compiling fortran, c, and c++ source\n"
+    + "files, such as the source files that come with MODFLOW. The program\n"
+    + "works by building a directed acyclic graph of the module dependencies\n"
+    + "and then compiling the source files in the proper order."
+)
