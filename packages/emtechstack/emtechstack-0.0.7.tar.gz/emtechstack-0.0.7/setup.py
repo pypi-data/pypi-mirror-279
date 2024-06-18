85a5eeed755f38adc96f7dfefa8910600a856c78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-18 03:04:58 +0000

    Add package emtechstack/0.0.7/emtechstack-0.0.7.tar.gz

---
+++ packages/emtechstack/emtechstack-0.0.7.tar.gz/emtechstack-0.0.7/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='emtechstack',
+    version='0.0.7',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'click',
+        'requests',
+        'tabulate',
+        'termcolor',
+    ],
+    entry_points={
+        'console_scripts': [
+            'emtechstack=emtechstack.cli:cli',
+            'e9k=emtechstack.cli:cli',
+        ],
+    },
+)
