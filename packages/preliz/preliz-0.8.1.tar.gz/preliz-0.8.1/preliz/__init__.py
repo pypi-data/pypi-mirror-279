567af9a99cc2213fa159df575f24b0e8262d7c30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-18 05:04:25 +0000

    Add package preliz/0.8.1/preliz-0.8.1.tar.gz

---
+++ packages/preliz/preliz-0.8.1.tar.gz/preliz-0.8.1/preliz/__init__.py
@@ -0,0 +1,29 @@
+"""
+PreliZ.
+
+Tools to help you pick a prior
+"""
+import logging
+
+from matplotlib import rcParams
+
+from .distributions import *
+from .predictive import *
+from .unidimensional import *
+
+
+__all__ = ["maxent", "mle", "ppa", "roulette", "quartile"]
+
+__version__ = "0.8.1"
+
+_log = logging.getLogger("preliz")
+
+if not logging.root.handlers:
+    _log.setLevel(logging.INFO)
+    if len(_log.handlers) == 0:
+        handler = logging.StreamHandler()
+        _log.addHandler(handler)
+
+# Allow legend outside plot in maxent to be included when saving a figure
+# We may want to make this more explicit by having preliz.rcParams
+rcParams["savefig.bbox"] = "tight"
---
+++ packages/preliz/preliz-0.8.1.tar.gz/preliz-0.8.1/preliz/unidimensional/__init__.py
@@ -0,0 +1,9 @@
+from .beta_mode import beta_mode
+from .maxent import maxent
+from .mle import mle
+from .quartile import quartile
+from .quartile_int import quartile_int
+from .roulette import roulette
+from .dirichlet_mode import dirichlet_mode
+
+__all__ = ["beta_mode", "maxent", "mle", "roulette", "quartile", "quartile_int", "dirichlet_mode"]
