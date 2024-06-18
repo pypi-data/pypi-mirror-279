65e0fabfe016969a77d3e7a918a8bed6541f950d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-18 03:04:56 +0000

    Add package bsb-core/4.1.1/bsb_core-4.1.1-py3-none-any.whl

---
+++ packages/bsb-core/bsb_core-4.1.1-py3-none-any.whl/bsb/connectivity/__init__.py
@@ -0,0 +1,8 @@
+# isort: off
+# Load module before others to prevent partially initialized modules
+from .strategy import ConnectionStrategy
+
+# isort: on
+from .detailed import *
+from .general import *
+from .import_ import CsvImportConnectivity
