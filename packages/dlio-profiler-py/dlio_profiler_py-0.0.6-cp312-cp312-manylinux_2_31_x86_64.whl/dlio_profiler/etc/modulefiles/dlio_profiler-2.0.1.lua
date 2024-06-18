a274c55f24e2524757e87dde1ea8f137a552eb61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-18 14:05:09 +0000

    Add package dlio-profiler-py/0.0.6/dlio_profiler_py-0.0.6-cp312-cp312-manylinux_2_31_x86_64.whl

---
+++ packages/dlio-profiler-py/dlio_profiler_py-0.0.6-cp312-cp312-manylinux_2_31_x86_64.whl/dlio_profiler/etc/modulefiles/dlio_profiler-2.0.1.lua
@@ -0,0 +1,49 @@
+-- LMod module file for DLIO_PROFILER
+
+-- CMAKE_INSTALL_PREFIX: /home/runner/work/dlio-profiler/dlio-profiler/build/lib.linux-x86_64-cpython-312/dlio_profiler
+-- CMAKE_BUILD_TYPE: Release
+-- C Compiler: /usr/bin/cc
+-- C FLAGS:  -fPIC -Wall -Wextra -pedantic -Wno-unused-parameter -Wno-deprecated-declarations
+-- C FLAGS_DEBUG: -g
+-- C FLAGS_RELWITHDEBINFO: -O2 -g -DNDEBUG
+-- C FLAGS_RELEASE: -O3 -DNDEBUG
+-- CXX Compiler: /usr/bin/c++
+-- CXX FLAGS:  -fPIC -Wall -Wextra -pedantic -Wno-unused-parameter -Wnon-virtual-dtor -Wno-deprecated-declarations
+-- CXX FLAGS_DEBUG: -g
+-- CXX FLAGS_RELWITHDEBINFO: -O2 -g -DNDEBUG
+-- CXX FLAGS_RELEASE: -O3 -DNDEBUG
+-- DLIO_PROFILER_GNU_LINUX: TRUE
+-- DLIO_PROFILER_HAS_DOXYGEN: 
+-- DLIO_PROFILER_HAS_STD_FILESYSTEM: TRUE
+-- DLIO_PROFILER_HAS_STD_FSTREAM_FD: TRUE
+
+help(
+[[
+DYnamic and Asynchronous Data streamliner (DLIO_PROFILER) version .
+]])
+
+whatis("Package: DLIO_PROFILER")
+whatis("Version: ")
+whatis("Description: DYnamic and Asynchronous Data streamliner (DLIO_PROFILER).")
+whatis("URL: https://github.com/flux-framework/dlio_profiler")
+whatis("CMAKE_INSTALL_PREFIX: /home/runner/work/dlio-profiler/dlio-profiler/build/lib.linux-x86_64-cpython-312/dlio_profiler")
+whatis("CMAKE_BUILD_TYPE: Release")
+whatis("C Compiler: /usr/bin/cc")
+whatis("C FLAGS:  -fPIC -Wall -Wextra -pedantic -Wno-unused-parameter -Wno-deprecated-declarations")
+whatis("C FLAGS_DEBUG: -g")
+whatis("C FLAGS_RELWITHDEBINFO: -O2 -g -DNDEBUG")
+whatis("C FLAGS_RELEASE: -O3 -DNDEBUG")
+whatis("CXX Compiler: /usr/bin/c++")
+whatis("CXX FLAGS:  -fPIC -Wall -Wextra -pedantic -Wno-unused-parameter -Wnon-virtual-dtor -Wno-deprecated-declarations")
+whatis("CXX FLAGS_DEBUG: -g")
+whatis("CXX FLAGS_RELWITHDEBINFO: -O2 -g -DNDEBUG")
+whatis("CXX FLAGS_RELEASE: -O3 -DNDEBUG")
+whatis("DLIO_PROFILER_GNU_LINUX: TRUE")
+whatis("DLIO_PROFILER_HAS_DOXYGEN: ")
+whatis("DLIO_PROFILER_HAS_STD_FILESYSTEM: TRUE")
+whatis("DLIO_PROFILER_HAS_STD_FSTREAM_FD: TRUE")
+
+prepend_path("PATH","/home/runner/work/dlio-profiler/dlio-profiler/build/lib.linux-x86_64-cpython-312/dlio_profiler/bin")
+prepend_path("LD_LIBRARY_PATH","/home/runner/work/dlio-profiler/dlio-profiler/build/lib.linux-x86_64-cpython-312/dlio_profiler/lib")
+
+pushenv("DLIO_PROFILER_DIR","/home/runner/work/dlio-profiler/dlio-profiler/build/lib.linux-x86_64-cpython-312/dlio_profiler/")
