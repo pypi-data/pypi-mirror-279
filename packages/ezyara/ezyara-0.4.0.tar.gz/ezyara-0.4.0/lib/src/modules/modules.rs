bb3ba103bdf904969263fd1e72edcc900070e0df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-18 14:04:41 +0000

    Add package ezyara/0.4.0/ezyara-0.4.0.tar.gz

---
+++ packages/ezyara/ezyara-0.4.0.tar.gz/ezyara-0.4.0/lib/src/modules/modules.rs
@@ -0,0 +1,31 @@
+// File generated automatically by build.rs. Do not edit.
+#[cfg(feature = "string-module")]
+mod string;
+#[cfg(feature = "macho-module")]
+mod macho;
+#[cfg(feature = "pe-module")]
+mod pe;
+#[cfg(feature = "elf-module")]
+mod elf;
+#[cfg(feature = "text-module")]
+mod text;
+#[cfg(feature = "dotnet-module")]
+mod dotnet;
+#[cfg(feature = "lnk-module")]
+mod lnk;
+#[cfg(feature = "python-module")]
+mod python;
+#[cfg(feature = "hash-module")]
+mod hash;
+#[cfg(feature = "magic-module")]
+mod magic;
+#[cfg(feature = "math-module")]
+mod math;
+#[cfg(feature = "test_proto2-module")]
+mod test_proto2;
+#[cfg(feature = "time-module")]
+mod time;
+#[cfg(feature = "test_proto3-module")]
+mod test_proto3;
+#[cfg(feature = "console-module")]
+mod console;
