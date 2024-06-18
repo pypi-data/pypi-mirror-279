25f59d417bef153717fa6bf7c42fe1983294ec52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-18 03:04:25 +0000

    Add package knowledge-graph-rag/0.0.1/knowledge_graph_rag-0.0.1.tar.gz

---
+++ packages/knowledge-graph-rag/knowledge_graph_rag-0.0.1.tar.gz/knowledge_graph_rag-0.0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+with open("README.MD", "r") as f:
+    readme_content = f.read()
+
+setup(
+    name="knowledge_graph_rag",
+    version="0.0.1",
+    packages=find_packages(),
+    long_description=readme_content,
+    long_description_content_type="text/markdown",
+    install_requires=[
+        "numpy==1.26.0",
+        "networkx==3.2.1",
+        "nltk==3.8.1",
+        "litellm==1.34.0",
+    ],
+)
