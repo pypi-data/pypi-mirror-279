84816d36b141782de9d5a6da32e6bdd602a8bd94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-18 16:05:27 +0000

    Add package PistolMagazine/0.5.2/PistolMagazine-0.5.2-py3-none-any.whl

---
+++ packages/PistolMagazine/PistolMagazine-0.5.2-py3-none-any.whl/pistol_magazine/data_exporter/db_conf_template.py
@@ -0,0 +1,10 @@
+# db_config_template.py
+
+def get_db_config():
+    return {
+        "host": "your_database_host",
+        "port": 3306,  # or the appropriate port
+        "user": "your_database_user",
+        "password": "your_database_password",
+        "database": "your_database_name"
+    }
