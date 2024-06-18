7dc557221c73bf035ca5299f8776cd61da719d6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-18 16:05:28 +0000

    Add package ZYNacos/0.0.4/ZYNacos-0.0.4.tar.gz

---
+++ packages/ZYNacos/ZYNacos-0.0.4.tar.gz/ZYNacos-0.0.4/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name='ZYNacos',
+    version='0.0.4',
+    description='nacos python zy30651自用',
+    author='zy30651',
+    author_email='zy30651@qq.com',
+    packages=find_packages(),
+    url='https://github.com/zy30651/python-nacos',
+    license="apache 3.0",
+    install_requires=[],
+)
