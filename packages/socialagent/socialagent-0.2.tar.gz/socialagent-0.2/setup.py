045a2482759c9ebcc8511882b80bfd5239743467
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-18 15:06:20 +0000

    Add package socialagent/0.2/socialagent-0.2.tar.gz

---
+++ packages/socialagent/socialagent-0.2.tar.gz/socialagent-0.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='socialagent',
+    version='0.2',
+    description='socialagent is a Python module designed for generating user agents specific to social media platforms under Meta (formerly Facebook).',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Moh Iqbal Hidayat',
+    author_email='iqbalmh18.dev@email.com',
+    url='https://github.com/iqbalmh18/socialagent',
+    packages=['socialagent'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.8',
+)
