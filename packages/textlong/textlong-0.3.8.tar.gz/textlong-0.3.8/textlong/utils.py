5275abd91f6353f9563ab5109e383fdf982826da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-18 16:05:32 +0000

    Add package textlong/0.3.8/textlong-0.3.8.tar.gz

---
+++ packages/textlong/textlong-0.3.8.tar.gz/textlong-0.3.8/textlong/utils.py
@@ -0,0 +1,11 @@
+from typing import List, Union
+from langchain_core.documents import Document
+
+def raise_not_install(packages):
+    print(f"please install package: '{packages}' with pip or poetry")
+    # auto install package
+    # subprocess.check_call([sys.executable, "-m", "pip", "install", package_name])
+
+def raise_not_supply_all(info: str, *args):
+    if all(arg is None for arg in args):
+        raise ValueError(info)
