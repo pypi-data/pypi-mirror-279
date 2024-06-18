53ff0d0a1b985649cdb0251daf67faefa90f5da1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-18 05:05:06 +0000

    Add package modern-treasury/1.35.1/modern_treasury-1.35.1.tar.gz

---
+++ packages/modern-treasury/modern_treasury-1.35.1.tar.gz/modern_treasury-1.35.1/src/modern_treasury/types/payment_order_subtype.py
@@ -0,0 +1,8 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from typing import Optional
+from typing_extensions import Literal
+
+__all__ = ["PaymentOrderSubtype"]
+
+PaymentOrderSubtype = Optional[Literal["0C", "0N", "0S", "CCD", "CIE", "CTX", "IAT", "PPD", "TEL", "WEB"]]
