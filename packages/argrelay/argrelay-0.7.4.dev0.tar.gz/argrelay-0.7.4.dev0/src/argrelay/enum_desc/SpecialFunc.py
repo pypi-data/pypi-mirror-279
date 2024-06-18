7287b330c94b7db1b86eb76ad45351020d93b2b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-18 05:04:26 +0000

    Add package argrelay/0.7.4.dev0/argrelay-0.7.4.dev0.tar.gz

---
+++ packages/argrelay/argrelay-0.7.4.dev0.tar.gz/argrelay-0.7.4.dev0/src/argrelay/enum_desc/SpecialFunc.py
@@ -0,0 +1,32 @@
+from enum import Enum, auto
+
+
+class SpecialFunc(Enum):
+    """
+    See also: FS_80_45_89_81 / integrated functions
+    """
+
+    func_id_unplugged = auto()
+    """
+    Function which is not plugged anywhere.
+    """
+
+    intercept_invocation_func = auto()
+    """
+    Implements FS_88_66_66_73 intercept func.
+    """
+
+    help_hint_func = auto()
+    """
+    Implements FS_71_87_33_52 help hint.
+    """
+
+    echo_args_func = auto()
+    """
+    Implements FS_43_50_57_71 `echo_args` func.
+    """
+
+    query_enum_items_func = auto()
+    """
+    Implements FS_02_25_41_81 `query_enum_items_func`.
+    """
