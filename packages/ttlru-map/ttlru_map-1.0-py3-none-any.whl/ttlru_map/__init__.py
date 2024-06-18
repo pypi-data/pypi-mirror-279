84430e1ac0d0852c2a034fc044c58841ca7e2db7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-18 03:04:15 +0000

    Add package ttlru-map/1.0/ttlru_map-1.0-py3-none-any.whl

---
+++ packages/ttlru-map/ttlru_map-1.0-py3-none-any.whl/ttlru_map/__init__.py
@@ -0,0 +1,10 @@
+from ._exceptions import TTLDictError, TTLDictInvalidConfigError
+from ._ttl_dict import TTLDict
+
+__version__ = "1.0"
+
+__all__ = [
+    "TTLDict",
+    "TTLDictError",
+    "TTLDictInvalidConfigError",
+]
