91e86941f53877dce3d9a886e03dbdb17b2ed789
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-18 16:04:57 +0000

    Add package nixt/5/nixt-5.tar.gz

---
+++ packages/nixt/nixt-5.tar.gz/nixt-5/nixt/mod/ini.py
@@ -0,0 +1,26 @@
+# This file is placed in the Public Domain.
+
+
+"init"
+
+
+from nixt.run.main   import Cfg
+from nixt.run.thread import launch
+from nixt.run.utils  import getmods, spl
+
+
+def ini(event):
+    "run init on modules."
+    if not event.args:
+        event.reply("ini <name>")
+        return
+    mods = getmods(Cfg)
+    if not mods:
+        event.reply("modules are not available")
+        return
+    for name in event.args:
+        for nme in spl(name):
+            mod = getattr(mods, nme, None)
+            if "init" in dir(mod):
+                event.reply(f"launched {nme}")
+                launch(mod.init)
