c8a9da751108e65129eb02ca7c886f1da57e135f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-18 05:04:38 +0000

    Add package focustuner/0.1.5/focustuner-0.1.5.tar.gz

---
+++ packages/focustuner/focustuner-0.1.5.tar.gz/focustuner-0.1.5/setup.py
@@ -0,0 +1,19 @@
+from setuptools import find_packages, setup
+
+with open("README.md", 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='focustuner',
+    packages=find_packages(include=['focustuner']),
+    version='0.1.5',
+    description='Focus Load Tuner Control Functions in Python',
+    long_description=long_description,
+    license="MIT",
+    url="https://github.com/RFPAL-Github-Organization/LoadTuner-repo",
+    author='Grace Gomez',
+    install_requires=[],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest==4.4.1'],
+    test_suite='tests'
+)
