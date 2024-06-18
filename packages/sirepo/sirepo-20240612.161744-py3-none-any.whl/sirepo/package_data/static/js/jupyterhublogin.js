436399727dbde9a00a3b7a381cbf2336379d7647
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-18 05:04:27 +0000

    Add package sirepo/20240612.161744/sirepo-20240612.161744-py3-none-any.whl

---
+++ packages/sirepo/sirepo-20240612.161744-py3-none-any.whl/sirepo/package_data/static/js/jupyterhublogin.js
@@ -0,0 +1,50 @@
+'use strict';
+
+var srlog = SIREPO.srlog;
+var srdbg = SIREPO.srdbg;
+
+SIREPO.app.controller('JupyterhubMigrateController', function(authState, jupyterhubloginService, requestSender, $scope) {
+    const self = this;
+    self.isLoading = true;
+    requestSender.sendRequest(
+        'redirectJupyterHub',
+        () => {self.isLoading = false;}
+    );
+    self.migrate = function(doMigration) {
+        jupyterhubloginService.doMigration(doMigration);
+    };
+});
+
+SIREPO.app.controller('NameConflictController', function(requestSender, jupyterhubloginService, $route, $scope) {
+    const self = this;
+
+    self.noMigration = function() {
+        jupyterhubloginService.doMigration(false);
+    };
+
+    self.logout = function() {
+        requestSender.globalRedirect(
+            'authLogout',
+            {'<simulation_type>': SIREPO.APP_SCHEMA.simulationType,}
+        );
+    };
+});
+
+SIREPO.app.directive('appHeader', function(jupyterhubloginService) {
+    return {
+        restrict: 'A',
+        scope: {
+            nav: '=appHeader',
+        },
+        template: `
+            <div data-app-header-brand="nav"></div>
+            <div data-app-header-right="nav"></div>
+        `
+    };
+});
+
+SIREPO.app.factory('jupyterhubloginService', function(appState, requestSender) {
+    const self = {};
+    appState.setAppService(self);
+    return self;
+});
