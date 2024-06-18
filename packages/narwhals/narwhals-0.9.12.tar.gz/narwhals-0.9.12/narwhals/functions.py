a16e808517a7c65f7e82b2f7d7dcdf2a283b946f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-18 05:04:31 +0000

    Add package narwhals/0.9.12/narwhals-0.9.12.tar.gz

---
+++ packages/narwhals/narwhals-0.9.12.tar.gz/narwhals-0.9.12/narwhals/functions.py
@@ -0,0 +1,34 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+from typing import Iterable
+from typing import Literal
+
+from narwhals.utils import validate_laziness
+from narwhals.utils import validate_same_library
+
+if TYPE_CHECKING:
+    from narwhals.dataframe import DataFrame
+    from narwhals.dataframe import LazyFrame
+
+
+def concat(
+    items: Iterable[DataFrame | LazyFrame],
+    *,
+    how: Literal["horizontal", "vertical"] = "vertical",
+) -> DataFrame | LazyFrame:
+    if how not in ("horizontal", "vertical"):
+        raise NotImplementedError(
+            "Only horizontal and vertical concatenations are supported"
+        )
+    if not items:
+        raise ValueError("No items to concatenate")
+    items = list(items)
+    validate_same_library(items)
+    validate_laziness(items)
+    first_item = items[0]
+    plx = first_item.__narwhals_namespace__()
+    return first_item.__class__(
+        plx.concat([df._dataframe for df in items], how=how),
+        is_polars=first_item._is_polars,
+    )
