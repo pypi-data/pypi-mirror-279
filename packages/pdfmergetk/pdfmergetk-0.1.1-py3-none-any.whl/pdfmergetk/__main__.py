23a0a72589a5d01785a0c27780dfbb575bcf0254
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-18 05:04:32 +0000

    Add package pdfmergetk/0.1.1/pdfmergetk-0.1.1-py3-none-any.whl

---
+++ packages/pdfmergetk/pdfmergetk-0.1.1-py3-none-any.whl/pdfmergetk/__main__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Run application.
+"""
+
+from pdfmergetk.gui import main
+
+if __name__ == '__main__':
+    main()
---
+++ packages/pdfmergetk/pdfmergetk-0.1.1-py3-none-any.whl/pdfmergetk/reset.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+"""
+
+from src import AvoidOpeningThemMultipleTimes
+
+
+def main():
+    AvoidOpeningThemMultipleTimes.delete()
+
+
+if __name__ == '__main__':
+    main()
