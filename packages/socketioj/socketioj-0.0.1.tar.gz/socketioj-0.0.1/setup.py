365a2ba9482fc5dd6bbc0592c5691574eeebe581
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-18 05:04:51 +0000

    Add package socketioj/0.0.1/socketioj-0.0.1.tar.gz

---
+++ packages/socketioj/socketioj-0.0.1.tar.gz/socketioj-0.0.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+
+
+
+setup(
+  name='socketioj',
+  version='0.0.1',
+  author='erthqe4art5hqa3ewr4yh',
+  author_email='w22273352@gmail.com',
+  description='This is the simplest module for quick work with files.',
+  long_description="",
+  long_description_content_type='text/markdown',
+  url='https://github.com/paorhjugphawepr/socketioj',
+  packages=find_packages(),
+  install_requires=['requests>=2.25.1'],
+  classifiers=[
+    'Programming Language :: Python :: 3.11',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent'
+  ],
+  keywords='files speedfiles ',
+  project_urls={
+    'GitHub': 'https://github.com/paorhjugphawepr/socketioj'
+  },
+  python_requires='>=3.6'
+)
