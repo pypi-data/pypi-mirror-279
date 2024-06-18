983026cde2e31796898265ef145be8b4004f3ae2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-18 05:04:36 +0000

    Add package ygame-engine/0.1.2/ygame-engine-0.1.2.tar.gz

---
+++ packages/ygame-engine/ygame-engine-0.1.2.tar.gz/ygame-engine-0.1.2/README.md
@@ -0,0 +1,6 @@
+Engine for the ygame bot challenge in UFDS
+
+# Usage
+```py
+import ygame
+```
---
+++ packages/ygame-engine/ygame-engine-0.1.2.tar.gz/ygame-engine-0.1.2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ygame-engine",
+    version="0.1.2",
+    author="Oviyan Gandhi",
+    author_email="oviyangandhi@gmail.com",
+    description="Engine for the ygame bot challenge in UFDS",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    url="https://youtu.be/dQw4w9WgXcQ",
+    long_description="file: README.md"
+)
