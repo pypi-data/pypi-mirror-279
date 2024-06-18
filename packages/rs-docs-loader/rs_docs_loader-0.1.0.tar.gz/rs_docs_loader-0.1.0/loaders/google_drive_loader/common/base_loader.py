412bc630dfadb5f58167c7bd9620e67026e5aba7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-18 04:04:09 +0000

    Add package rs-docs-loader/0.1.0/rs_docs_loader-0.1.0.tar.gz

---
+++ packages/rs-docs-loader/rs_docs_loader-0.1.0.tar.gz/rs_docs_loader-0.1.0/loaders/google_drive_loader/common/base_loader.py
@@ -0,0 +1,19 @@
+from abc import abstractmethod
+from collections.abc import Iterator
+
+from loaders.common.base_loader import Document, RsBaseLoader
+
+
+class BaseLoader(RsBaseLoader):
+    def __init__(self, credentials_path, token_path, folder_id, recursive):
+        self.credentials_path = credentials_path
+        self.token_path = token_path
+        self.folder_id = folder_id
+        self.recursive = recursive
+
+    @abstractmethod
+    def lazy_load(self) -> Iterator[Document]:
+        """
+        Used to load documents one by one lazily. Use for production code.
+        Implement this method using generators to avoid loading all Documents into memory at once.
+        """
---
+++ packages/rs-docs-loader/rs_docs_loader-0.1.0.tar.gz/rs_docs_loader-0.1.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="rs-docs-loader",
+    version="0.1.0",
+    description="RS Docs Loader will help you to integrate different data sources",
+    author="Rootstrap",
+    packages=find_packages(),
+    install_requires=[
+        "chroma==0.2.0",
+        "chromadb==0.5.0",
+        "google-api-python-client==2.129.0",
+        "google-auth-httplib2==0.2.0",
+        "google-auth-oauthlib==1.2.0",
+        "langchain-core==0.2.0",
+        "langchain-google-community==1.0.4",
+        "langchain-openai==0.1.7",
+        "langchain_text_splitters==0.2.0",
+        "notion-client==2.2.1",
+        "openai==1.31.1",
+        "tiktoken==0.7.0",
+        "psycopg2==2.9.9",
+        "pydantic==2.7.1",
+        "python-dotenv==1.0.1",
+        "unstructured[pdf]==0.14.2",
+    ],
+)
