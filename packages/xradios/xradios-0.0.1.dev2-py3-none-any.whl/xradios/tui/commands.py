4e8a02468085a6e5c2b08d28eb03e8e5f433512c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-19 07:08:36 +0000

    Add package xradios/0.0.1.dev2/xradios-0.0.1.dev2-py3-none-any.whl

---
+++ packages/xradios/xradios-0.0.1.dev2-py3-none-any.whl/xradios/tui/commands.py
@@ -0,0 +1,139 @@
+import logging
+
+from cmd_parser.core import parse, asdict
+
+from xradios.tui.constants import DISPLAY_BUFFER
+from xradios.tui.constants import LISTVIEW_BUFFER
+from xradios.tui.constants import POPUP_BUFFER
+from xradios.tui.constants import HELP_TEXT
+from xradios.tui.client import proxy
+from xradios.tui.utils import stations
+from xradios.tui.utils import tags as _tags
+
+
+log = logging.getLogger('xradios')
+
+COMMAND_TO_HANDLER = {}
+
+
+def get_commands():
+    return COMMAND_TO_HANDLER.keys()
+
+
+def get_command_help(command):
+    return COMMAND_TO_HANDLER[command].__doc__
+
+
+def has_command_handler(command):
+    return command in COMMAND_TO_HANDLER
+
+
+def call_command_handler(command, *args, **kwargs):
+    COMMAND_TO_HANDLER[command](*args, **kwargs)
+
+
+def command_line_handler(event):
+    if not event.current_buffer.text:
+        return
+
+    command_string = ':' + event.current_buffer.text
+    options = asdict(parse(string=command_string))
+    command = options.get('command', '')
+
+    if not has_command_handler(command):
+        return
+
+    del options['command']
+    args = options['args']
+    kwargs = options['kwargs']
+    call_command_handler(command, event, *args, **kwargs)
+
+
+def cmd(name):
+    """
+    Decorator to register commands in this namespace
+    """
+    def decorator(func):
+        COMMAND_TO_HANDLER[name] = func
+
+    return decorator
+
+
+@cmd('exit')
+def exit(event, **kwargs):
+    """exit Ctrl + Q"""
+    proxy.stop()
+    event.app.exit()
+
+
+@cmd('play')
+def play(event, *args, **kwargs):
+    if args:
+        index = int(args[0]) - 1
+    else:
+        index = int(event.current_buffer.document.cursor_position_row)
+
+    station = stations[index]
+    proxy.play(**station.serialize())
+    display_buffer = event.app.layout.get_buffer_by_name(DISPLAY_BUFFER)
+    metadata = proxy.now_playing()
+    display_buffer.update(metadata)
+
+
+@cmd('stop')
+def stop(event, **kwargs):
+    display_buffer = event.app.layout.get_buffer_by_name(DISPLAY_BUFFER)
+    display_buffer.clear()
+    proxy.stop()
+
+
+@cmd('pause')
+def pause(event, **kwargs):
+    proxy.pause()
+
+
+@cmd('search')
+def search(event, **kwargs):
+
+    list_buffer = event.app.layout.get_buffer_by_name(LISTVIEW_BUFFER)
+    stations.new(*proxy.search(**kwargs))
+    list_buffer.update(str(stations))
+
+
+@cmd('tags')
+def tags(event, **kwargs):
+    list_buffer = event.app.layout.get_buffer_by_name(LISTVIEW_BUFFER)
+    _tags.new(*proxy.tags())
+    list_buffer.update(str(_tags))
+
+
+@cmd('help')
+def help(event, **kwargs):
+    """Show help"""
+    popup_buffer = event.app.layout.get_buffer_by_name(POPUP_BUFFER)
+    popup_buffer.update(HELP_TEXT)
+    event.app.layout.focus(popup_buffer)
+
+
+@cmd('bookmarks')
+def bookmark(event, *args, **kwargs):
+    list_view_buffer = event.app.layout.get_buffer_by_name(LISTVIEW_BUFFER)
+
+    match kwargs:
+        case {'add': _}:
+            index = int(kwargs['add']) - 1
+            station = stations[index]
+            station = station.serialize()
+            proxy.add_favorite(**station)
+            stations.new(*proxy.favorites())
+            list_view_buffer.update(str(stations))
+        case {'rm': _}:
+            index = int(kwargs['rm']) - 1
+            station = stations[index]
+            station = station.serialize()
+            proxy.remove_favorite(**station)
+            stations.new(*proxy.favorites())
+            list_view_buffer.update(str(stations))
+        case _:
+            stations.new(*proxy.favorites())
+            list_view_buffer.update(str(stations))
---
+++ packages/xradios/xradios-0.0.1.dev2-py3-none-any.whl/xradios/tui/widget/listview.py
@@ -0,0 +1,61 @@
+from prompt_toolkit.keys import Keys
+from prompt_toolkit.layout import BufferControl
+from prompt_toolkit.layout.containers import Window
+from prompt_toolkit.layout.margins import NumberedMargin
+from prompt_toolkit.layout.margins import ScrollbarMargin
+from prompt_toolkit.widgets import Box
+from prompt_toolkit.widgets import Frame
+from prompt_toolkit.key_binding import KeyBindings
+
+from xradios.tui.commands import call_command_handler
+
+
+class ListView:
+    def __init__(self, buffer):
+
+        self.buffer_control = BufferControl(
+            buffer=buffer,
+            focusable=True,
+            key_bindings=self._get_key_bindings(),
+            focus_on_click=True,
+        )
+
+        self.window = Window(
+            content=self.buffer_control,
+            right_margins=[ScrollbarMargin(display_arrows=True)],
+            left_margins=[NumberedMargin()],
+        )
+        self.window = Frame(self.window)
+        self.container = self.window
+
+    def _get_key_bindings(self):
+        "Key bindings for the List."
+        kb = KeyBindings()
+
+        @kb.add("p")
+        @kb.add("enter")
+        def _(event):
+            call_command_handler('play', event)
+
+        @kb.add("s")
+        def _(event):
+            call_command_handler('stop', event)
+
+        @kb.add(Keys.ControlD)
+        def _(event):
+            index = int(event.current_buffer.document.cursor_position_row) + 1
+            call_command_handler(
+                'bookmarks', event, **{'add': index}
+            )
+        # TODO: change shortcut to remove bookmark -> Ctrl + ??
+        @kb.add(Keys.ControlA)
+        def _(event):
+            index = int(event.current_buffer.document.cursor_position_row) + 1
+            call_command_handler(
+                'bookmarks', event, **{'rm': index}
+            )
+
+        return kb
+
+    def __pt_container__(self):
+        return self.container
