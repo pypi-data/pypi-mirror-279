d232a22a7b1d20f5e49f3c5a8d6be468cb038597
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-19 04:05:44 +0000

    Add package simo/2.1.5/simo-2.1.5.tar.gz

---
+++ packages/simo/simo-2.1.5.tar.gz/simo-2.1.5/src/simo/core/apps.py
@@ -0,0 +1,13 @@
+from django.apps import AppConfig
+
+
+
+class SIMOCoreAppConfig(AppConfig):
+    name = 'simo.core'
+
+    def ready(self):
+        from actstream import registry
+        registry.register(self.get_model('Component'))
+        registry.register(self.get_model('Gateway'))
+
+
