edfffb6a6a5cfdf47c5762952a7105fe898eb6ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-19 07:10:23 +0000

    Add package SimpleQAgents/1.0.1/SimpleQAgents-1.0.1-py3-none-any.whl

---
+++ packages/SimpleQAgents/SimpleQAgents-1.0.1-py3-none-any.whl/SimpleQAgent/agents/__init__.py
@@ -0,0 +1,1 @@
+from SimpleQAgent.agents.q_agent import *
---
+++ packages/SimpleQAgents/SimpleQAgents-1.0.1-py3-none-any.whl/SimpleQAgent/networks/deep_q_network.py
@@ -0,0 +1,36 @@
+import os
+from tensorflow.keras.models import Model
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.losses import MeanSquaredError
+from networks import LinearNetwork, ConvNetwork
+
+class DeepQNetwork(Model):
+    def __init__(self, net_type, lr, n_actions, name, 
+                 ckpt_dir):
+        super().__init__()
+        self.net_type = net_type
+        self.lr = lr
+        self.n_actions = n_actions
+        self.checkpoint_dir = ckpt_dir
+        self.checkpoint_file = os.path.join(self.checkpoint_dir, name)
+        
+        if self.net_type == "linear":
+            self.network = LinearNetwork(self.n_actions)
+        else:
+            self.network = ConvNetwork(self.n_actions)
+        
+        self.optimizer = Adam(learning_rate = self.lr)
+        self.loss = MeanSquaredError()
+
+    def call(self, state):
+        actions = self.network(state)
+
+        return actions
+
+    def save_checkpoint(self):
+        print("... saving checkpoint ...")
+        self.save_weights(f"{self.checkpoint_file}.weights.h5")
+
+    def load_checkpoint(self):
+        print("... loading checkpoint ...")
+        self.load_weights(f"{self.checkpoint_file}.weights.h5")
