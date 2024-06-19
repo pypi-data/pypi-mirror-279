e7317e67fbaee62ebf555f732f5498b79f1ff670
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-19 06:11:14 +0000

    Add package sudokuwizard/0.0.1/sudokuwizard-0.0.1.tar.gz

---
+++ packages/sudokuwizard/sudokuwizard-0.0.1.tar.gz/sudokuwizard-0.0.1/SudokuWizard/main.py
@@ -0,0 +1,26 @@
+from sudoku_algorithms import *
+from sudoku_wizard import SudokuWizard
+import cv2 as cv
+import pandas as pd
+import time
+import os
+import re
+
+res_path = 'res/photos/sudoku'
+
+def solve_all():
+    for path in os.listdir(res_path):
+        if not re.search(r'\.(jpg|jpeg|png|webp)$', path):
+            continue
+
+        sw = SudokuWizard()
+        sw.load_image(os.path.join(res_path, path))
+        sw.run(verbose=False, ocr=True)
+
+
+# Load the image and get the sudoku.
+img_path = os.path.join(res_path, 'sudokuLibro3.jpeg')
+
+sw = SudokuWizard()
+sw.load_image(img_path)
+sw.run()
