14c53c71fa5c721116ada9d324a03781b865daf6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-19 05:06:38 +0000

    Add package thermolib/0.5.3/thermolib-0.5.3.tar.gz

---
+++ packages/thermolib/thermolib-0.5.3.tar.gz/thermolib-0.5.3/src/lib.rs
@@ -0,0 +1,25 @@
+//!
+//! thermolib
+//! =========
+//!
+//! An open-source library for
+//! the calculation of fluid properties.
+//!
+//!
+/// Python wrappers
+mod python;
+/// Vdw EOS
+mod vdw;
+pub use vdw::Vdw;
+/// Rk EOS
+mod rk;
+pub use rk::Rk;
+/// Srk EOS
+mod srk;
+pub use srk::Srk;
+/// Pr EOS
+mod pr;
+pub use pr::Pr;
+/// Helmholtz EOS
+mod helmholtz;
+pub use helmholtz::Helmholtz;
