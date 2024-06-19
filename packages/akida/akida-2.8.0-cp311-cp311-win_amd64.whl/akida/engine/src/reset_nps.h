8f318b85a33f469ab520cbc9fd9b4807781f7257
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-19 06:10:16 +0000

    Add package akida/2.8.0/akida-2.8.0-cp311-cp311-win_amd64.whl

---
+++ packages/akida/akida-2.8.0-cp311-cp311-win_amd64.whl/akida/engine/src/reset_nps.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <vector>
+
+#include "infra/hardware_driver.h"
+
+namespace akida {
+
+// This method resets logic and configuration of all NPs. It is available on
+// versions > nsoc v1
+void reset_nps_logic_and_cfg(HardwareDriver* driver);
+
+}  // namespace akida
