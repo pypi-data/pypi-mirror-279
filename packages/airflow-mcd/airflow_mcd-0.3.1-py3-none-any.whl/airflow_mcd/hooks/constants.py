ab093c148ab166648ba748160d8c89462ec78240
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-19 14:05:26 +0000

    Add package airflow-mcd/0.3.1/airflow_mcd-0.3.1-py3-none-any.whl

---
+++ packages/airflow-mcd/airflow_mcd-0.3.1-py3-none-any.whl/airflow_mcd/hooks/constants.py
@@ -0,0 +1,6 @@
+MCD_GATEWAY_CONNECTION_TYPE = "mcd_gateway"
+MCD_GATEWAY_SCOPE = "AirflowCallbacks"
+MCD_GATEWAY_HOSTS = [
+    "https://integrations.getmontecarlo.com",
+    "https://integrations.dev.getmontecarlo.com",
+]
