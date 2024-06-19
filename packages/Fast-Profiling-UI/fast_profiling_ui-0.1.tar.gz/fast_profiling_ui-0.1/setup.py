d26356b33ebc8ad2a0c5935f7f3228b0c293a3ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-19 14:05:24 +0000

    Add package Fast-Profiling-UI/0.1/fast_profiling_ui-0.1.tar.gz

---
+++ packages/Fast-Profiling-UI/fast_profiling_ui-0.1.tar.gz/fast_profiling_ui-0.1/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.1'
+
+# Read the contents of your README file
+with open("README.txt", "r") as fh:
+    long_description = fh.read()
+
+# Setting up
+setup(
+    name='Fast_Profiling_UI',
+    version=VERSION,
+    description='An Exploratory Data Analysis Package for Data Engineers With UI',
+    long_description=long_description,
+    long_description_content_type='text/plain',  # Specify the format of the long description
+    author='Mukesh',
+    author_email='your_email@example.com',  # Optional but recommended
+    url='https://github.com/yourusername/Fast_Profiling_UI',  # Optional but recommended
+    packages=find_packages(),  # Automatically find packages
+    install_requires=[
+        'pandas',
+        'numpy',
+        'tkinter'
+    ],  # List your dependencies here
+    keywords=['python', 'pandas', 'numpy', 'tkinter', 'eda', 'data analysis'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License",  # Example license
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    python_requires='>=3.6',  # Specify your required Python versions
+)
