e828b8bcc30bac3775739ea4f28736cbf4bee748
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-19 07:07:17 +0000

    Add package holger11/0.0/holger11-0.0.tar.gz

---
+++ packages/holger11/holger11-0.0.tar.gz/holger11-0.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+# Read the contents of your README file
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text() #Gets the long description from Readme file
+
+setup(
+    name='holger11',
+    version='0.0',
+    packages=find_packages(),
+    install_requires=[
+        'pyperclip',
+    ],  # Add a comma here
+    author='Yaromir Gusev',
+    author_email='yaromirarhideus@gmail.com',
+    description='asdasdasda',
+
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license='MIT'
+)
