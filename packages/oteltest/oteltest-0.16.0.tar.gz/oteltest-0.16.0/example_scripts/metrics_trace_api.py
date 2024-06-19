f4c977525821d143785941527c80f3fe673550a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-19 07:10:25 +0000

    Add package oteltest/0.16.0/oteltest-0.16.0.tar.gz

---
+++ packages/oteltest/oteltest-0.16.0.tar.gz/oteltest-0.16.0/example_scripts/metrics_trace_api.py
@@ -0,0 +1,41 @@
+from typing import Mapping, Optional, Sequence
+
+
+def print_time(iterations):
+    import time
+    from opentelemetry import metrics, trace
+
+    meter = metrics.get_meter("time-printer", "1.0")
+    counter = meter.create_counter("loop-counter", unit="1", description="my desc")
+    tracer = trace.get_tracer("my.tracer.name")
+    for i in range(iterations):
+        time.sleep(1)
+        counter.add(1)
+        with tracer.start_as_current_span("my-span"):
+            print(f"{i + 1}/{iterations} current time: {round(time.time())}")
+
+
+# We have the option to not inherit from the OtelTest base class, in which case we name our class so it contains
+# "OtelTest". This has the benefit of not requiring a dependency on oteltest in the script's environment.
+class MyOtelTest:
+
+    def environment_variables(self) -> Mapping[str, str]:
+        return {}
+
+    def requirements(self) -> Sequence[str]:
+        return ("splunk-opentelemetry[all]",)
+
+    def wrapper_command(self) -> str:
+        # return "opentelemetry-instrument"
+        return "splunk-py-trace"
+
+    def on_start(self) -> Optional[float]:
+        print("started")
+        return None
+
+    def on_stop(self, tel, stdout: str, stderr: str, returncode: int) -> None:
+        print(f"stopped")
+
+
+if __name__ == "__main__":
+    print_time(10)
