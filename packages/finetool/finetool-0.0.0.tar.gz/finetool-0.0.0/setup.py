a5818e9d18c14d1618edb1b662a8206d830168a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-19 07:06:59 +0000

    Add package finetool/0.0.0/finetool-0.0.0.tar.gz

---
+++ packages/finetool/finetool-0.0.0.tar.gz/finetool-0.0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="finetool",
+    version="0.0.0",
+    description="",
+    author="System Design Studio",
+    author_email="silveryfu@gmail.com",
+    license="Apache License, Version 2.0",
+    packages=find_packages(exclude=("tests",)),
+    python_requires='>=3.10',
+    include_package_data=True,
+    install_requires = open('requirements.txt').readlines(),
+)
+
