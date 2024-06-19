9820fdda78e337523f3d9609edb6a08e84457d9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-19 06:11:05 +0000

    Add package Kenar/0.2.0/kenar-0.2.0.tar.gz

---
+++ packages/Kenar/kenar-0.2.0.tar.gz/kenar-0.2.0/samples/sample_chat.py
@@ -0,0 +1,28 @@
+from kenar.api_client.chatmessage import SendMessageV2Request, BotButton, SetNotifyChatPostConversationsRequest
+from kenar.app import KenarApp
+from samples.sample_app import app_conf
+
+app = KenarApp(app_conf)
+
+if __name__ == '__main__':
+    app.chat.send_message(
+        message_data=SendMessageV2Request(
+            user_id='USER_UUID',
+            peer_id='PEER_UUID',
+            post_token='gZ6QmeWD',
+            type='TEXT',
+            message='سلام این پیام تستی است',
+            sender_btn=BotButton(
+                data=BotButton.ButtonData(icon_name='SEND', caption='direct link', direct_link='https://divar.ir'),
+                action=BotButton.Action.DIRECT_LINK),
+            receiver_btn=BotButton(
+                data=BotButton.ButtonData(icon_name='DELETE', caption='dynamic link', direct_link='https://divar2.ir'),
+                action=BotButton.Action.LINK)
+
+        ),
+        access_token='ACCESS_TOKEN_HERE')
+
+    app.chat.set_notify_chat_post_conversations(
+        data=SetNotifyChatPostConversationsRequest(post_token='gZ6QmeWD', endpoint='https://test2.com',
+                                                   identification_key='thest-identification-key'),
+        access_token='ACCESS_TOKEN_HERE')
