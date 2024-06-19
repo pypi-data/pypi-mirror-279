afca3474d0adf2377ef988864e64aa9fb69529a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-19 06:10:22 +0000

    Add package napari-moltrack/0.0.17/napari_moltrack-0.0.17.tar.gz

---
+++ packages/napari-moltrack/napari_moltrack-0.0.17.tar.gz/napari_moltrack-0.0.17/src/moltrack/dev/measure_speed.py
@@ -0,0 +1,94 @@
+import trackpy as tp
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import numpy as np
+from scipy.spatial.distance import squareform, pdist
+import pickle
+
+path = r"D:\1 Rho-PAmCherry FOV\24-05-02 RhoPAM-plasmid-NusGsfGFP poorM9_17.03.43_18_Channel1_moltrack_tracks.csv"
+path = r"D:\1 Rho-PAmCherry FOV\24-05-02 RhoPAM-plasmid-NusGsfGFP poorM9_17.03.43_18_dataset_channel_moltrack_tracks.csv"
+
+df = pd.read_csv(path, sep=",")
+
+# Constants for the conversion
+pixel_size_nm = 100
+pixel_size_um = 100 *1e-3
+exposure_time_ms = 10
+exposure_time_s = exposure_time_ms * 1e-3
+fps = 1 / exposure_time_s
+
+diffusion_coefficients = []
+
+msd_values = []
+
+
+
+for particle in df.particle.unique():
+    
+    particle_df = df[df.particle == particle].copy()
+    particle_df = particle_df[["particle", "frame", "x", "y"]]
+    
+    # Calculate MSD using trackpy
+    msd_df = tp.motion.msd(particle_df, mpp=pixel_size_um, fps=fps, max_lagtime=100)
+    
+    msd_df = msd_df[["lagt","msd"]]
+    
+    msd_df = msd_df[msd_df["msd"] > 0]
+    
+    msd_values.append(msd_df)
+    
+    break
+    
+# Saving the objects:
+# with open('msd.pkl', 'wb') as f:
+#     pickle.dump(msd_values, f)
+
+# # Getting back the objects:
+# with open('msd.pkl', "rb") as f:
+#     msd_values = pickle.load(f)
+    
+# msd_values = pd.concat(msd_values)
+# msd_values = msd_values.reset_index(drop=True)
+
+
+# grouped = msd_values.groupby('lagt')['msd'].agg(['mean', 'std',"sem"]).reset_index()
+
+# plt.figure(figsize=(10, 6))
+# plt.errorbar(grouped['lagt'], grouped['mean'], yerr=grouped['sem'], fmt='o', capsize=5, capthick=2)
+# plt.xlabel('Time (S)')
+# plt.ylabel('Mean MSD')
+# plt.title('Mean MSD with Error Bars')
+# plt.grid(True)
+# plt.show()
+    
+
+
+
+
+    
+    # print(len(msd_df), len(particle_df))
+
+    
+    # break
+    
+    
+#     # Linear fit to first 10 points of msd vs time
+#     msd_values = msd_df['msd'].values
+#     time = msd_df['lagt'].values
+    
+#     if len(time) >= 10 and len(msd_values) >= 10:  # Ensure there are enough points to fit
+#         slope, intercept = np.polyfit(time[:10], msd_values[:10], 1)
+#         apparent_diffusion = slope / 4  # the slope of MSD vs time gives 4D in 2D
+        
+#         if apparent_diffusion > 0:
+#             diffusion_coefficients.append(apparent_diffusion)
+
+# bin_edges = np.histogram_bin_edges(diffusion_coefficients, bins=50)
+# bin_edges = bin_edges[2:]  # Removes the first bin edge
+
+# # Plotting the histogram of diffusion coefficients
+# plt.hist(diffusion_coefficients, bins=bin_edges)
+# plt.xlabel('Apparent Diffusion Coefficient ($\mu m^2/s$)')
+# plt.ylabel('Frequency')
+# plt.show()
