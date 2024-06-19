e4daf1ac57a5f3ba84109a58cc189ff94fb27f29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-19 14:05:06 +0000

    Add package maitai-python/1.0.17b0/maitai_python-1.0.17b0.tar.gz

---
+++ packages/maitai-python/maitai_python-1.0.17b0.tar.gz/maitai_python-1.0.17b0/setup.py
@@ -0,0 +1,35 @@
+# setup.py
+
+from setuptools import setup, find_packages
+with open("version.txt") as version_file:
+    version = version_file.read().strip()
+
+setup(
+    name='maitai-python',
+    version=version,
+    packages=find_packages(exclude=("maitai_back", "maitai_back.*")),
+    install_requires=[
+        'requests',
+        'openai',
+        'betterproto',
+        'httpx',
+        'aiohttp',
+        'betterproto==2.0.0b6',
+        'websocket-client',
+    ],
+    # Optional metadata
+    author='Maitai',
+    author_email='support@trymaitai.ai',
+    description='Maitai SDK for Python',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://docs.trymaitai.ai',
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+    package_data={
+        '': ['version.txt'],  # Ensure version.txt is included in the package
+    },
+)
