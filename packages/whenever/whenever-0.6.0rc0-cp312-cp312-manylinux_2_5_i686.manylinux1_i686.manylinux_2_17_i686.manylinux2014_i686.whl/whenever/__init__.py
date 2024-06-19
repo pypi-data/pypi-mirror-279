39cb9e044b4678e7e19853debe6b1be8b1fbac0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-19 05:07:10 +0000

    Add package whenever/0.6.0rc0/whenever-0.6.0rc0-cp312-cp312-manylinux_2_5_i686.manylinux1_i686.manylinux_2_17_i686.manylinux2014_i686.whl

---
+++ packages/whenever/whenever-0.6.0rc0-cp312-cp312-manylinux_2_5_i686.manylinux1_i686.manylinux_2_17_i686.manylinux2014_i686.whl/whenever/__init__.py
@@ -0,0 +1,35 @@
+try:
+    from ._whenever import *
+    from ._whenever import (  # pragma: no cover
+        _unpkl_date,
+        _unpkl_ddelta,
+        _unpkl_dtdelta,
+        _unpkl_local,
+        _unpkl_naive,
+        _unpkl_offset,
+        _unpkl_tdelta,
+        _unpkl_time,
+        _unpkl_utc,
+        _unpkl_zoned,
+    )
+
+except ModuleNotFoundError as e:
+    if e.name != "whenever._whenever":  # pragma: no cover
+        raise e
+    from ._pywhenever import *
+    from ._pywhenever import (
+        __all__,
+        __version__,
+        _AwareDateTime,
+        _DateTime,
+        _unpkl_date,
+        _unpkl_ddelta,
+        _unpkl_dtdelta,
+        _unpkl_local,
+        _unpkl_naive,
+        _unpkl_offset,
+        _unpkl_tdelta,
+        _unpkl_time,
+        _unpkl_utc,
+        _unpkl_zoned,
+    )
