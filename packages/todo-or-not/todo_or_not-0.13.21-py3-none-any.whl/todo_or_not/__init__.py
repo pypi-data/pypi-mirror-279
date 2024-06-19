b63b108f5b5483979a3f72f90146b17816f4e845
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-19 14:04:48 +0000

    Add package todo-or-not/0.13.21/todo_or_not-0.13.21-py3-none-any.whl

---
+++ packages/todo-or-not/todo_or_not-0.13.21-py3-none-any.whl/todo_or_not/__init__.py
@@ -0,0 +1,13 @@
+import datetime
+        
+#####################################################
+# VITAL: SEE /scripts/_update_versions.py !!!
+#
+# If updating manually, also consider pyproject.toml
+#
+#####################################################
+
+__version__ = "0.13.21"
+iso_string = "2024-06-18"
+
+version_date = datetime.date.fromisoformat(iso_string)
