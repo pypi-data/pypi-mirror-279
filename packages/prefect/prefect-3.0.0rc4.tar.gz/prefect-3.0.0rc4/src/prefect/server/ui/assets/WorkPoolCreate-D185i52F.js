b61103e3861cca33b5d454d9f089da6d281f36e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-19 14:04:56 +0000

    Add package prefect/3.0.0rc4/prefect-3.0.0rc4.tar.gz

---
+++ packages/prefect/prefect-3.0.0rc4.tar.gz/prefect-3.0.0rc4/src/prefect/server/ui/assets/WorkPoolCreate-D185i52F.js
@@ -0,0 +1,2 @@
+import{d as r,c as s,g as e,h as n,o as l,j as o,k as a,d9 as u,da as _}from"./index-Ba3ionci.js";import{u as c}from"./usePageTitle-B8-40T7W.js";const k=r({__name:"WorkPoolCreate",setup(p){return c("Create Work Pool"),(d,f)=>{const t=n("p-layout-default");return l(),s(t,null,{header:e(()=>[o(a(u))]),default:e(()=>[o(a(_))]),_:1})}}});export{k as default};
+//# sourceMappingURL=WorkPoolCreate-D185i52F.js.map
---
+++ packages/prefect/prefect-3.0.0rc4.tar.gz/prefect-3.0.0rc4/src/prefect/server/ui/assets/usePrefectApi-81jKyPw1.js
@@ -0,0 +1,2 @@
+import{cO as e,cP as r}from"./index-Ba3ionci.js";function c(){return e(r)}export{c as u};
+//# sourceMappingURL=usePrefectApi-81jKyPw1.js.map
