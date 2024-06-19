ead28eae91b8f540f9b679c3becf74b3787b4169
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-19 07:06:57 +0000

    Add package moveread-boxes/0.1.10/moveread_boxes-0.1.10.tar.gz

---
+++ packages/moveread-boxes/moveread_boxes-0.1.10.tar.gz/moveread_boxes-0.1.10/src/moveread/boxes/__init__.pyi
@@ -0,0 +1,8 @@
+from .annotations import Annotations, Rectangle, Vec2, Contours, exportable, ExportableGrid, ExportableContours, ExportableAnnotations, MissingMeta
+from .export_ import export
+
+__all__ = [
+  'Annotations', 'Rectangle', 'Vec2', 'Contours', 'MissingMeta',
+  'exportable', 'ExportableGrid', 'ExportableContours', 'ExportableAnnotations',
+  'export',
+]
