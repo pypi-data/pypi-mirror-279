52956968deb0fc6ac18056744eaa54f05d189d56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-19 06:10:43 +0000

    Add package bezzanlabs.treemachine/1.1.4/bezzanlabs.treemachine-1.1.4-py3-none-any.whl

---
+++ packages/bezzanlabs.treemachine/bezzanlabs.treemachine-1.1.4-py3-none-any.whl/bezzanlabs/treemachine/deep_trees/__init__.py
@@ -0,0 +1,7 @@
+# flake8: noqa
+"""
+Implementation of deep continuous trees, using a tensorflow backend.
+"""
+from .base import BaseDeep
+from .classifier import DeepTreeClassifier
+from .regressor import DeepTreeRegressor
