226f09f217676ebe7c3776e20881166b25888670
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-19 06:11:08 +0000

    Add package morebuiltins/0.0.6/morebuiltins-0.0.6-py3-none-any.whl

---
+++ packages/morebuiltins/morebuiltins-0.0.6-py3-none-any.whl/morebuiltins/__init__.py
@@ -0,0 +1,8 @@
+__version__ = "0.0.6"
+__all__ = [
+    "morebuiltins.utils",
+    "morebuiltins.functools",
+    "morebuiltins.ipc",
+    "morebuiltins.request",
+    "morebuiltins.download_python",
+]
