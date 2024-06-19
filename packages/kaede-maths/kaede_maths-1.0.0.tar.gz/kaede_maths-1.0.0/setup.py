17459b283805b73d2d5c7e82a6590a93ddaec09e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-19 05:07:39 +0000

    Add package kaede-maths/1.0.0/kaede_maths-1.0.0.tar.gz

---
+++ packages/kaede-maths/kaede_maths-1.0.0.tar.gz/kaede_maths-1.0.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='kaede-maths',
+    version='1.0.0',
+    author='Kaede Dev Kento Hinode ',
+    author_email='cleaverdeath@gmail.com',
+    description='A comprehensive library for mathematical operations without external dependencies',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
