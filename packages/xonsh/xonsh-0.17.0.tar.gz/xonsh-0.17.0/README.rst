1607c9a76dcef23a19766d5424c93cd912acb1bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-19 05:06:40 +0000

    Add package xonsh/0.17.0/xonsh-0.17.0.tar.gz

---
+++ packages/xonsh/xonsh-0.17.0.tar.gz/xonsh-0.17.0/README.rst
@@ -0,0 +1,96 @@
+xonsh
+=====
+
+.. class:: center
+
+    **xonsh** is a Python-powered, cross-platform, Unix-gazing shell language and command prompt.
+
+    The language is a superset of Python 3.6+ with additional shell primitives.
+    xonsh (pronounced *conch*) is meant for the daily use of experts and novices alike.
+
+    .. image:: https://raw.githubusercontent.com/xonsh/xonsh/main/docs/_static/what_is_xonsh.png
+            :alt: What is xonsh?
+            :align: center
+
+.. class:: center
+
+    If you like xonsh, :star: the repo, `write a tweet`_ and stay tuned by watching releases.
+
+    .. image:: https://img.shields.io/badge/Zulip%20Community-xonsh-green
+            :target: https://xonsh.zulipchat.com/join/hbvue5rimpdkwkdjuiqfs7tv/
+            :alt: Join to xonsh.zulipchat.com
+
+    .. image:: https://github.com/xonsh/xonsh/actions/workflows/test.yml/badge.svg
+            :target: https://github.com/xonsh/xonsh/actions/workflows/test.yml
+            :alt: GitHub Actions
+
+    .. image:: https://codecov.io/gh/xonsh/xonsh/branch/master/graphs/badge.svg?branch=main
+            :target: https://codecov.io/github/xonsh/xonsh?branch=main
+            :alt: codecov.io
+
+    .. image:: https://repology.org/badge/tiny-repos/xonsh.svg
+            :target: https://repology.org/project/xonsh/versions
+            :alt: repology.org
+
+First steps
+***********
+
+Install xonsh from pip:
+
+.. code-block:: xonshcon
+
+    python -m pip install 'xonsh[full]'
+
+And visit https://xon.sh for more information:
+
+- `Installation <https://xon.sh/contents.html#installation>`_ - using packages, docker or AppImage.
+- `Tutorial <https://xon.sh/tutorial.html>`_ - step by step introduction in xonsh.
+
+Extensions
+**********
+
+Xonsh has an extension/plugin system.  We call these additions ``xontribs``.
+
+- `Xontribs on Github <https://github.com/topics/xontrib>`_
+- `Awesome xontribs <https://github.com/xonsh/awesome-xontribs>`_
+- `Core xontribs <https://xon.sh/api/_autosummary/xontribs/xontrib.html>`_
+- `Create a xontrib step by step from template <https://github.com/xonsh/xontrib-template>`_
+
+Projects that use xonsh or compatible
+*************************************
+
+- `gitsome <https://github.com/donnemartin/gitsome>`_: Supercharged Git/shell autocompleter with GitHub integration.
+- `xxh <https://github.com/xxh/xxh>`_: Using xonsh wherever you go through the SSH.
+- `rever <https://regro.github.io/rever-docs/>`_: Cross-platform software release tool.
+- `Regro autotick bot <https://github.com/regro/cf-scripts>`_: Regro Conda-Forge autoticker.
+- `zoxide <https://github.com/ajeetdsouza/zoxide>`_: A smarter cd command.
+- `any-nix-shell <https://github.com/haslersn/any-nix-shell>`_: xonsh support for the ``nix run`` and ``nix-shell`` environments of the Nix package manager.
+- `Starship <https://starship.rs/>`_: Cross-shell prompt.
+
+Jupyter-based interactive notebooks via `xontrib-jupyter <https://github.com/xonsh/xontrib-jupyter>`_:
+
+- `Jupyter and JupyterLab <https://jupyter.org/>`_: Interactive notebook platform.
+- `euporie <https://github.com/joouha/euporie>`_: Terminal based interactive computing environment.
+- `Jupytext <https://jupytext.readthedocs.io/>`_: Clear and meaningful diffs when doing Jupyter notebooks version control.
+
+The xonsh shell community
+*************************
+
+The xonsh shell is developed by a community of volunteers. There are a few ways to help out:
+
+- Solve a `popular issue <https://github.com/xonsh/xonsh/issues?q=is%3Aissue+is%3Aopen+sort%3Areactions-%2B1-desc>`_. You can start with the `Developer guide <https://xon.sh/devguide.html>`_.
+- Solve a `good first issue <https://github.com/xonsh/xonsh/issues?q=is%3Aopen+is%3Aissue+label%3A%22good+first+issue%22+sort%3Areactions-%2B1-desc>`_.
+- Take an `idea <https://github.com/xonsh/xontrib-template/issues?q=is%3Aopen+is%3Aissue+label%3Aidea+sort%3Areactions-%2B1-desc>`_ and `create a new xontrib <https://github.com/xonsh/xontrib-template#why-use-this-template>`_.
+- Become xonsh core by deep diving into xonsh and improve the threading and subprocess logic.
+- `Become a sponsor to xonsh <https://github.com/sponsors/xonsh>`_.
+- `Write a tweet`_, post or an article to spread the good word about xonsh in the world.
+- Give a star to xonsh repository and to `xontribs <https://github.com/topics/xontrib>`_ you like.
+
+We welcome new contributors!
+
+.. _write a tweet: https://twitter.com/intent/tweet?text=xonsh%20is%20a%20Python-powered,%20cross-platform,%20Unix-gazing%20shell%20language%20and%20command%20prompt.&url=https://github.com/xonsh/xonsh
+
+Credits
+*******
+
+- Thanks to `Zulip <https://zulip.com/>`_ for supporting the xonsh community!
---
+++ packages/xonsh/xonsh-0.17.0.tar.gz/xonsh-0.17.0/tests/test_aliases.py
@@ -0,0 +1,216 @@
+"""Testing built_ins.Aliases"""
+
+import inspect
+import os
+import sys
+
+import pytest
+
+from xonsh.aliases import Aliases, ExecAlias
+
+
+def cd(args, stdin=None):
+    return args
+
+
+def make_aliases():
+    ales = Aliases(
+        {"o": ["omg", "lala"]},
+        color_ls=["ls", "--color=true"],
+        ls="ls '-  -'",
+        cd=cd,
+        indirect_cd="cd ..",
+    )
+    return ales
+
+
+def test_imports(xession):
+    ales = make_aliases()
+    expected = {
+        "o": ["omg", "lala"],
+        "ls": ["ls", "-  -"],
+        "color_ls": ["ls", "--color=true"],
+        "cd": "FuncAlias",
+        "indirect_cd": ["cd", ".."],
+    }
+    raw = ales._raw
+    raw["cd"] = type(ales["cd"]).__name__
+    assert raw == expected
+
+
+def test_eval_normal(xession):
+    ales = make_aliases()
+    assert ales.get("o") == ["omg", "lala"]
+
+
+def test_eval_self_reference(xession):
+    ales = make_aliases()
+    assert ales.get("ls") == ["ls", "-  -"]
+
+
+def test_eval_recursive(xession):
+    ales = make_aliases()
+    assert ales.get("color_ls") == ["ls", "-  -", "--color=true"]
+
+
+def test_eval_recursive_callable_partial(xonsh_execer, xession):
+    ales = make_aliases()
+    xession.env["HOME"] = os.path.expanduser("~")
+    assert ales.get("indirect_cd")(["arg2", "arg3"]) == ["..", "arg2", "arg3"]
+
+
+def _return_to_sender_all(args, stdin, stdout, stderr, spec, stack):
+    return (
+        args,
+        {
+            "stdin": stdin,
+            "stdout": stdout,
+            "stderr": stderr,
+            "spec": spec,
+            "stack": stack,
+        },
+    )
+
+
+def test_recursive_callable_partial_all(xession):
+    ales = Aliases({"rtn": _return_to_sender_all, "rtn-recurse": ["rtn", "arg1"]})
+    alias = ales.get("rtn-recurse")
+    assert callable(alias)
+    args, obs = alias(["arg2"], stdin="a", stdout="b", stderr="c", spec="d", stack="e")
+    assert args == ["arg1", "arg2"]
+    assert len(obs) == 5
+    exp = {"stdin": "a", "stdout": "b", "stderr": "c", "spec": "d", "stack": "e"}
+    assert obs == exp
+
+
+def _return_to_sender_handles(args, stdin, stdout, stderr):
+    return args, {"stdin": stdin, "stdout": stdout, "stderr": stderr}
+
+
+def test_recursive_callable_partial_handles(xession):
+    ales = Aliases({"rtn": _return_to_sender_handles, "rtn-recurse": ["rtn", "arg1"]})
+    alias = ales.get("rtn-recurse")
+    assert callable(alias)
+    args, obs = alias(["arg2"], stdin="a", stdout="b", stderr="c")
+    assert args == ["arg1", "arg2"]
+    assert len(obs) == 3
+    exp = {"stdin": "a", "stdout": "b", "stderr": "c"}
+    assert obs == exp
+
+
+def _return_to_sender_none():
+    return "wakka", {}
+
+
+def test_recursive_callable_partial_none(xession):
+    ales = Aliases({"rtn": _return_to_sender_none, "rtn-recurse": ["rtn"]})
+    alias = ales.get("rtn-recurse")
+    assert callable(alias)
+    args, obs = alias()
+    assert args == "wakka"
+    assert len(obs) == 0
+
+
+@pytest.mark.parametrize(
+    "alias",
+    [
+        "echo 'hi' and echo 'there'",
+        "echo 'hi' or echo 'there'",
+        "echo 'hi' && echo 'there'",
+        "echo 'hi' || echo 'there'",
+        "echo 'hi';  echo 'there'",
+    ],
+)
+def test_subprocess_logical_operators(xession, alias):
+    ales = make_aliases()
+    ales["echocat"] = alias
+    assert isinstance(ales["echocat"], ExecAlias)
+
+
+@pytest.mark.parametrize(
+    "alias",
+    [
+        "echo 'hi' | grep h",
+        "echo 'hi' > file",
+        "cat < file",
+        "COMMAND1 e>o < input.txt | COMMAND2 > output.txt e>> errors.txt",
+        "echo 'h|i' | grep h",
+        "echo 'h|i << x > 3' | grep x",
+    ],
+)
+def test_subprocess_io_operators(xession, alias):
+    ales = make_aliases()
+    ales["echocat"] = alias
+    assert isinstance(ales["echocat"], ExecAlias)
+
+
+@pytest.mark.parametrize(
+    "alias",
+    [
+        {"echocat": "ls"},
+    ],
+)
+def test_dict_merging(xession, alias):
+    ales = make_aliases()
+    assert (ales | alias)["echocat"] == ["ls"]
+    assert (alias | ales)["echocat"] == ["ls"]
+    assert "echocat" not in ales
+
+
+@pytest.mark.parametrize(
+    "alias",
+    [
+        {"echocat": "echo Why do people still use python2.7?"},
+        {"echocat": "echo Why?"},
+    ],
+)
+def test_dict_merging_assignment(xession, alias):
+    ales = make_aliases()
+    ales |= alias
+
+    assert "echocat" in ales
+    assert " ".join(ales["echocat"]) == alias["echocat"]
+
+    ales = make_aliases()
+    alias |= ales
+
+    assert "o" in alias
+    assert alias["o"] == ales["o"]
+
+
+def test_exec_alias_args(xession):
+    stack = inspect.stack()
+    try:
+        ExecAlias("myargs = $args")(["arg0"], stack=stack)
+        ExecAlias("myarg0 = $arg0")(["arg0"], stack=stack)
+    except KeyError:
+        assert False  # noqa
+
+    assert stack[0][0].f_locals["myargs"] == ["arg0"]
+    assert stack[0][0].f_locals["myarg0"] == "arg0"
+
+
+@pytest.mark.parametrize(
+    "exp_rtn",
+    [0, 1, 2],
+)
+def test_exec_alias_return_value(exp_rtn, xonsh_session, monkeypatch):
+    monkeypatch.setitem(xonsh_session.env, "RAISE_SUBPROC_ERROR", False)
+    stack = inspect.stack()
+    rtn = ExecAlias(f"{sys.executable} -c 'exit({exp_rtn})'")([], stack=stack)
+    assert rtn == exp_rtn
+
+
+def test_register_decorator(xession):
+    aliases = Aliases()
+
+    @aliases.register
+    def debug(): ...
+
+    @aliases.register("name")
+    def with_options(): ...
+
+    @aliases.register
+    def _private(): ...
+
+    assert set(aliases) == {"debug", "name", "private"}
