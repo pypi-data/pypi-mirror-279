daf440dbf186a8e1873ac9012551bfb4d8b098f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-19 04:04:53 +0000

    Add package pytoshop-fix-packbits/1.1.3/pytoshop-fix-packbits-1.1.3.tar.gz

---
+++ packages/pytoshop-fix-packbits/pytoshop-fix-packbits-1.1.3.tar.gz/pytoshop-fix-packbits-1.1.3/setup.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+from setuptools import setup
+from setuptools.extension import Extension
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = [
+    "cython",
+    "numpy>=1.11",
+    "six",
+    "typing",
+    "packbits",
+]
+
+test_requirements = [
+    'pytest'
+]
+
+
+try:
+    from Cython.Build import cythonize
+except ImportError:
+    extensions = []
+else:
+    extensions = cythonize([
+        Extension(
+            "pytoshop.packbits",
+            ["pytoshop/packbits.pyx"]
+        )
+    ])
+
+
+setup(
+    name='pytoshop-fix-packbits',
+    version='1.1.3',
+    description="A Python-based library to write Photoshop PSD files",
+    author="original author: Michael Droettboom, modified by: radius5",
+    author_email='info@radius5.co.jp',
+    url='https://github.com/radius5-study/pytoshop-fix-packbits',
+    packages=[
+        'pytoshop',
+        'pytoshop.user'
+    ],
+    package_dir={'pytoshop':
+                 'pytoshop'},
+    include_package_data=True,
+    install_requires=requirements,
+    license="BSD license",
+    zip_safe=False,
+    keywords='pytoshop',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    test_suite='tests',
+    tests_require=test_requirements,
+    ext_modules=extensions
+)
