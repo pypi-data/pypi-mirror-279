7dbc949417bd6fff9c55ba201b87c78354bec0cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-19 04:04:59 +0000

    Add package robotframework-testrail-sync/0.0.8/robotframework_testrail_sync-0.0.8.tar.gz

---
+++ packages/robotframework-testrail-sync/robotframework_testrail_sync-0.0.8.tar.gz/robotframework_testrail_sync-0.0.8/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="robotframework-testrail-sync",
+    version="0.0.8",
+    description="A tool to synchronize Robot Framework tests with TestRail",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author="Viacheslav Shneidmillier",
+    author_email="v.shneidmiller@gmail.com",
+    url="https://github.com/vshneidmiller/robotframework-testrail-sync",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "requests",
+        "PyYAML",
+        "robotframework"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    entry_points={
+        'console_scripts': [
+            'robotestrail=src:main',
+        ],
+    },
+    python_requires='>=3.6',
+)
