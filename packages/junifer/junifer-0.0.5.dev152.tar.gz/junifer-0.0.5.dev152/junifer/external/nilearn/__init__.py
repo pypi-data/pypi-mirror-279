d63e98ba3a884c7284439604feeb9efd1ecfa240
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-19 05:07:50 +0000

    Add package junifer/0.0.5.dev152/junifer-0.0.5.dev152.tar.gz

---
+++ packages/junifer/junifer-0.0.5.dev152.tar.gz/junifer-0.0.5.dev152/junifer/external/nilearn/__init__.py
@@ -0,0 +1,6 @@
+"""Custom objects adapted from nilearn."""
+
+# Authors: Synchon Mandal <s.mandal@fz-juelich.de>
+# License: AGPL
+
+from .junifer_nifti_spheres_masker import JuniferNiftiSpheresMasker
