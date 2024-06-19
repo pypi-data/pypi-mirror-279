f107447038518f2d322dae908cfe1dc5fe8bb47e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-19 07:06:58 +0000

    Add package qarray/1.2.0/qarray-1.2.0-py3-none-any.whl

---
+++ packages/qarray/qarray-1.2.0-py3-none-any.whl/qarray/__init__.py
@@ -0,0 +1,17 @@
+"""
+Qarray, a GPU accelerated quantum dot array simulator, leveraging parallelised Rust and JAX XLA acceleration
+to compute charge stability diagrams of large both open and closed arrays in milliseconds.
+"""
+__version__ = "1.2.0"
+
+from .DotArrays import (DotArray, GateVoltageComposer, ChargeSensedDotArray)
+from .functions import (_optimal_Vg, dot_occupation_changes, charge_state_contrast)
+from .latching_models import *
+from .noise_models import *
+
+__all__ = [
+    'DotArray', 'GateVoltageComposer', 'ChargeSensedDotArray',
+    '_optimal_Vg', 'dot_occupation_changes', 'charge_state_contrast',
+]
+
+submodules = ['latching_models', 'noise_models']
