77e63586e3428fdb22004e779755af6c88858484
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-19 06:10:51 +0000

    Add package wsgi-auth-middleware/0.1.2/wsgi_auth_middleware-0.1.2.tar.gz

---
+++ packages/wsgi-auth-middleware/wsgi_auth_middleware-0.1.2.tar.gz/wsgi_auth_middleware-0.1.2/src/wsgi_auth_middleware/frontends/negotiate.py
@@ -0,0 +1,26 @@
+import base64
+
+
+class NegotiateFrontend:
+    def __init__(self, auth_backends=None):
+        self.auth_backends = auth_backends or []
+
+    def __call__(self, scheme, data):
+        if scheme.lower() != "negotiate":
+            return None
+
+        try:
+            client_token = base64.b64decode(data, validate=True)
+        except Exception:
+            # self.logger.exception("Could not base64 decode the client token")
+            return None
+
+        for ab in self.auth_backends:
+            user = ab(client_token)
+            if user is not None:
+                return user
+        return None
+
+    # TODO: think about supporting mutual authentication
+    def www_authenticate(self):
+        return "Negotiate"
