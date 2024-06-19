d8e633f3aa42c2a2628a077af46a0b1778380a62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-19 04:04:52 +0000

    Add package wicspy/0.0.7/wicspy-0.0.7-py3-none-any.whl

---
+++ packages/wicspy/wicspy-0.0.7-py3-none-any.whl/wicspy/timer_tool.py
@@ -0,0 +1,11 @@
+'''
+Author: wicsp wicspa@gmail.com
+Date: 2024-06-18 19:12:15
+LastEditors: wicsp wicspa@gmail.com
+LastEditTime: 2024-06-18 19:12:27
+FilePath: /wicspy/wicspy/timer.py
+Description: 
+
+Copyright (c) 2024 by wicsp, All Rights Reserved. 
+'''
+import timer
