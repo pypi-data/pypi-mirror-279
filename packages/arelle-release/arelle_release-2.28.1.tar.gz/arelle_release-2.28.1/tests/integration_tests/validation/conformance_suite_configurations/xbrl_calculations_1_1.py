e7985c74c4c40e6db2d58c7e2d54c7ac20130df7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-19 05:08:02 +0000

    Add package arelle-release/2.28.1/arelle_release-2.28.1.tar.gz

---
+++ packages/arelle-release/arelle_release-2.28.1.tar.gz/arelle_release-2.28.1/tests/integration_tests/validation/conformance_suite_configurations/xbrl_calculations_1_1.py
@@ -0,0 +1,17 @@
+from pathlib import PurePath, Path
+from tests.integration_tests.validation.conformance_suite_config import ConformanceSuiteConfig, ConformanceSuiteAssetConfig
+
+config = ConformanceSuiteConfig(
+    assets=[
+        ConformanceSuiteAssetConfig.conformance_suite(
+            Path('calculation-1.1-conformance-2023-12-20.zip'),
+            entry_point=Path('calculation-1.1-conformance-2023-12-20/index.xml'),
+        ),
+    ],
+    info_url='https://specifications.xbrl.org/work-product-index-calculations-2-calculations-1-1.html',
+    membership_url='https://www.xbrl.org/join',
+    name=PurePath(__file__).stem,
+    network_or_cache_required=False,
+    plugins=frozenset({'loadFromOIM', '../../tests/plugin/testcaseCalc11ValidateSetup.py'}),
+    test_case_result_options='match-any',
+)
