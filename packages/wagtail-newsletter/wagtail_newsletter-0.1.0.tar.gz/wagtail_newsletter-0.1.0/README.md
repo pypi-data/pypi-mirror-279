0b750ac7c20c6ed05aff3c6cc4cd5890e0ccf80a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-19 05:06:54 +0000

    Add package wagtail-newsletter/0.1.0/wagtail_newsletter-0.1.0.tar.gz

---
+++ packages/wagtail-newsletter/wagtail_newsletter-0.1.0.tar.gz/wagtail_newsletter-0.1.0/README.md
@@ -0,0 +1,30 @@
+# Wagtail Newsletter
+
+Turn Wagtail pages into newsletters.
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-blue.svg)](https://opensource.org/licenses/MIT)
+[![PyPI version](https://badge.fury.io/py/wagtail-newsletter.svg)](https://badge.fury.io/py/wagtail-newsletter)
+[![Newsletter CI](https://github.com/wagtail/wagtail-newsletter/actions/workflows/test.yml/badge.svg)](https://github.com/wagtail/wagtail-newsletter/actions/workflows/test.yml)
+
+## Links
+
+- [Documentation](https://github.com/wagtail/wagtail-newsletter/blob/main/README.md)
+- [Changelog](https://github.com/wagtail/wagtail-newsletter/blob/main/CHANGELOG.md)
+- [Contributing](https://github.com/wagtail/wagtail-newsletter/blob/main/CONTRIBUTING.md)
+- [Discussions](https://github.com/wagtail/wagtail-newsletter/discussions)
+- [Security](https://github.com/wagtail/wagtail-newsletter/security)
+
+## Supported versions
+
+- Python (3.8, 3.9, 3.10, 3.11, 3.12)
+- Django (4.2, 5.0)
+- Wagtail (5.2, 6.0)
+
+## Installation
+
+- `python -m pip install wagtail-newsletter`
+- ...
+
+## Sample project
+
+There is a sample project in the [demo](./demo/) subdirectory. Check out its README for instructions on how to get it running.
---
+++ packages/wagtail-newsletter/wagtail_newsletter-0.1.0.tar.gz/wagtail_newsletter-0.1.0/wagtail_newsletter/actions.py
@@ -0,0 +1,53 @@
+from typing import cast
+
+from wagtail.admin import messages
+
+from wagtail_newsletter.forms import SendTestEmailForm
+
+from . import campaign_backends
+from .models import NewsletterPageMixin
+
+
+def save_campaign(request, page: NewsletterPageMixin) -> None:
+    backend = campaign_backends.get_backend()
+    revision = cast(NewsletterPageMixin, page.latest_revision.as_object())
+    subject = revision.newsletter_subject or revision.title
+
+    try:
+        campaign_id = backend.save_campaign(
+            campaign_id=page.newsletter_campaign,
+            recipients=revision.newsletter_recipients,
+            subject=subject,
+            html=revision.get_newsletter_html(),
+        )
+
+    except campaign_backends.CampaignBackendError:
+        messages.error(request, "Failed to save newsletter campaign")
+        return
+
+    page.newsletter_campaign = campaign_id
+    page.save(update_fields=["newsletter_campaign"])
+
+    messages.success(
+        request, f"Newsletter campaign {subject!r} has been saved to {backend.name}"
+    )
+
+
+def send_test_email(request, page: NewsletterPageMixin):
+    form = SendTestEmailForm(request.POST, prefix="newsletter-test")
+    if not form.is_valid():
+        for field, errors in form.errors.items():
+            for message in errors:
+                messages.error(request, f"{field!r}: {message}")
+        return
+
+    email = form.cleaned_data["email"]
+
+    save_campaign(request, page)
+
+    backend = campaign_backends.get_backend()
+    backend.send_test_email(
+        campaign_id=page.newsletter_campaign,
+        email=email,
+    )
+    messages.success(request, f"Test message sent to {email!r}")
