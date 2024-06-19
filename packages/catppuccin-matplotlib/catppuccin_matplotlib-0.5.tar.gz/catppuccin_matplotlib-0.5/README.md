d2e481907de3515e273a9f1d36fbf27fdc91de2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-19 04:05:02 +0000

    Add package catppuccin-matplotlib/0.5/catppuccin_matplotlib-0.5.tar.gz

---
+++ packages/catppuccin-matplotlib/catppuccin_matplotlib-0.5.tar.gz/catppuccin_matplotlib-0.5/README.md
@@ -0,0 +1,6 @@
+<h2 align="center">🚧 This repository has moved to <a href="https://github.com/catppuccin/python/tree/main">catppuccin/python</a> 🚧</h2>
+
+See https://github.com/catppuccin/python#matplotlib for the documentation and instructions for updating your code.
+
+<p align="center"><img src="https://raw.githubusercontent.com/catppuccin/catppuccin/main/assets/footers/gray0_ctp_on_line.svg?sanitize=true" /></p>
+<p align="center">Copyright &copy; 2021-present <a href="https://github.com/catppuccin" target="_blank">Catppuccin Org</a>
---
+++ packages/catppuccin-matplotlib/catppuccin_matplotlib-0.5.tar.gz/catppuccin_matplotlib-0.5/src/mplcatppuccin/__init__.py
@@ -0,0 +1,28 @@
+"""Soothing pastel theme for matplotlib"""
+
+import warnings
+
+import matplotlib.style
+import matplotlib as mpl
+import pkg_resources
+from .colormaps import get_colormap_from_list
+
+__version__ = pkg_resources.require("catppuccin-matplotlib")[0].version
+__author__ = "Bram de Wilde <contact@bramdewilde.com>"
+__all__ = []
+
+warning_message = """The catppuccin-matplotlib package is deprecated, please upgrade to https://github.com/catppuccin/python (pip install catppuccin)
+
+"""
+
+warnings.warn(warning_message, DeprecationWarning, stacklevel=2)
+
+# Register the included stylesheet in the mpl style library
+data_path = pkg_resources.resource_filename("mplcatppuccin", "data/")
+catppuccin_stylesheets = mpl.style.core.read_style_directory(data_path)
+mpl.style.core.update_nested_dict(mpl.style.library, catppuccin_stylesheets)
+
+# Register default colormaps
+for palette_name in ["latte", "frappe", "macchiato", "mocha"]:
+    cmap = get_colormap_from_list(palette_name, ["base", "blue"])
+    mpl.colormaps.register(cmap=cmap, name=palette_name)
