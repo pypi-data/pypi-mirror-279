b814d9fe7a38daf4906cc65b1ad8f612945e9a6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-19 06:10:46 +0000

    Add package XDCweb3/1.0.1/xdcweb3-1.0.1.tar.gz

---
+++ packages/XDCweb3/xdcweb3-1.0.1.tar.gz/xdcweb3-1.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='XDCweb3',
+    version='1.0.1',
+    description='XDCweb3.py is a Python library for interacting with XDC (XinFin Digital Contract) tokens using web3.py',
+    long_description=open('README.md', 'r', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/Ziusz/XDCweb3.py',
+    author='Ziusz',
+    author_email='ziusz@outlook.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    keywords='xdc sdk web3.py smartcontract token xrc20 xrc721 xinfin digital contract web3',
+    packages=find_packages("XDCweb3"),
+    install_requires=[
+        'web3', 
+    ],
+)
