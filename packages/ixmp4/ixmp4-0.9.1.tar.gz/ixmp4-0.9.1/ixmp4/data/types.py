49963bb9344fa33149e83a23bcd50aa0152feaa2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-19 04:04:56 +0000

    Add package ixmp4/0.9.1/ixmp4-0.9.1.tar.gz

---
+++ packages/ixmp4/ixmp4-0.9.1.tar.gz/ixmp4-0.9.1/ixmp4/data/types.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+from typing import Any
+
+from sqlalchemy.orm import Mapped as Mapped
+
+Boolean = Mapped[bool]
+DateTime = Mapped[datetime]
+Float = Mapped[float]
+Integer = Mapped[int]
+JsonList = Mapped[list[float | int | str]]
+JsonDict = Mapped[dict[str, Any]]
+String = Mapped[str]
