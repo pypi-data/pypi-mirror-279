8493ea776ad76eed28df1a84c3ce483911d73f64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-19 06:11:02 +0000

    Add package gridemissions/0.1.6/gridemissions-0.1.6.tar.gz

---
+++ packages/gridemissions/gridemissions-0.1.6.tar.gz/gridemissions-0.1.6/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="gridemissions",
+    package_dir={"": "src"},
+    packages=find_packages("src"),
+    version="0.1.6",
+    python_requires=">=3.7",
+    install_requires=["requests", "pandas>=2.0", "matplotlib>=3.4.2"],
+    extras_require={
+        "all": [
+            "dask[complete]",
+            "joblib",
+            "cmocean",
+            "cvxpy>=1.4.3",
+            "scipy>=1.10.1",
+            "seaborn",
+            "syrupy",
+            "pytest",
+        ]
+    },
+    entry_points={
+        "console_scripts": [
+            "ge_download=gridemissions.scripts.download:main",
+            "ge_update_live_dataset=gridemissions.scripts.update_live_dataset:main",
+            "ge_report=gridemissions.scripts.report:main",
+        ],
+    },
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+)
