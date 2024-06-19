44c50ca6a632ed88e5732269533dd6e0b4ab9e2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-19 04:05:28 +0000

    Add package robust-extraction2/0.1.0/robust_extraction2-0.1.0-py3-none-any.whl

---
+++ packages/robust-extraction2/robust_extraction2-0.1.0-py3-none-any.whl/robust_extraction2/templates/models.py
@@ -0,0 +1,10 @@
+from .templates import ExtendedModel
+
+FCDE = ExtendedModel(
+  boxWidth=0.14509394397496592,
+  rows=25,
+  columns=[None, 0.02818371763906058, 0.03653445043604164, None, 0.02818371763906058, 0.03653445043604164, None],
+  pre_row_offsets=[1/25],
+  post_row_offsets=[1.8/25, 0.8/25]
+)
+"""Official model of the Catalan Chess Federation (Federació Catalana d'Escacs)"""
