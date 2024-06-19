7cdd068caadab1fb7102f1efe4b27398d170702b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-19 07:09:38 +0000

    Add package jump-portrait/0.0.17/jump_portrait-0.0.17.tar.gz

---
+++ packages/jump-portrait/jump_portrait-0.0.17.tar.gz/jump_portrait-0.0.17/src/jump_portrait/workflow.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env jupyter
+"""
+Workflow 1: Fetch one image for a given item and a control
+"""
+from jump_portrait.fetch import get_jump_image, get_sample
+from jump_portrait.save import download_item_images
+
+sample = get_sample()
+
+source, batch, plate, well, site, *rest = sample.row(0)
+channel = "DNA"
+correction = None#"Illum"
+
+img = get_jump_image(source, batch, plate, well, channel, site, correction)
+
+"""
+Workflow 2: Fetch all images for a given item and their controls
+"""
+
+item_name = "MYT1"  # Item or Compound of interest - (GC)OI
+channels = ["DNA"]  # Standard channels are ER, AGP, Mito DNA and RNA
+corrections = ["Orig"]  # Can also be "Illum"
+controls = True  # Fetch controls in plates alongside (GC)OI?
+
+download_item_images(item_name, channels, corrections=corrections, controls=controls)
+
+"""
+Workflow 3: Fetch bright field channel
+Note that this is hacky and may not work for all sources.
+"""
+
+sample = get_sample()
+
+# channel = "DNA"
+channel = "bf"
+correction = None#"Illum"
+
+import polars as pl
+source, batch, plate, well, site, *rest = sample.row(0)
+img = get_jump_image(source, batch, plate, well, channel, site, correction)
