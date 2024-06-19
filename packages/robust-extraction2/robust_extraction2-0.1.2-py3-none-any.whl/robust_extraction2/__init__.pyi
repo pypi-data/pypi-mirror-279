39d87db697e20394b56ec694752d6c7e19ee81cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-19 07:06:47 +0000

    Add package robust-extraction2/0.1.2/robust_extraction2-0.1.2-py3-none-any.whl

---
+++ packages/robust-extraction2/robust_extraction2-0.1.2-py3-none-any.whl/robust_extraction2/__init__.pyi
@@ -0,0 +1,12 @@
+from ._types import Segment, Segments, Vec2, Vecs2, Contour, Contours, Img, Img2D, Img3D, Pads, RequiredPads, Corners
+from .templates import ExtendedModel
+from .pipeline import Result, Error, extract
+from .contours import boxes
+from . import contours, lines, vectors, templates, perspective, pipeline, match1d
+
+__all__ = [
+  'Segment', 'Segments', 'Vec2', 'Vecs2', 'Contour', 'Contours', 'Img', 'Img2D', 'Img3D',
+  'Pads', 'RequiredPads', 'Corners', 'perspective', 'match1d',
+  'contours', 'lines', 'vectors', 'templates', 'ExtendedModel', 'pipeline',
+  'Result', 'Error', 'extract', 'boxes',
+]
