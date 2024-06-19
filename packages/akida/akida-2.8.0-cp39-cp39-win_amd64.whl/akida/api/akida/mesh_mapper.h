2be70a7a62c1d638f68660e3f1715d5e139d204a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-19 06:10:24 +0000

    Add package akida/2.8.0/akida-2.8.0-cp39-cp39-win_amd64.whl

---
+++ packages/akida/akida-2.8.0-cp39-cp39-win_amd64.whl/akida/api/akida/mesh_mapper.h
@@ -0,0 +1,52 @@
+#pragma once
+
+#include <memory>
+#include <tuple>
+#include <vector>
+
+#include "akida/np.h"
+#include "infra/exports.h"
+
+namespace akida {
+
+class MeshMapper;
+
+using MeshMapperPtr = std::shared_ptr<MeshMapper>;
+
+class AKIDASHAREDLIB_EXPORT MeshMapper {
+ public:
+  virtual ~MeshMapper() {}
+  /**
+   * @brief Select a set of Neural Processors (NP)
+   *
+   * This allows to select from a predefined list a specified number of NPs.
+   */
+  virtual hw::IdentVector select_nps(const hw::IdentVector& source_nps,
+                                     size_t num_nps, hw::Type type) = 0;
+
+  /**
+   * @brief Get the maximum width allowed for a CNP
+   */
+  virtual uint32_t cnp_max_width() = 0;
+
+  /**
+   * @brief Get the maximum height allowed for a CNP
+   */
+  virtual uint32_t cnp_max_height() = 0;
+
+  /**
+   * @brief Get the maximum number of filters allowed for a CNP
+   */
+  virtual uint32_t cnp_max_filters() = 0;
+
+  /**
+   * @brief Override the default MeshMapper
+   *
+   * Passing nullptr to this method restores the default MeshMapper.
+   *
+   * @param : a pointer to a MeshMapper instance, or nullptr
+   */
+  static void replace(MeshMapperPtr mapper);
+};
+
+}  // namespace akida
