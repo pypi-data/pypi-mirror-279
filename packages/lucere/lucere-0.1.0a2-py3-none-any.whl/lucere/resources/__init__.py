9ba2257da22123a2f3932a14d117e6796a53c9c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-19 05:06:54 +0000

    Add package lucere/0.1.0a2/lucere-0.1.0a2-py3-none-any.whl

---
+++ packages/lucere/lucere-0.1.0a2-py3-none-any.whl/lucere/resources/__init__.py
@@ -0,0 +1,33 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from .models import (
+    ModelsResource,
+    AsyncModelsResource,
+    ModelsResourceWithRawResponse,
+    AsyncModelsResourceWithRawResponse,
+    ModelsResourceWithStreamingResponse,
+    AsyncModelsResourceWithStreamingResponse,
+)
+from .chat_completions import (
+    ChatCompletionsResource,
+    AsyncChatCompletionsResource,
+    ChatCompletionsResourceWithRawResponse,
+    AsyncChatCompletionsResourceWithRawResponse,
+    ChatCompletionsResourceWithStreamingResponse,
+    AsyncChatCompletionsResourceWithStreamingResponse,
+)
+
+__all__ = [
+    "ModelsResource",
+    "AsyncModelsResource",
+    "ModelsResourceWithRawResponse",
+    "AsyncModelsResourceWithRawResponse",
+    "ModelsResourceWithStreamingResponse",
+    "AsyncModelsResourceWithStreamingResponse",
+    "ChatCompletionsResource",
+    "AsyncChatCompletionsResource",
+    "ChatCompletionsResourceWithRawResponse",
+    "AsyncChatCompletionsResourceWithRawResponse",
+    "ChatCompletionsResourceWithStreamingResponse",
+    "AsyncChatCompletionsResourceWithStreamingResponse",
+]
