59bf41f8f87478eb9066eb2ad10c455897174cde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-19 14:05:10 +0000

    Add package xradios/0.0.1.dev3/xradios-0.0.1.dev3.tar.gz

---
+++ packages/xradios/xradios-0.0.1.dev3.tar.gz/xradios-0.0.1.dev3/xradios/tui/buffers/command_line.py
@@ -0,0 +1,53 @@
+from prompt_toolkit.buffer import Buffer
+from prompt_toolkit.document import Document
+from prompt_toolkit.completion import WordCompleter
+
+from xradios.tui.constants import COMMAND_LINE_BUFFER
+
+
+COMMANDS = [
+    "play",
+    "stop",
+    "exit",
+    "search",
+    "name=",
+    "name_exact=",
+    "country=",
+    "country_exact=",
+    "countrycode=",
+    "state=",
+    "state_exact=",
+    "language=",
+    "language_exact=",
+    "tag=",
+    "tag_exact=",
+    "bitrate_min=",
+    "bitrate_max=",
+    "has_geo_info=",
+    "has_extended_info=",
+    "is_https=",
+    "order=",
+    "reverse=",
+    "limit=",
+    "hidebroken=",
+    "bookmarks",
+    "home",
+    "add=",
+    "rm=",
+    "tags",
+]
+
+
+completer = WordCompleter(COMMANDS, ignore_case=True)
+
+
+class CommandLineBuffer(Buffer):
+    def __init__(self, *args, **kwargs):
+        super().__init__(
+            completer=completer,
+            complete_while_typing=True,
+            name=COMMAND_LINE_BUFFER
+        )
+
+
+buffer = CommandLineBuffer()
