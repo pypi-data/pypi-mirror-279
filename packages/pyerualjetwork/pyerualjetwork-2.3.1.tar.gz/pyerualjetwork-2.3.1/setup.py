c7e2348f947ae5e9afc88a66e9f53f05e2d74280
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-19 07:06:26 +0000

    Add package pyerualjetwork/2.3.1/pyerualjetwork-2.3.1.tar.gz

---
+++ packages/pyerualjetwork/pyerualjetwork-2.3.1.tar.gz/pyerualjetwork-2.3.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+# Setting Up
+
+setup(
+      
+      name = "pyerualjetwork",
+      version = "2.3.1",
+      author = "Hasan Can Beydili",
+      author_email = "tchasancan@gmail.com",
+      description= "Weights now noramlized, new function: manuel_balancer. And scaler_params added for scaled models.",
+      packages = find_packages(),
+      keywords = ["model evaluation", "classifcation", 'pruning learning artficial neural networks'],
+
+      
+      )
