c3d67527ed738c504097431e72e3e147a263f1ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-19 04:04:56 +0000

    Add package ixmp4/0.9.1/ixmp4-0.9.1-py3-none-any.whl

---
+++ packages/ixmp4/ixmp4-0.9.1-py3-none-any.whl/ixmp4/data/db/optimization/indexset/model.py
@@ -0,0 +1,38 @@
+from typing import ClassVar
+
+from sqlalchemy import UniqueConstraint
+from sqlalchemy.orm import validates
+
+from ixmp4 import db
+from ixmp4.data import types
+from ixmp4.data.abstract import optimization as abstract
+
+from .. import base
+
+
+class IndexSet(base.BaseModel):
+    NotFound: ClassVar = abstract.IndexSet.NotFound
+    NotUnique: ClassVar = abstract.IndexSet.NotUnique
+    DeletionPrevented: ClassVar = abstract.IndexSet.DeletionPrevented
+
+    name: types.String = db.Column(db.String(255), nullable=False, unique=False)
+    elements: types.JsonList = db.Column(db.JsonType, nullable=False, default=[])
+
+    created_at: types.DateTime = db.Column(db.DateTime, nullable=True)
+    created_by: types.String = db.Column(db.String(255), nullable=True)
+
+    run__id: types.Integer = db.Column(
+        db.Integer, db.ForeignKey("run.id"), nullable=False, index=True
+    )
+
+    __table_args__ = (UniqueConstraint(name, run__id),)
+
+    @validates("elements")
+    def validate_elements(self, key, value: list[float | int | str]):
+        unique = set()
+        for element in value:
+            if element in unique:
+                raise ValueError(f"{element} already defined for IndexSet {self.name}!")
+            else:
+                unique.add(element)
+        return value
