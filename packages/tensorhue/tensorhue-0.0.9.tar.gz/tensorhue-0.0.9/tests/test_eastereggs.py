fbb6928e27b448efe837d160dd5734dd3959c281
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-19 05:07:08 +0000

    Add package tensorhue/0.0.9/tensorhue-0.0.9.tar.gz

---
+++ packages/tensorhue/tensorhue-0.0.9.tar.gz/tensorhue-0.0.9/tests/test_eastereggs.py
@@ -0,0 +1,8 @@
+from tensorhue.eastereggs import pride
+
+
+def test_pride_output(capsys):
+    pride()
+    captured = capsys.readouterr()
+    assert len(captured.out.split("\n")) == 5
+    assert captured.out.count("▀") == 30
