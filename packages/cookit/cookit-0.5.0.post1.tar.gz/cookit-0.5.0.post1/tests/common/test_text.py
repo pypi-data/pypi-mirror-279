8c549ed6c63987e8680d2d882ff8a97ece1d2646
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-19 06:10:45 +0000

    Add package cookit/0.5.0.post1/cookit-0.5.0.post1.tar.gz

---
+++ packages/cookit/cookit-0.5.0.post1.tar.gz/cookit-0.5.0.post1/tests/common/test_text.py
@@ -0,0 +1,46 @@
+def test_camel_case_lower_first():
+    from cookit.common.text import camel_case
+
+    assert camel_case("hello_world") == "helloWorld"
+
+
+def test_camel_case_upper_first():
+    from cookit.common.text import camel_case
+
+    assert camel_case("hello_world", upper_first=True) == "HelloWorld"
+
+
+def test_camel_case_single_word():
+    from cookit.common.text import camel_case
+
+    assert camel_case("hello") == "hello"
+
+
+def test_camel_case_empty_string():
+    from cookit.common.text import camel_case
+
+    assert camel_case("") == ""
+
+
+def test_full_to_half():
+    from cookit.common.text import full_to_half
+
+    assert full_to_half("Ｈｅｌｌｏ　Ｗｏｒｌｄ！") == "Hello World!"
+    assert full_to_half("１２３４５６７８９０") == "1234567890"
+    assert full_to_half("ＡＢＣＤＥＦＧ") == "ABCDEFG"
+    assert full_to_half("！＠＃＄％＾＆＊") == "!@#$%^&*"
+    assert full_to_half("［］｛｝（）＜＞＝＋－／＊＆％＃＠") == "[]{}()<>=+-/*&%#@"
+    assert (
+        full_to_half("ａｂｃｄｅｆｇｈｉｊｋｌｍｎｏｐｑｒｓｔｕｖｗｘｙｚ")
+        == "abcdefghijklmnopqrstuvwxyz"
+    )
+    assert (
+        full_to_half("ＡＢＣＤＥＦＧＨＩＪＫＬＭＮＯＰＱＲＳＴＵＶＷＸＹＺ")
+        == "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    )
+    assert (
+        full_to_half(
+            "１２３４５６７８９０！＠＃＄％＾＆＊（）＿＋｛｝［］：＂；＇＜＞，．／？",
+        )
+        == "1234567890!@#$%^&*()_+{}[]:\";'<>,./?"
+    )
