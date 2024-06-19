99af45a935d1a0daf1325e961f562ffdd29f6995
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-19 14:05:21 +0000

    Add package spot2mp3/0.1.1/spot2mp3-0.1.1.tar.gz

---
+++ packages/spot2mp3/spot2mp3-0.1.1.tar.gz/spot2mp3-0.1.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="spot2mp3",
+    version="0.1.1",
+    description="A CLI tool to download Spotify playlist/album tracks as mp3 files.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Oliver Huang",
+    author_email="ohuang4131@gmail.com",
+    url="https://github.com/gooosexe/spot2mp3",
+    license="GPLv3",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+		"License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+    entry_points={
+        'console_scripts': [
+            'spot2mp3=spot2mp3.cli:main',
+        ],
+    },
+)
