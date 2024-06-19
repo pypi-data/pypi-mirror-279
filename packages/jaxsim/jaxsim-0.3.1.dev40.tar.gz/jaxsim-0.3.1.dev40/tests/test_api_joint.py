33226599d449133b1ad73ca83afca4d087fe36a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-19 06:10:48 +0000

    Add package jaxsim/0.3.1.dev40/jaxsim-0.3.1.dev40.tar.gz

---
+++ packages/jaxsim/jaxsim-0.3.1.dev40.tar.gz/jaxsim-0.3.1.dev40/tests/test_api_joint.py
@@ -0,0 +1,35 @@
+import jax.numpy as jnp
+import pytest
+
+import jaxsim.api as js
+
+
+def test_joint_index(
+    jaxsim_models_types: js.model.JaxSimModel,
+):
+
+    model = jaxsim_models_types
+
+    # =====
+    # Tests
+    # =====
+
+    for idx, joint_name in enumerate(model.joint_names()):
+        assert js.joint.name_to_idx(model=model, joint_name=joint_name) == idx
+        assert js.joint.idx_to_name(model=model, joint_index=idx) == joint_name
+
+    assert js.joint.names_to_idxs(
+        model=model, joint_names=model.joint_names()
+    ) == pytest.approx(jnp.arange(model.number_of_joints()))
+
+    assert (
+        js.joint.idxs_to_names(
+            model=model,
+            joint_indices=tuple(
+                js.joint.names_to_idxs(
+                    model=model, joint_names=model.joint_names()
+                ).tolist()
+            ),
+        )
+        == model.joint_names()
+    )
