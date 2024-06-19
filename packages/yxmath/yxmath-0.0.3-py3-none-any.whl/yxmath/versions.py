2ff2022990ca159a1c97227113f7d7ee0707e524
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-19 07:07:55 +0000

    Add package yxmath/0.0.3/yxmath-0.0.3-py3-none-any.whl

---
+++ packages/yxmath/yxmath-0.0.3-py3-none-any.whl/yxmath/versions.py
@@ -0,0 +1,24 @@
+def get_versions():
+    return versions[0]["number"]
+
+
+versions = [
+    {
+        "number": "0.0.3",
+        "features": [
+            "1. add histogram_ploter function",
+        ],
+    },
+    {
+        "number": "0.0.2",
+        "features": [
+            "1. add plot module",
+        ],
+    },
+    {
+        "number": "0.0.1",
+        "features": [
+            "1. init",
+        ],
+    },
+]
