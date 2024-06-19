7f14edd4ce16f522878f1e80bc933d6fc6b9a4d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-19 04:05:29 +0000

    Add package smufolib/0.1.5/smufolib-0.1.5.tar.gz

---
+++ packages/smufolib/smufolib-0.1.5.tar.gz/smufolib-0.1.5/smufolib/__init__.py
@@ -0,0 +1,12 @@
+"""where SMuFL meets UFO
+
+"""
+from smufolib._version import __version__
+from smufolib.objects.engravingDefaults import EngravingDefaults
+from smufolib.objects.font import Font
+from smufolib.objects.glyph import Glyph
+from smufolib.objects.layer import Layer
+from smufolib.objects.range import Range
+from smufolib.objects.smufl import Smufl
+from smufolib.request import Request
+from smufolib.request import URLWarning
