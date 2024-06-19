93945b7626922a3bcdd764bb8ebffc793e42b5d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-19 04:04:52 +0000

    Add package magpylib/5.0.4/magpylib-5.0.4.tar.gz

---
+++ packages/magpylib/magpylib-5.0.4.tar.gz/magpylib-5.0.4/magpylib/_src/display/backend_plotly.py
@@ -0,0 +1,363 @@
+"""plotly backend"""
+
+# pylint: disable=C0302
+# pylint: disable=too-many-branches
+import inspect
+from functools import lru_cache
+
+import numpy as np
+
+try:
+    import plotly.graph_objects as go
+except ImportError as missing_module:  # pragma: no cover
+    raise ModuleNotFoundError(
+        """In order to use the plotly plotting backend, you need to install plotly via pip or conda,
+        see https://github.com/plotly/plotly.py"""
+    ) from missing_module
+
+from magpylib._src.defaults.defaults_utility import linearize_dict
+from magpylib._src.display.traces_utility import get_scene_ranges
+
+SYMBOLS_TO_PLOTLY = {
+    ".": "circle",
+    "o": "circle",
+    "+": "cross",
+    "D": "diamond",
+    "d": "diamond",
+    "s": "square",
+    "x": "x",
+}
+
+LINESTYLES_TO_PLOTLY = {
+    "solid": "solid",
+    "-": "solid",
+    "dashed": "dash",
+    "--": "dash",
+    "dashdot": "dashdot",
+    "-.": "dashdot",
+    "dotted": "dot",
+    ".": "dot",
+    ":": "dot",
+    (0, (1, 1)): "dot",
+    "loosely dotted": "longdash",
+    "loosely dashdotted": "longdashdot",
+}
+
+SIZE_FACTORS_TO_PLOTLY = {
+    "line_width": 2.2,
+    "marker_size": 0.7,
+}
+
+
+@lru_cache(maxsize=None)  # Cache all results
+def match_args(ttype: str):
+    """
+    Return named arguments of a Plotly graph object function.
+
+    Parameters
+    ----------
+    ttype : str
+        Type of Plotly graph object (e.g., 'scatter' for go.Scatter).
+
+    Returns
+    -------
+    set
+        Names of the named arguments of the specified function.
+    """
+    func = getattr(go, ttype.capitalize())
+    sig = inspect.signature(func)
+    params = sig.parameters
+    named_args = [
+        name
+        for name, param in params.items()
+        if param.default != inspect.Parameter.empty
+    ]
+    return set(named_args)
+
+
+def apply_fig_ranges(fig, ranges, apply2d=True):
+    """This is a helper function which applies the ranges properties of the provided `fig` object
+    according to a provided ranges. All three space direction will be equal and match the
+    maximum of the ranges needed to display all objects, including their paths.
+
+    Parameters
+    ----------
+    ranges: array of dimension=(3,2)
+        min and max graph range
+
+    apply2d: bool, default = True
+        applies fixed range also on 2d traces
+
+    Returns
+    -------
+    None: NoneType
+    """
+    fig.update_scenes(
+        **{
+            f"{k}axis": {"range": ranges[i], "autorange": False, "title": f"{k} (m)"}
+            for i, k in enumerate("xyz")
+        },
+        aspectratio={k: 1 for k in "xyz"},
+        aspectmode="manual",
+        camera_eye={"x": 1, "y": -1.5, "z": 1.4},
+    )
+    if apply2d:
+        apply_2d_ranges(fig)
+
+
+def apply_2d_ranges(fig, factor=0.05):
+    """Apply Figure ranges of 2d plots"""
+    traces = fig.data
+    ranges = {}
+    for t in traces:
+        for k in "xy":
+            try:
+                ax_str = getattr(t, f"{k}axis")
+                ax_suff = ax_str.replace(k, "")
+                if ax_suff not in ranges:
+                    ranges[ax_suff] = {"x": [], "y": []}
+                vals = getattr(t, k)
+                ranges[ax_suff][k].append([min(vals), max(vals)])
+            except AttributeError:
+                pass
+    for ax, r in ranges.items():
+        for k in "xy":
+            m, M = [np.min(r[k]), np.max(r[k])]
+            getattr(fig.layout, f"{k}axis{ax}").range = [
+                m - (M - m) * factor,
+                M + (M - m) * factor,
+            ]
+
+
+def animate_path(
+    fig,
+    frames,
+    path_indices,
+    frame_duration,
+    animation_slider=False,
+    update_layout=True,
+    rows=None,
+    cols=None,
+):
+    """This is a helper function which attaches plotly frames to the provided `fig` object
+    according to a certain zoom level. All three space direction will be equal and match the
+    maximum of the ranges needed to display all objects, including their paths.
+    """
+    fps = int(1000 / frame_duration)
+    if animation_slider:
+        sliders_dict = {
+            "active": 0,
+            "yanchor": "top",
+            "font": {"size": 10},
+            "xanchor": "left",
+            "currentvalue": {
+                "prefix": f"Fps={fps}, Path index: ",
+                "visible": True,
+                "xanchor": "right",
+            },
+            "pad": {"b": 10, "t": 10},
+            "len": 0.9,
+            "x": 0.1,
+            "y": 0,
+            "steps": [],
+        }
+
+    buttons_dict = {
+        "buttons": [
+            {
+                "args": [
+                    None,
+                    {
+                        "frame": {"duration": frame_duration},
+                        "transition": {"duration": 0},
+                        "fromcurrent": True,
+                    },
+                ],
+                "label": "Play",
+                "method": "animate",
+            },
+            {
+                "args": [[None], {"frame": {"duration": 0}, "mode": "immediate"}],
+                "label": "Pause",
+                "method": "animate",
+            },
+        ],
+        "direction": "left",
+        "pad": {"r": 10, "t": 20},
+        "showactive": False,
+        "type": "buttons",
+        "x": 0.1,
+        "xanchor": "right",
+        "y": 0,
+        "yanchor": "top",
+    }
+
+    for ind in path_indices:
+        if animation_slider:
+            slider_step = {
+                "args": [
+                    [str(ind + 1)],
+                    {
+                        "frame": {"duration": 0, "redraw": True},
+                        "mode": "immediate",
+                    },
+                ],
+                "label": str(ind + 1),
+                "method": "animate",
+            }
+            sliders_dict["steps"].append(slider_step)
+
+    # update fig
+    fig.frames = frames
+    frame0 = fig.frames[0]
+    fig.add_traces(
+        frame0.data,
+        rows=rows,
+        cols=cols,
+    )
+    title = frame0.layout.title.text
+    if update_layout:
+        fig.update_layout(
+            height=None,
+            title=title,
+        )
+    fig.update_layout(
+        updatemenus=[buttons_dict],
+        sliders=[sliders_dict] if animation_slider else None,
+    )
+
+
+def generic_trace_to_plotly(trace):
+    """Transform a generic trace into a plotly trace"""
+    if "scatter" in trace["type"]:
+        if trace.get("line_width", None):
+            trace["line_width"] *= SIZE_FACTORS_TO_PLOTLY["line_width"]
+        dash = trace.get("line_dash", None)
+        if dash is not None:
+            trace["line_dash"] = LINESTYLES_TO_PLOTLY.get(dash, "solid")
+        symb = trace.get("marker_symbol", None)
+        if symb is not None:
+            trace["marker_symbol"] = SYMBOLS_TO_PLOTLY.get(symb, "circle")
+        if "marker_size" in trace:
+            trace["marker_size"] = (
+                np.array(trace["marker_size"], dtype="float")
+                * SIZE_FACTORS_TO_PLOTLY["marker_size"]
+            )
+    return trace
+
+
+def process_extra_trace(model):
+    "process extra trace attached to some magpylib object"
+    ttype = model["constructor"].lower()
+    kwargs = {**model["kwargs_extra"], **model["kwargs"]}
+    trace3d = {"type": ttype, **kwargs}
+    if ttype == "scatter3d":
+        for k in ("marker", "line"):
+            trace3d[f"{k}_color"] = trace3d.get(f"{k}_color", kwargs["color"])
+    elif ttype == "mesh3d":
+        trace3d["showscale"] = trace3d.get("showscale", False)
+        trace3d["color"] = trace3d.get("color", kwargs["color"])
+    # need to match parameters to the constructor
+    # the color parameter is added by default  int the `traces_generic` module but is not
+    # compatible with some traces (e.g. `go.Surface`)
+    allowed_prefs = match_args(ttype)
+    trace3d = {
+        k: v
+        for k, v in trace3d.items()
+        if k in {"row", "col", "type"}
+        or k in allowed_prefs
+        or ("_" in k and k.split("_")[0] in allowed_prefs)
+    }
+    trace3d.update(linearize_dict(trace3d, separator="_"))
+    return trace3d
+
+
+def display_plotly(
+    data,
+    zoom=1,
+    canvas=None,
+    renderer=None,
+    return_fig=False,
+    update_layout=True,
+    max_rows=None,
+    max_cols=None,
+    subplot_specs=None,
+    fig_kwargs=None,
+    show_kwargs=None,
+    **kwargs,  # pylint: disable=unused-argument
+):
+    """Display objects and paths graphically using the plotly library."""
+
+    fig_kwargs = {} if not fig_kwargs else fig_kwargs
+    show_kwargs = {} if not show_kwargs else show_kwargs
+    show_kwargs = {"renderer": renderer, **show_kwargs}
+
+    fig = canvas
+    show_fig = False
+    extra_data = False
+    if fig is None:
+        if not return_fig:
+            show_fig = True
+        fig = go.Figure()
+
+    if not (max_rows is None and max_cols is None):
+        # pylint: disable=protected-access
+        if fig._grid_ref is None:
+            fig = fig.set_subplots(
+                rows=max_rows,
+                cols=max_cols,
+                specs=subplot_specs.tolist(),
+            )
+
+    frames = data["frames"]
+    for fr in frames:
+        new_data = []
+        for tr in fr["data"]:
+            new_data.append(generic_trace_to_plotly(tr))
+        for model in fr["extra_backend_traces"]:
+            extra_data = True
+            new_data.append(process_extra_trace(model))
+        fr["data"] = new_data
+        fr.pop("extra_backend_traces", None)
+    with fig.batch_update():
+        for frame in frames:
+            rows_list = []
+            cols_list = []
+            for tr in frame["data"]:
+                row = tr.pop("row", None)
+                col = tr.pop("col", None)
+                rows_list.append(row)
+                cols_list.append(col)
+        if max_rows is None and max_cols is None:
+            rows_list = cols_list = None
+        isanimation = len(frames) != 1
+        if not isanimation:
+            fig.add_traces(frames[0]["data"], rows=rows_list, cols=cols_list)
+        else:
+            animation_slider = data["input_kwargs"].get("animation_slider", False)
+            animate_path(
+                fig,
+                frames,
+                data["path_indices"],
+                data["frame_duration"],
+                animation_slider=animation_slider,
+                update_layout=update_layout,
+                rows=rows_list,
+                cols=cols_list,
+            )
+        ranges = data["ranges"]
+        if extra_data:
+            ranges = get_scene_ranges(*frames[0]["data"], zoom=zoom)
+        if update_layout:
+            apply_fig_ranges(fig, ranges, apply2d=isanimation)
+            fig.update_layout(
+                legend_itemsizing="constant",
+                # legend_groupclick="toggleitem",
+            )
+        fig.update(fig_kwargs)
+
+    if return_fig and not show_fig:
+        return fig
+    if show_fig:
+        fig.show(**show_kwargs)
+    return None
---
+++ packages/magpylib/magpylib-5.0.4.tar.gz/magpylib-5.0.4/magpylib/_src/utility.py
@@ -0,0 +1,400 @@
+""" some utility functions"""
+
+# pylint: disable=import-outside-toplevel
+# pylint: disable=cyclic-import
+# import numbers
+from functools import lru_cache
+from inspect import signature
+from math import log10
+from typing import Callable
+from typing import Sequence
+
+import numpy as np
+
+from magpylib._src.exceptions import MagpylibBadUserInput
+
+
+def get_allowed_sources_msg():
+    "Return allowed source message"
+
+    srcs = list(get_registered_sources())
+    return f"""Sources must be either
+- one of type {srcs}
+- Collection with at least one of the above
+- 1D list of the above
+- string {srcs}"""
+
+
+ALLOWED_OBSERVER_MSG = """Observers must be either
+- array_like positions of shape (N1, N2, ..., 3)
+- Sensor object
+- Collection with at least one Sensor
+- 1D list of the above"""
+
+ALLOWED_SENSORS_MSG = """Sensors must be either
+- Sensor object
+- Collection with at least one Sensor
+- 1D list of the above"""
+
+
+def wrong_obj_msg(*objs, allow="sources"):
+    """return error message for wrong object type provided"""
+    assert len(objs) <= 1, "only max one obj allowed"
+    allowed = allow.split("+")
+    prefix = "No" if len(allowed) == 1 else "Bad"
+    msg = f"{prefix} {'/'.join(allowed)} provided"
+    if "sources" in allowed:
+        msg += "\n" + get_allowed_sources_msg()
+    if "observers" in allowed:
+        msg += "\n" + ALLOWED_OBSERVER_MSG
+    if "sensors" in allowed:
+        msg += "\n" + ALLOWED_SENSORS_MSG
+    if objs:
+        obj = objs[0]
+        msg += f"\nreceived {obj!r} of type {type(obj).__name__!r} instead." ""
+    return msg
+
+
+def format_star_input(inp):
+    """
+    *inputs are always wrapped in tuple. Formats *inputs of form "src", "src, src"
+    but also "[src, src]" or ""(src,src") so that 1D lists/tuples come out.
+    """
+    if len(inp) == 1:
+        return inp[0]
+    return list(inp)
+
+
+def format_obj_input(*objects: Sequence, allow="sources+sensors", warn=True) -> list:
+    """tests and flattens potential input sources (sources, Collections, sequences)
+    ### Args:
+    - sources (sequence): input sources
+    ### Returns:
+    - list: flattened, ordered list of sources
+    ### Info:
+    - exits if invalid sources are given
+    """
+    from magpylib._src.obj_classes.class_BaseExcitations import BaseSource
+    from magpylib._src.obj_classes.class_Sensor import Sensor
+
+    obj_list = []
+    flatten_collection = not "collections" in allow.split("+")
+    for obj in objects:
+        try:
+            if isinstance(obj, (BaseSource, Sensor)):
+                obj_list += [obj]
+            else:
+                if flatten_collection or isinstance(obj, (list, tuple)):
+                    obj_list += format_obj_input(
+                        *obj,
+                        allow=allow,
+                        warn=warn,
+                    )  # recursive flattening
+                else:
+                    obj_list += [obj]
+        except Exception as error:
+            raise MagpylibBadUserInput(wrong_obj_msg(obj, allow=allow)) from error
+    obj_list = filter_objects(obj_list, allow=allow, warn=False)
+    return obj_list
+
+
+def format_src_inputs(sources) -> list:
+    """
+    - input: allow only bare src objects or 1D lists/tuple of src and col
+    - out: sources, src_list
+    ### Args:
+    - sources
+    ### Returns:
+    - sources: ordered list of sources
+    - src_list: ordered list of sources with flattened collections
+    ### Info:
+    - raises an error if sources format is bad
+    """
+
+    from magpylib._src.obj_classes.class_BaseExcitations import BaseSource
+    from magpylib._src.obj_classes.class_Collection import Collection
+
+    # store all sources here
+    src_list = []
+
+    # if bare source make into list
+    if not isinstance(sources, (list, tuple)):
+        sources = [sources]
+
+    if not sources:
+        raise MagpylibBadUserInput(wrong_obj_msg(allow="sources"))
+
+    for src in sources:
+        if isinstance(src, Collection):
+            child_sources = format_obj_input(src, allow="sources")
+            if not child_sources:
+                raise MagpylibBadUserInput(wrong_obj_msg(src, allow="sources"))
+            src_list += child_sources
+        elif isinstance(src, BaseSource):
+            src_list += [src]
+        else:
+            raise MagpylibBadUserInput(wrong_obj_msg(src, allow="sources"))
+    return list(sources), src_list
+
+
+def check_static_sensor_orient(sensors):
+    """test which sensors have a static orientation"""
+    # pylint: disable=protected-access
+    static_sensor_rot = []
+    for sens in sensors:
+        if len(sens._position) == 1:  # no sensor path (sensor is static)
+            static_sensor_rot += [True]
+        else:  # there is a sensor path
+            rot = sens.orientation.as_quat()
+            if np.all(rot == rot[0]):  # path with static orient (e.g. translation)
+                static_sensor_rot += [True]
+            else:  # sensor rotation changes along path
+                static_sensor_rot += [False]
+    return static_sensor_rot
+
+
+def check_duplicates(obj_list: Sequence) -> list:
+    """checks for and eliminates source duplicates in a list of sources
+    ### Args:
+    - obj_list (list): list with source objects
+    ### Returns:
+    - list: obj_list with duplicates removed
+    """
+    obj_list_new = []
+    for src in obj_list:
+        if src not in obj_list_new:
+            obj_list_new += [src]
+
+    if len(obj_list_new) != len(obj_list):
+        print("WARNING: Eliminating duplicates")
+
+    return obj_list_new
+
+
+def check_path_format(inp):
+    """check if each object path has same length
+    of obj.pos and obj.rot
+    Parameters
+    ----------
+    inp: single BaseGeo or list of BaseGeo objects
+    Returns
+    -------
+    no return
+    """
+    # pylint: disable=protected-access
+    if not isinstance(inp, list):
+        inp = [inp]
+    result = all(len(obj._position) == len(obj._orientation) for obj in inp)
+
+    if not result:
+        msg = "Bad path format (rot-pos with different lengths)"
+        raise MagpylibBadUserInput(msg)
+
+
+def filter_objects(obj_list, allow="sources+sensors", warn=True):
+    """
+    return only allowed objects - e.g. no sensors. Throw a warning when something is eliminated.
+    """
+    from magpylib._src.obj_classes.class_BaseExcitations import BaseSource
+    from magpylib._src.obj_classes.class_Collection import Collection
+    from magpylib._src.obj_classes.class_Sensor import Sensor
+
+    # select wanted
+    allowed_classes = ()
+    if "sources" in allow.split("+"):
+        allowed_classes += (BaseSource,)
+    if "sensors" in allow.split("+"):
+        allowed_classes += (Sensor,)
+    if "collections" in allow.split("+"):
+        allowed_classes += (Collection,)
+    new_list = []
+    for obj in obj_list:
+        if isinstance(obj, allowed_classes):
+            new_list += [obj]
+        else:
+            if warn:
+                print(f"Warning, cannot add {obj!r} to Collection.")
+    return new_list
+
+
+_UNIT_PREFIX = {
+    -24: "y",  # yocto
+    -21: "z",  # zepto
+    -18: "a",  # atto
+    -15: "f",  # femto
+    -12: "p",  # pico
+    -9: "n",  # nano
+    -6: "µ",  # micro
+    -3: "m",  # milli
+    0: "",
+    3: "k",  # kilo
+    6: "M",  # mega
+    9: "G",  # giga
+    12: "T",  # tera
+    15: "P",  # peta
+    18: "E",  # exa
+    21: "Z",  # zetta
+    24: "Y",  # yotta
+}
+
+
+def unit_prefix(number, unit="", precision=3, char_between="") -> str:
+    """
+    displays a number with given unit and precision and uses unit prefixes for the exponents from
+    yotta (y) to Yocto (Y). If the exponent is smaller or bigger, falls back to scientific notation.
+    Parameters
+    ----------
+    number : int, float
+        can be any number
+    unit : str, optional
+        unit symbol can be any string, by default ""
+    precision : int, optional
+        gives the number of significant digits, by default 3
+    char_between : str, optional
+        character to insert between number of prefix. Can be " " or any string, if a space is wanted
+        before the unit symbol , by default ""
+    Returns
+    -------
+    str
+        returns formatted number as string
+    """
+    digits = int(log10(abs(number))) // 3 * 3 if number != 0 else 0
+    prefix = _UNIT_PREFIX.get(digits, "")
+
+    if prefix == "":
+        digits = 0
+    new_number_str = f"{number / 10 ** digits:.{precision}g}"
+    return f"{new_number_str}{char_between}{prefix}{unit}"
+
+
+def add_iteration_suffix(name):
+    """
+    adds iteration suffix. If name already ends with an integer it will continue iteration
+    examples:
+        'col' -> 'col_01'
+        'col' -> 'col_01'
+        'col1' -> 'col2'
+        'col_02' -> 'col_03'
+    """
+    # pylint: disable=import-outside-toplevel
+    import re
+
+    m = re.search(r"\d+$", name)
+    n = "00"
+    endstr = None
+    midchar = "_" if name[-1] != "_" else ""
+    if m is not None:
+        midchar = ""
+        n = m.group()
+        endstr = -len(n)
+    name = f"{name[:endstr]}{midchar}{int(n)+1:0{len(n)}}"
+    return name
+
+
+def cart_to_cyl_coordinates(observer):
+    """
+    cartesian observer positions to cylindrical coordinates
+    observer: ndarray, shape (n,3)
+    """
+    x, y, z = observer.T
+    r, phi = np.sqrt(x**2 + y**2), np.arctan2(y, x)
+    return r, phi, z
+
+
+def cyl_field_to_cart(phi, Br, Bphi=None):
+    """
+    transform Br,Bphi to Bx, By
+    """
+    if Bphi is not None:
+        Bx = Br * np.cos(phi) - Bphi * np.sin(phi)
+        By = Br * np.sin(phi) + Bphi * np.cos(phi)
+    else:
+        Bx = Br * np.cos(phi)
+        By = Br * np.sin(phi)
+
+    return Bx, By
+
+
+def rec_obj_remover(parent, child):
+    """remove known child from parent collection"""
+    # pylint: disable=protected-access
+    from magpylib._src.obj_classes.class_Collection import Collection
+
+    for obj in parent:
+        if obj == child:
+            parent._children.remove(child)
+            parent._update_src_and_sens()
+            return True
+        if isinstance(obj, Collection):
+            if rec_obj_remover(obj, child):
+                break
+    return None
+
+
+def get_subclasses(cls, recursive=False):
+    """Return a dictionary of subclasses by name,"""
+    sub_cls = {}
+    for class_ in cls.__subclasses__():
+        sub_cls[class_.__name__] = class_
+        if recursive:
+            sub_cls.update(get_subclasses(class_, recursive=recursive))
+    return sub_cls
+
+
+def get_registered_sources():
+    """Return all registered sources"""
+    # pylint: disable=import-outside-toplevel
+    from magpylib._src.obj_classes.class_BaseExcitations import BaseCurrent
+    from magpylib._src.obj_classes.class_BaseExcitations import BaseMagnet
+    from magpylib._src.obj_classes.class_BaseExcitations import BaseSource
+
+    return {
+        k: v
+        for k, v in get_subclasses(BaseSource, recursive=True).items()
+        if not v in (BaseCurrent, BaseMagnet, BaseSource)
+    }
+
+
+def is_notebook() -> bool:  # pragma: no cover
+    """Check if execution is within a IPython environment"""
+    # pylint: disable=import-outside-toplevel
+    try:
+        from IPython import get_ipython
+
+        shell = get_ipython().__class__.__name__
+        if shell == "ZMQInteractiveShell":
+            return True  # Jupyter notebook or qtconsole
+        if shell == "TerminalInteractiveShell":
+            return False  # Terminal running IPython
+        return False  # Other type (?)
+    except NameError:
+        return False  # Probably standard Python interpreter
+
+
+def open_animation(filepath, embed=True):
+    """Display video or gif file using tkinter or IPython"""
+    # pylint: disable=import-outside-toplevel
+    if is_notebook():
+        if filepath.endswith(".gif"):
+            from IPython.display import Image as IPyImage
+            from IPython.display import display
+
+            display(IPyImage(data=filepath, embed=embed))
+        elif filepath.endswith(".mp4"):
+            from IPython.display import Video
+            from IPython.display import display
+
+            display(Video(data=filepath, embed=embed))
+        else:  # pragma: no cover
+            raise TypeError("Filetype not supported, only 'mp4 or 'gif' allowed")
+    else:
+        import webbrowser
+
+        webbrowser.open(filepath)
+
+
+@lru_cache(maxsize=None)
+def has_parameter(func: Callable, param_name: str) -> bool:
+    """Check if input function has a specific parameter"""
+    sig = signature(func)
+    return param_name in sig.parameters
