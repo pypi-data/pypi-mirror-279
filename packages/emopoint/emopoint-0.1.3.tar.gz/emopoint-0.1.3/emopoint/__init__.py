6e7cdbe1a734539506fd4870a9784f2161f819c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-19 05:06:44 +0000

    Add package emopoint/0.1.3/emopoint-0.1.3.tar.gz

---
+++ packages/emopoint/emopoint-0.1.3.tar.gz/emopoint-0.1.3/emopoint/__init__.py
@@ -0,0 +1,50 @@
+from .lib import DimLabel, EmoModel
+from .generated import MODELS
+
+EMOTIONS = """admiration
+amusement
+anger
+annoyance
+approval
+caring
+confusion
+curiosity
+desire
+disappointment
+disapproval
+disgust
+embarrassment
+excitement
+fear
+gratitude
+grief
+joy
+love
+nervousness
+optimism
+pride
+realization
+relief
+remorse
+sadness
+surprise
+neutral""".split("\n")
+
+
+EKMAN_MAP = {
+    "anger": ["anger", "annoyance", "disapproval"],
+    "disgust": ["disgust"],
+    "fear": ["fear", "nervousness"],
+    "joy": ["joy", "amusement", "approval", "excitement", "gratitude",  "love", "optimism", "relief", "pride", "admiration", "desire", "caring"],
+    "sadness": ["sadness", "disappointment", "embarrassment", "grief",  "remorse"],
+    "surprise": ["surprise", "realization", "confusion", "curiosity"],
+}
+
+
+__all__ = [
+    "EMOTIONS",
+    "EKMAN_MAP",
+    "DimLabel",
+    "EmoModel",
+    "MODELS",
+]
