4104fc2da7051e1e96e76628ab03678acd546146
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-19 04:05:19 +0000

    Add package pricecypher-sdk/3.0.0/pricecypher_sdk-3.0.0-py2.py3-none-any.whl

---
+++ packages/pricecypher-sdk/pricecypher_sdk-3.0.0-py2.py3-none-any.whl/pricecypher/contracts/dataclasses/__init__.py
@@ -0,0 +1,4 @@
+from .predict_result import PredictStep, PredictValues, PredictResult
+from .test_result import ElementTestResult, ElementTest, TestSuite, TestResult
+
+__all__ = ['PredictStep', 'PredictValues', 'PredictResult', 'ElementTestResult', 'ElementTest', 'TestSuite', 'TestResult']
