aa0448235061a6d89a164574aca311487f9ac7a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-19 15:05:30 +0000

    Add package liechains/0.4.1/liechains-0.4.1.tar.gz

---
+++ packages/liechains/liechains-0.4.1.tar.gz/liechains-0.4.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="liechains",
+    version="0.4.1",
+    author="leeliewhite",
+    author_email="leeliewhite@gmail.com",
+    description="toolkit for blockchains",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "web3",
+        "websocket-client",
+        "cryptography"
+    ],
+    python_requires=">=3.9",
+)
