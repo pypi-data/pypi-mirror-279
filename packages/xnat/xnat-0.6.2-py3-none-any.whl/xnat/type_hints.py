3a69fdfbc1a930a5efa59b0a5662ff9033654fd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-19 06:11:05 +0000

    Add package xnat/0.6.2/xnat-0.6.2-py3-none-any.whl

---
+++ packages/xnat/xnat-0.6.2-py3-none-any.whl/xnat/type_hints.py
@@ -0,0 +1,5 @@
+from typing import Any, List, Dict, Optional, Tuple, Union
+
+
+TimeoutType = Optional[Union[float, Tuple[float, float]]]
+JSONType = Union[None, int, str, bool, List[Any], Dict[str, Any]]
